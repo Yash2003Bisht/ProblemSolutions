1c52c73f61d3e27e80fd7d335458139f42ac51b5
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question INTRDSGN

---
+++ solutions/INTRDSGN/INTRDSGN_1.py
@@ -0,0 +1,12 @@
+# QUESTION URL: https://www.codechef.com/problems/INTRDSGN
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split(" "))
+    total_rupee_1 = a + b
+    total_rupee_2 = c  + d
+    
+    if total_rupee_1 > total_rupee_2:
+        print(total_rupee_2)
+    else:
+        print(total_rupee_1)
+
