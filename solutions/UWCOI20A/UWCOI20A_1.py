4057e998e36a6632317972532bf5380340ace692
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question UWCOI20A

---
+++ solutions/UWCOI20A/UWCOI20A_1.py
@@ -0,0 +1,12 @@
+# QUESTION URL: https://www.codechef.com/problems/UWCOI20A
+
+for _ in range(int(input())):
+    n = int(input())
+    max_mountain = 0
+    for _ in range(n):
+        mountain = int(input())
+        if mountain > max_mountain:
+            max_mountain = mountain
+    
+    print(max_mountain)
+
---
+++ solutions/UWCOI20A/UWCOI20A_2.py
@@ -0,0 +1,10 @@
+# QUESTION URL: https://www.codechef.com/problems/UWCOI20A
+
+for _ in range(int(input())):
+    n = int(input())
+    lst = []
+    for _ in range(n):
+        lst.append(int(input()))
+    
+    print(max(lst))
+
