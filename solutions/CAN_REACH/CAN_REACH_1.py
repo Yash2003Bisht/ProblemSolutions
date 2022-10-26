4c506ceb240dd8f73e7cdf22f2523a6ae09d157e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CAN_REACH

---
+++ solutions/CAN_REACH/CAN_REACH_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/CAN_REACH
+
+for _ in range(int(input())):
+    x, y, k = map(int, input().split(" "))
+    if x%k == 0 and y%k == 0:
+        print("YES")
+    else:
+        print("NO")
+
