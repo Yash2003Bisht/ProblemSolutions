f016c899c2db74c07b684d1e1915b708265a425b
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question F1RULE

---
+++ solutions/F1RULE/F1RULE_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/F1RULE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &x,&y);
+        if (y<=x+(x*7/100)){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
