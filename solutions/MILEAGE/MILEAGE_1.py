c90ff814f3d27825ace26532ce8b9c88cfd15b6e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question MILEAGE

---
+++ solutions/MILEAGE/MILEAGE_1.py
@@ -0,0 +1,14 @@
+# QUESTION URL: https://www.codechef.com/problems/MILEAGE
+
+for _ in range(int(input())):
+    n, x, y, a, b = map(int, input().split(" "))
+    total_price_petrol = (n/a)*x
+    total_price_diesel = (n/b)*y
+    
+    if total_price_petrol > total_price_diesel:
+        print('DIESEL')
+    elif total_price_diesel > total_price_petrol:
+        print('PETROL')
+    else:
+        print('ANY')
+
