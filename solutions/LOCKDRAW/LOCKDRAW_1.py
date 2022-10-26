c59360ac1eba3533fad3ad6df10debace255cc04
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question LOCKDRAW

---
+++ solutions/LOCKDRAW/LOCKDRAW_1.py
@@ -0,0 +1,7 @@
+# QUESTION URL: https://www.codechef.com/problems/LOCKDRAW
+
+for _ in range(int(input())):
+    points = list(map(int, input().split(" ")))
+    max_points = max(points)
+    print('YES' if 2*max_points == sum(points) else 'NO')
+
