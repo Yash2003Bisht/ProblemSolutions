f07349556fc773c352d10724226f55aa9488368d
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question SLEEP

---
+++ solutions/SLEEP/SLEEP_2.java
@@ -0,0 +1,29 @@
+// QUESTION URL: https://www.codechef.com/problems/SLEEP
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		for (int i=0; i<t; i++){
+		    int x = sc.nextInt();
+		    sc.nextLine();
+		    if (x < 7){
+		        System.out.println("YES");
+		    } else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
+
+
