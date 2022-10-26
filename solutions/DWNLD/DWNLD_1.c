3454699f791561c744684646bfe46ca1a007c879
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question DWNLD

---
+++ solutions/DWNLD/DWNLD_1.c
@@ -0,0 +1,26 @@
+// QUESTION URL: https://www.codechef.com/problems/DWNLD
+
+#include <stdio.h>
+
+int main(void) {
+    int tc,n,k,t,d,i,r;
+    scanf("%d", &tc);
+    while(tc--){
+        scanf("%d%d", &n,&k);
+        r=0;
+        for (i=0; i<n; i++){
+            scanf("%d%d", &t,&d);
+            if (k>=t){
+                k -= t;
+            } else if (k<t){
+                r += (t-k)*d;
+                k = 0;
+            }
+        }
+        printf("%d\n", r);
+    }
+	return 0;
+}
+
+
+
