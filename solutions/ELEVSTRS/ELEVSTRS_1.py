5fb67624afb63f1672b88f1533bdbe48413accff
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question ELEVSTRS

---
+++ solutions/ELEVSTRS/ELEVSTRS_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/ELEVSTRS
+
+import math
+for _ in range(int(input())):
+    details = list(map(int, input().split()))
+    
+    if (details[0]*math.sqrt(2))/details[1] > (details[0]*2)/details[2]:
+        print("Elevator")
+    else:
+        print("Stairs")
+
---
+++ solutions/ELEVSTRS/ELEVSTRS_2.c
@@ -0,0 +1,26 @@
+// QUESTION URL: https://www.codechef.com/problems/ELEVSTRS
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, v1, v2;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &v1, &v2);
+        // dust -> d = s*t
+        // t = d/s
+        float stairs = (n*1.4142)/v1;
+        float elevator = (float) (2*n)/v2;
+        
+        if (stairs > elevator){
+            printf("Elevator\n");
+        } else{
+            printf("Stairs\n");
+        }
+    }
+	return 0;
+}
+
+
+
