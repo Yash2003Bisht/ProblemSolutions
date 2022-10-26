3bb416193d67e31eb256d204b2b5f5a9408fd2f0
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question TRICOIN

---
+++ solutions/TRICOIN/TRICOIN_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/TRICOIN
+
+for _ in range(int(input())):
+    n = int(input())
+    height = 1
+    while True:
+        if height*(height+1)/2 > n:
+            print(height-1)
+            break
+        height += 1
+
