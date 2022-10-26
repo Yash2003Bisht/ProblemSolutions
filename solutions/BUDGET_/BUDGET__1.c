e72e31ff4f13448874edeb33b310145b248b7e75
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question BUDGET_

---
+++ solutions/BUDGET_/BUDGET__1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/BUDGET_
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,m;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &n,&m);
+        if (m*30 <= n){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
