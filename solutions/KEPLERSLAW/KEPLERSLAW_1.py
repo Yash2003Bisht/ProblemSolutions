abe6da1bbad0d631575f9a185308e4765d2c11b2
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question KEPLERSLAW

---
+++ solutions/KEPLERSLAW/KEPLERSLAW_1.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.codechef.com/problems/KEPLERSLAW
+
+for _ in range(int(input())):
+    s = list(map(int, input().split(" ")))
+    print('Yes' if s[0]**2/s[2]**3 == s[1]**2/s[3]**3 else 'No')
+
