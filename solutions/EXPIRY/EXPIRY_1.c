bc5d7b81b55978245629e68d3f42d7aa5bc25251
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question EXPIRY

---
+++ solutions/EXPIRY/EXPIRY_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/EXPIRY
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,m,k;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d", &n,&m,&k);
+        if (m*k >= n){
+            printf("YES\n");
+        } else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
