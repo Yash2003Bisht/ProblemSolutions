c7b07863293f2d994bda4768d8457463b8a7d1bc
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question BOMBTHEBASE

---
+++ solutions/BOMBTHEBASE/BOMBTHEBASE_1.py
@@ -0,0 +1,17 @@
+# QUESTION URL: https://www.codechef.com/problems/BOMBTHEBASE
+
+from bisect import bisect
+
+for _ in range(int(input())):
+    n, x = map(int, input().split(" "))
+    a = list(map(int, input().split(" ")))
+    
+    if  x > min(a):
+        last_index = 0
+        for index, house in enumerate(a):
+            if house < x:
+                last_index = index
+        print(last_index+1)
+    else:
+        print(0)
+
