f947ea064da84ed37c91052e38fdaf85cb91202f
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question SALESEASON

---
+++ solutions/SALESEASON/SALESEASON_1.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.codechef.com/problems/SALESEASON
+
+for _ in range(int(input())):
+    x = int(input())
+    if x <= 100:
+        print(x)
+    elif x <= 1000:
+        print(x-25)
+    elif x <= 5000:
+        print(x-100)
+    else:
+        print(x-500)
+
