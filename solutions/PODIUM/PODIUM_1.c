3acb1efe8c9c176409d0e7a28810aaba52464093
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question PODIUM

---
+++ solutions/PODIUM/PODIUM_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/PODIUM
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &a,&b);
+        printf("%d\n", a+b);
+    }
+	return 0;
+}
+
+
+
