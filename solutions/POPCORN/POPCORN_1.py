13c079367a682cb5757196d449eb267c93df41ae
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question POPCORN

---
+++ solutions/POPCORN/POPCORN_1.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.codechef.com/problems/POPCORN
+
+for _ in range(int(input())):
+    a = sum(map(int, input().split(" ")))
+    b = sum(map(int, input().split(" ")))
+    c = sum(map(int, input().split(" ")))
+    print(max(a,b,c))
+
---
+++ solutions/POPCORN/POPCORN_2.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/POPCORN
+
+for _ in range(int(input())):
+    a1, a2 = map(int, input().split(" "))
+    b1, b2 = map(int, input().split(" "))
+    c1, c2 = map(int, input().split(" "))
+    a = a1+a2
+    b = b1+b2
+    c = c1+c2
+    print(max(a,b,c))
+
