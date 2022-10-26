6a2a3a210a29c2f12085d060022ac2a32201e99e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question ENCMSG

---
+++ solutions/ENCMSG/ENCMSG_1.py
@@ -0,0 +1,26 @@
+# QUESTION URL: https://www.codechef.com/problems/ENCMSG
+
+from string import ascii_lowercase
+
+
+def encoded_string(n, s, ascii_lowercase_reversed):
+    swapped_s = ''
+    counter = 0
+    while n > 0:
+        if n == 1:
+            swapped_s += ascii_lowercase_reversed[ascii_lowercase.index(s[len(s)-1])]
+            break
+        swapped_s += ascii_lowercase_reversed[ascii_lowercase.index(s[counter+1])]
+        swapped_s += ascii_lowercase_reversed[ascii_lowercase.index(s[counter])]
+        counter += 2
+        n -= 2
+    return swapped_s
+
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    encoded_message = encoded_string(n, s, ascii_lowercase[::-1])
+    print(encoded_message)
+
+
---
+++ solutions/ENCMSG/ENCMSG_2.py
@@ -0,0 +1,34 @@
+# QUESTION URL: https://www.codechef.com/problems/ENCMSG
+
+from string import ascii_lowercase
+
+
+def swap(n, s):
+    swapped_s = ''
+    counter = 0
+    while n > 0:
+        if n == 1:
+            swapped_s += s[len(s)-1]
+            break
+        swapped_s += (s[counter+1])
+        swapped_s += (s[counter])
+        counter += 2
+        n -= 2
+    return swapped_s
+
+
+def replace(swapped_s, ascii_lowercase_reversed):
+    replaced_s = ''
+    for character in swapped_s:
+        replaced_s += ascii_lowercase_reversed[ascii_lowercase.index(character)]
+    return replaced_s
+
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    swapped_s = swap(n, s)
+    repaced_s = replace(swapped_s, ascii_lowercase[::-1])
+    print(repaced_s)
+
+
