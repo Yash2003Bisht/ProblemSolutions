7322f07b98e4066039d31a16cc11807d380de7c6
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question CNDLOVE

---
+++ solutions/CNDLOVE/CNDLOVE_1.c
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/CNDLOVE
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        int total=0;
+        for(int i=0; i<n; i++){
+            scanf("%d", &a);
+            total += a;
+        }
+        if (total%2 == 0){
+            printf("NO\n");
+        } else{
+            printf("YES\n");
+        }
+    }
+	return 0;
+}
+
+
+
