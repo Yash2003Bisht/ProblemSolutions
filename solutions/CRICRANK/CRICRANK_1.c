50233d5ba01354fbf47c1fc772951d798f866e8d
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CRICRANK

---
+++ solutions/CRICRANK/CRICRANK_1.c
@@ -0,0 +1,37 @@
+// QUESTION URL: https://www.codechef.com/problems/CRICRANK
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,a1,a2,a3,b1,b2,b3;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d%d", &a1,&a2,&a3);
+	    scanf("%d%d%d", &b1,&b2,&b3);
+	    int a = 0;
+	    int b = 0;
+	    if (a1>b1){
+	        a++;
+	    } else {
+	        b++;
+	    }
+	    if (a2>b2){
+	        a++;
+	    } else {
+	        b++;
+	    }
+	    if (a3>b3){
+	        a++;
+	    } else {
+	        b++;
+	    }
+	    
+	    char best = a>b?'A':'B';
+	    printf("%c\n", best);
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/CRICRANK/CRICRANK_2.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/CRICRANK
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,a1,a2,a3,b1,b2,b3;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d%d", &a1,&a2,&a3);
+	    scanf("%d%d%d", &b1,&b2,&b3);
+	    float a = (a1+a2+a3)/2;
+	    float b = (b1+b2+b3)/2;
+	    char best = a>b?'A':'B';
+	    printf("%c\n", best);
+	}
+	return 0;
+}
+
+
+
