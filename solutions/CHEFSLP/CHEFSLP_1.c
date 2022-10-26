765338c92bea754fff5158080a4109675f5bf50d
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question CHEFSLP

---
+++ solutions/CHEFSLP/CHEFSLP_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/CHEFSLP
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,l,p,x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d",&n,&l,&x);
+        p = n-l>l?l:n-l;
+        printf("%d\n", p*x);
+    }
+	return 0;
+}
+
+
+
