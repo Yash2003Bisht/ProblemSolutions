9df5a2d4881c043cb7877487d756fda4244728f3
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question DOMINANT2

---
+++ solutions/DOMINANT2/DOMINANT2_1.py
@@ -0,0 +1,22 @@
+# QUESTION URL: https://www.codechef.com/problems/DOMINANT2
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())  # not required in python
+    a = Counter(list(map(int, input().split(" "))))
+    count = 0
+    equal = False
+    if sum(a.values()) == len(a):
+        print('NO')
+    else:
+        for i, j in a.items():
+            if j > count:
+                equal = False
+                count = j
+            elif j == count:
+                equal = True
+                count = j
+        print("YES" if not equal else "NO")
+
+
