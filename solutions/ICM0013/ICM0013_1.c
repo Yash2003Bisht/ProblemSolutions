dc88ecae7869847ec6c2bf73ad5f0cefc58d4f63
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question ICM0013

---
+++ solutions/ICM0013/ICM0013_1.c
@@ -0,0 +1,16 @@
+// QUESTION URL: https://www.codechef.com/problems/ICM0013
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        printf("%d\n", n);
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/ICM0013/ICM0013_2.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/ICM0013
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,counter;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        counter = 0;
+        for (int i=1; i<=n; i++){
+            if (n%i<=n/2){
+                counter++;
+            }
+        }
+        printf("%d\n", counter);
+    }
+	return 0;
+}
+
+
+
