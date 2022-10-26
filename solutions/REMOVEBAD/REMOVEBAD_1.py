e8af317d67a21de96b82a4cef783c58387d6c5d7
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question REMOVEBAD

---
+++ solutions/REMOVEBAD/REMOVEBAD_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/REMOVEBAD
+
+from collections import Counter
+
+for _ in range(int(input())):
+    input()
+    print(sum(sorted(Counter(map(int, input().split())).values(), reverse=True)[1:]))
+
+
