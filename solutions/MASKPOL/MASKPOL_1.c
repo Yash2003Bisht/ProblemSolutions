785b91eb79f2bd5c450ad511c27bf16c6e175c38
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question MASKPOL

---
+++ solutions/MASKPOL/MASKPOL_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/MASKPOL
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,n;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&n,&a);
+        int uninfected = n-a;
+        if(uninfected>a){
+            printf("%d\n",a);
+        } else{
+            printf("%d\n",uninfected);
+        }
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/MASKPOL/MASKPOL_2.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/MASKPOL
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,n;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&n,&a);
+        if(n-a>a){
+            printf("%d\n",a);
+        } else{
+            printf("%d\n",n-a);
+        }
+    }
+	return 0;
+}
+
+
+
