54ad4757cf66308c0fca0893b64f3be71f0400e8
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CHEGLOVE

---
+++ solutions/CHEGLOVE/CHEGLOVE_2.c
@@ -0,0 +1,45 @@
+// QUESTION URL: https://www.codechef.com/problems/CHEGLOVE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,i;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        
+        int l[n], g[n], j=0, k=n-1, front=0, back=0;
+        
+        for (i=0; i<n*2; i++){
+            if (i<n){
+                scanf("%d", &l[i]);
+            } else{
+                scanf("%d", &g[j]);
+                if (g[j] >= l[j]){
+                    front++;
+                }
+                if (g[j] >= l[k]){
+                    back++;
+                    k--;
+                }
+                j++;
+            }
+        }
+        
+        if (front==n && back==n){
+            printf("both\n");
+        } else if(front==n){
+            printf("front\n");
+        } else if(back==n){
+            printf("back\n");
+        } else{
+            printf("none\n");
+        }
+        
+        
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/CHEGLOVE/CHEGLOVE_4.c
@@ -0,0 +1,58 @@
+// QUESTION URL: https://www.codechef.com/problems/CHEGLOVE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,i;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        
+        int l[n], g[n], j=0, front=0, back=0;
+        
+        for (i=0; i<n*2; i++){
+            if (i<n){
+                scanf("%d", &l[i]);
+            } else{
+                scanf("%d", &g[j]);
+                j++;
+            }
+        }
+        
+        // check front
+        for (int i=0; i<n; i++){
+            if (g[i] >= l[i]){
+                front = 1;
+            } else{
+                front = 0;
+                break;
+            }
+        }
+        
+        // check back
+        for (int i=0; i<n; i++){
+            if (g[i] == l[n-i-1]){
+                back = 1;
+            } else{
+                back = 0;
+                break;
+            }
+        }
+        
+        if (front && back == 0){
+            printf("front\n");
+        } else if(back && front == 0){
+            printf("back\n");
+        } else if(back && front){
+            printf("both\n");
+        } else{
+            printf("none\n");
+        }
+        
+        
+    }
+	return 0;
+}
+
+
+
