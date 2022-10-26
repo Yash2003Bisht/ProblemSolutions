7a884f57527f6543ac7b0fb511a39b2e4da51e03
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question ATM2

---
+++ solutions/ATM2/ATM2_1.py
@@ -0,0 +1,14 @@
+# QUESTION URL: https://www.codechef.com/problems/ATM2
+
+for _ in range(int(input())):
+    n, k = map(int, input().split(" "))
+    a = map(int, input().split(" "))
+    counter = ''
+    for i in a:
+        if i <= k:
+            counter += '1'
+            k -= i
+        else:
+            counter += '0'
+    print(counter)
+
