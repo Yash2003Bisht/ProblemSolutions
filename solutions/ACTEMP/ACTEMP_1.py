8d586227710bda5d2ebfcbda58bdb57966a4f3d1
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question ACTEMP

---
+++ solutions/ACTEMP/ACTEMP_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/ACTEMP
+
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if b>=a and b>=c:
+        print('Yes')
+    else:
+        print('No')
+
