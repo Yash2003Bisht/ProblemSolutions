13019beb3fbf28418409b3e94d763a42911c0c88
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question BROKENPHONE

---
+++ solutions/BROKENPHONE/BROKENPHONE_1.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/BROKENPHONE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &x, &y);
+        if (x<y){
+            printf("REPAIR\n");
+        } else if(y<x){
+            printf("NEW PHONE\n");
+        } else{
+            printf("ANY\n");
+        }
+    }
+	return 0;
+}
+
+
+
