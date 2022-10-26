948c8a6799ff55ecb833c481d4ff1009fc8b65bd
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CO92JUDG

---
+++ solutions/CO92JUDG/CO92JUDG_1.py
@@ -0,0 +1,19 @@
+# QUESTION URL: https://www.codechef.com/problems/CO92JUDG
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split(" ")))
+    b = list(map(int, input().split(" ")))
+    
+    total_a = sum(a) - max(a)
+    total_b = sum(b) - max(b)
+    
+    if total_a > total_b:
+        print('Bob')
+    elif total_b > total_a:
+        print('Alice')
+    else:
+        print('Draw')
+
+    
+
---
+++ solutions/CO92JUDG/CO92JUDG_2.py
@@ -0,0 +1,21 @@
+# QUESTION URL: https://www.codechef.com/problems/CO92JUDG
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split(" ")))
+    b = list(map(int, input().split(" ")))
+    x = max(a)
+    y = max(b)
+    
+    total_a = sum(a) - x
+    total_b = sum(b) - y
+    
+    if total_a > total_b:
+        print('Bob')
+    elif total_b > total_a:
+        print('Alice')
+    else:
+        print('Draw')
+
+    
+
