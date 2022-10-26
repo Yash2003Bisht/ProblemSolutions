39d66429ec76f1a8714f27aea9cd82f537f1d792
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question DIET

---
+++ solutions/DIET/DIET_2.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.codechef.com/problems/DIET
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    remaining = 0
+    for index, i in enumerate(a):
+        if i+remaining >= k:
+            remaining = i+remaining-k
+        else:
+            print('NO', index+1)
+            break
+    else:
+        print('YES')
+        
+
