bfe119ba74320e378a249dfe36810d9cc9b36d60
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question MY1STCONTEST

---
+++ solutions/MY1STCONTEST/MY1STCONTEST_1.py
@@ -0,0 +1,7 @@
+# QUESTION URL: https://www.codechef.com/problems/MY1STCONTEST
+
+x, y, z =  map(int, input().split(" "))
+total_user_made_submissions = x - y
+total_user_solve_atleast_one_problem = total_user_made_submissions - z
+print(total_user_made_submissions, total_user_solve_atleast_one_problem)
+
