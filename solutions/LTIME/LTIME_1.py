8de70abcc00c615c7ca9f60d1fcfb5c3db4319d6
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question LTIME

---
+++ solutions/LTIME/LTIME_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/LTIME
+
+for _ in range(int(input())):
+    x = int(input())
+    if x >= 1 and x <= 4:
+        print("YES")
+    else:
+        print("NO")
+
