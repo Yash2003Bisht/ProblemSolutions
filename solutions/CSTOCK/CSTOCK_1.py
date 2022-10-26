019d2a50943e6fac31068f4b8e2517d2863e2d76
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question CSTOCK

---
+++ solutions/CSTOCK/CSTOCK_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/CSTOCK
+
+for _ in range(int(input())):
+    s,a,b,c = map(int, input().split())
+    stock_current_price = s+(s*c/100)
+    if a<=stock_current_price<=b:
+        print("Yes")
+    else:
+        print("No")
+
+
---
+++ solutions/CSTOCK/CSTOCK_2.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/CSTOCK
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    double s,a,b,c;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%lf%lf%lf%lf", &s,&a,&b,&c);
+        double current_stock_price = s+(s*c/100);
+        if (current_stock_price >= a && current_stock_price <= b){
+            printf("Yes\n");
+        } else{
+            printf("No\n");
+        }
+    }
+	return 0;
+}
+
+
+
