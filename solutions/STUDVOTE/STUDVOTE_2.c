a11cff6e0a649c9077cbbfb8aa1d9112f5a6610e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question STUDVOTE

---
+++ solutions/STUDVOTE/STUDVOTE_2.c
@@ -0,0 +1,39 @@
+// QUESTION URL: https://www.codechef.com/problems/STUDVOTE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,k;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &n,&k);
+        int a[n], b[n];
+        for (int i=0; i<n; i++){
+            scanf("%d", &a[i]);
+            b[i] = 0;
+        }
+        
+        for (int i=0; i<n; i++){
+            if (a[i]-1 != i && b[a[i]-1] >= 0){
+                b[a[i]-1] += 1;
+            } else{
+                b[a[i]-1] = -1;
+            }
+        }
+        
+        int total = 0;
+        
+        for (int i=0; i<n; i++){
+            if (b[i] >= k){
+                total++;
+            }
+        }
+        
+        printf("%d\n", total);
+        
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/STUDVOTE/STUDVOTE_3.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.codechef.com/problems/STUDVOTE
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    student_government = [0] * n
+    for index, student in enumerate(a):
+        if index != student - 1 and student_government[student - 1] >= 0:
+            student_government[student - 1] += 1
+
+        else:
+            student_government[student - 1] = -1
+
+    print(sum(map(lambda x: 1 if x >= k else 0, student_government)))
+
+
