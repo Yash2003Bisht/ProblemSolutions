9777a474d85a1afa971cb15cbc80d7763658fccd
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question WGHTS

---
+++ solutions/WGHTS/WGHTS_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/WGHTS
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,w,x,y,z;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d%d%d", &w,&x,&y,&z);
+	    if(w == x || w == y || w == z || x+y == w || x+z == w || y+z == w || x+y+z == w){
+	        printf("YES\n");
+	    }else{
+	        printf("NO\n");	        
+	    }
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/WGHTS/WGHTS_2.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/WGHTS
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,w,x,y,z;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d%d%d", &w,&x,&y,&z);
+	    if(x+y == w || x+z == w || y+z == w || w == x || w == y || w == z){
+	        printf("YES\n");
+	    }else{
+	        printf("NO\n");	        
+	    }
+	}
+	return 0;
+}
+
+
+
