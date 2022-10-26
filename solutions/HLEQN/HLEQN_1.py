4708dae16002f34f6ff2483fc703437fb3330f49
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question HLEQN

---
+++ solutions/HLEQN/HLEQN_1.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.codechef.com/problems/HLEQN
+
+for _ in range(int(input())):
+    n = int(input())
+    x = 0
+    for i in range(1, (int(n ** 0.5))):
+        if (n - 2 * i) % (i + 2) == 0 and (n != 2 * i):
+            x = x + 1
+            break
+
+    if x == 0:
+        print('NO')
+    else:
+        print('YES')
+
+
