68401175dbc27b891882e2b4b88218b94578ac67
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question HOLES

---
+++ solutions/HOLES/HOLES_1.py
@@ -0,0 +1,15 @@
+# QUESTION URL: https://www.codechef.com/problems/HOLES
+
+for _ in range(int(input())):
+    s = input()
+    holes = ['A', 'D', 'O', 'P', 'Q', 'R']
+    count = 0
+    
+    for i in s:
+        if i in holes:
+            count += 1
+        elif i == 'B':
+            count += 2
+    
+    print(count)
+
---
+++ solutions/HOLES/HOLES_3.c
@@ -0,0 +1,29 @@
+// QUESTION URL: https://www.codechef.com/problems/HOLES
+
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+    int t,length,i;
+    char s[100];
+    scanf("%d", &t);
+    
+    while (t--){
+        scanf("%s", s);
+        int count = 0;
+        length = strlen(s);
+        for(i=0; i<length; i++){
+            if (s[i] == 'A' || s[i] == 'D' || s[i] == 'O' || s[i] == 'P' || s[i] == 'Q' || s[i] == 'R'){
+                count++;
+            }
+            if (s[i] == 'B'){
+                count++;
+            }
+        }
+        printf("%d\n", count);
+    }
+	return 0;
+}
+
+
+
