33189aa70cfd4c2668e08147a54f08a1b29b7091
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question THREEBOX

---
+++ solutions/THREEBOX/THREEBOX_1.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/THREEBOX
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,b,c,d;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d%d", &a,&b,&c,&d);
+        if (a+b+c <= d){
+            printf("1\n");
+        } else if(a+b <= d){
+            printf("2\n");
+        } else{
+            printf("3\n");
+        }
+    }
+	return 0;
+}
+
+
+
