2a6068c07904ddb91019643a15dac4dad6458b0d
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question PIZZA_BURGER

---
+++ solutions/PIZZA_BURGER/PIZZA_BURGER_1.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/PIZZA_BURGER
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        if (x >= y){
+            printf("PIZZA\n");
+        } else if (x >= z){
+            printf("BURGER\n");
+        } else{
+            printf("NOTHING\n");
+        }
+    }
+	return 0;
+}
+
+
+
