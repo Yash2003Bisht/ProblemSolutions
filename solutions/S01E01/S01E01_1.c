60dee5860a5c83fdec4dadc7a93bf46b6719540e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question S01E01

---
+++ solutions/S01E01/S01E01_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/S01E01
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        if (n>=21){  // 1 + 2 + 3 + 4 + 5 + 6 = 21 --> smallest possible number
+            printf("YES\n");
+        } else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
