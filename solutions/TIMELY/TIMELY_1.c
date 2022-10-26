69cab4413edf40bbf17a70a50ed74ca2aa6dbfc3
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question TIMELY

---
+++ solutions/TIMELY/TIMELY_1.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/TIMELY
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        
+        scanf("%d", &n);
+        
+        if (n >= 30)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
