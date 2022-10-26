707f0b473c3963c3c1aa5e23be6a04029d96a663
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question LOSTWKND

---
+++ solutions/LOSTWKND/LOSTWKND_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/LOSTWKND
+
+for _ in range(int(input())):
+    d = list(map(int, input().split()))
+    p = d[5]
+    if sum(d[:len(d)-1])*p > 120:
+        print("Yes")
+    else:
+        print("No")
+    
+
---
+++ solutions/LOSTWKND/LOSTWKND_2.c
@@ -0,0 +1,26 @@
+// QUESTION URL: https://www.codechef.com/problems/LOSTWKND
+
+#include <stdio.h>
+
+int main(void) {
+    int t,i,r,p,a[5];
+    scanf("%d", &t);
+    while(t--){
+        for(i=0; i<5; i++){
+            scanf("%d", &a[i]);
+        }
+        scanf("%d", &p);
+        for(i=0; i<5; i++){
+           r += a[i]*p;
+        }
+        if (r>120){
+            printf("YES\n");
+        } else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
