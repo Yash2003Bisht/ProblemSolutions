4e4bf52a0f08bf6c0fbbbd771d3fa4dda4beef2f
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question REACHTARGET

---
+++ solutions/REACHTARGET/REACHTARGET_1.c
@@ -0,0 +1,16 @@
+// QUESTION URL: https://www.codechef.com/problems/REACHTARGET
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &x,&y);
+        printf("%d\n", x-y);
+    }
+	return 0;
+}
+
+
+
