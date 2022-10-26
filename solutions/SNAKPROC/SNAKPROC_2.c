12e737059db771a127266760d4785bed1831348b
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question SNAKPROC

---
+++ solutions/SNAKPROC/SNAKPROC_2.c
@@ -0,0 +1,35 @@
+// QUESTION URL: https://www.codechef.com/problems/SNAKPROC
+
+#include <stdio.h>
+
+int main(void) {
+    int r, l,h,valid;
+    char l_string[501];
+    scanf("%d", &r);
+    while(r--){
+        scanf("%d", &l);
+        scanf("%s", &l_string);
+        h = 0, valid=1;
+        for (int i=0; i<l; i++){
+            if (l_string[i] == 'H' && !h){
+                h = 1;
+            } else if (l_string[i] == 'T' && h){
+                h = 0;
+            } else if (l_string[i] != '.'){
+                valid = 0;
+                break;
+            }
+        }
+        
+        if (valid && !h){
+            printf("Valid\n");
+        } else{
+            printf("Invalid\n");
+        }
+        
+    }
+	return 0;
+}
+
+
+
