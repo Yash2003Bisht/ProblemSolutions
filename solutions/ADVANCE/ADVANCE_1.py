5c6f63a892de726b7316c3c8a74fdc34a815db3a
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question ADVANCE

---
+++ solutions/ADVANCE/ADVANCE_1.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.codechef.com/problems/ADVANCE
+
+for _ in range(int(input())):
+    n, x = map(int, input().split(" "))
+    print("yes" if x >= n and x <= n+200 else "no")
+
