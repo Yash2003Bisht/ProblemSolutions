ca1afd31df0626828cff5981ee45dd81838789a2
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question LCH15JAB

---
+++ solutions/LCH15JAB/LCH15JAB_1.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.codechef.com/problems/LCH15JAB
+
+from collections import Counter
+
+for _ in range(int(input())):
+    s = Counter(input())
+    max_value = max(s.values())
+    value_sum = list(s.values())
+    value_sum.remove(max_value)
+    
+    if sum(value_sum) == max_value:
+        print("YES")
+    else:
+        print("NO")
+
+
