638aa30ebbcbd1ec0cc44866043905a3cbcbba95
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question EQUALIZEAB

---
+++ solutions/EQUALIZEAB/EQUALIZEAB_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/EQUALIZEAB
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,b,x;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d%d", &a,&b,&x);
+        if ((a-b)%(2*x) == 0){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/EQUALIZEAB/EQUALIZEAB_2.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/EQUALIZEAB
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int t,a,b,x;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d%d", &a,&b,&x);
+        if (abs(a-b)%2*x == 0){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
