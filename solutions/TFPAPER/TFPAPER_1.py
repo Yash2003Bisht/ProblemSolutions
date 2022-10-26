13b4114809bc924650308d9e4de8b4eb84bc177d
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question TFPAPER

---
+++ solutions/TFPAPER/TFPAPER_1.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.codechef.com/problems/TFPAPER
+
+for _ in range(int(input())):
+    n, k = map(int, input().split(" "))
+    print(n - k)
+
---
+++ solutions/TFPAPER/TFPAPER_2.java
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/TFPAPER
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int T = sc.nextInt();
+	    sc.nextLine();
+	    for(int i = 0; i < T; i++){
+	        int[] n_k = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+	        System.out.println(n_k[0] - n_k[1]);
+	    }
+	}
+}
+
+
