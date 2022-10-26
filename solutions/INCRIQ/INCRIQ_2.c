745e6acd34c992750f1431f24f51e84707fae196
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question INCRIQ

---
+++ solutions/INCRIQ/INCRIQ_2.c
@@ -0,0 +1,18 @@
+// QUESTION URL: https://www.codechef.com/problems/INCRIQ
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int d;
+	scanf("%d", &d);
+	if (d > 170){
+	    printf("YES\n");
+	} else{
+	    printf("NO\n");
+	}
+	return 0;
+}
+
+
+
