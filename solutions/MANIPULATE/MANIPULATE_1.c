a4439acd1717cf66c9a92dd57adc19b64fc2fbe8
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question MANIPULATE

---
+++ solutions/MANIPULATE/MANIPULATE_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/MANIPULATE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&x,&y);
+        if(x<y){
+            printf("NO\n");
+        }else{
+            printf("YES\n");
+        }
+    }
+	return 0;
+}
+
+
+
