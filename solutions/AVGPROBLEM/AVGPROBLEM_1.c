8ff202cb1fcf4f2ac305724f4462d437bd4fdf87
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question AVGPROBLEM

---
+++ solutions/AVGPROBLEM/AVGPROBLEM_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/AVGPROBLEM
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,b,c;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d%d",&a,&b,&c);
+        if ((float)(a+b)/2 > c){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
