d0f27a3857450a7fe103c19f6a14c820bc4d24e9
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question BUYLAMP

---
+++ solutions/BUYLAMP/BUYLAMP_2.py
@@ -0,0 +1,18 @@
+# QUESTION URL: https://www.codechef.com/problems/BUYLAMP
+
+# n -> number of lamps chef want to buy
+# k -> number of lamps should be red
+# x -> red lamp cost
+# y -> blue lamp cost
+
+for _ in range(int(input())):
+    n, k, x, y = map(int, input().split(" "))
+    
+    if n == k and k == 0:
+        print(x*n)
+    
+    else:
+        red_lamps_cost = x * k
+        print(red_lamps_cost + (n - k) * min(x,y))
+        
+
