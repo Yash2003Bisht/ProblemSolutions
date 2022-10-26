9ed7605710f69e727badf7fec17fe72fb99ea209
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CANDY123

---
+++ solutions/CANDY123/CANDY123_1.py
@@ -0,0 +1,19 @@
+# QUESTION URL: https://www.codechef.com/problems/CANDY123
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    candy = 1
+    while True:
+        if candy%2 == 0:
+            b -= candy
+            if b < 0:
+                print('Limak')
+                break
+        else:
+            a -= candy
+            if a < 0:
+                print('Bob')
+                break
+        candy += 1
+
+
---
+++ solutions/CANDY123/CANDY123_2.py
@@ -0,0 +1,21 @@
+# QUESTION URL: https://www.codechef.com/problems/CANDY123
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    candy = 1
+    while True:
+        if candy%2 == 0:
+            if b >= candy:
+                b -= candy
+            else:
+                print('Limak')
+                break
+        else:
+            if a >= candy:
+                a -= candy
+            else:
+                print('Bob')
+                break
+        candy += 1
+
+
