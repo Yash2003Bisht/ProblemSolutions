7918dc92e0407e85c860af958ad5e1ff2bef7f26
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question MARKSTW

---
+++ solutions/MARKSTW/MARKSTW_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/MARKSTW
+
+#include <stdio.h>
+
+int main(void) {
+    int x,y;
+    scanf("%d%d", &x,&y);
+    if (x>=2*y){
+        printf("YES\n");
+    }else{
+        printf("NO\n");
+    }
+	return 0;
+}
+
+
+
