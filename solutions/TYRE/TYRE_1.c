73ac27bd3ec4ed806d2fe59873a40f6058e1b3ca
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question TYRE

---
+++ solutions/TYRE/TYRE_1.c
@@ -0,0 +1,16 @@
+// QUESTION URL: https://www.codechef.com/problems/TYRE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,m;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &n,&m);
+        printf("%d\n", (n*2)+(m*4));
+    }
+	return 0;
+}
+
+
+
