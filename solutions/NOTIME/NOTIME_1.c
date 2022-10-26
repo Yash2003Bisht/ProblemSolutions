41082470818be9af7c2ce8888b8f6b96e2f5fa69
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question NOTIME

---
+++ solutions/NOTIME/NOTIME_1.c
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/NOTIME
+
+#include <stdio.h>
+
+int main(void) {
+    int n,h,x,t,y=0;
+    scanf("%d%d%d", &n,&h,&x);
+    int z = h-x;
+    for (int i=0; i<n; i++){
+        scanf("%d", &t);
+        if (t>=z){
+            y=1;
+            break;
+        }
+    }
+    if(y){
+        printf("YES\n");
+    } else{
+        printf("NO\n");
+    }
+	return 0;
+}
+
+
+
