cc183d8250802902ffcf0d2ce5e2682fabd72e94
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question PROGLANG

---
+++ solutions/PROGLANG/PROGLANG_1.cpp
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/PROGLANG
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,a1,a2,b,b1,b2;
+        cin>>a>>b>>a1>>b1>>a2>>b2;
+        if((a==a1 || a==b1) && (b==a1 || b==b1))
+            cout<<1<<endl;
+        else if((a==a2 || a==b2) && (b==a2 || b==b2))
+            cout<<2<<endl;
+        else{
+            cout<<0<<endl;
+        }
+    }
+    
+	return 0;
+}
+
