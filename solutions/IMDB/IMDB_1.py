d4bf6b8f4f99f1fc95cf8d4686882ba8e178ec55
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question IMDB

---
+++ solutions/IMDB/IMDB_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/IMDB
+
+for _ in range(int(input())):
+    imdb = 0
+    n,x = map(int, input().split())
+    for _ in range(n):
+        s, r = map(int, input().split())
+        if x>=s and r>imdb:
+            imdb = r
+    print(imdb)
+
