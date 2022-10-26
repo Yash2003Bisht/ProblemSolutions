fd77cf53033ab45f1aaf72c7607a5d3285f594bc
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question KTTABLE

---
+++ solutions/KTTABLE/KTTABLE_1.py
@@ -0,0 +1,17 @@
+# QUESTION URL: https://www.codechef.com/problems/KTTABLE
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split(" ")))
+    b = list(map(int, input().split(" ")))
+    count = 0
+    for i in range(len(a)):
+        if i == 0:
+            count = 1 if b[i] <= a[i] else 0
+        else:
+            count += 1 if b[i] <= a[i] - a[i - 1] else 0
+
+    print(count)
+
+
+
---
+++ solutions/KTTABLE/KTTABLE_2.py
@@ -0,0 +1,19 @@
+# QUESTION URL: https://www.codechef.com/problems/KTTABLE
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split(" ")))
+    b = list(map(int, input().split(" ")))
+    time_unit = []
+    for i in range(len(a)):
+        if i == 0:
+            time_unit.append(a[i])
+        else:
+            time_unit.append(a[i] - a[i-1])
+
+    count = [1 for i in range(len(b)) if b[i] <= time_unit[i]]
+
+    print(sum(count))
+
+
+
