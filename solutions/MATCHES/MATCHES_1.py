e42552637f73c1a8c3a61f813d1150db1daa1e6c
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question MATCHES

---
+++ solutions/MATCHES/MATCHES_1.py
@@ -0,0 +1,23 @@
+# QUESTION URL: https://www.codechef.com/problems/MATCHES
+
+matches = {
+    '0': 6,
+    '1': 2,
+    '2': 5,
+    '3': 5,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 3,
+    '8': 7,
+    '9': 6
+}
+
+for _ in range(int(input())):
+    a, b = map(int, input().split(" "))
+    total = a + b
+    total_matches = 0
+    for i in str(total):
+        total_matches += matches[i]
+    print(total_matches)
+
