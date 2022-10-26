4bd6127b6bc8bb33573435081997ca5cea2d44c7
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:35:06 +0530

    attempted question BOBBANK

---
+++ solutions/BOBBANK/BOBBANK_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/BOBBANK
+
+#include <stdio.h>
+
+int main(void) {
+    int t, w, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &w, &x, &y, &z);
+        printf("%d\n", w+((x-y)*z));
+    }
+	return 0;
+}
+
+
+
