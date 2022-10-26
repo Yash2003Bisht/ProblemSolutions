115ceada3563018c6af8e83ecfae219aad3bc04f
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question MARARUN

---
+++ solutions/MARARUN/MARARUN_1.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.codechef.com/problems/MARARUN
+
+from bisect import bisect
+
+categories = [10, 21, 42]
+
+for _ in range(int(input())):
+    a, b, c, d, e = map(int, input().split(" "))
+    price_pool = [c, d, e]
+    total_km_traveled_by_chef = a*b
+    index = bisect(categories, total_km_traveled_by_chef) - 1
+    print(0 if index < 0 else price_pool[index])
+
