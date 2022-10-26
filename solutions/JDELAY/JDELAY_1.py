c91448e16fa63fd9d8a05e1ac77ee1e4088c1019
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question JDELAY

---
+++ solutions/JDELAY/JDELAY_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/JDELAY
+
+for _ in range(int(input())):
+    n = int(input())
+    counter = 0
+    for _ in range(n):
+        a, b = map(int, input().split(" "))
+        if b-a > 5:
+            counter += 1
+    print(counter)
+
