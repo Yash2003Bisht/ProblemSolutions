75a2293a46f0704a95a6c2b1e79d85b68bac744f
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question DARLIG

---
+++ solutions/DARLIG/DARLIG_1.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.codechef.com/problems/DARLIG
+
+for _ in range(int(input())):
+    a, b = map(int, input().split(" "))
+    
+    if b:
+        if a%4 == 0:
+            print('Off' if b == 0 else 'On')
+        else:
+            print('Ambiguous')
+    else:
+        if a%4 == 0:
+            print('Off')
+        else:
+            print('On')
+
---
+++ solutions/DARLIG/DARLIG_2.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/DARLIG
+
+for _ in range(int(input())):
+    a, b = map(int, input().split(" "))
+    if a%4 == 0:
+        print('Off' if b == 0 else 'On')
+    else:
+        print('Ambiguous')
+
