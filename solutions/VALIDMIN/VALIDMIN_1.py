85cd85034a45eed13fed7c43a58d62fc6bf854f6
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question VALIDMIN

---
+++ solutions/VALIDMIN/VALIDMIN_1.py
@@ -0,0 +1,10 @@
+# QUESTION URL: https://www.codechef.com/problems/VALIDMIN
+
+for _ in range(int(input())):
+    lst = list(map(int, input().split(" ")))
+    lst.sort()
+    if lst[0] == lst[1]:
+        print('YES')
+    else:
+        print('NO')
+
---
+++ solutions/VALIDMIN/VALIDMIN_2.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/VALIDMIN
+
+for _ in range(int(input())):
+    a, b, c = map(int, input().split(" "))
+    if min(a, b) <= min(b, c) <= min(c,a):
+        print("YES")
+    else:
+        print("NO")
+
