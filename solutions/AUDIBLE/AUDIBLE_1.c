0011a3e9f2c74f608a2cbbb5bfbd26a7725721ac
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question AUDIBLE

---
+++ solutions/AUDIBLE/AUDIBLE_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/AUDIBLE
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &x);
+        if (x >= 67 && x <= 45000){
+            printf("YES\n");
+        } else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
