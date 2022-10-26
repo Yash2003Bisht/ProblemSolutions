269f12109cfb9fbfabccb188d7fe11dcb9e59442
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question TALLER

---
+++ solutions/TALLER/TALLER_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/TALLER
+
+for _ in range(int(input())):
+    x, y = map(int, input().split(" "))
+    if x > y:
+        print("A")
+    else:
+        print("B")
+
