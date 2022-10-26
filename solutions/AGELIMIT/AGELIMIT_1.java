a3527fc5d9496237046ea9bcb29b05ac7bbe02fd
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question AGELIMIT

---
+++ solutions/AGELIMIT/AGELIMIT_1.java
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/AGELIMIT
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    sc.nextLine();
+	    for (int i = 0; i < t; i++){
+	        int[] data = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+	        if(data[2] >= data[0] && data[2] < data[1]){
+	            System.out.println("YES");
+	        } else{
+	            System.out.println("NO");
+	        }
+	    }
+	}
+}
+
+
