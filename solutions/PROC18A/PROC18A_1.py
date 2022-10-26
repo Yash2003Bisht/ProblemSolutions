ecadb4b894e74b38d8cfdec11d4a448989ae836a
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question PROC18A

---
+++ solutions/PROC18A/PROC18A_1.py
@@ -0,0 +1,15 @@
+# QUESTION URL: https://www.codechef.com/problems/PROC18A
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    g = list(map(int, input().split()))
+    max_g = 0
+    for i in range(n-1):
+        total = sum(g[i:k+i])
+        if total > max_g:
+            max_g = total
+
+    print(max_g)
+
+
+
