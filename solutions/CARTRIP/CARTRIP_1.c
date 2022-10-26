255fb9237b09e385e8aa8243ad47082a1256f1da
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question CARTRIP

---
+++ solutions/CARTRIP/CARTRIP_1.c
@@ -0,0 +1,15 @@
+// QUESTION URL: https://www.codechef.com/problems/CARTRIP
+
+#include <stdio.h>
+
+int main(){
+    int t, x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &x);
+        int cost = x<=300?3000:x*10;
+        printf("%d\n", cost);
+    }
+    return 0;
+}
+
