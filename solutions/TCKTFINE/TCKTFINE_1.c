a053c0f0e5ff852168343990e9f52ba5db5b6458
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question TCKTFINE

---
+++ solutions/TCKTFINE/TCKTFINE_1.c
@@ -0,0 +1,16 @@
+// QUESTION URL: https://www.codechef.com/problems/TCKTFINE
+
+#include <stdio.h>
+
+int main(void) {
+	int t,x,p,q;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d%d", &x,&p,&q);
+	    printf("%d\n",(p-q)*x);
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/TCKTFINE/TCKTFINE_2.c
@@ -0,0 +1,16 @@
+// QUESTION URL: https://www.codechef.com/problems/TCKTFINE
+
+#include <stdio.h>
+
+int main(void) {
+	int t,x,p,q;
+	scanf("%d", &t);
+	for (int i=0; i<t; i++){
+	    scanf("%d%d%d", &x,&p,&q);
+	    printf("%d\n",(p-q)*x);
+	}
+	return 0;
+}
+
+
+
