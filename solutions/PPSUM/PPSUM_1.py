f1c86e89b1bd63ac8dbef5e95dc9fca935ce0213
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question PPSUM

---
+++ solutions/PPSUM/PPSUM_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/PPSUM
+
+for _ in range(int(input())):
+    a, b = map(int, input().split(" "))
+    counter = b
+    for i in range(a):
+        counter += sum(range(1, counter))
+    print(counter)
+
---
+++ solutions/PPSUM/PPSUM_2.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.codechef.com/problems/PPSUM
+
+for _ in range(int(input())):
+    a, b = map(int, input().split(" "))
+    print(sum(range(1, a*b+1)))
+
