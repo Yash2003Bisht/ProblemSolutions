edb4d603345d1aa8656f1dbc02f0137f1711cd36
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question HEADBOB

---
+++ solutions/HEADBOB/HEADBOB_1.py
@@ -0,0 +1,12 @@
+# QUESTION URL: https://www.codechef.com/problems/HEADBOB
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    if s.count('I') >= 1:
+        print("INDIAN")
+    elif len(set(s)) == 1 and s[0] == 'N':
+        print("NOT SURE")
+    else:
+        print("NOT INDIAN")
+
---
+++ solutions/HEADBOB/HEADBOB_4.c
@@ -0,0 +1,36 @@
+// QUESTION URL: https://www.codechef.com/problems/HEADBOB
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d",&n);
+        char s[n];
+        scanf("%s",&s);
+        int indian = 0;
+        int cant_say = 0;
+        
+        for(int i=0; i<n; i++){
+            if(s[i] == 'I'){
+                indian = 1;
+                break;
+            } else if(s[i] == 'N'){
+                cant_say++;
+            }
+        }
+        
+        if(indian){
+            printf("INDIAN\n");
+        }else if(cant_say == n){
+            printf("NOT SURE");
+        }else{
+            printf("NOT INDIAN\n");
+        }
+    }
+	return 0;
+}
+
+
+
