f37e856d8eb0aa4cd0a376adf630bf2394995b50
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question EQUALISE

---
+++ solutions/EQUALISE/EQUALISE_1.py
@@ -0,0 +1,20 @@
+# QUESTION URL: https://www.codechef.com/problems/EQUALISE
+
+for _ in range(int(input())):
+    x, y = map(int, input().split(" "))
+    if x == y:
+        print('YES')
+    else:
+        min_value = min(x, y)
+        max_value = max(x, y)
+        while True:
+            min_value *= 2
+
+            if min_value == max_value:
+                print('YES')
+                break
+            elif min_value > max_value:
+                print('NO')
+                break
+
+
---
+++ solutions/EQUALISE/EQUALISE_2.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.codechef.com/problems/EQUALISE
+
+for _ in range(int(input())):
+    x, y = map(int, input().split(" "))
+    if x == y:
+        print("YES")
+    else:
+        if x % 2 == 0 and y % 2 == 0:
+            print("NO")
+        else:
+            print("YES")
+
+
