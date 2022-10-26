4bc3a089e151484a00d4af1782f57059fc7bd32e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question FEVER

---
+++ solutions/FEVER/FEVER_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/FEVER
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &x);
+        if(x>98){
+            printf("YES\n");
+        } else{
+            printf("NO\n");
+        }
+        
+    }
+	return 0;
+}
+
+
+
