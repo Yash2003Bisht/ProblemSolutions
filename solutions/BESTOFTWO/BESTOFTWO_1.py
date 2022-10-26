e85c4d346459ac33b12c3e2794d438479884548f
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question BESTOFTWO

---
+++ solutions/BESTOFTWO/BESTOFTWO_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/BESTOFTWO
+
+for _ in range(int(input())):
+    x, y = map(int, input().split(" "))
+    if x > y:
+        print(x)
+    else:
+        print(y)
+
