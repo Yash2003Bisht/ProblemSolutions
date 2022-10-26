ccc23ead74977b4d7ee526c9d7bdbd2c5291f208
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CREDSCORE

---
+++ solutions/CREDSCORE/CREDSCORE_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/CREDSCORE
+
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    scanf("%d", &x);
+    if (x>=750){
+        printf("YES\n");
+    } else{
+        printf("NO\n");
+    }
+	return 0;
+}
+
+
+
