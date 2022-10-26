1fd48e06f491a2e978bd5a1294334541c67b9868
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question FOOTCUP

---
+++ solutions/FOOTCUP/FOOTCUP_1.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.codechef.com/problems/FOOTCUP
+
+for _ in range(int(input())):
+    n, x = map(int, input().split(" "))
+    print("yes" if n == x and n >= 1 else "no")
+
