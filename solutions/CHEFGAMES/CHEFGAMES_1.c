a3f375c6c441199d596825e149bdf3948319d481
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CHEFGAMES

---
+++ solutions/CHEFGAMES/CHEFGAMES_1.c
@@ -0,0 +1,28 @@
+// QUESTION URL: https://www.codechef.com/problems/CHEFGAMES
+
+#include <stdio.h>
+
+int main(void) {
+    int t, r[4];
+    scanf("%d",&t);
+    while(t--){
+        int a=1;
+        for (int i=0; i<4; i++){
+            scanf("%d",&r[i]);
+        }
+        for (int i=0; i<4; i++){
+            if(r[i] == 1){
+                a = 0;
+                printf("OUT\n");
+                break;
+            }
+        }
+        if(a){
+            printf("IN\n");
+        }
+    }
+	return 0;
+}
+
+
+
