b07199322bcb344cd7790e7264b6e9ad55a34c67
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question SPECIALITY

---
+++ solutions/SPECIALITY/SPECIALITY_1.py
@@ -0,0 +1,15 @@
+# QUESTION URL: https://www.codechef.com/problems/SPECIALITY
+
+for _ in range(int(input())):
+    lst = list(map(int, input().split()))
+    max_lst_index = lst.index(max(lst))
+    
+    if max_lst_index == 0:
+        print('Setter')
+    
+    elif max_lst_index == 1:
+        print('Tester')
+        
+    else:
+        print('Editorialist')
+
