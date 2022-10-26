607e3fe663a2e3e41a7ce10d79eebd1d57d736bd
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question LUCKYFR

---
+++ solutions/LUCKYFR/LUCKYFR_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/LUCKYFR
+
+from collections import Counter
+
+for _ in range(int(input())):
+    s = Counter(input())
+    occurrence = s.get('4')
+    print(0 if occurrence is None else occurrence)
+
