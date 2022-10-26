57575150c4dca0a74f56e2fc9624289ea754c849
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question BALLBOX

---
+++ solutions/BALLBOX/BALLBOX_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/BALLBOX
+
+#include <stdio.h>
+
+int main(void) {
+	int t,n,k;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d", &n,&k);
+	    
+	    if(n>=k*(k+1)/2){
+	        printf("YES\n");
+	    } else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/BALLBOX/BALLBOX_3.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/BALLBOX
+
+#include <stdio.h>
+
+int main(void) {
+	int t,n,k;
+	scanf("%d", &t);
+	while(t--){
+	    scanf("%d%d", &n,&k);
+	    
+	    if(k==n && k==1){
+	        printf("YES\n");
+	    } else if(k>n || n==k){
+	        printf("NO\n");
+	    } else{
+	        printf("YES\n");
+	    }
+	}
+	return 0;
+}
+
+
+
