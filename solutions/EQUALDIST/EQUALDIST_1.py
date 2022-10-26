ca2a364f6be67b0176cf852123024ced3f54d8fe
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question EQUALDIST

---
+++ solutions/EQUALDIST/EQUALDIST_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/EQUALDIST
+
+for _ in range(int(input())):
+    a, b = map(int, input().split(" "))
+    if (a+b) % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
+
