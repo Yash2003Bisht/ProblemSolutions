4e359ab7191f7651bc46d142aac26331206fc993
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question NEARESTCOURT

---
+++ solutions/NEARESTCOURT/NEARESTCOURT_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/NEARESTCOURT
+
+import math
+
+for _ in range(int(input())):
+    x, y = map(int, input().split(" "))
+    print(math.ceil((max(x,y)-min(x,y))/2))
+
+
---
+++ solutions/NEARESTCOURT/NEARESTCOURT_2.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/NEARESTCOURT
+
+for _ in range(int(input())):
+    x, y = map(int, input().split(" "))
+    abs_value = abs(x-y)
+    if abs_value%2 == 0:
+        print(abs_value//2)
+    else:
+        print(abs_value//2 + 1)
+        
+
