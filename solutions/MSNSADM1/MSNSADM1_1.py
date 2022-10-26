c760342694b96bd525246abe316653df12b9d591
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question MSNSADM1

---
+++ solutions/MSNSADM1/MSNSADM1_1.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.codechef.com/problems/MSNSADM1
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    bt_scr = 0
+    for i,j in zip(a,b):
+        crr_scr = i*20 - j*10
+        if crr_scr>bt_scr:
+            bt_scr = crr_scr
+    print(bt_scr)
+
---
+++ solutions/MSNSADM1/MSNSADM1_2.c
@@ -0,0 +1,31 @@
+// QUESTION URL: https://www.codechef.com/problems/MSNSADM1
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,i;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        int a[n];
+        int b,r=0;
+        for(i=0; i<n; i++){
+            scanf("%d", &a[i]);
+        }
+        scanf("\n");
+        for(i=0; i<n; i++){
+            scanf("%d", &b);
+            int s = a[i]*20 - b*10;
+            if (s>r){
+                r = s;
+            }
+        }
+        
+        printf("%d\n", r);
+        
+    }
+	return 0;
+}
+
+
+
