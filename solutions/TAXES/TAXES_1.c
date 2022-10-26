5d19bacad9d5e80d4e932df3d454348f063c5704
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question TAXES

---
+++ solutions/TAXES/TAXES_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/TAXES
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &x);
+        if (x>100){
+            printf("%d\n", x-10);
+        } else{
+            printf("%d\n", x);
+        }
+    }
+	return 0;
+}
+
+
+
