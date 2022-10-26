aeebfdaac5ede21f9dfaf0e93ed637eeea9aa200
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question HOTCOLD

---
+++ solutions/HOTCOLD/HOTCOLD_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/HOTCOLD
+
+for _ in range(int(input())):
+    x = int(input())
+    if x > 20:
+        print("HOT")
+    else:
+        print("COLD")
+
