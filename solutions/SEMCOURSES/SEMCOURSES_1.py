7b301b20c0b1349c91d972e9e49bdaddeda87f9c
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question SEMCOURSES

---
+++ solutions/SEMCOURSES/SEMCOURSES_1.py
@@ -0,0 +1,7 @@
+# QUESTION URL: https://www.codechef.com/problems/SEMCOURSES
+
+for _ in range(int(input())):
+    x, y, z = map(int, input().split(" "))
+    total_units = x * y
+    print(total_units * z)
+
