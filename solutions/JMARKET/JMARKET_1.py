7c36c61b12ca23b5052dabb87f4cf5a2312a7ae2
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question JMARKET

---
+++ solutions/JMARKET/JMARKET_1.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.codechef.com/problems/JMARKET
+
+for _ in range(int(input())):
+    x, a, b, c = map(int, input().split(" "))
+    fruit_prices = [a, b, c]
+    fruit_prices.sort()
+    print((x - 1) * fruit_prices[0] + fruit_prices[1])
+
---
+++ solutions/JMARKET/JMARKET_2.py
@@ -0,0 +1,10 @@
+# QUESTION URL: https://www.codechef.com/problems/JMARKET
+
+for _ in range(int(input())):
+    x, a, b, c = map(int, input().split(" "))
+    fruits_prices = [a, b, c]
+    first_fruit = min(fruits_prices)
+    fruits_prices.remove(first_fruit) # remove the fruit
+    second_fruit = min(fruits_prices)
+    print(second_fruit + first_fruit*(x-1))
+
