e240b027f85a57af16ce8c628052eb0db4b98ba1
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question PASSTHEEXAM

---
+++ solutions/PASSTHEEXAM/PASSTHEEXAM_1.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.codechef.com/problems/PASSTHEEXAM
+
+for _ in range(int(input())):
+    n, x, y = map(int, input().split(" "))
+    print("pass" if sum([n,x,y]) >= 100 and min([n, x, y]) >= 10 else "fail")
+
