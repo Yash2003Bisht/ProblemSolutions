9bba08e881086eca0c2414486b942c421c9f9e31
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question HOSTELROOM

---
+++ solutions/HOSTELROOM/HOSTELROOM_1.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/HOSTELROOM
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,x,a;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&n,&x);
+        int max_a=x,counter=x;
+        for(int i=0; i<n; i++){
+            scanf("%d", &a);
+            counter += a;
+            if (counter > max_a){
+                max_a = counter;
+            }
+        }
+        printf("%d\n", max_a);
+    }
+	return 0;
+}
+
+
+
