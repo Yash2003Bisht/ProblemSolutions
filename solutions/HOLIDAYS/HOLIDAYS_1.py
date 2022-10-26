da12d56cd3126236527462b714b07e4915cedbb5
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question HOLIDAYS

---
+++ solutions/HOLIDAYS/HOLIDAYS_1.py
@@ -0,0 +1,15 @@
+# QUESTION URL: https://www.codechef.com/problems/HOLIDAYS
+
+for _ in range(int(input())):
+    n, w = map(int, input().split())
+    a = list(map(int, input().split()))
+    total = 0
+    work = 0
+    while total < w:
+        _max = max(a)
+        total += _max
+        work += 1
+        a.remove(_max)
+
+    print(n - work)
+
---
+++ solutions/HOLIDAYS/HOLIDAYS_2.py
@@ -0,0 +1,17 @@
+# QUESTION URL: https://www.codechef.com/problems/HOLIDAYS
+
+for _ in range(int(input())):
+    n, w = map(int, input().split())
+    a = list(map(int, input().split()))
+    a.sort(reverse=True)
+    total = 0
+    work = 0
+    for i in a:
+        if total >= w:
+            break
+        else:
+            total += i
+            work += 1
+    
+    print(n-work)
+
