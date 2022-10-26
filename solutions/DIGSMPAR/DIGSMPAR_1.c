71c82ed2d0b0edab38343e5f2aa333efb7cf1849
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question DIGSMPAR

---
+++ solutions/DIGSMPAR/DIGSMPAR_1.c
@@ -0,0 +1,44 @@
+// QUESTION URL: https://www.codechef.com/problems/DIGSMPAR
+
+#include <stdio.h>
+
+int digitSum(int num){
+    int sum = 0;
+
+    while(num){
+        sum += num%10;
+        num = num/10;
+    }
+    
+    return sum;
+}
+
+
+int main(void) {
+    int t, n, sum;
+    char flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        sum = digitSum(n);
+        
+        if (sum%2 == 0) flag = 'e';
+        else flag = 'o';
+        
+        while ((sum%2 != 0 && flag == 'o') || (sum%2 == 0 && flag == 'e')){
+            n++;
+            sum = digitSum(n);
+        }
+        
+        printf("%d\n", n);
+        
+        
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/DIGSMPAR/DIGSMPAR_3.c
@@ -0,0 +1,47 @@
+// QUESTION URL: https://www.codechef.com/problems/DIGSMPAR
+
+#include <stdio.h>
+
+int digitSum(int num){
+    int sum = 0;
+
+    while(num){
+        sum += num%10;
+        num = num/10;
+    }
+    
+    return sum;
+}
+
+
+int main(void) {
+    int t, n, sum;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        sum = digitSum(n);
+        
+        if (sum%2 == 0){
+            while (sum%2 == 0){
+                n++;
+                sum = digitSum(n);
+            }
+        } else{
+            while (sum%2 != 0){
+                n++;
+                sum = digitSum(n);
+            }
+        }
+        
+        printf("%d\n", n);
+        
+        
+        
+    }
+    
+	return 0;
+}
+
+
+
