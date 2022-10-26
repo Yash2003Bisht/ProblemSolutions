8bced07ef2ccefa9b9de436061fdb74f8e543428
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question ODDSUMPAIR

---
+++ solutions/ODDSUMPAIR/ODDSUMPAIR_1.c
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/ODDSUMPAIR
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &a, &b, &c);
+        x = a+b;
+        y = a+c;
+        z = b+c;
+        
+        if (x%2!=0 || y%2!=0 || z%2!=0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
