9b21796bbbcac444547c5c11279ba06d297f84f8
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question TRAVELPS

---
+++ solutions/TRAVELPS/TRAVELPS_1.c
@@ -0,0 +1,26 @@
+// QUESTION URL: https://www.codechef.com/problems/TRAVELPS
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,a,b;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d", &n,&a,&b);
+        char s[n];
+        int t_time=0;
+        scanf("%s", &s);
+        for(int i=0; i<n; i++){
+            if (s[i] == '0'){
+                t_time += a;
+            } else{
+                t_time += b;
+            }
+        }
+        printf("%d\n", t_time);
+    }
+	return 0;
+}
+
+
+
