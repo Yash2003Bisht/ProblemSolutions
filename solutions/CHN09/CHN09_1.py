d7bcb02857f49b5ed1075a6f6da598dcfd6f2e53
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question CHN09

---
+++ solutions/CHN09/CHN09_1.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.codechef.com/problems/CHN09
+
+from collections import Counter
+
+for _ in range(int(input())):
+    s = Counter(input())
+    print(0 if s.get('a') is None or s.get('b') is None else min(s.get('a'), s.get('b')))
+
