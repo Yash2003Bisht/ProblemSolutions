579870287550e6c5a02733ac25a19af949b2a263
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question QUEENATTACK

---
+++ solutions/QUEENATTACK/QUEENATTACK_1.c
@@ -0,0 +1,19 @@
+// QUESTION URL: https://www.codechef.com/problems/QUEENATTACK
+
+#include <stdio.h>
+#define MIN(a,b) (a>b?b:a)
+
+int main(void) {
+    int t, n, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &x, &y);
+        printf("%d\n", (n*2)-2 + MIN(x-1, y-1) + MIN(n-x, y-1) + MIN(x-1, n-y) + MIN(n-x, n-y));
+        
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/QUEENATTACK/QUEENATTACK_2.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/QUEENATTACK
+
+#include <stdio.h>
+#define MIN(a,b) (a>b?b:a)
+
+int main(void) {
+    int t, n, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &x, &y);
+        int count = (n*2)-2 + MIN(x-1, y-1) + MIN(n-x, y-1) + MIN(x-1, n-y) + MIN(n-x, n-y);
+        printf("%d\n", count);
+        
+    }
+	return 0;
+}
+
+
+
