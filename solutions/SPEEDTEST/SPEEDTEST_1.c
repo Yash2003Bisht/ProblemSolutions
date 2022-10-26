36537f52932c16bbcae9c3642317dec7cd0e1f40
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question SPEEDTEST

---
+++ solutions/SPEEDTEST/SPEEDTEST_1.c
@@ -0,0 +1,26 @@
+// QUESTION URL: https://www.codechef.com/problems/SPEEDTEST
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,x,b,y;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d%d", &a,&x,&b,&y);
+        float k_ph_a = (float) a/x;
+        float k_ph_b = (float) b/y;
+        
+        if(k_ph_a > k_ph_b){
+            printf("Alice\n");
+        } else if(k_ph_a < k_ph_b){
+            printf("Bob\n");
+        } else{
+            printf("Equal\n");
+        }
+        
+    }
+	return 0;
+}
+
+
+
