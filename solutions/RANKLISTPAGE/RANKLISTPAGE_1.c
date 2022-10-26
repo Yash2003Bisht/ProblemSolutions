275c96bf75411be92629969bf0deb1ef142f4f34
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question RANKLISTPAGE

---
+++ solutions/RANKLISTPAGE/RANKLISTPAGE_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/RANKLISTPAGE
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &x);
+        int page = x%25 == 0 ? x/25 : (x/25)+1;
+        printf("%d\n", page);
+    }
+	return 0;
+}
+
+
+
