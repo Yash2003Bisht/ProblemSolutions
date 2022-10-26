7cceebe4b295c720404dda32d8007e07057e0c84
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question BATTERYLOW

---
+++ solutions/BATTERYLOW/BATTERYLOW_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/BATTERYLOW
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t, b;
+	scanf("%d", &t);
+	while (t--){
+	    scanf("%d", &b);
+	    if (b <= 15){
+	        printf("YES\n");
+	    } else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
+
+
