8babf6b8d9df75c178ae429d0b74d94c391d8553
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question PLAYPIAN

---
+++ solutions/PLAYPIAN/PLAYPIAN_2.py
@@ -0,0 +1,26 @@
+# QUESTION URL: https://www.codechef.com/problems/PLAYPIAN
+
+for _ in range(int(input())):
+    entries = input()
+
+    if len(set(entries)) == 1:
+        print('no')
+
+    else:
+        next_turn = 'A' if entries[0] == 'B' else 'B'
+        for i in range(1, len(entries)):
+            if next_turn != entries[i]:
+                print('no')
+                break
+
+            if i % 2 != 0:
+                next_turn = entries[i]
+            else:
+                next_turn = 'A' if entries[i] == 'B' else 'B'
+
+        else:
+            print('yes')
+
+
+
+
