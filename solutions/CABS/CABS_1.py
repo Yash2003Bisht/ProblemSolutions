81e0f27ba1c8c32aafbece07af1cb4ceb1114820
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question CABS

---
+++ solutions/CABS/CABS_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/CABS
+
+for _ in range(int(input())):
+    n, x = map(int, input().split(" "))
+    if n < x:
+        print("first")
+    elif x < n:
+        print("second")
+    else:
+        print("any")
+
