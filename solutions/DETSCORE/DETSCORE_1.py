f6d8064f28b462c5468c33a8b73aae60106749a5
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question DETSCORE

---
+++ solutions/DETSCORE/DETSCORE_1.py
@@ -0,0 +1,7 @@
+# QUESTION URL: https://www.codechef.com/problems/DETSCORE
+
+for _ in range(int(input())):
+    x, n = map(int, input().split(" "))
+    each_case_marks = x//10
+    print(n*each_case_marks)
+
