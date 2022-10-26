ce975552d8ba46bb34832278f5987662106f750e
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question MUFFINS3

---
+++ solutions/MUFFINS3/MUFFINS3_1.c
@@ -0,0 +1,16 @@
+// QUESTION URL: https://www.codechef.com/problems/MUFFINS3
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &a);
+        printf("%d\n", (a/2)+1);
+    }
+	return 0;
+}
+
+
+
