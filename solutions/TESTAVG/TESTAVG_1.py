1274b47dfdae607c730f5422462ac2b1e69d3acc
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question TESTAVG

---
+++ solutions/TESTAVG/TESTAVG_1.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/TESTAVG
+
+for _ in range(int(input())):
+    marks = list(map(int, input().split()))
+    marks.sort()
+    avg = (marks[0] + marks[1]) / 2
+    if avg < 35:
+        print('Fail')
+    else:
+        print('Pass')
+
