cb478fcf8dbed21fc4e036120eb74adbb3b4db47
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question LASTLEVELS

---
+++ solutions/LASTLEVELS/LASTLEVELS_3.py
@@ -0,0 +1,17 @@
+# QUESTION URL: https://www.codechef.com/problems/LASTLEVELS
+
+# x -> levels remaining in the game
+# y -> minutes to complete
+# z -> break time
+
+for _ in range(int(input())):
+    x, y, z = map(int, input().split(" "))
+    if x <= 3:
+        print(y*x)
+    else:
+        if x % 3 != 0:
+            break_time = (x%3) + 1
+        else:
+            break_time = (x//3) - 1
+        print((y*x)+(break_time*z))
+
