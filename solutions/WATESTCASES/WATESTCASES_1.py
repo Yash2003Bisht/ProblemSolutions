73766c753916a7dc08ea3bc08802083c12151f01
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:13 +0530

    attempted question WATESTCASES

---
+++ solutions/WATESTCASES/WATESTCASES_1.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.codechef.com/problems/WATESTCASES
+
+for _ in range(int(input())):
+    n = int(input())
+    s = list(map(int, input().split()))
+    v = input()
+    smallest = 101
+    for i in range(len(v)):
+        if v[i] == '0' and smallest > s[i]:
+            smallest = s[i]
+    print(smallest)
+            
+
---
+++ solutions/WATESTCASES/WATESTCASES_3.c
@@ -0,0 +1,33 @@
+// QUESTION URL: https://www.codechef.com/problems/WATESTCASES
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	int t;
+	scanf("%d", &t);
+	while(t--){
+		int n;
+		scanf("%d", &n);
+		int s[n];
+		char v[n];
+		for (int i=0; i<n; i++){
+			scanf("%d", &s[i]);
+		}
+		scanf("\n"); // point to the next line
+		for (int i=0; i<n; i++){
+			scanf("%c", &v[i]);
+		}
+		int  smallest = s[0];
+		for (int i=1; i<=n; i++){
+			if (v[i] == '0' && smallest > s[i]){
+				smallest = s[i];
+			}
+		}
+		printf("%d\n", smallest);
+	}
+	return 0;
+}
+
+
+
