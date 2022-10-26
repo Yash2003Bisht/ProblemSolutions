b428ec10f6088d71c9284d59ecefa60e217803c5
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question MAGICHF

---
+++ solutions/MAGICHF/MAGICHF_4.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/MAGICHF
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,x,s,a,b,i;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d", &n,&x,&s);
+        for(i=0; i<s; i++){
+            scanf("%d%d", &a,&b);
+            if (a!=x){
+                x = a;
+            } else{
+                x = b;
+            }
+        }
+        printf("%d\n", x);
+    }
+	return 0;
+}
+
+
+
