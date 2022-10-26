93b4000abd6dd4e960d47ffbcb2feb597c20cc5b
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:11 +0530

    attempted question TVDISC

---
+++ solutions/TVDISC/TVDISC_1.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.codechef.com/problems/TVDISC
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split(" "))
+    cost_a = a - c
+    cost_b = b - d
+    if cost_a < cost_b:
+        print("First")
+    elif cost_b < cost_a:
+        print("Second")
+    else:
+        print("Any")
+
