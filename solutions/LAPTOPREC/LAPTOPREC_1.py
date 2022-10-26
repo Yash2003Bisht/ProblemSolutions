763eb4bf83737507c19d2964d8484746c99d42a7
yash2003bisht <yash2003bisht@gmail.com>
2022-10-26 20:22:12 +0530

    attempted question LAPTOPREC

---
+++ solutions/LAPTOPREC/LAPTOPREC_1.py
@@ -0,0 +1,14 @@
+# QUESTION URL: https://www.codechef.com/problems/LAPTOPREC
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())
+    a = Counter(map(int, input().split()))
+    most_common = list(a.most_common()[0])
+    repeats = [value for _, value in a.items()]
+    if repeats.count(most_common[1]) > 1:
+        print("CONFUSED")
+    else:
+        print(most_common[0])
+
