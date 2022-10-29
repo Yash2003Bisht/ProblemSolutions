ab8926d989d333a2ef9bd1583c3238a724a86844
yash2003bisht <yash2003bisht@gmail.com>
2022-10-29 17:12:35 +0530

    attempted question MAXDIFFMIN

---
+++ solutions/MAXDIFFMIN/MAXDIFFMIN_1.c
@@ -0,0 +1,36 @@
+// QUESTION URL: https://www.codechef.com/problems/MAXDIFFMIN
+
+#include <stdio.h>
+
+int main(void) {
+    int t,a,b,c;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &a);
+        scanf("%d", &b);
+        scanf("%d", &c);
+        
+        int max, min;
+        
+        if (a > b && a > c)
+            max = a;
+        else if (b > a && b > c)
+            max = b;
+        else
+            max = c;
+        
+        if (a < b && a < c)
+            min = a;
+        else if (b < a && b < c)
+            min = b;
+        else
+            min = c;
+            
+        printf("%d\n", max-min);
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/MAXDIFFMIN/MAXDIFFMIN_2.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/MAXDIFFMIN
+
+import sys
+def get_ints(): return map(int, sys.stdin.readline().strip().split())
+ 
+for _ in range(int(input())):
+    lst = list(get_ints())
+    sys.stdout.write(str(max(lst) - min(lst)) + '\n')
+
