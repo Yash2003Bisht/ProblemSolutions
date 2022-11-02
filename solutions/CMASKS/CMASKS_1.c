95679c8f3cf8eb8a3de768eb2c0c72689d64bcd9
yash2003bisht <yash2003bisht@gmail.com>
2022-11-02 15:46:29 +0530

    attempted question CMASKS - (TESTING)

---
+++ solutions/CMASKS/CMASKS_1.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/CMASKS
+
+#include <stdio.h>
+int main(void) 
+{
+	int t,i,x,a,b,y;
+	scanf("%d",&t);
+	for(i=1;i<=t;i++)
+	{
+	    scanf("%d %d", &x,&y);
+	    a=x*100;
+	    b=y*10;
+	    if(a>=b)
+	     printf("cloth\n");
+	    else
+	      printf("disposable\n");
+	}
+	return 0;
+}
+
+
+
