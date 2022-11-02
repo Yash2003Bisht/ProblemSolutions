a6e5cfc4b3fd954ff4d1cf07922562b5cb972b5c
yash2003bisht <yash2003bisht@gmail.com>
2022-11-02 15:43:50 +0530

    attempted question DIFFCONSEC - (TESTING)

---
+++ solutions/DIFFCONSEC/DIFFCONSEC_1.c
@@ -0,0 +1,38 @@
+// QUESTION URL: https://www.codechef.com/problems/DIFFCONSEC
+
+#include <stdio.h>
+#include <string.h>
+void clr();
+void clr()
+{
+    int x,i=0,k;
+    scanf("%d",&x);
+    char s[x];
+    scanf("%s",&s);
+    while(i<x)
+    {
+        if(s[i]==s[i+1]) k++;
+        
+        i++;
+    }
+
+    printf("%d\n",k);
+}
+
+
+
+
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	while(n--)
+	{
+	    clr();
+	}
+	return 0;
+}
+
+
+
+
