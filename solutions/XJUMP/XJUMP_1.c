37960751789fe73bebe506f76d50bc984fe91361
yash2003bisht <yash2003bisht@gmail.com>
2022-11-02 14:39:29 +0530

    attempted question XJUMP - (TESTING)

---
+++ solutions/XJUMP/XJUMP_1.c
@@ -0,0 +1,18 @@
+// QUESTION URL: https://www.codechef.com/problems/XJUMP
+
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    printf("%d\n",(x/y)+(x%y));
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/XJUMP/XJUMP_2.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.codechef.com/problems/XJUMP
+
+# cook your dish here
+import math
+for _ in range(int(input())):
+    n, h=map(int,input().split())
+    print(n//h+n%h)
+
