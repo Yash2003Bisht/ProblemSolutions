0fa00189b2ef3d8898870764f84a8c7a8a42814f
yash2003bisht <yash2003bisht@gmail.com>
2022-11-02 07:30:33 +0530

    attempted question CHEFSCORE - (TESTING)

---
+++ solutions/CHEFSCORE/CHEFSCORE_1.c
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.codechef.com/problems/CHEFSCORE
+
+#include <stdio.h>
+
+int main(void) {
+	int t,x,y,z;
+	scanf("%d",&t);
+	
+	while(t--) {
+	    scanf("%d%d%d",&x,&y,&z);
+	    if(z%y==0)
+	        printf("yes\n");
+	    else 
+	        printf("no\n");
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/CHEFSCORE/CHEFSCORE_2.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.codechef.com/problems/CHEFSCORE
+
+for _ in range(int(input())):
+    n,x,y = map(int,input().split())
+    if(y/x == int(y/x)):
+        print("YES")
+    else:
+        print("NO")
+    
+    
+
