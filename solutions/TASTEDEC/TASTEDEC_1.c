47062045f9231ef5dbf80c6a35c17bd928558ed4
yash2003bisht <yash2003bisht@gmail.com>
2022-11-06 14:12:58 +0530

    attempted question TASTEDEC - TESTING

---
+++ solutions/TASTEDEC/TASTEDEC_1.c
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/TASTEDEC
+// STATUS: accepted
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d %d",&x,&y);
+        if(2*x>5*y)
+            printf("Chocolate");
+        if(2*x<5*y)
+            printf("Candy");
+        if(2*x==5*y)
+            printf("Either");
+        putchar('\n');
+    }
+	
+	return 0;
+	
+}
+
+
+
