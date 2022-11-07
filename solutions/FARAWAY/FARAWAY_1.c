63c00abdea9718ec6a63a84e14452b6a84051ac9
yash2003bisht <yash2003bisht@gmail.com>
2022-11-07 10:21:32 +0530

    attempted question FARAWAY

---
+++ solutions/FARAWAY/FARAWAY_1.c
@@ -0,0 +1,33 @@
+// QUESTION URL: https://www.codechef.com/problems/FARAWAY
+// STATUS: accepted
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    long long int t ,n, m, a, b, total;
+    scanf("%lld", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &m);
+        total = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%lld", &a);
+            if (a <= m/2) b = m;
+            else b = 1;
+            
+            total += abs(a-b);
+            
+        }
+        
+        printf("%lld\n", total);
+        
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/FARAWAY/FARAWAY_3.c
@@ -0,0 +1,35 @@
+// QUESTION URL: https://www.codechef.com/problems/FARAWAY
+// STATUS: wrong answer
+
+#include <stdio.h>
+
+int main(void) {
+    int t ,n, m, a, b, total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &m);
+        total = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            if (a <= m/2){
+                b = m;
+            } else{
+                b = 1;
+            }
+            
+            total += abs(a-b);
+            
+        }
+        
+        printf("%d\n", total);
+        
+        
+    }
+    
+	return 0;
+}
+
+
+
