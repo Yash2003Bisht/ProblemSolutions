62167c315a3e2d4ec785b15993275734604b2ef3
yash2003bisht <yash2003bisht@gmail.com>
2022-11-08 07:12:20 +0530

    attempted question TWODISH

---
+++ solutions/TWODISH/TWODISH_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/TWODISH
+// STATUS: accepted
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,a,b,c;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d%d", &n,&a,&b,&c);
+        if (b>=n && a+c>=n){
+            printf("YES\n");
+        } else{
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
+
+
+
