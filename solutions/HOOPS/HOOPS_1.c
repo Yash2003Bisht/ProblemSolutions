4389442512a926d17efcf857471c5cd70e40d20c
yash2003bisht <yash2003bisht@gmail.com>
2022-11-08 07:12:20 +0530

    attempted question HOOPS

---
+++ solutions/HOOPS/HOOPS_1.c
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.codechef.com/problems/HOOPS
+// STATUS: accepted
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        printf("%d\n", (n/2)+1);
+    }
+	return 0;
+}
+
+
+
