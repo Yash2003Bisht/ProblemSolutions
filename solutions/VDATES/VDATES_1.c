17d3bc932fb05ea0f2c2b6b7f26f6bd3207057d1
yash2003bisht <yash2003bisht@gmail.com>
2022-11-08 07:12:20 +0530

    attempted question VDATES

---
+++ solutions/VDATES/VDATES_1.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/VDATES
+// STATUS: accepted
+
+#include <stdio.h>
+
+int main(void) {
+    int t,d,l,r;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d", &d,&l,&r);
+        if (d<l){
+            printf("Too Early\n");
+        } else if (d>r){
+            printf("Too Late\n");
+        } else{
+            printf("Take second dose now\n");
+        }
+    }
+	return 0;
+}
+
+
+
