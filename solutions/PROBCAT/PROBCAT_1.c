ee7b4c81fe9601276dcb5d06b3cc4498ac513fbc
yash2003bisht <yash2003bisht@gmail.com>
2022-11-08 07:12:20 +0530

    attempted question PROBCAT

---
+++ solutions/PROBCAT/PROBCAT_1.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/PROBCAT
+// STATUS: accepted
+
+#include <stdio.h>
+
+int main(void) {
+    int t,x;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &x);
+        if (x<100){
+            printf("Easy\n");
+        } else if (x<200){
+            printf("Medium\n");
+        } else{
+            printf("Hard\n");
+        }
+    }
+	return 0;
+}
+
+
+
