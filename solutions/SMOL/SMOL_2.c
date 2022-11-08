2b149aa443d617609c2c817cc3dfff0eef4e0e8f
yash2003bisht <yash2003bisht@gmail.com>
2022-11-08 07:12:20 +0530

    attempted question SMOL

---
+++ solutions/SMOL/SMOL_2.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/SMOL
+// STATUS: runtime error(SIGFPE)
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,k;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &n,&k);
+        if (n<k){
+            printf("%d\n", n);
+        } else{
+            printf("%d\n", n%k);
+        }
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/SMOL/SMOL_3.c
@@ -0,0 +1,32 @@
+// QUESTION URL: https://www.codechef.com/problems/SMOL
+// STATUS: time limit exceeded
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n,k;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d", &n,&k);
+        if (n==k){
+            printf("0\n");
+        } else if(n<k){
+            printf("%d\n",n);
+        } else{
+            int counter;
+            while (1){
+                counter = n-k;
+                if (counter >= 0){
+                    n = counter;
+                } else{
+                    break;
+                }
+            }
+            printf("%d\n", n);
+        }
+    }
+	return 0;
+}
+
+
+
