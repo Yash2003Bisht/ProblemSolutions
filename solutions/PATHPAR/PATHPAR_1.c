de8a752ed8b6c8e00bbab6356da112d335811a29
yash2003bisht <yash2003bisht@gmail.com>
2022-11-18 07:39:44 +0530

    attempted question PATHPAR

---
+++ solutions/PATHPAR/PATHPAR_1.c
@@ -0,0 +1,34 @@
+// QUESTION URL: https://www.codechef.com/problems/PATHPAR
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58);c = getchar_unlocked()) 
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+
+
+int main(void) {
+    int t, k;
+    long long int n;
+    scanint(&t);
+    
+    while(t--){
+        scanint(&n); scanint(&k);
+        if (n%2 == 0 || k == 1) printf("Yes\n");
+        else printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/PATHPAR/PATHPAR_3.c
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/PATHPAR
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, k;
+    long long int n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n,&k);
+        
+        if (n%2 == 0 || k == 1) printf("Yes\n");
+        else printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
