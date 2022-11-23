1276b20f96fddb000b6dbc8f74fd63dabada3a4e
yash2003bisht <yash2003bisht@gmail.com>
2022-11-23 06:05:07 +0530

    attempted question PLAYSTR

---
+++ solutions/PLAYSTR/PLAYSTR_1.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.codechef.com/problems/PLAYSTR
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.5M
+
+for _ in range(int(input())):
+    input()
+    s = input()
+    r = input()
+    
+    if s.count('1') == r.count('1') and s.count('0') == r.count('0'):
+        print("YES")
+    else:
+        print("NO")
+    
+
---
+++ solutions/PLAYSTR/PLAYSTR_3.c
@@ -0,0 +1,35 @@
+// QUESTION URL: https://www.codechef.com/problems/PLAYSTR
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1], r[n+1];
+        int a[2] = {0}, b[2] = {0};
+        
+        scanf("%s", &s);
+        scanf("%s", &r);
+        
+        for(int i=0; s[i] != '\0'; i++){
+            a[s[i] - 48]++;
+            b[r[i] - 48]++;
+        }
+        
+        
+        if (a[0] == b[0] && a[1] == b[1]) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
