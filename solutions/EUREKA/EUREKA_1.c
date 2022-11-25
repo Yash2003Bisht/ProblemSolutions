2f747a968cd0cff563f4214886e10f2c2307773b
yash2003bisht <yash2003bisht@gmail.com>
2022-11-25 06:14:37 +0530

    attempted question EUREKA

---
+++ solutions/EUREKA/EUREKA_1.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/EUREKA
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t,n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int ans = round(pow(0.143*n, n));
+        printf("%d\n", ans);
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/EUREKA/EUREKA_2.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.codechef.com/problems/EUREKA
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.7M
+
+for _ in range(int(input())):
+    n = int(input())
+    print(round((0.143*n)**n))
+
