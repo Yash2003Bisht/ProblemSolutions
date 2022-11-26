f8d3446eea4d89da1e60d3fcca9bc6b80a81996b
yash2003bisht <yash2003bisht@gmail.com>
2022-11-26 17:24:11 +0530

    attempted question MOBKUN

---
+++ solutions/MOBKUN/MOBKUN_1.py
@@ -0,0 +1,14 @@
+# QUESTION URL: https://www.codechef.com/problems/MOBKUN
+# STATUS: accepted
+# TIME: 0.54
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    n, m, k, x = map(int, input().split())
+    last_day = x - (x // (k*n+m))*((k*n)+m)
+    
+    if last_day > (k-1)*n or last_day == 0:
+        print("YES")
+    else:
+        print("NO")
+
---
+++ solutions/MOBKUN/MOBKUN_2.py
@@ -0,0 +1,15 @@
+# QUESTION URL: https://www.codechef.com/problems/MOBKUN
+# STATUS: accepted
+# TIME: 0.54
+# MEMORY: 9.7M
+
+for _ in range(int(input())):
+    n, m, k, x = map(int, input().split())
+    mob_occurrence = x // (k*n+m)
+    last_day = x - mob_occurrence*((k*n)+m)
+    
+    if last_day > (k-1)*n or last_day == 0:
+        print("YES")
+    else:
+        print("NO")
+
