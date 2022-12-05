8bfef1ebd277763d54150f2f72072281d58e5a28
yash2003bisht <yash2003bisht@gmail.com>
2022-12-05 07:01:57 +0530

    attempted question POOK

---
+++ solutions/POOK/POOK_1.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/POOK
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t,n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        if (n == 2 || n == 3) printf("%d\n", n-1);
+        else printf("%d\n", n);
+    }
+    
+	return 0;
+}
+
+
+
