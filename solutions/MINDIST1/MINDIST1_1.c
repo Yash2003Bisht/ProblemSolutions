bd8225a68ef5d53c4cfdb3e3b3d478ebf6b49d9b
yash2003bisht <yash2003bisht@gmail.com>
2022-12-22 06:58:32 +0530

    attempted question MINDIST1

---
+++ solutions/MINDIST1/MINDIST1_1.c
@@ -0,0 +1,35 @@
+// QUESTION URL: https://www.codechef.com/problems/MINDIST1
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, odd, even;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        scanf("%s", &s);
+        odd = 0, even = 0;
+        
+        for (int i=0; s[i] != '\0'; i++){
+            if (s[i] == '1'){
+                if ((i+1)%2 == 0) even = 1;
+                else odd = 1;
+            }
+            
+            if (odd && even) break;
+        }
+        
+        if (odd && even) printf("%d\n", 1);
+        else printf("%d\n", 2);
+        
+    }
+	return 0;
+}
+
+
+
