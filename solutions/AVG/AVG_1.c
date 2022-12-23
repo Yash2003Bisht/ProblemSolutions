2695e729f0f0577ff39021f86855403c85b711cf
yash2003bisht <yash2003bisht@gmail.com>
2022-12-23 06:56:57 +0530

    attempted question AVG

---
+++ solutions/AVG/AVG_1.c
@@ -0,0 +1,31 @@
+// QUESTION URL: https://www.codechef.com/problems/AVG
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, v, sum, num, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &k, &v);
+        sum = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &temp);
+            sum += temp;
+        }
+        
+        num = v*(k+n) - sum;
+        
+        if (num > 0 && num%k == 0) printf("%d\n", num/k);
+        else printf("-1\n");
+        
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/AVG/AVG_3.py
@@ -0,0 +1,15 @@
+# QUESTION URL: https://www.codechef.com/problems/AVG
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    n, k, v = map(int, input().split())
+    a = sum(map(int , input().split()))
+    deleted_number = ((v*(k+n)) - a)
+    
+    if deleted_number > 0 and deleted_number % k == 0:
+        print(deleted_number//k)
+    else:
+        print(-1)
+
