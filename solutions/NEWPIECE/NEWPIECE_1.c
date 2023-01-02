4c57199326239e14a80293014b10b4d7f70f0f28
yash2003bisht <yash2003bisht@gmail.com>
2023-01-02 07:20:47 +0530

    attempted question NEWPIECE

---
+++ solutions/NEWPIECE/NEWPIECE_1.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/NEWPIECE
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, p, q;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &p, &q);
+        if (a == p && b == q) printf("0\n");
+        else if ((a+b)%2 == (p+q)%2) printf("2\n");
+        else printf("1\n");
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/NEWPIECE/NEWPIECE_2.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/NEWPIECE
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, p, q;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &p, &q);
+        if (a == p && b == q) printf("0\n");
+        else if (((a+b)%2 == 0 && (p+q)%2 == 0) || \
+                (a+b)%2 != 0 && (p+q)%2 != 0) printf("2\n");
+        else printf("1\n");
+    }
+
+	return 0;
+}
+
+
+
