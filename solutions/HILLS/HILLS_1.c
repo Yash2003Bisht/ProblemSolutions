340f7b34d8b78e63de799a3ec1cb6cb656c8c5f8
yash2003bisht <yash2003bisht@gmail.com>
2023-01-05 08:03:09 +0530

    attempted question HILLS

---
+++ solutions/HILLS/HILLS_1.c
@@ -0,0 +1,46 @@
+// QUESTION URL: https://www.codechef.com/problems/HILLS
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, n, u, d, h1, h2, count, parachute, done;
+	scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d%d%d", &n, &u, &d);
+	    h2 = -1, count = 0, parachute = 0, done = 0;
+
+	    for(int i=0 ; i<n; i++){
+	        scanf("%d", &h1);
+	        
+	        if (!done){
+	            if (h2 == -1) h2 = h1;
+	        
+    	        else{
+    	            if (h1 == h2) count++;
+    	            else if (h1 > h2 && h1 - h2 <= u) count++;
+    	            else if (h1 < h2 && h2 - h1 <= d) count++;
+    	            else if (h1 < h2 && h2 - h1 > d && !parachute){
+    	                count++;
+    	                parachute = 1;
+    	            }
+    	            else done = 1;
+    	            
+    	            h2 = h1;
+    	            
+    	        }
+	        }
+	        
+	    }
+	    
+	    printf("%d\n", count+1);
+	    
+	}
+	return 0;
+}
+
+
+
---
+++ solutions/HILLS/HILLS_2.c
@@ -0,0 +1,38 @@
+// QUESTION URL: https://www.codechef.com/problems/HILLS
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, n, u, d, parachute, count;
+	scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d%d%d", &n, &u, &d);
+	    int h[n];
+	    count = 0, parachute = 0;
+
+	    for (int i=0; i<n; i++) scanf("%d", &h[i]);
+	    
+	    for (int i=0; i<n-1; i++){
+	        if (h[i] == h[i+1]) count++;
+	        else if (h[i] < h[i+1] && h[i+1] - h[i] <= u) count++;
+	        else if (h[i] > h[i+1] && h[i] - h[i+1] <= d) count++;
+	        else if (h[i] > h[i+1] && h[i] - h[i+1] > d && !parachute){
+                count++;
+                parachute = 1;
+	        }
+	        else break;
+	    }
+	    
+	    printf("%d\n", count+1);
+
+
+	}
+	return 0;
+}
+
+
+
