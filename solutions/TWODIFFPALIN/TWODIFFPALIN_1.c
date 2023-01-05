327a3450b7ac17f67b31bad74843b0f7ec857c1e
yash2003bisht <yash2003bisht@gmail.com>
2023-01-05 08:03:09 +0530

    attempted question TWODIFFPALIN

---
+++ solutions/TWODIFFPALIN/TWODIFFPALIN_1.c
@@ -0,0 +1,34 @@
+// QUESTION URL: https://www.codechef.com/problems/TWODIFFPALIN
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);
+	    c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58); c = getchar_unlocked())
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+
+int main(void) {
+    int t, a, b;
+    scanint(&t);
+
+    while(t--){
+        scanint(&a);
+        scanint(&b);
+
+        if ((a%2 != 0 && b%2 != 0) || (b == 1 || a == 1)) printf("No\n");
+        else printf("Yes\n");
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/TWODIFFPALIN/TWODIFFPALIN_2.c
@@ -0,0 +1,23 @@
+// QUESTION URL: https://www.codechef.com/problems/TWODIFFPALIN
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &a, &b);
+
+        if ((a%2 != 0 && b%2 != 0) || (b == 1 || a == 1)) printf("No\n");
+        else printf("Yes\n");
+    }
+
+	return 0;
+}
+
+
+
