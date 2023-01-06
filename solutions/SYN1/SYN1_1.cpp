9d714b0781f4eaa6e15db8ca55ab1840ba945d63
yash2003bisht <yash2003bisht@gmail.com>
2023-01-06 08:20:16 +0530

    attempted question SYN1

---
+++ solutions/SYN1/SYN1_1.cpp
@@ -0,0 +1,15 @@
+// QUESTION URL: https://www.codechef.com/problems/SYN1
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  cout << 12 ;
+  
+  return 0;
+}
+
