7b777909741820792a2c995cc3270c0ab4e5a9ac
yash2003bisht <yash2003bisht@gmail.com>
2023-01-09 07:50:38 +0530

    attempted question REPEAT

---
+++ solutions/REPEAT/REPEAT_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/REPEAT
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, s;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &n, &k, &s);
+        int diff = s - (n*n);
+        printf("%d\n", diff/(k-1));
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/REPEAT/REPEAT_2.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.codechef.com/problems/REPEAT
+# STATUS: wrong answer
+# TIME: 0.00
+# MEMORY: 0M
+
+# cook your dish here
+
+
