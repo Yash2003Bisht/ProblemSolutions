755f4b28478e5d0e67fb3d5db969d82dbd141241
yash2003bisht <yash2003bisht@gmail.com>
2023-01-11 07:56:51 +0530

    attempted question COUPON2

---
+++ solutions/COUPON2/COUPON2_1.c
@@ -0,0 +1,44 @@
+// QUESTION URL: https://www.codechef.com/problems/COUPON2
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);
+	    c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58); c = getchar_unlocked())
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+int main(void) {
+    int t, d, c, d1_cost, d2_cost, temp;
+    scanint(&t);
+
+    while(t--){
+        scanint(&d);
+        scanint(&c);
+        d1_cost = 0, d2_cost = 0;
+
+        for (int i=0; i<6; i++){
+            scanint(&temp);
+            if (i < 3) d1_cost += temp;
+            else d2_cost += temp;
+        }
+
+        temp = d1_cost + d2_cost;
+
+        if ((temp + c < temp + 2*d && d1_cost >= 150 && d2_cost >= 150) || \
+           ((temp + c + d < temp + 2*d) && (d1_cost >= 150 || \
+           d2_cost >= 150))) printf("YES\n");
+        else printf("NO\n");
+
+    }
+	return 0;
+}
+
+
+
---
+++ solutions/COUPON2/COUPON2_2.c
@@ -0,0 +1,34 @@
+// QUESTION URL: https://www.codechef.com/problems/COUPON2
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, d, c, d1_cost, d2_cost, temp;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &d, &c);
+        d1_cost = 0, d2_cost = 0;
+        
+        for (int i=0; i<6; i++){
+            scanf("%d", &temp);
+            if (i < 3) d1_cost += temp;
+            else d2_cost += temp;
+        }
+        
+        temp = d1_cost + d2_cost;
+
+        if ((temp + c < temp + 2*d && d1_cost >= 150 && d2_cost >= 150) || \
+           ((temp + c + d < temp + 2*d) && (d1_cost >= 150 || \
+           d2_cost >= 150))) printf("YES\n");
+        else printf("NO\n");
+
+    }
+	return 0;
+}
+
+
+
