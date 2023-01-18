93aaf1df2d6a3178772eb2872c043f22316a1384
yash2003bisht <yash2003bisht@gmail.com>
2023-01-18 08:23:15 +0530

    attempted question BESTBATS

---
+++ solutions/BESTBATS/BESTBATS_2.py
@@ -0,0 +1,19 @@
+# QUESTION URL: https://www.codechef.com/problems/BESTBATS
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.7M
+
+import math
+
+for _ in range(int(input())):
+    n = list(map(int, input().split()))
+    k = int(input())
+    n.sort()
+
+    max_lst = n[-k:]
+    min_ = max_lst[0]
+    total_comb = math.comb(n.count(min_), max_lst.count(min_))
+
+    print(total_comb)
+
+
---
+++ solutions/BESTBATS/BESTBATS_3.py
@@ -0,0 +1,24 @@
+# QUESTION URL: https://www.codechef.com/problems/BESTBATS
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.8M
+
+import math
+
+for _ in range(int(input())):
+    n = list(map(int, input().split()))
+    k = int(input())
+    n.sort()
+
+    max_lst = n[-k:]
+    total_comb = 0
+    
+    for i in set(max_lst):
+        comb = math.comb(n.count(i), max_lst.count(i))
+        
+        if comb > 1:
+            total_comb += comb
+    
+    print(total_comb if total_comb != 0 else 1)
+
+
