8e8e7b14bf3a70d3025a73d6810275365aff49bf
yash2003bisht <yash2003bisht@gmail.com>
2023-01-19 11:10:03 +0530

    attempted question CHFM

---
+++ solutions/CHFM/CHFM_1.c
@@ -0,0 +1,43 @@
+// QUESTION URL: https://www.codechef.com/problems/CHFM
+// STATUS: accepted
+// TIME: 0.10
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int a[n], flag = 0;
+        long long int total = 0;
+        double mean;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d\n", &a[i]);
+            total += a[i];
+        }
+        
+        mean = (double) total/n;
+        
+        for (int i=0; i<n; i++){
+            if (mean == a[i]){
+                printf("%d\n", i+1);
+                flag = 1;
+                break;
+            }
+        }
+        
+        if (!flag) printf("Impossible\n");
+        
+        
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/CHFM/CHFM_2.py
@@ -0,0 +1,18 @@
+# QUESTION URL: https://www.codechef.com/problems/CHFM
+# STATUS: accepted
+# TIME: 0.18
+# MEMORY: 26M
+
+import math
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    num = sum(a) / n
+
+    if int(num) in a and num.is_integer():
+        print(a.index(int(num)) + 1)
+    else:
+        print("Impossible")
+
+
