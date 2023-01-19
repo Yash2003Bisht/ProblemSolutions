23667390cd32cc36edfd51d2687a3c2daa6078aa
yash2003bisht <yash2003bisht@gmail.com>
2023-01-19 11:10:03 +0530

    attempted question CRDGAME3

---
+++ solutions/CRDGAME3/CRDGAME3_1.c
@@ -0,0 +1,27 @@
+// QUESTION URL: https://www.codechef.com/problems/CRDGAME3
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, pc, pr, pc_digit, pr_digit;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &pc, &pr);
+        pc_digit = pc / 9;
+        pr_digit = pr / 9;
+        
+        if (pc_digit >= pr_digit)
+            printf("1 %d\n", pr % 9 == 0 ? pr_digit : pr_digit+1);
+        else if (pc_digit < pr_digit)
+            printf("0 %d\n", pc % 9 == 0 ? pc_digit : pc_digit+1);
+    }
+    
+	return 0;
+}
+
+
+
