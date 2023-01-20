c6ffe2e8ccbfbae8010abc8f0efb8468c37409ab
yash2003bisht <yash2003bisht@gmail.com>
2023-01-20 07:47:37 +0530

    attempted question S02E10

---
+++ solutions/S02E10/S02E10_1.c
@@ -0,0 +1,39 @@
+// QUESTION URL: https://www.codechef.com/problems/S02E10
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int t, n, x, k, b;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &n, &x, &k);
+        int a[n], count = 0, temp;
+
+        for (int i=0; i<n*2; i++){
+            if (i<n) 
+                scanf("%d", &a[i]);
+
+            else{
+                scanf("%d", &b);
+                temp = abs(a[i-n] - b);
+
+                if (temp <= k) count++;
+
+            }
+
+        }
+
+        if (count >= x) printf("YES\n");
+        else printf("NO\n");
+
+    }
+
+	return 0;
+}
+
+
