8de0513b6a70a907c9ea7ba1c5796df4f5b47448
yash2003bisht <yash2003bisht@gmail.com>
2023-01-21 08:27:28 +0530

    attempted question HORSES

---
+++ solutions/HORSES/HORSES_2.c
@@ -0,0 +1,90 @@
+// QUESTION URL: https://www.codechef.com/problems/HORSES
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+void merge(long long int arr[], int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(long long int arr[], int l, int r){
+    if (l < r) {
+        // large l and h
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int s[n], min_value = 1000000000, temp;
+        
+        for (int i=0; i<n; i++)
+            scanf("%lld", &s[i]);
+        
+        mergeSort(s, 0, n-1);
+        
+        for (int i=0; i<n-1; i++){
+            temp = s[i+1] - s[i];
+            if (temp < min_value) min_value = temp;
+        }
+        
+        printf("%lld\n", min_value);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/HORSES/HORSES_3.c
@@ -0,0 +1,35 @@
+// QUESTION URL: https://www.codechef.com/problems/HORSES
+// STATUS: accepted
+// TIME: 0.18
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int s[n], min_value = 1000000000, temp;
+        
+        for (int i=0; i<n; i++)
+            scanf("%lld", &s[i]);
+        
+        for (int i=0; i<n; i++){
+            for (int j=i+1; j<n; j++){
+                temp = abs(s[i] - s[j]);
+                if (temp < min_value) min_value = temp;
+            }
+        }
+        
+        printf("%lld\n", min_value);
+        
+    }
+    
+	return 0;
+}
+
+
+
