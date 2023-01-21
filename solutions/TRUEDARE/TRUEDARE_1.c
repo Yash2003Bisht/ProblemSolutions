bccd2fb6ab3c62e6b1b404c7ca123ea9012de930
yash2003bisht <yash2003bisht@gmail.com>
2023-01-21 08:27:28 +0530

    attempted question TRUEDARE

---
+++ solutions/TRUEDARE/TRUEDARE_1.c
@@ -0,0 +1,63 @@
+// QUESTION URL: https://www.codechef.com/problems/TRUEDARE
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int search(int arr[], int num, int size){
+    
+    for (int i=0; i<size; i++){
+        if (num == arr[i])
+            return 0;
+    }
+    
+    return 1;
+    
+}
+
+int main(void) {
+    int t, n, t_size, d_size, value, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        flag = 0;
+        
+        scanf("%d", &t_size);
+        int truth[t_size];
+        for (int i=0; i<t_size; i++) 
+            scanf("%d", &truth[i]);
+        
+        scanf("%d", &d_size);
+        int dare[d_size];
+        for (int i=0; i<d_size; i++) 
+            scanf("%d", &dare[i]);
+        
+        scanf("%d", &n);
+        for (int i=0; i<n; i++){
+            scanf("%d", &value);
+            
+            if (!flag)
+                flag = search(truth, value, t_size);
+            
+        }
+        
+        scanf("%d", &n);
+        for (int i=0; i<n; i++){
+            scanf("%d", &value);
+
+            if (!flag)
+                flag = search(dare, value, d_size);
+
+        }
+        
+        if (flag) printf("no\n");
+        else printf("yes\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/TRUEDARE/TRUEDARE_2.py
@@ -0,0 +1,37 @@
+# QUESTION URL: https://www.codechef.com/problems/TRUEDARE
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    input()
+    t1 = list(map(int, input().split()))
+    input()
+    d1 = list(map(int, input().split()))
+
+    input()
+    t2 = list(map(int, input().split()))
+    input()
+    d2 = list(map(int, input().split()))
+
+    flag = False
+
+    # check truth task
+    for i in t2:
+        if i not in t1:
+            flag = True
+            break
+
+    # check dare task
+    if not flag:
+        for i in d2:
+            if i not in d1:
+                flag = True
+                break
+
+    if flag:
+        print("no")
+    else:
+        print("yes")
+
+
