f5b02bd1829eee135c184d989cc80b8bfa6b0077
yash2003bisht <yash2003bisht@gmail.com>
2023-01-23 15:26:24 +0530

    attempted question MANAPTS

---
+++ solutions/MANAPTS/MANAPTS_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/MANAPTS
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    printf("%d\n",y/x);
+	}
+	return 0;
+}
+
+
