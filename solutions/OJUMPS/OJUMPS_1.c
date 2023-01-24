ea20038f705b0a17169b2026ef51880d50497ee5
yash2003bisht <yash2003bisht@gmail.com>
2023-01-24 08:19:06 +0530

    attempted question OJUMPS

---
+++ solutions/OJUMPS/OJUMPS_1.c
@@ -0,0 +1,19 @@
+// QUESTION URL: https://www.codechef.com/problems/OJUMPS
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    long long int n;
+    scanf("%lld", &n);
+    
+    if (n%3 == 0 || (n-1)%6 == 0) printf("yes\n");
+    else printf("no\n");
+    
+	return 0;
+}
+
+
+
