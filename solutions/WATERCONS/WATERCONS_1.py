bceaf0c9e9928ab4fca411edad1efe024c429d5c
yash2003bisht <yash2003bisht@gmail.com>
2023-01-26 18:38:42 +0530

    attempted question WATERCONS

---
+++ solutions/WATERCONS/WATERCONS_1.py
@@ -0,0 +1,14 @@
+# QUESTION URL: https://www.codechef.com/problems/WATERCONS
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    m = int(input())
+    
+    if m >= 2000:
+        print("YES")
+    else:
+        print("NO")
+    
+
