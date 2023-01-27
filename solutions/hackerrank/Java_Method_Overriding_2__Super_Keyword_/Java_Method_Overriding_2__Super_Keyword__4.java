49b253ebb4f7ea93ef7d553246e22e802256f4cb
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:14 +0530

    attempted question Java_Method_Overriding_2__Super_Keyword_

---
+++ solutions/hackerrank/Java_Method_Overriding_2__Super_Keyword_/Java_Method_Overriding_2__Super_Keyword__4.java
@@ -0,0 +1,14 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem
+// STATUS: Accepted
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello I am a motorcycle, I am a cycle with an engine.");
+        System.out.println("My ancestor is a cycle who is a vehicle with pedals.");
+    }
+}
