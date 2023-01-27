4e55399f818606694b8ba54401613ec96fb0ee31
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:33 +0530

    attempted question Python_If-Else

---
+++ solutions/hackerrank/Python_If-Else/Python_If-Else_1.py
@@ -0,0 +1,24 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/py-if-else/problem
+# STATUS: Accepted
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 != 0:
+        print("Weird")
+    else:
+        if n >= 2 and n <= 5:
+            print("Not Weird")
+        elif n >= 6 and n <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
