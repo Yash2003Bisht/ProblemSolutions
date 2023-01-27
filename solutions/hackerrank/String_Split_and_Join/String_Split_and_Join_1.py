785a46ffccb771cb90f34e10a8f236b8694116d3
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:17 +0530

    attempted question String_Split_and_Join

---
+++ solutions/hackerrank/String_Split_and_Join/String_Split_and_Join_1.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+# STATUS: Accepted
+
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    return "-".join(line)
+
