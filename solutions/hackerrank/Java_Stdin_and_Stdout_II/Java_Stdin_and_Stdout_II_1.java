a7d2010f5d04bfa0de973275e78f8bfaf46a0202
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:32 +0530

    attempted question Java_Stdin_and_Stdout_II

---
+++ solutions/hackerrank/Java_Stdin_and_Stdout_II/Java_Stdin_and_Stdout_II_1.java
@@ -0,0 +1,19 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+// STATUS: Accepted
+
+import java.util.*;
+
+public class chck {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = Integer.parseInt(sc.nextLine());
+        double d = Double.parseDouble(sc.nextLine());
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+    }
+}
