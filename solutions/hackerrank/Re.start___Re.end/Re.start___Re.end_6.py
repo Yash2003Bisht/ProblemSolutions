05d637215a7604ece0a88c491e071a6787cc229e
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:25:29 +0530

    attempted question Re.start___Re.end

---
+++ solutions/hackerrank/Re.start___Re.end/Re.start___Re.end_6.py
@@ -0,0 +1,19 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/re-start-re-end/problem
+# STATUS: Wrong Answer
+
+import re
+s = input()
+k = input()
+reserve = 0
+reserve_s = len(s)
+for _ in range(len(s)):
+    match = re.search(k, s)
+    if bool(match):
+        if reserve+1 >= reserve_s:
+            break
+        print((match.start(), match.end()-1))
+        reserve = len(s[:match.start()+1])
+        s = "%"*reserve + s[match.start()+1:]
+    else:
+        print((-1,-1))
+        break
