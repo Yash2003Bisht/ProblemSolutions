9e188f0b8093cfb85d1bad621498ec6b1dcd8f1b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:19 +0530

    attempted question Integers_Come_In_All_Sizes

---
+++ solutions/hackerrank/Integers_Come_In_All_Sizes/Integers_Come_In_All_Sizes_1.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+# STATUS: Accepted
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
