9effbec1a0d8f0946411875914ca59588c926600
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:35 +0530

    attempted question Time_Conversion

---
+++ solutions/hackerrank/Time_Conversion/Time_Conversion_6.py
@@ -0,0 +1,38 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/time-conversion/problem
+# STATUS: Wrong Answer
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    if "AM" in s:
+        return s
+    else:
+        tm = int(s[0:2])+12
+        return f'{tm}{s[2:][:-2]}'
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+ 
