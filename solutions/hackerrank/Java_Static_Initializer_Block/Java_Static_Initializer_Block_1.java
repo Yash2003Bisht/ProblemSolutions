eefee66e5a76e4d1ebb95f35672ba83014fbfc6f
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:31 +0530

    attempted question Java_Static_Initializer_Block

---
+++ solutions/hackerrank/Java_Static_Initializer_Block/Java_Static_Initializer_Block_1.java
@@ -0,0 +1,34 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-static-initializer-block/problem
+// STATUS: Accepted
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+ 
+    static int B, H;
+    static boolean flag = true;
+    static {
+
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        sc.close();
+        try {
+            if (B <= 0 || H <= 0)
+                throw new Exception("Breadth and height must be positive");
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+}
