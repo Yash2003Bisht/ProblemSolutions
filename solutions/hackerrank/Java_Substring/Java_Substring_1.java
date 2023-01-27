7d01d3221f102ce46d2f35505187fa147cee9159
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:16 +0530

    attempted question Java_Substring

---
+++ solutions/hackerrank/Java_Substring/Java_Substring_1.java
@@ -0,0 +1,15 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-substring/problem
+// STATUS: Accepted
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+}
---
+++ solutions/hackerrank/Java_Substring/Java_Substring_2.java
@@ -0,0 +1,13 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-substring/problem
+// STATUS: Compilation error
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+}
