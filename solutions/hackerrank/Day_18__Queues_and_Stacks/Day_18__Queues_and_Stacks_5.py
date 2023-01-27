e28705be020c659c6a3e44c30c80c7a8de78d0d7
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:21 +0530

    attempted question Day_18__Queues_and_Stacks

---
+++ solutions/hackerrank/Day_18__Queues_and_Stacks/Day_18__Queues_and_Stacks_5.py
@@ -0,0 +1,24 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-queues-stacks/problem
+# STATUS: Accepted
+
+
+
+class Solution:
+    # Write your code here
+    
+    stack_ = []
+    queue_ = []
+
+    def pushCharacter(self, s):
+        self.stack_.append(s)
+
+    def enqueueCharacter(self, q):
+        self.queue_.append(q)
+
+    def popCharacter(self):
+        return self.stack_.pop(0)
+
+    def dequeueCharacter(self):
+        return self.queue_.pop(len(self.queue_)-1)
+
+        
