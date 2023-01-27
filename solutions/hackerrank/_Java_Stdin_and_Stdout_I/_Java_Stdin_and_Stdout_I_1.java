ecfd330f6efc25031f77b7972daf3f27272956e8
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:22 +0530

    attempted question _Java_Stdin_and_Stdout_I

---
+++ solutions/hackerrank/_Java_Stdin_and_Stdout_I/_Java_Stdin_and_Stdout_I_1.java
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+// STATUS: Accepted
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
