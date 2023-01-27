815c8a2efbd66c3093e74ead095198b12ca0bbde
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:16 +0530

    attempted question Day_12__Inheritance

---
+++ solutions/hackerrank/Day_12__Inheritance/Day_12__Inheritance_1.py
@@ -0,0 +1,41 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-inheritance/problem
+# STATUS: Accepted
+
+
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def __init__(self, firstName, lastName, idNumber, score):
+        super().__init__(firstName, lastName, idNumber)
+        self.score = score
+    
+    def calculate(self):
+        from functools import reduce
+        grade = (reduce(lambda x,y: x+y, self.score))//len(self.score)
+        if grade >= 90 and grade <= 100:
+            return "O"
+        elif grade >= 80 and grade < 90:
+            return "E"
+        elif grade >= 70 and grade < 80:
+            return "A"
+        elif grade >= 55 and grade < 70:
+            return "P"
+        elif grade >= 40 and grade < 55:
+            return "D"
+        elif grade < 40:
+            return "T"
+        
