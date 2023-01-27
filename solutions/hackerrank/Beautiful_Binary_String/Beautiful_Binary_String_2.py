f4716b3a3589c92122cf2de104807607099be9e9
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:33 +0530

    attempted question Beautiful_Binary_String

---
+++ solutions/hackerrank/Beautiful_Binary_String/Beautiful_Binary_String_2.py
@@ -0,0 +1,10 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/beautiful-binary-string/problem
+# STATUS: Accepted
+
+n = input()
+b = input()
+total = 0
+occurrence = [False, False, False]
+
+print(b.count("010"))
+        
