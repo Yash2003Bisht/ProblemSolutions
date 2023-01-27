f7ecdfc39518b1266719fb78a819d62aa3511ae6
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:33 +0530

    attempted question Sum_vs_XOR

---
+++ solutions/hackerrank/Sum_vs_XOR/Sum_vs_XOR_7.py
@@ -0,0 +1,34 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-sum-vs-xor/problem
+# STATUS: Terminated due to timeout
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def sumXor(n):
+    # Write your code here
+    if n == 0:
+        return 1
+    return sum([1 for i in range(n) if n + i == n^i])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
