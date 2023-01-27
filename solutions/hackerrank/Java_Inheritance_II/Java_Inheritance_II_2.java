bd10d9228dcaa9af04350aae8cdb761a1197d96b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:30 +0530

    attempted question Java_Inheritance_II

---
+++ solutions/hackerrank/Java_Inheritance_II/Java_Inheritance_II_2.java
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-inheritance-2/problem
+// STATUS: Wrong Answer
+
+public class chck{
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
+
+class Arithmetic{
+
+    int add(int a, int b){
+        return a + b;
+    }
+
+}
+
+class Adder extends Arithmetic{
+
+}
