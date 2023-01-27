95d094ede7ab4b16d17412d842d5dd379547ee9b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:20 +0530

    attempted question Mod_Divmod

---
+++ solutions/hackerrank/Mod_Divmod/Mod_Divmod_1.py
@@ -0,0 +1,10 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/python-mod-divmod/problem
+# STATUS: Accepted
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+
+print(a//b)
+print(a%b)
+print(divmod(a,b))
