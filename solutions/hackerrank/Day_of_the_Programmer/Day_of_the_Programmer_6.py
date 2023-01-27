afdbbd26288e16b1b0ef566b8299e77cf38694a2
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:19 +0530

    attempted question Day_of_the_Programmer

---
+++ solutions/hackerrank/Day_of_the_Programmer/Day_of_the_Programmer_6.py
@@ -0,0 +1,40 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+# STATUS: Wrong Answer
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import calendar
+
+#
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+def dayOfProgrammer(year):
+    # Write your code here
+    if "29" in calendar.month(year, 2) == False:
+        return f"13.09.{year}"        
+    elif year % 400 == 0:
+        return f"12.09.{year}"
+    elif year % 4 == 0:
+        return f"12.09.{year}"
+    else:
+        return f"13.09.{year}"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
