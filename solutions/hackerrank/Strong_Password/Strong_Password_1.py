35b72423c34b4c485f741209d2e3e891160d2c5d
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:34 +0530

    attempted question Strong_Password

---
+++ solutions/hackerrank/Strong_Password/Strong_Password_1.py
@@ -0,0 +1,24 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/strong-password/problem
+# STATUS: Accepted
+
+import re
+input()
+password = input()
+add_more = 0
+
+
+if not re.search(r"\d", password):
+    add_more += 1
+
+if not re.search(r"[a-z]", password):
+    add_more += 1
+
+if not re.search(r"[A-Z]", password):
+    add_more += 1
+
+if not re.search(r"[-+!@#$%^&*()]", password):
+    add_more += 1
+
+
+print(max(add_more, 6-len(password)))
+
