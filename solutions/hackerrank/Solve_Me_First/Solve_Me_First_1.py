2f01801aa940f340fa0f1826cd953abec0ed3636
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:20 +0530

    attempted question Solve_Me_First

---
+++ solutions/hackerrank/Solve_Me_First/Solve_Me_First_1.py
@@ -0,0 +1,14 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/solve-me-first/problem
+# STATUS: Accepted
+
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
