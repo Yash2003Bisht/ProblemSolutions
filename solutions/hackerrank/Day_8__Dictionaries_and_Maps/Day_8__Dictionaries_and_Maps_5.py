12d25b71d72005e62dfd6093d00f8957f436b89b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:23 +0530

    attempted question Day_8__Dictionaries_and_Maps

---
+++ solutions/hackerrank/Day_8__Dictionaries_and_Maps/Day_8__Dictionaries_and_Maps_5.py
@@ -0,0 +1,26 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+# STATUS: Wrong Answer
+
+n = int(input())
+dict_lst = [input() for _ in range(n)]
+dict_ = {}
+name = []
+try:
+    while True:
+        name.append(input())
+
+except EOFError:
+    for i in dict_lst:
+        try:
+            var = i.split()
+            dict_.update({var[0]: var[1]})
+
+        except IndexError:
+            pass
+
+    var = list(dict_.keys())
+    for i in range(0, len(name)):
+        if dict_.get(name[i]) != None:
+            print(f"{var[i]}={dict_.get(name[i])}")
+        else:
+            print("Not found")
