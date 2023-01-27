c3f37922bbf23bac665809eb9e31f7aa9e13b807
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:29 +0530

    attempted question HackerRank_in_a_String!

---
+++ solutions/hackerrank/HackerRank_in_a_String!/HackerRank_in_a_String!_1.py
@@ -0,0 +1,17 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem
+# STATUS: Accepted
+
+correct_string = "hackerrank"
+for _ in range(int(input())):
+    string = input()
+    i = 0
+    j = 0
+    while i != len(string):
+        if string[i] == correct_string[j]:
+            j += 1
+        if j == 9:
+            print("YES")
+            break
+        i += 1
+    else:
+        print("NO")
