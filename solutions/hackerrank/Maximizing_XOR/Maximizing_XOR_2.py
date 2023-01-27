49aa964a960c554a745bae1b2df1eecc0634945b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:12 +0530

    attempted question Maximizing_XOR

---
+++ solutions/hackerrank/Maximizing_XOR/Maximizing_XOR_2.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/maximizing-xor/problem
+# STATUS: Accepted
+
+l = int(input())
+r = int(input())
+print(max([i^j for i in range(l, r+1) for j in range(i, r+1)]))
