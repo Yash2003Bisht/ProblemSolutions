8abc6b2abff8665f50c9ea87672f0ee657b35770
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:18 +0530

    attempted question Extra_Long_Factorials

---
+++ solutions/hackerrank/Extra_Long_Factorials/Extra_Long_Factorials_2.py
@@ -0,0 +1,25 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/extra-long-factorials/problem
+# STATUS: Accepted
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    print(math.factorial(n))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
