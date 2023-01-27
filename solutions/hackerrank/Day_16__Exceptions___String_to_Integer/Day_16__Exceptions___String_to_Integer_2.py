5e9dd136e01b11eeb1c43b702921e765f40424d2
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:16 +0530

    attempted question Day_16__Exceptions___String_to_Integer

---
+++ solutions/hackerrank/Day_16__Exceptions___String_to_Integer/Day_16__Exceptions___String_to_Integer_2.py
@@ -0,0 +1,19 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+# STATUS: Processed
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    S = input()
+    try:
+        print(int(S))
+    except Exception:
+        print("Bad String")
