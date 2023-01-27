70007d2554ca50e9b41e0e07453c6cd70a3f191b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:22 +0530

    attempted question Find_a_string

---
+++ solutions/hackerrank/Find_a_string/Find_a_string_3.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/find-a-string/problem
+# STATUS: Wrong Answer
+
+def count_substring(string, sub_string):
+    string = string.split(sub_string)
+    return len(string)
+
+
