a71c60051a7a10044a8cdc04b15b920383d42c4f
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:18 +0530

    attempted question Save_the_Prisoner_

---
+++ solutions/hackerrank/Save_the_Prisoner_/Save_the_Prisoner__4.py
@@ -0,0 +1,47 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/save-the-prisoner/problem
+# STATUS: Wrong Answer
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'saveThePrisoner' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER s
+#
+
+def saveThePrisoner(n, m, s):
+    # Write your code here
+    val = (n+m+s-1)%n
+    if val==0:
+        print(n)
+    else:
+        print(val)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        s = int(first_multiple_input[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
