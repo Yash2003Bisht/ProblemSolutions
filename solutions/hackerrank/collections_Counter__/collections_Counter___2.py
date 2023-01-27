73c85c3fe7ea3a788c67e0757c487c642c92f79b
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:11 +0530

    attempted question collections_Counter__

---
+++ solutions/hackerrank/collections_Counter__/collections_Counter___2.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/collections-counter/problem
+# STATUS: Accepted
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+X = int(input())
+shoe_size = Counter(list(map(int, input().split(" "))))
+N = int(input())
+earned = 0
+
+for _ in range(N):
+    data = list(map(int, input().split(" ")))
+    if shoe_size[data[0]] != 0:
+        earned += data[1]
+        shoe_size[data[0]] -= 1
+print(earned)
