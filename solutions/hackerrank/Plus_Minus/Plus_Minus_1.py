b50f60e028221b1897198792afd1b7e530db0bbe
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:16 +0530

    attempted question Plus_Minus

---
+++ solutions/hackerrank/Plus_Minus/Plus_Minus_1.py
@@ -0,0 +1,41 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem
+# STATUS: Accepted
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    
+    positive = 0
+    negative = 0
+    zero = 0
+    
+    for i in arr:
+        if i == 0:
+            zero += 1
+        elif i < 0:
+            negative += 1
+        else:
+            positive += 1
+    
+    print(positive/n)
+    print(negative/n)
+    print(zero/n)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
