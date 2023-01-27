15039c68751be5515093271d50065d433761ae05
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:12 +0530

    attempted question Day_22__Binary_Search_Trees

---
+++ solutions/hackerrank/Day_22__Binary_Search_Trees/Day_22__Binary_Search_Trees_2.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+# STATUS: Wrong Answer
+
+
+
+    def getHeight(self,root):
+        #Write your code here
+        return root.data
+
