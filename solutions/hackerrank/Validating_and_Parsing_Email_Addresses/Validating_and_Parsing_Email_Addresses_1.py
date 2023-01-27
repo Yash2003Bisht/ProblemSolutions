6ce6a3b79aa2cac24de2f689b85cbdbdedfe63ae
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:15 +0530

    attempted question Validating_and_Parsing_Email_Addresses

---
+++ solutions/hackerrank/Validating_and_Parsing_Email_Addresses/Validating_and_Parsing_Email_Addresses_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/validating-named-email-addresses/problem
+# STATUS: Accepted
+
+import re
+for _ in range(int(input())):
+    name, user_email = input().split(" ")
+    if bool(re.search(r"^(<[a-zA-Z])[a-zA-Z-_.\d]+@[a-zA-Z]*\.[a-zA-Z]{1,3}>$", user_email)):
+        print(name, user_email)
+
