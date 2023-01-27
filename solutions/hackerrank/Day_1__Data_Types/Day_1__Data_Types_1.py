d367000426c4a7016351e6639b2cb9dc1baacf80
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:20 +0530

    attempted question Day_1__Data_Types

---
+++ solutions/hackerrank/Day_1__Data_Types/Day_1__Data_Types_1.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-data-types/problem
+# STATUS: Accepted
+
+
+# Declare second integer, double, and String variables.
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+# Read and save an integer, double, and String to your variables.
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+# Print the sum of the double variables on a new line.
+print(d + d2)
+# Concatenate and print the String variables on a new line
+print(s + s2)
+# The 's' variable above should be printed first.
