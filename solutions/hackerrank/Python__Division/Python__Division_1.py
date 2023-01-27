4e6130f29231310815fbd06c7fb1e93c65d14c89
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:14 +0530

    attempted question Python__Division

---
+++ solutions/hackerrank/Python__Division/Python__Division_1.py
@@ -0,0 +1,8 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/python-division/problem
+# STATUS: Accepted
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
