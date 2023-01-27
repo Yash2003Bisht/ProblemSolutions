503b2f55c28500eb2a44aba81aee5165203272af
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:21 +0530

    attempted question Validating_phone_numbers

---
+++ solutions/hackerrank/Validating_phone_numbers/Validating_phone_numbers_7.py
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/validating-the-phone-number/problem
+# STATUS: Wrong Answer
+
+import re
+for _ in range(int(input())):
+    num = bool(re.search(r"[7|8|9]([0-9]{9}$)", input()))
+
+    if num:
+        print("YES")
+    else:
+        print("NO")
