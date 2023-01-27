2b7b7f3a5de26cbec62c223cc67dea45b19cb185
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:31 +0530

    attempted question Write_a_function

---
+++ solutions/hackerrank/Write_a_function/Write_a_function_3.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/write-a-function/problem
+# STATUS: Wrong Answer
+
+def is_leap(year):
+    return True if year%4 == 0 and (year%400 == 0 and year % 400) else False
+
---
+++ solutions/hackerrank/Write_a_function/Write_a_function_5.py
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/write-a-function/problem
+# STATUS: Wrong Answer
+
+def is_leap(year):
+    return True if year%4 == 0 or year%400 == 0 else False
+
