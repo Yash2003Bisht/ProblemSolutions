ab850685ea517e9e62ca521fba0e268249a2e162
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:12 +0530

    attempted question Re_start_____Re_end__

---
+++ solutions/hackerrank/Re_start_____Re_end__/Re_start_____Re_end___1.py
@@ -0,0 +1,12 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/re-start-re-end/problem
+# STATUS: Accepted
+
+S = input()
+k = input()
+import re
+pattern = re.compile(k)
+r = pattern.search(S)
+if not r: print("(-1, -1)")
+while r:
+    print("({0}, {1})".format(r.start(), r.end() - 1))
+    r = pattern.search(S,r.start() + 1)
