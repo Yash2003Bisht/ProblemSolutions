0700e0a85ba7bb98e529a8131988ef6b1075c33f
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:18 +0530

    attempted question No_Idea_

---
+++ solutions/hackerrank/No_Idea_/No_Idea__5.py
@@ -0,0 +1,13 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/no-idea/problem
+# STATUS: Wrong Answer
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = map(int, input().split(' '))
+array = map(int, input().split(' '))
+a = list(map(int, input().split(' ')))
+b = list(map(int, input().split(' ')))
+b_total = 0
+a_total = 0
+for i in array:
+    pass
+print(a_total-b_total)
