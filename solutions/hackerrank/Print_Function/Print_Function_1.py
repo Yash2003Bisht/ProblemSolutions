271d109e4b6b3d559e9567e2f3d818d6740f2ae4
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:17 +0530

    attempted question Print_Function

---
+++ solutions/hackerrank/Print_Function/Print_Function_1.py
@@ -0,0 +1,9 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/python-print/problem
+# STATUS: Accepted
+
+if __name__ == '__main__':
+    n = int(input())
+    var = ""
+    for i in range(1, n+1):
+        var += str(i)
+    print(var)
