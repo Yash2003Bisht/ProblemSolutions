92bf7ad80774ff2e2e8b3ca8954ed4974f620b22
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:32 +0530

    attempted question Java_End-of-file

---
+++ solutions/hackerrank/Java_End-of-file/Java_End-of-file_1.java
@@ -0,0 +1,17 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-end-of-file/problem
+// STATUS: Accepted
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            counter++;
+            System.out.println(counter + " " + sc.nextLine());
+        }
+    }
+}
