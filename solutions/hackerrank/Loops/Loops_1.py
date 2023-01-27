e84d7bcd64a58949dc47e585c7d1a18e1c9f26dd
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:35 +0530

    attempted question Loops

---
+++ solutions/hackerrank/Loops/Loops_1.py
@@ -0,0 +1,7 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/python-loops/problem
+# STATUS: Accepted
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i**2)
