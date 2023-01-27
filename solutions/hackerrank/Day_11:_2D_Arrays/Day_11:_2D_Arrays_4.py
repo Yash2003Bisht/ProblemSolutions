fd09547f004a79814e31f91c09d4bc8e78869eb0
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 15:01:29 +0530

    attempted question Day_11:_2D_Arrays

---
+++ solutions/hackerrank/Day_11:_2D_Arrays/Day_11:_2D_Arrays_4.py
@@ -0,0 +1,44 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-2d-arrays/problem
+# STATUS: Accepted
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from functools import reduce
+
+
+if __name__ == '__main__':
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    
+    counter = 0
+    a,b,c,d,e,f,g = 0,1,2,1,0,1,2
+    new_lst = []
+    final_lst = []
+    for i in arr:
+        if i == arr[counter]:
+            try:
+                for q in i:
+                    new_lst.append([i[a], i[b], i[c], arr[counter+1][d], arr[counter+2][e], arr[counter+2][f], arr[counter+2][g]])
+                    a += 1
+                    b += 1
+                    c += 1
+                    d += 1
+                    e += 1
+                    f += 1
+                    g += 1
+            except IndexError:
+                counter += 1
+        a,b,c,d,e,f,g = 0,1,2,1,0,1,2
+    for i in new_lst:
+        final_lst.append(reduce(lambda x,y: x+y, i))
+
+    print(max(final_lst))
+
