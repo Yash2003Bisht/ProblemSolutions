c2e8ebde2208d758b9e7805dc672cd626317cf12
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:20 +0530

    attempted question Day_20__Sorting

---
+++ solutions/hackerrank/Day_20__Sorting/Day_20__Sorting_4.py
@@ -0,0 +1,36 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/30-sorting/problem
+# STATUS: Wrong Answer
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    # Write your code here
+    counter = 0
+    for _ in a:
+        for i in range(n+1):
+            try:
+                if a[i] > a[i+1]:
+                    a.insert(i, a.pop(i+1))
+                    counter += 1
+                else:
+                    break
+            except IndexError:
+                continue
+
+    print(f"Array is sorted in {counter} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[len(a)-1]}")
+
+    
