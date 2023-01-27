f7e545dc57113373966048f831e0cf77a089e0fa
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 13:01:36 +0530

    attempted question Playing_With_Characters

---
+++ solutions/hackerrank/Playing_With_Characters/Playing_With_Characters_1.c
@@ -0,0 +1,15 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/playing-with-characters/problem
+// STATUS: Accepted
+
+#include <stdio.h>
+
+int main(){
+    char ch[100], s[100], sen[100];
+    scanf("%[^\n]%*c", &ch);
+    scanf("%[^\n]%*c", &s);
+    scanf("%[^\n]%*c", &sen);
+    printf("%s\n",ch);
+    printf("%s\n",s);
+    printf("%s\n",sen);    
+    return 0;
+}
