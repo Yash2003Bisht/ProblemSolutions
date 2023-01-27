edf2561eb37d2b9ad0873b339da2995ac5740733
yash2003bisht <yash2003bisht@gmail.com>
2023-01-27 19:11:11 +0530

    attempted question Java_If_Else

---
+++ solutions/hackerrank/Java_If_Else/Java_If_Else_1.java
@@ -0,0 +1,19 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/java-if-else/problem
+// STATUS: Accepted
+
+import java.util.Scanner;
+
+public class Solution{
+    public static void main(String[] args){
+        int n = new Scanner(System.in).nextInt();
+        if (n%2 != 0){
+            System.out.println("Weird");
+        } else if(n%2 == 0 && (n >= 2 && n <= 5)){
+            System.out.println("Not Weird");
+        } else if(n%2 == 0 && (n >= 6 && n <= 20)){
+            System.out.println("Weird");
+        } else{
+            System.out.println("Not Weird");
+        }
+    }
+}
