162723dc697584018f3a0b41879214146e1e429f
yash2003bisht <yash2003bisht@gmail.com>
2023-01-28 07:47:57 +0530

    attempted question TAXSAVING

---
+++ solutions/codechef/TAXSAVING/TAXSAVING_1.c
@@ -0,0 +1,21 @@
+// QUESTION URL: https://www.codechef.com/problems/TAXSAVING
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d%d", &x, &y);
+	    printf("%d\n", x - y);
+	}
+	
+	return 0;
+}
+
+
+
