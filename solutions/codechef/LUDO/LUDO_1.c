1785da8e829bf9f18a58480853dec3f20426f84d
yash2003bisht <yash2003bisht@gmail.com>
2023-01-28 07:47:57 +0530

    attempted question LUDO

---
+++ solutions/codechef/LUDO/LUDO_1.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/LUDO
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        if (x == 6) printf("YES\n");
+        else printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
