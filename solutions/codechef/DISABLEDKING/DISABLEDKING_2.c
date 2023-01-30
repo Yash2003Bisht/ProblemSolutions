eaf9a3a0a9ff363307831d9eabe90a15a848e106
yash2003bisht <yash2003bisht@gmail.com>
2023-01-30 08:13:58 +0530

    attempted question DISABLEDKING

---
+++ solutions/codechef/DISABLEDKING/DISABLEDKING_2.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/DISABLEDKING
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+
+        if (n % 2 == 0 || n == 1) printf("%d\n", n);
+        else printf("%d\n", n-1);
+
+    }
+
+	return 0;
+}
+
+
+
