051408a4c9cfcb822b8a080bc3162952587b68c2
yash2003bisht <yash2003bisht@gmail.com>
2023-02-01 17:50:05 +0530

    attempted question SUSSTR

---
+++ solutions/codechef/SUSSTR/SUSSTR_1.c
@@ -0,0 +1,46 @@
+// QUESTION URL: https://www.codechef.com/problems/SUSSTR
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, right, left;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1], new_s[(n*2)+1];
+        right = n, left = n;
+        scanf("%s", s);
+
+        for (int i=0; i<n/2; ++i){
+            // front
+            if (left == right) new_s[left] = s[i];
+            else if (s[i] == '0') new_s[--left] = s[i];
+            else new_s[++right] = s[i];
+            
+            // back
+            if (s[n - i - 1] == '0') new_s[++right] = s[n - i - 1];
+            else new_s[--left] = s[n - i - 1];
+        }
+
+        if (n%2 != 0){
+            if (left == right) new_s[left] = s[n/2];
+            else if (s[n/2] == '0') new_s[--left] = s[n/2];
+            else new_s[++right] = s[n/2];
+        }
+
+        new_s[++right] = '\0';
+
+        printf("%s\n", new_s + left);
+        
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/SUSSTR/SUSSTR_3.c
@@ -0,0 +1,51 @@
+// QUESTION URL: https://www.codechef.com/problems/SUSSTR
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+void add_value(char value, char player, char s_arr[]){
+    int size = strlen(s_arr);
+
+    if ((player == 'a' && value == '0') || (player == 'b' && value == '1')){
+        for (int i=0; s_arr[i] != '\0'; i++) s_arr[size-i] = s_arr[size-1-i];
+        s_arr[0] = value;
+
+    } else s_arr[size] = value;
+}
+
+int main(void) {
+    int t, n, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[1001] = {'\0'}, new_s[1001] = {'\0'}, value, name;
+        temp = 0;
+        scanf("%s", s);
+        
+        for (int i=0; i<n; i++){
+            if (i%2 == 0){
+                value = s[temp];
+                name = 'a';  // alice
+                temp += 1;
+            } else {
+                value = s[n-temp];
+                name = 'b';  // bob
+            }
+            
+            add_value(value, name, new_s);
+        
+        }
+        
+        printf("%s\n", new_s);
+        
+
+    }
+    
+	return 0;
+}
+
+
+
