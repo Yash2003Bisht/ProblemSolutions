ea5b8867b5bca010d69ace05e9ce34bada6e3d28
yash2003bisht <yash2003bisht@gmail.com>
2023-02-02 08:01:10 +0530

    attempted question READPAGES

---
+++ solutions/codechef/READPAGES/READPAGES_1.c
@@ -0,0 +1,26 @@
+// QUESTION URL: https://www.codechef.com/problems/READPAGES
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, x, y;
+
+    scanf("%d", &t);
+
+    while(t--){
+
+        scanf("%d%d%d", &n, &x, &y);
+    
+        if (x*y >= n) printf("YES\n");
+        else printf("NO\n");
+    
+
+    }
+	return 0;
+}
+
+
+
