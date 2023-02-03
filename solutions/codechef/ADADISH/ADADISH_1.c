b027b419927f1b194c057494c3cff3891454c6ce
yash2003bisht <yash2003bisht@gmail.com>
2023-02-03 08:38:20 +0530

    attempted question ADADISH

---
+++ solutions/codechef/ADADISH/ADADISH_1.c
@@ -0,0 +1,43 @@
+// QUESTION URL: https://www.codechef.com/problems/ADADISH
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <stdlib.h>
+#define max(a,b)(a>b?a:b)
+
+int sortlogic (const void * a, const void * b) {
+   return ( *(int*)b - *(int*)a );
+}
+
+
+int main(void) {
+    int t, n, t1, t2;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int c[n];
+
+        for (int i=0; i<n; i++) scanf("%d", &c[i]);
+
+        // sort the array
+        qsort(c, n, sizeof(int), sortlogic);
+        
+        t1 = 0, t2 = 0;
+        
+        for (int i=0; i<n; i++){
+            if (t1 < t2) t1 += c[i];
+            else t2 += c[i];
+        }
+        
+        printf("%d\n", max(t1, t2));
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/ADADISH/ADADISH_3.c
@@ -0,0 +1,47 @@
+// QUESTION URL: https://www.codechef.com/problems/ADADISH
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int sortlogic (const void * a, const void * b) {
+   return ( *(int*)b - *(int*)a );
+}
+
+
+int main(void) {
+    int t, n, total_min, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int c[n];
+
+        for (int i=0; i<n; i++) scanf("%d", &c[i]);
+
+        // sort the array
+        qsort(c, n, sizeof(int), sortlogic);
+        
+        total_min = c[0], temp = c[0];
+        
+        for (int i=1; i<n; i++){
+            if (temp >= c[i]) temp -= c[i];
+            else if (temp < c[i]){
+                temp = c[i] - temp;
+                total_min += temp;
+            }
+        }
+        
+        printf("%d\n", total_min);
+
+
+    }
+    
+	return 0;
+}
+
+
+
