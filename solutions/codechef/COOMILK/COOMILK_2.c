6b14751ded174d6f12a3e6b7122316c3a5956194
yash2003bisht <yash2003bisht@gmail.com>
2023-02-04 17:36:19 +0530

    attempted question COOMILK

---
+++ solutions/codechef/COOMILK/COOMILK_2.c
@@ -0,0 +1,37 @@
+// QUESTION URL: https://www.codechef.com/problems/COOMILK
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int t, n, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        char ate[7];
+        flag = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%s", ate);
+
+            if (!strcmp(ate, "cookie") && !flag) flag = 1;
+            else if (!strcmp(ate, "milk") && flag != -1) flag = 0;
+            else flag = -1;
+
+        }
+
+        if (!flag) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/COOMILK/COOMILK_4.c
@@ -0,0 +1,39 @@
+// QUESTION URL: https://www.codechef.com/problems/COOMILK
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int t, n, flag, drink_milk;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        char ate[20];
+        flag = 0, drink_milk = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%s", ate);
+
+            if (!flag){
+                if (!strcmp(ate, "cookie") && !drink_milk) drink_milk = 1;
+                else if (!strcmp(ate, "milk")) drink_milk = 0;
+                else flag = 1;
+            }
+
+        }
+
+        if (!flag && !drink_milk) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
