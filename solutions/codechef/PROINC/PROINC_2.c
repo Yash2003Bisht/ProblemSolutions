5c82ffa04f95b589f07d485a337e36db9fec9679
yash2003bisht <yash2003bisht@gmail.com>
2023-02-05 17:14:16 +0530

    attempted question PROINC

---
+++ solutions/codechef/PROINC/PROINC_2.c
@@ -0,0 +1,22 @@
+// QUESTION URL: https://www.codechef.com/problems/PROINC
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, new_profit;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        new_profit = x*0.1 + y;
+        printf("%d\n", new_profit);
+    }
+    
+	return 0;
+}
+
+
+
