546daf09ccdcb2374514deeae3c233dcea78cdde
yash2003bisht <yash2003bisht@gmail.com>
2023-02-06 08:44:52 +0530

    attempted question AXNODR

---
+++ solutions/codechef/AXNODR/AXNODR_2.c
@@ -0,0 +1,37 @@
+// QUESTION URL: https://www.codechef.com/problems/AXNODR
+// STATUS: wrong answer
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);
+	    c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58); c = getchar_unlocked())
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+
+int main(void) {
+    int t;
+    long long int n;
+    
+    scanint(&t);
+    
+    while(t--){
+        scanint(&n);
+        
+        if (n%4 == 0) printf("%lld\n", n+3);
+        else if (n%4 == 1) printf("%lld\n", n);
+        else printf("3\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/AXNODR/AXNODR_3.c
@@ -0,0 +1,27 @@
+// QUESTION URL: https://www.codechef.com/problems/AXNODR
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int n;
+    
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld", &n);
+        
+        if (n%4 == 0) printf("%lld\n", n+3);
+        else if (n%4 == 1) printf("%lld\n", n);
+        else printf("3\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
