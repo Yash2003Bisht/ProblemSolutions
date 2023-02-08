3d33e4de2fd7567d165b8c369a8be05ae4411bb0
yash2003bisht <yash2003bisht@gmail.com>
2023-02-08 08:17:12 +0530

    attempted question WALK

---
+++ solutions/codechef/WALK/WALK_1.c
@@ -0,0 +1,38 @@
+// QUESTION URL: https://www.codechef.com/problems/WALK
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);
+	    c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58); c = getchar_unlocked())
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+
+int main(void) {
+    int t, n, w, min_velocity;
+    scanint(&t);
+    
+    while(t--){
+        scanint(&n);
+        min_velocity = 0;
+        
+        for (int i=0; i<n; i++){
+            scanint(&w);
+            if (min_velocity < w+i) min_velocity = w+i;
+        }
+        
+        printf("%d\n", min_velocity);
+
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/WALK/WALK_2.c
@@ -0,0 +1,28 @@
+// QUESTION URL: https://www.codechef.com/problems/WALK
+// STATUS: accepted
+// TIME: 0.11
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, w, min_velocity;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        min_velocity = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &w);
+            if (min_velocity < w+i) min_velocity = w+i;
+        }
+        
+        printf("%d\n", min_velocity);
+
+    }
+    
+	return 0;
+}
+
+
