af5db5045b75540b11a4a4445fbe2917be50d2ab
yash2003bisht <yash2003bisht@gmail.com>
2023-02-08 08:17:12 +0530

    attempted question BRKBKS

---
+++ solutions/codechef/BRKBKS/BRKBKS_3.c
@@ -0,0 +1,25 @@
+// QUESTION URL: https://www.codechef.com/problems/BRKBKS
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, s, w1, w2, w3;
+	scanf("%d",&t);
+
+	while(t--){
+	    scanf("%d%d%d%d",&s,&w1,&w2,&w3);
+
+	    if(s >= w1+w2+w3) printf("1\n");
+	    else if((s >= w1+w2) || (s >= w2+w3)) printf("2\n");
+	    else printf("3\n");
+
+	}
+	
+	return 0;
+
+}
+
+
---
+++ solutions/codechef/BRKBKS/BRKBKS_5.c
@@ -0,0 +1,35 @@
+// QUESTION URL: https://www.codechef.com/problems/BRKBKS
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, s, w, count, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &s);
+        count = 1, temp = s;
+
+        for (int i=0; i<3; i++){
+            scanf("%d", &w);
+            
+            if (temp >= w) temp -= w;
+            else{
+                count++;
+                temp = s - w;
+            }
+
+        }
+        
+        printf("%d\n", count);
+
+    }
+    
+	return 0;
+}
+
+
+
