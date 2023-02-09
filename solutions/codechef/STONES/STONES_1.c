8886a5791de010a0072416bb5cf8c7d210f858b3
yash2003bisht <yash2003bisht@gmail.com>
2023-02-09 08:31:48 +0530

    attempted question STONES

---
+++ solutions/codechef/STONES/STONES_1.c
@@ -0,0 +1,37 @@
+// QUESTION URL: https://www.codechef.com/problems/STONES
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        char s[101] = {'\0'}, j[101] = {'\0'};
+        count = 0;
+        scanf("%s", j);
+        scanf("%s", s);
+        
+        for (int i=0; s[i] != '\0'; i++){
+            for (int k=0; j[k] != '\0'; k++){
+                if (s[i] == j[k]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        
+        printf("%d\n", count);
+        
+    }
+    
+    
+    
+	return 0;
+}
+
+
+
