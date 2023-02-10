32a07627a38ef6fb612b164c6c89cb268cb7afb5
yash2003bisht <yash2003bisht@gmail.com>
2023-02-10 07:30:31 +0530

    attempted question BTRYHLTH

---
+++ solutions/codechef/BTRYHLTH/BTRYHLTH_1.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/BTRYHLTH
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        
+        if (x >= 80) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
