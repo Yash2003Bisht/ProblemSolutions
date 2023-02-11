8cab803a51c26528bcab44e792ff1cd90f4dfe22
yash2003bisht <yash2003bisht@gmail.com>
2023-02-11 08:38:12 +0530

    attempted question TOTR

---
+++ solutions/codechef/TOTR/TOTR_1.c
@@ -0,0 +1,39 @@
+// QUESTION URL: https://www.codechef.com/problems/TOTR
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void) {
+    int t;
+    char m[27], s[101], temp;
+
+    scanf("%d", &t);
+    scanf("%s", m);
+    
+    while(t--){
+        scanf("%s", s);
+        
+        for (int i=0; s[i] != '\0'; i++){
+            
+            if (s[i] >= 65 && s[i] <= 90){
+                temp = toupper(m[s[i] - 65]);
+                printf("%c", temp);
+            } else if (s[i] >= 97 && s[i] <= 122){
+                temp = m[s[i] - 97];
+                printf("%c", temp);
+            } else if (s[i] == '_') printf(" ");
+              else printf("%c", s[i]);
+
+        }
+        
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/TOTR/TOTR_2.c
@@ -0,0 +1,43 @@
+// QUESTION URL: https://www.codechef.com/problems/TOTR
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void) {
+    int t;
+    char m_lower[27], m_upper[27], s[101], temp;
+
+    scanf("%d", &t);
+    scanf("%s", m_lower);
+    
+    for (int i=0; m_lower[i] != '\0'; i++)
+        m_upper[i] = toupper(m_lower[i]);
+
+    
+    while(t--){
+        scanf("%s", s);
+        
+        for (int i=0; s[i] != '\0'; i++){
+            
+            if (s[i] >= 65 && s[i] <= 90){
+                temp = m_upper[s[i] - 65];
+                printf("%c", temp);
+            } else if (s[i] >= 97 && s[i] <= 122){
+                temp = m_lower[s[i] - 97];
+                printf("%c", temp);
+            } else if (s[i] == '_') printf(" ");
+              else printf("%c", s[i]);
+
+        }
+        
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
