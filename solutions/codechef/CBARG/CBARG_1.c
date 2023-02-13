8867456b48a107a370aa992126c800782be9e922
yash2003bisht <yash2003bisht@gmail.com>
2023-02-13 08:35:23 +0530

    attempted question CBARG

---
+++ solutions/codechef/CBARG/CBARG_1.c
@@ -0,0 +1,38 @@
+// QUESTION URL: https://www.codechef.com/problems/CBARG
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int m1 = 0, m2 = 0, total;
+        
+        for (int i=0; i<n; i++){
+            if (!m1){
+                scanf("%lld", &m1);
+                total = m1;
+            } else
+                scanf("%lld", &m2);
+                
+            if (m1 && m2){
+                if (m1 < m2) total += m2 - m1;
+                m1 = m2;
+            }
+
+        }
+
+        printf("%lld\n", total);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CBARG/CBARG_3.c
@@ -0,0 +1,33 @@
+// QUESTION URL: https://www.codechef.com/problems/CBARG
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int m[n], total = 0;
+        
+        for (int i=0; i<n; i++)
+            scanf("%lld", &m[i]);
+            
+        total = m[0];
+            
+        for (int i=0; i<n-1; i++){
+            if (m[i] < m[i+1]) total += m[i+1] - m[i];
+        }
+        
+        printf("%lld\n", total);
+        
+    }
+    
+	return 0;
+}
+
+
+
