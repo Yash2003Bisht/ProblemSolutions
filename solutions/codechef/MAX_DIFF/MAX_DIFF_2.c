8edbb6da4267a5164718551605f8a282bd3c5ea3
yash2003bisht <yash2003bisht@gmail.com>
2023-02-14 12:40:45 +0530

    attempted question MAX_DIFF

---
+++ solutions/codechef/MAX_DIFF/MAX_DIFF_2.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/MAX_DIFF
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, s;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &s);
+        
+        if (n >= s) printf("%d\n", s);
+        else printf("%d\n", n - (s-n));
+        
+    }
+    
+	return 0;
+}
+
+
+
