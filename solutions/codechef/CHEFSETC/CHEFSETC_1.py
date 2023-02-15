98701804c1f4e0a0f96a772eefff1160e8c9bb5d
yash2003bisht <yash2003bisht@gmail.com>
2023-02-15 09:00:48 +0530

    attempted question CHEFSETC

---
+++ solutions/codechef/CHEFSETC/CHEFSETC_1.py
@@ -0,0 +1,23 @@
+# QUESTION URL: https://www.codechef.com/problems/CHEFSETC
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.7M
+
+from itertools import combinations
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    lst = []
+    
+    for i in range(1, 5):
+        all_comb = combinations([a, b, c, d], i)
+        for comb in all_comb:
+            lst.append(list(comb))
+    
+    for comb in lst:
+        if sum(comb) == 0:
+            print("Yes")
+            break
+    else:
+        print("No")
+
---
+++ solutions/codechef/CHEFSETC/CHEFSETC_2.c
@@ -0,0 +1,29 @@
+// QUESTION URL: https://www.codechef.com/problems/CHEFSETC
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c, d;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        
+        if (a == 0 || b == 0 || c == 0 || d == 0 || \
+            a+b == 0 || a+c == 0 || a+d == 0 || \
+            b+c == 0 || b+d == 0 || c+d == 0 || \
+            a+b+c == 0 || a+c+d == 0 || b+c+d == 0 || \
+            a+b+d == 0 || a+b+c+d == 0 ) printf("Yes\n");
+
+        else printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
