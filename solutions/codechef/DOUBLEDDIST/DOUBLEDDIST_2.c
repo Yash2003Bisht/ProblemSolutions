0d1b0c3847a362cf6f195edeea7af06ed6aa4785
yash2003bisht <yash2003bisht@gmail.com>
2023-02-15 09:00:48 +0530

    attempted question DOUBLEDDIST

---
+++ solutions/codechef/DOUBLEDDIST/DOUBLEDDIST_2.c
@@ -0,0 +1,89 @@
+// QUESTION URL: https://www.codechef.com/problems/DOUBLEDDIST
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+void merge(long long int arr[], int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    long long int L[n1], R[n2];
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0, j = 0, k = l;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+
+void mergeSort(long long int arr[], int l, int r){
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+
+int main(void) {
+    int t, n, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int a[n];
+        flag = 1;;
+        
+        for (int i=0; i<n; i++) scanf("%lld", &a[i]);
+        
+        mergeSort(a, 0, n-1);
+        
+        for (int i=1; i<n-1; i++){
+            if (a[i]-a[i-1] != 2*(a[i+1]-a[i]) && 2*(a[i]-a[i-1]) != (a[i+1]-a[i])){
+                flag = 0;
+                break;
+            }
+        }
+        
+        if (flag) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/DOUBLEDDIST/DOUBLEDDIST_3.c
@@ -0,0 +1,42 @@
+// QUESTION URL: https://www.codechef.com/problems/DOUBLEDDIST
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int main(void) {
+    int t, n, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int a[n];
+        flag = 1;
+        
+        for (int i=0; i<n; i++) scanf("%lld", &a[i]);
+        
+        qsort(a, n, sizeof(long long int), cmpfunc);
+        
+        for (int i=1; i<n-1; i++){
+            if (a[i]-a[i-1] != 2*(a[i+1]-a[i]) && 2*(a[i]-a[i-1]) != (a[i+1]-a[i])){
+                flag = 0;
+                break;
+            }
+        }
+        
+        if (flag) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
