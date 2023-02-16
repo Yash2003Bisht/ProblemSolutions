7f410ae4e015e34df4f778cfeabc076e2f40b173
yash2003bisht <yash2003bisht@gmail.com>
2023-02-16 08:17:13 +0530

    attempted question JERRYCHASE

---
+++ solutions/codechef/JERRYCHASE/JERRYCHASE_1.c
@@ -0,0 +1,24 @@
+// QUESTION URL: https://www.codechef.com/problems/JERRYCHASE
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (x >= y) printf("NO\n");
+        else printf("YES\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
