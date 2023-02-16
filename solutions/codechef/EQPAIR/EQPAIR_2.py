0f3b834ac46602b6cacd2e06c07f8277ac805830
yash2003bisht <yash2003bisht@gmail.com>
2023-02-16 08:17:13 +0530

    attempted question EQPAIR

---
+++ solutions/codechef/EQPAIR/EQPAIR_2.py
@@ -0,0 +1,20 @@
+# QUESTION URL: https://www.codechef.com/problems/EQPAIR
+# STATUS: accepted
+# TIME: 0.09
+# MEMORY: 27.5M
+
+from collections import Counter
+import math
+
+for _ in range(int(input())):
+    n = int(input())
+    a = Counter(map(int, input().split()))
+    count = 0
+    
+    for value in a.values():
+        if value > 1:
+            count += math.comb(value, 2)
+    
+    print(count)
+
+
