3426943a8d0abe0e1074602ef842137e4a282090
yash2003bisht <yash2003bisht@gmail.com>
2023-02-18 08:50:33 +0530

    attempted question LKDNGOLF

---
+++ solutions/codechef/LKDNGOLF/LKDNGOLF_1.c
@@ -0,0 +1,40 @@
+// QUESTION URL: https://www.codechef.com/problems/LKDNGOLF
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);
+	    c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58); c = getchar_unlocked())
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+int main(void) {
+    long long int t, n, x, k;
+    int flag;
+    scanint(&t);
+    
+    while(t--){
+        scanint(&n);
+        scanint(&x);
+        scanint(&k);
+        flag = 0;
+        
+        if (x % k == 0 || ((n+1)-x) % k == 0) flag = 1;
+        
+        
+        if (flag) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
