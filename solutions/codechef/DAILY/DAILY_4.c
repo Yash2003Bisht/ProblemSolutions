f9d651c46e62b83f7fd00d07489324b3262696a6
Yash Bisht <102852115+Yash2003Bisht@users.noreply.github.com>
2023-02-20 21:22:46 +0530

    symbol updated

--- solutions/codechef/DAILY/DAILY_4.c
+++ solutions/codechef/DAILY/DAILY_4.c
@@ -44,7 +44,7 @@ int main(void) {
 
         } 
         
-        // n! / (k! * (n â€“ k)!)
+        // n! / (k! * (n – k)!)
         for (int i=0; i<9; i++){
             counter += fact(a[i]) / (fact(x) * fact(a[i] - x));   
         }
