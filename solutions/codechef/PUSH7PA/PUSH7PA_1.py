179845b4cb0946ebb73628c1ca429f4fe679b1e0
yash2003bisht <yash2003bisht@gmail.com>
2023-02-21 21:04:34 +0530

    attempted question PUSH7PA

---
+++ solutions/codechef/PUSH7PA/PUSH7PA_1.py
@@ -0,0 +1,21 @@
+# DATE: 20/02/2023, 08:15:10
+# PROBLEM NAME: Pushpa 
+# PROBLEM URL: https://www.codechef.com/problems/PUSH7PA
+# PROBLEM DIFFICULTY RATTING: 1266
+# STATUS: accepted
+# TIME: 0.21
+# MEMORY: 19.3M
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())
+    h = Counter(map(int, input().split()))
+    max_value = 0
+
+    for key in h:
+        max_value = max(max_value, key+h[key]-1)
+
+    print(max_value)
+
+
---
+++ solutions/codechef/PUSH7PA/PUSH7PA_2.py
@@ -0,0 +1,20 @@
+# DATE: 20/02/2023, 08:14:08
+# PROBLEM NAME: Pushpa 
+# PROBLEM URL: https://www.codechef.com/problems/PUSH7PA
+# PROBLEM DIFFICULTY RATTING: 1266
+# STATUS: accepted
+# TIME: 0.21
+# MEMORY: 19.2M
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())
+    h = Counter(map(int, input().split()))
+    max_value = 0
+
+    for key in h:
+        max_value = max(max_value, key+h[key]-1)
+
+    print(max_value)
+
