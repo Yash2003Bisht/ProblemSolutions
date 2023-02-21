e2b7868830e85a046b3c2166f3add27a91747508
yash2003bisht <yash2003bisht@gmail.com>
2023-02-21 21:04:34 +0530

    attempted question SUBANAGR

---
+++ solutions/codechef/SUBANAGR/SUBANAGR_1.py
@@ -0,0 +1,27 @@
+# DATE: 21/02/2023, 08:45:56
+# PROBLEM NAME: Subanagrams
+# PROBLEM URL: https://www.codechef.com/problems/SUBANAGR
+# PROBLEM DIFFICULTY RATTING: 1239
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.7M
+
+a = None
+for _ in range(int(input())):
+    if a is None:
+        a = input()
+    else:
+        b = input()
+        temp  = ""
+        
+        for i in set(a):
+            temp += i*min(a.count(i), b.count(i))
+        
+        a = temp
+
+if a == "":
+    print("no such string")
+else:
+    print("".join(sorted(a)))
+
+
---
+++ solutions/codechef/SUBANAGR/SUBANAGR_2.py
@@ -0,0 +1,30 @@
+# DATE: 21/02/2023, 08:44:01
+# PROBLEM NAME: Subanagrams
+# PROBLEM URL: https://www.codechef.com/problems/SUBANAGR
+# PROBLEM DIFFICULTY RATTING: 1239
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.6M
+
+a = None
+for _ in range(int(input())):
+    if a is None:
+        a = input()
+    else:
+        b = input()
+        temp  = ""
+        
+        for i in a:
+            if i in temp:
+                continue
+
+            temp += i*min(a.count(i), b.count(i))
+        
+        a = temp
+
+if a == "":
+    print("no such string")
+else:
+    print("".join(sorted(a)))
+
+
