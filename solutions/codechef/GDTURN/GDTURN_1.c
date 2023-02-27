335eb9c027f42e0da0911d2b792ff24d6cf78ef9
yash2003bisht <yash2003bisht@gmail.com>
2023-02-27 21:48:50 +0530

    attempted question GDTURN

---
+++ solutions/codechef/GDTURN/GDTURN_1.c
@@ -0,0 +1,26 @@
+// DATE: 25/02/2023, 06:49:47
+// PROBLEM NAME: Good Turn
+// PROBLEM URL: https://www.codechef.com/problems/GDTURN
+// PROBLEM DIFFICULTY RATTING: 238
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+
+        if (x+y > 6) printf("YES\n");
+        else printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
