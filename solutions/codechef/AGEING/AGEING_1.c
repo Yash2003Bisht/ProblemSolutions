4f5ce0c26b01d4531b80db6bc1ef1554e765cd8f
yash2003bisht <yash2003bisht@gmail.com>
2023-03-03 08:34:21 +0530

    attempted question AGEING

---
+++ solutions/codechef/AGEING/AGEING_1.c
@@ -0,0 +1,24 @@
+// DATE: 03/03/2023, 08:26:28
+// PROBLEM NAME: Ageing
+// PROBLEM URL: https://www.codechef.com/problems/AGEING
+// PROBLEM DIFFICULTY RATTING: 299
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &x);
+        printf("%d\n", 10+(x-20));
+    }
+    
+	return 0;
+}
+
+
+
