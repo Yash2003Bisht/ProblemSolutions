f03d07c612b2d2f50825a4fdc0e29ac04557369f
yash2003bisht <yash2003bisht@gmail.com>
2023-03-04 17:01:40 +0530

    attempted question FRUITS

---
+++ solutions/codechef/FRUITS/FRUITS_1.c
@@ -0,0 +1,31 @@
+// DATE: 04/03/2023, 08:30:10
+// PROBLEM NAME: Chef and Fruits
+// PROBLEM URL: https://www.codechef.com/problems/FRUITS
+// PROBLEM DIFFICULTY RATTING: 1126
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, n, m, k, diff;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &m, &k);
+        diff = abs(n-m);
+        
+        if (diff <= k)
+            printf("0\n");
+        else
+            printf("%d\n", diff - k);
+        
+    }
+    
+	return 0;
+}
+
+
+
