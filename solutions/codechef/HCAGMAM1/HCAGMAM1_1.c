4c5b73eb13464f30b1c8b4688e19ecc207c79325
yash2003bisht <yash2003bisht@gmail.com>
2023-03-06 16:34:05 +0530

    attempted question HCAGMAM1

---
+++ solutions/codechef/HCAGMAM1/HCAGMAM1_1.c
@@ -0,0 +1,44 @@
+// DATE: 06/03/2023, 07:14:50
+// PROBLEM NAME: Chef And Salary Pay
+// PROBLEM URL: https://www.codechef.com/problems/HCAGMAM1
+// PROBLEM DIFFICULTY RATTING: 1269
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, total_count, temp, lg_streak;
+    char s[31];
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        scanf("%s", s);
+        temp = 0, total_count = 0, lg_streak = 0;
+
+        for (int i=0; s[i] != '\0'; i++){
+            if (s[i] == '1'){
+                temp++;
+            } else{
+                total_count += temp;
+                lg_streak = lg_streak < temp ? temp : lg_streak;
+                temp = 0;
+            }
+        }
+
+        if (temp){
+            total_count += temp;
+            lg_streak = lg_streak < temp ? temp : lg_streak;
+        }
+
+        printf("%d\n", total_count*x + lg_streak*y);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/HCAGMAM1/HCAGMAM1_2.c
@@ -0,0 +1,44 @@
+// DATE: 06/03/2023, 07:12:00
+// PROBLEM NAME: Chef And Salary Pay
+// PROBLEM URL: https://www.codechef.com/problems/HCAGMAM1
+// PROBLEM DIFFICULTY RATTING: 1269
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, total_count, count, lg_streak;
+    char s[31];
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        scanf("%s", s);
+        count = 0, total_count = 0, lg_streak = 0;
+
+        for (int i=0; s[i] != '\0'; i++){
+            if (s[i] == '1'){
+                count++;
+            } else{
+                total_count += count;
+                lg_streak = lg_streak < count ? count : lg_streak;
+                count = 0;
+            }
+        }
+
+        if (count){
+            total_count += count;
+            lg_streak = lg_streak < count ? count : lg_streak;
+        }
+
+        printf("%d\n", total_count*x + lg_streak*y);
+        
+    }
+    
+	return 0;
+}
+
+
+
