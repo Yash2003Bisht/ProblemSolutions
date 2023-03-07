de38a8e768800bfd2519c5cbd9499e79a98bcba6
yash2003bisht <yash2003bisht@gmail.com>
2023-03-07 13:34:02 +0530

    attempted question CLIPLX

---
+++ solutions/codechef/CLIPLX/CLIPLX_1.c
@@ -0,0 +1,25 @@
+// DATE: 07/03/2023, 01:33:26
+// PROBLEM NAME: IPL and RCB
+// PROBLEM URL: https://www.codechef.com/problems/CLIPLX
+// PROBLEM DIFFICULTY RATTING: 1167
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        printf("%d\n", max(0, x-y));
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CLIPLX/CLIPLX_2.c
@@ -0,0 +1,29 @@
+// DATE: 07/03/2023, 01:30:23
+// PROBLEM NAME: IPL and RCB
+// PROBLEM URL: https://www.codechef.com/problems/CLIPLX
+// PROBLEM DIFFICULTY RATTING: 1167
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (x <= y)
+            printf("0\n");
+        else
+            printf("%d\n", x-y);
+
+    }
+    
+	return 0;
+}
+
+
+
