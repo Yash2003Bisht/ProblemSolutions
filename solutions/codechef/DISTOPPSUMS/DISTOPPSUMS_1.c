8c6568698a5834b2446e5b97250ea587f853c9c1
yash2003bisht <yash2003bisht@gmail.com>
2023-03-07 13:34:02 +0530

    attempted question DISTOPPSUMS

---
+++ solutions/codechef/DISTOPPSUMS/DISTOPPSUMS_1.c
@@ -0,0 +1,32 @@
+// DATE: 07/03/2023, 01:09:52
+// PROBLEM NAME: Distinct Opposite Sums
+// PROBLEM URL: https://www.codechef.com/problems/DISTOPPSUMS
+// PROBLEM DIFFICULTY RATTING: 1135
+// STATUS: accepted
+// TIME: 0.06
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        for (int i=1; i<=n/2; i++)
+            printf("%d ", i);
+
+        for (int i=n; i>n/2; i--)
+            printf("%d ", i);
+
+        printf("\n");
+
+    }
+
+	return 0;
+}
+
+
+
