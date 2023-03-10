84692647540bb608e318c569ea0919599a385c7e
yash2003bisht <yash2003bisht@gmail.com>
2023-03-10 08:18:31 +0530

    attempted question EQUALELE

---
+++ solutions/codechef/EQUALELE/EQUALELE_1.c
@@ -0,0 +1,40 @@
+// DATE: 10/03/2023, 08:01:30
+// PROBLEM NAME: Equal Elements
+// PROBLEM URL: https://www.codechef.com/problems/EQUALELE
+// PROBLEM DIFFICULTY RATTING: 1123
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        int counter[n], repeat = 1;
+        
+        for (int i=0; i<n; i++)
+            counter[i] = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            counter[a-1]++;
+        }
+        
+        for (int i=0; i<n; i++){
+            if (counter[i] > repeat)
+                repeat = counter[i];
+        }
+
+        printf("%d\n", n - repeat);
+
+    }
+    
+	return 0;
+}
+
+
+
