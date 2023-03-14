0d21ed23b9d38b5e6621fbee2d87d7e4ec01252a
yash2003bisht <yash2003bisht@gmail.com>
2023-03-14 13:15:02 +0530

    attempted question PAR2

---
+++ solutions/codechef/PAR2/PAR2_1.c
@@ -0,0 +1,29 @@
+// DATE: 14/03/2023, 01:08:49
+// PROBLEM NAME: Parity
+// PROBLEM URL: https://www.codechef.com/problems/PAR2
+// PROBLEM DIFFICULTY RATTING: 295
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n%2 == 0)
+            printf("Yes\n");
+        else
+            printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
