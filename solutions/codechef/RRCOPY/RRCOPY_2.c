e5b467a5de98fe48e8e099bdcae31f8203429d1c
yash2003bisht <yash2003bisht@gmail.com>
2023-03-14 13:15:02 +0530

    attempted question RRCOPY

---
+++ solutions/codechef/RRCOPY/RRCOPY_2.c
@@ -0,0 +1,35 @@
+// DATE: 13/03/2023, 08:02:18
+// PROBLEM NAME: Copy-Paste
+// PROBLEM URL: https://www.codechef.com/problems/RRCOPY
+// PROBLEM DIFFICULTY RATTING: 1280
+// STATUS: accepted
+// TIME: 0.14
+// MEMORY: 5.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int b[1000000] = {0}, count = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            if (b[a] == 0){
+                b[a] = 1;
+                count++;
+            }
+        }
+        
+        printf("%d\n", count);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/RRCOPY/RRCOPY_3.py
@@ -0,0 +1,14 @@
+# DATE: 13/03/2023, 07:57:22
+# PROBLEM NAME: Copy-Paste
+# PROBLEM URL: https://www.codechef.com/problems/RRCOPY
+# PROBLEM DIFFICULTY RATTING: 1280
+# STATUS: accepted
+# TIME: 0.24
+# MEMORY: 26M
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(len(set(a)))
+
+
