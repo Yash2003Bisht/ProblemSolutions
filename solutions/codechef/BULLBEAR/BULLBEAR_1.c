1506f5e1460b41bad870f98dee837252189597b6
yash2003bisht <yash2003bisht@gmail.com>
2023-03-14 13:15:02 +0530

    attempted question BULLBEAR

---
+++ solutions/codechef/BULLBEAR/BULLBEAR_1.c
@@ -0,0 +1,31 @@
+// DATE: 14/03/2023, 01:11:22
+// PROBLEM NAME: Bull or Bear
+// PROBLEM URL: https://www.codechef.com/problems/BULLBEAR
+// PROBLEM DIFFICULTY RATTING: 300
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+
+        if (x > y)
+            printf("LOSS\n");
+        else if (x == y)
+            printf("NEUTRAL\n");
+        else
+            printf("PROFIT\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
