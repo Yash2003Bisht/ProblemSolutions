ecc58214a5d41f207065643a713a8231fac4f4f8
yash2003bisht <yash2003bisht@gmail.com>
2023-03-14 13:15:02 +0530

    attempted question MAX2

---
+++ solutions/codechef/MAX2/MAX2_1.c
@@ -0,0 +1,30 @@
+// DATE: 13/03/2023, 08:32:10
+// PROBLEM NAME: Max power
+// PROBLEM URL: https://www.codechef.com/problems/MAX2
+// PROBLEM DIFFICULTY RATTING: 1282
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int n, count = 0;
+    scanf("%d", &n);
+
+    char s[n];
+    scanf("%s", &s);
+
+    for (int i=n-1; i>=0; i--){
+        if (s[i] != '1')
+            count++;
+        else
+            break;
+    }
+    
+    printf("%d", count);
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/MAX2/MAX2_2.c
@@ -0,0 +1,38 @@
+// DATE: 13/03/2023, 08:31:17
+// PROBLEM NAME: Max power
+// PROBLEM URL: https://www.codechef.com/problems/MAX2
+// PROBLEM DIFFICULTY RATTING: 1282
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int n, count;
+    scanf("%d", &n);
+
+    char s[n];
+    scanf("%s", &s);
+    
+    if (s[n-1] == '1'){
+        printf("0");
+    } else{
+        count = 0;
+
+        for (int i=n-1; i>=0; i--){
+            if (s[i] != '1')
+                count++;
+            else
+                break;
+        }
+        
+        printf("%d", count);
+        
+    }
+
+    
+	return 0;
+}
+
+
