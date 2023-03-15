73c8a6b9693d7d7aeb2b47e22933f7737e44d548
yash2003bisht <yash2003bisht@gmail.com>
2023-03-15 08:11:26 +0530

    attempted question EQUINOX

---
+++ solutions/codechef/EQUINOX/EQUINOX_2.c
@@ -0,0 +1,50 @@
+// DATE: 15/03/2023, 08:08:12
+// PROBLEM NAME: Equinox Strings
+// PROBLEM URL: https://www.codechef.com/problems/EQUINOX
+// PROBLEM DIFFICULTY RATTING: 1272
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, flag;
+    long long int a, b, sarthak_points, anuradha_points;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%lld%lld", &n, &a, &b);
+        char s[n+1], match[] = "EQUINOX";
+        sarthak_points = 0, anuradha_points = 0;
+        
+        while(n--){
+            scanf("%s", s);
+            flag = 0;
+
+            for (int j=0; match[j] != '\0'; j++){
+                if (s[0] == match[j]){
+                    sarthak_points += a;
+                    flag = 1;
+                    break;
+                }
+            }
+            
+            if (!flag)
+                anuradha_points += b;
+            
+        }
+        
+        if (sarthak_points > anuradha_points)
+            printf("SARTHAK\n");
+        else if (sarthak_points < anuradha_points)
+            printf("ANURADHA\n");
+        else
+            printf("DRAW\n");
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/EQUINOX/EQUINOX_5.c
@@ -0,0 +1,49 @@
+// DATE: 15/03/2023, 08:03:06
+// PROBLEM NAME: Equinox Strings
+// PROBLEM URL: https://www.codechef.com/problems/EQUINOX
+// PROBLEM DIFFICULTY RATTING: 1272
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 0M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, b, flag, sarthak_points, anuradha_points;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &a, &b);
+        char s[n+1], match[] = "EQUINOX";
+        sarthak_points = 0, anuradha_points = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%s", s);
+            flag = 0;
+
+            for (int j=0; match[j] != '\0'; j++){
+                if (s[0] == match[j]){
+                    sarthak_points += a;
+                    flag = 1;
+                    break;
+                }
+            }
+            
+            if (!flag)
+                anuradha_points += b;
+            
+        }
+        
+        if (sarthak_points > anuradha_points)
+            printf("SARTHAK\N");
+        else if (sarthak_points < anuradha_points)
+            printf("ANURADHA\n");
+        else
+            printf("DRAW\n");
+    }
+    
+	return 0;
+}
+
+
+
