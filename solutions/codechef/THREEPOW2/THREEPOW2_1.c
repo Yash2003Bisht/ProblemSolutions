3124a11aaade2e443b3d4a04970fdb0e46b89abf
yash2003bisht <yash2003bisht@gmail.com>
2023-03-15 08:11:25 +0530

    attempted question THREEPOW2

---
+++ solutions/codechef/THREEPOW2/THREEPOW2_1.c
@@ -0,0 +1,35 @@
+// DATE: 15/03/2023, 07:50:03
+// PROBLEM NAME: Three Powers of Two
+// PROBLEM URL: https://www.codechef.com/problems/THREEPOW2
+// PROBLEM DIFFICULTY RATTING: 1253
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int t, n, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        count = 0;
+        
+        scanf("%s", s);
+        
+        for (int i=0; s[i] != '\0'; i++)
+            if (s[i] == '1') count++;
+
+        if (count <= 3 && strcmp(s, "1") && strcmp(s, "10")) printf("YES\n");
+        else printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
