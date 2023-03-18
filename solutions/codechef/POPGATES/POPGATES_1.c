605f22b8dabc069010549739d4ce4916196628bc
yash2003bisht <yash2003bisht@gmail.com>
2023-03-18 09:09:00 +0530

    attempted question POPGATES

---
+++ solutions/codechef/POPGATES/POPGATES_1.c
@@ -0,0 +1,49 @@
+// DATE: 18/03/2023, 09:03:11
+// PROBLEM NAME: At the Gates
+// PROBLEM URL: https://www.codechef.com/problems/POPGATES
+// PROBLEM DIFFICULTY RATTING: 1275
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 0M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, count, counter;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &k);
+        char s[101] = {'\0'};
+        count = 0, counter = 0;
+
+        for (int i=0; i<n; i++)
+            scanf(" %c ", &s[i]);
+            
+        for (int i=n-1; i>=n-k; i--){
+            if (s[i] == 'H')
+                count++;
+        }
+        
+        if (count % 2 == 0){
+            for (int i=0; i<n-k; i++){
+                if (s[i] == 'H')
+                    counter++;
+            }
+        } else {
+            for (int i=0; i<n-k; i++){
+                if (s[i] == 'T')
+                    counter++;
+            }
+        }
+        
+        printf("%d\n", counter);
+
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/POPGATES/POPGATES_3.c
@@ -0,0 +1,50 @@
+// DATE: 18/03/2023, 08:37:24
+// PROBLEM NAME: At the Gates
+// PROBLEM URL: https://www.codechef.com/problems/POPGATES
+// PROBLEM DIFFICULTY RATTING: 1275
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &k);
+        char s[n+1];
+        count = 0;
+
+        for (int i=0; i<n; i++)
+            scanf(" %c ", &s[i]);
+            
+        for (int i=n-1; i>=n-k; i--){
+            if (s[i] == 'H'){
+                for (int j=0; s[j] != '\0'; j++){
+                    if (s[j] == 'H')
+                        s[j] = 'T';
+                    else
+                        s[j] = 'H';
+                }
+                
+            }
+            s[i] = '\0';
+        }
+        
+        for (int i=0; s[i] != '\0'; i++){
+            if (s[i] == 'H')
+                count++;
+        }
+        
+        printf("%d\n", count);
+
+
+    }
+    
+	return 0;
+}
+
+
+
