eeca5ae67b5335103f546da43a758503748c5be3
yash2003bisht <yash2003bisht@gmail.com>
2023-03-21 08:05:56 +0530

    attempted question FANCY

---
+++ solutions/codechef/FANCY/FANCY_1.c
@@ -0,0 +1,42 @@
+// DATE: 21/03/2023, 07:34:55
+// PROBLEM NAME: Fancy Quotes
+// PROBLEM URL: https://www.codechef.com/problems/FANCY
+// PROBLEM DIFFICULTY RATTING: 1279
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int t, flag;
+    char s[101];
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("\n%[^\n]", s);
+        char *split_s = strtok(s, " ");
+        flag = 0;
+        
+        while(split_s){
+            if (!strcmp(split_s, "not")){
+                flag = 1;
+                break;
+            }
+            split_s = strtok(NULL, " ");
+        }
+        
+        if (flag)
+            printf("Real Fancy\n");
+        else
+            printf("regularly fancy\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/FANCY/FANCY_3.py
@@ -0,0 +1,18 @@
+# DATE: 21/03/2023, 07:16:17
+# PROBLEM NAME: Fancy Quotes
+# PROBLEM URL: https://www.codechef.com/problems/FANCY
+# PROBLEM DIFFICULTY RATTING: 1279
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+
+    s = input().split()
+
+    if "not" in s:
+        print("Real Fancy")
+    else:
+        print("regularly fancy")
+
+
