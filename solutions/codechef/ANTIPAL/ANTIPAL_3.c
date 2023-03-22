3cf36f65300544d5789f2ae3c86c784bc1369a13
yash2003bisht <yash2003bisht@gmail.com>
2023-03-22 08:13:40 +0530

    attempted question ANTIPAL

---
+++ solutions/codechef/ANTIPAL/ANTIPAL_3.c
@@ -0,0 +1,38 @@
+// DATE: 22/03/2023, 08:05:28
+// PROBLEM NAME: Chef and Pal
+// PROBLEM URL: https://www.codechef.com/problems/ANTIPAL
+// PROBLEM DIFFICULTY RATTING: 1282
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n%2 != 0)
+            printf("-1\n");
+        else {
+            for (int i=0; i<n; i++){
+                if (i%2 == 0)
+                    printf("0");
+                else
+                    printf("1");
+            }
+            
+            printf("\n");
+            
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/ANTIPAL/ANTIPAL_4.c
@@ -0,0 +1,38 @@
+// DATE: 22/03/2023, 08:04:52
+// PROBLEM NAME: Chef and Pal
+// PROBLEM URL: https://www.codechef.com/problems/ANTIPAL
+// PROBLEM DIFFICULTY RATTING: 1282
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n%2 != 0)
+            printf("-1\n");
+        else {
+            for (int i=0; i<n; i++){
+                if (i%2 == 0)
+                    printf("1");
+                else
+                    printf("0");
+            }
+            
+            printf("\n");
+            
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
