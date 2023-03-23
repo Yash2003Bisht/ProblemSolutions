03d79ee729b96654c3fa446a0996943e66400ba9
yash2003bisht <yash2003bisht@gmail.com>
2023-03-23 12:22:40 +0530

    attempted question LCPESY

---
+++ solutions/codechef/LCPESY/LCPESY_2.c
@@ -0,0 +1,49 @@
+// DATE: 23/03/2023, 08:51:18
+// PROBLEM NAME: Longest Common Pattern
+// PROBLEM URL: https://www.codechef.com/problems/LCPESY
+// PROBLEM DIFFICULTY RATTING: 1284
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#define min(a, b)(a>b?b:a)
+
+void Counter(char arr[], int counter_arr[]){
+    for (int i=0; arr[i] != '\0'; i++){
+        if (arr[i] >= 'a' && arr[i] <= 'z')
+            counter_arr[arr[i] - 97]++;
+        else if (arr[i] >= 'A' && arr[i] <= 'Z')
+            counter_arr[arr[i] - 39]++;
+        else
+            counter_arr[arr[i] + 4]++;
+    }
+}
+
+int main(void) {
+    int t, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        char a[10001], b[10001];
+        int a_counter[62] = {0}, b_counter[62] = {0};
+        scanf("%s", a);
+        scanf("%s", b);
+        count = 0;
+
+        Counter(a, a_counter);
+        Counter(b, b_counter);
+        
+        for (int i=0; i<62; i++)
+            count += min(a_counter[i], b_counter[i]);
+            
+        printf("%d\n", count);
+        
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/LCPESY/LCPESY_3.py
@@ -0,0 +1,22 @@
+# DATE: 23/03/2023, 08:28:44
+# PROBLEM NAME: Longest Common Pattern
+# PROBLEM URL: https://www.codechef.com/problems/LCPESY
+# PROBLEM DIFFICULTY RATTING: 1284
+# STATUS: accepted
+# TIME: 0.10
+# MEMORY: 9.7M
+
+from collections import Counter
+
+for _ in range(int(input())):
+    a = Counter(input())
+    b = Counter(input())
+    count = 0
+
+    for key, value in a.items():
+        if b.get(key):
+            count += min(value, b.get(key))
+
+    print(count)
+
+
