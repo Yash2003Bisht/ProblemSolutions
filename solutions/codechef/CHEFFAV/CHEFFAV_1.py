d43dacc6af2b3818dd42ef864484af020a6eefc3
yash2003bisht <yash2003bisht@gmail.com>
2023-03-24 21:21:14 +0530

    attempted question CHEFFAV

---
+++ solutions/codechef/CHEFFAV/CHEFFAV_1.py
@@ -0,0 +1,24 @@
+# DATE: 24/03/2023, 08:09:59
+# PROBLEM NAME: Favourite String of Chef
+# PROBLEM URL: https://www.codechef.com/problems/CHEFFAV
+# PROBLEM DIFFICULTY RATTING: 1284
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.5M
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    
+    if "chef" in s:
+        s = s.split("chef")
+        
+        if "code" in s[0]:
+            print("AC")
+        else:
+            print("WA")
+        
+    else:
+        print("WA")
+
+
