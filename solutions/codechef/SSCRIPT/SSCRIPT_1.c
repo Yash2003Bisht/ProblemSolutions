0ab8dac94b4ab96676bdbafe3d75b43dd85632b6
yash2003bisht <yash2003bisht@gmail.com>
2023-03-28 08:46:25 +0530

    attempted question SSCRIPT

---
+++ solutions/codechef/SSCRIPT/SSCRIPT_1.c
@@ -0,0 +1,46 @@
+// DATE: 28/03/2023, 08:37:34
+// PROBLEM NAME: Strong Language
+// PROBLEM URL: https://www.codechef.com/problems/SSCRIPT
+// PROBLEM DIFFICULTY RATTING: 1291
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, flag, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &k);
+        char s[n+1];
+        flag = 0, count = 0;
+        
+        scanf("%s", s);
+        
+        for (int i=0; i<n; i++){
+            if (s[i] == '*')
+                count++;
+            else
+                count = 0;
+            
+            if (count == k){
+                flag = 1;
+                break;
+            }
+
+        }
+        
+        if (flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
