277608cf97fe0d3bbf6a1986c162557e69eaf7b7
yash2003bisht <yash2003bisht@gmail.com>
2023-03-30 08:03:02 +0530

    attempted question SALARY

---
+++ solutions/codechef/SALARY/SALARY_1.py
@@ -0,0 +1,14 @@
+# DATE: 29/03/2023, 09:02:40
+# PROBLEM NAME: The Minimum Number Of Moves
+# PROBLEM URL: https://www.codechef.com/problems/SALARY
+# PROBLEM DIFFICULTY RATTING: 1286
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.7M
+
+for _ in range(int(input())):
+    n = int(input())
+    w = list(map(int, input().split()))
+    print(sum(w) - n*min(w))
+
+
---
+++ solutions/codechef/SALARY/SALARY_2.py
@@ -0,0 +1,35 @@
+# DATE: 29/03/2023, 08:57:54
+# PROBLEM NAME: The Minimum Number Of Moves
+# PROBLEM URL: https://www.codechef.com/problems/SALARY
+# PROBLEM DIFFICULTY RATTING: 1286
+# STATUS: time limit exceeded
+# TIME: -
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    n = int(input())
+    w = list(map(int, input().split()))
+    count = 0
+
+    while True:
+        _max = max(w)
+        flag = False
+
+        for i in range(n):
+            if w[i] < _max:
+                flag = True
+                w[i] += 1
+
+        if flag:
+            count += 1
+            flag = False
+
+        for i in range(n-1):
+            if w[i] != w[i+1]:
+                break
+        else:
+            break
+
+    print(count)
+
+
