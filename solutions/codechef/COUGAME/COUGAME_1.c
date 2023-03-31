22d05451b2020305b8981ac1a1a6be93abbccbed
yash2003bisht <yash2003bisht@gmail.com>
2023-03-31 21:26:40 +0530

    attempted question COUGAME

---
+++ solutions/codechef/COUGAME/COUGAME_1.c
@@ -0,0 +1,24 @@
+// DATE: 31/03/2023, 09:22:13
+// PROBLEM NAME: Couple Game
+// PROBLEM URL: https://www.codechef.com/problems/COUGAME
+// PROBLEM DIFFICULTY RATTING: 347
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, g, b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &g, &b);
+        printf("%d\n", b-g);
+    }
+    
+	return 0;
+}
+
+
+
