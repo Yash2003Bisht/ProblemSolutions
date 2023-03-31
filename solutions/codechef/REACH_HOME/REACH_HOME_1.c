af9bbc909fc9bea52ddaa024386dcc71573a830f
yash2003bisht <yash2003bisht@gmail.com>
2023-03-31 21:26:39 +0530

    attempted question REACH_HOME

---
+++ solutions/codechef/REACH_HOME/REACH_HOME_1.c
@@ -0,0 +1,29 @@
+// DATE: 31/03/2023, 09:23:11
+// PROBLEM NAME: Reach Home
+// PROBLEM URL: https://www.codechef.com/problems/REACH_HOME
+// PROBLEM DIFFICULTY RATTING: 395
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (x*5 >= y)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
