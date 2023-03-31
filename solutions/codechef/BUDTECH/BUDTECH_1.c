449614c93627490ce77d9bb2e869576538b0b91a
yash2003bisht <yash2003bisht@gmail.com>
2023-03-31 21:26:40 +0530

    attempted question BUDTECH

---
+++ solutions/codechef/BUDTECH/BUDTECH_1.c
@@ -0,0 +1,25 @@
+// DATE: 31/03/2023, 09:26:03
+// PROBLEM NAME: Budget of Technex
+// PROBLEM URL: https://www.codechef.com/problems/BUDTECH
+// PROBLEM DIFFICULTY RATTING: 449
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, r;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &r);
+        r = (r*1000)/2;
+        printf("%d\n", r/5);
+    }
+    
+	return 0;
+}
+
+
+
