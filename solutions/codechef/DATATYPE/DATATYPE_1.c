28c3ff2ddc76e464abf2f025d5420cd76e1b78ce
yash2003bisht <yash2003bisht@gmail.com>
2023-04-01 21:29:02 +0530

    attempted question DATATYPE

---
+++ solutions/codechef/DATATYPE/DATATYPE_1.c
@@ -0,0 +1,25 @@
+// DATE: 01/04/2023, 09:27:43
+// PROBLEM NAME: Fit in Data Type
+// PROBLEM URL: https://www.codechef.com/problems/DATATYPE
+// PROBLEM DIFFICULTY RATTING: 1133
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &x);
+        printf("%d\n", x%(n+1));
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/DATATYPE/DATATYPE_2.c
@@ -0,0 +1,29 @@
+// DATE: 01/04/2023, 09:25:55
+// PROBLEM NAME: Fit in Data Type
+// PROBLEM URL: https://www.codechef.com/problems/DATATYPE
+// PROBLEM DIFFICULTY RATTING: 1133
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &x);
+        
+        if (n >= x)
+            printf("%d\n", x);
+        else
+            printf("%d\n", x-n-1);
+        
+    }
+    
+	return 0;
+}
+
+
+
