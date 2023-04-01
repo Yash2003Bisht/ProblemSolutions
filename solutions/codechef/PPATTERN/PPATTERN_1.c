09d2c160197c8ea9942b89bdfa50511112af9696
yash2003bisht <yash2003bisht@gmail.com>
2023-04-01 11:03:10 +0530

    attempted question PPATTERN

---
+++ solutions/codechef/PPATTERN/PPATTERN_1.c
@@ -0,0 +1,47 @@
+// DATE: 01/04/2023, 08:33:55
+// PROBLEM NAME: Print Pattern
+// PROBLEM URL: https://www.codechef.com/problems/PPATTERN
+// PROBLEM DIFFICULTY RATTING: 1283
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, temp, counter, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        temp = 1, counter = 1, count = 2;
+        int arr[n];
+
+        for (int i=0; i<n; i++)
+            arr[i] = i;
+
+        for (int i=0; i<n; i++){
+
+            for (int j=0; j<n; j++){
+                temp += arr[j];
+                printf("%d ", temp);
+            }
+
+            for (int j=0; j<n-1; j++)
+                arr[j] = arr[j+1];
+
+            arr[n-1] = n-i-1;
+            counter += 2 + i;
+            temp = counter - arr[0];
+
+            printf("\n");
+
+        }
+
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/PPATTERN/PPATTERN_2.c
@@ -0,0 +1,48 @@
+// DATE: 01/04/2023, 08:22:14
+// PROBLEM NAME: Print Pattern
+// PROBLEM URL: https://www.codechef.com/problems/PPATTERN
+// PROBLEM DIFFICULTY RATTING: 1283
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, temp, counter, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        temp = 1, counter = 1, count = 2;
+        int arr[n];
+
+        for (int i=0; i<n; i++)
+            arr[i] = i;
+
+        for (int i=0; i<n; i++){
+
+            for (int j=0; j<n; j++){
+                temp += arr[j];
+                printf("%d ", temp);
+            }
+
+            for (int j=0; j<n-1; j++)
+                arr[j] = arr[j+1];
+
+            arr[n-1] = n-i-1;
+            counter += count;
+            temp = counter - arr[0];
+            count++;
+
+            printf("\n");
+
+        }
+
+    }
+
+	return 0;
+}
+
+
+
