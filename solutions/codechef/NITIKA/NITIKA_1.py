e1c2c7fd6fb79396d31bd7cde075f17f880d81a5
yash2003bisht <yash2003bisht@gmail.com>
2023-04-03 08:44:09 +0530

    attempted question NITIKA

---
+++ solutions/codechef/NITIKA/NITIKA_1.py
@@ -0,0 +1,19 @@
+# DATE: 03/04/2023, 08:42:55
+# PROBLEM NAME: Whats in the Name
+# PROBLEM URL: https://www.codechef.com/problems/NITIKA
+# PROBLEM DIFFICULTY RATTING: 1299
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.7M
+
+for _ in range(int(input())):
+    name = list(map(lambda n: n.capitalize(), input().split()))
+
+    if len(name) == 1:
+        print(name[0])
+    elif len(name) == 2:
+        print(f"{name[0][0]}. {name[1]}")
+    else:
+        print(f"{name[0][0]}. {name[1][0]}. {name[2]}")
+
+
---
+++ solutions/codechef/NITIKA/NITIKA_3.py
@@ -0,0 +1,19 @@
+# DATE: 03/04/2023, 08:40:23
+# PROBLEM NAME: Whats in the Name
+# PROBLEM URL: https://www.codechef.com/problems/NITIKA
+# PROBLEM DIFFICULTY RATTING: 1299
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    name = input().split()
+
+    if len(name) == 1:
+        print(name[0].capitalize())
+    elif len(name) == 2:
+        print(f"{name[0][0].capitalize()}. {name[1].capitalize()}")
+    else:
+        print(f"{name[0][0].capitalize()}. {name[1][0].capitalize()}. {name[2].capitalize()}")
+
+
