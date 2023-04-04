0fffef5559d4d4beda7d5b843ff89d857b872dab
yash2003bisht <yash2003bisht@gmail.com>
2023-04-04 08:37:16 +0530

    attempted question CHFCHK

---
+++ solutions/codechef/CHFCHK/CHFCHK_1.c
@@ -0,0 +1,34 @@
+// DATE: 04/04/2023, 07:10:52
+// PROBLEM NAME: Chef Chick
+// PROBLEM URL: https://www.codechef.com/problems/CHFCHK
+// PROBLEM DIFFICULTY RATTING: 1295
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+    int t, n, a, min_value;
+    scanf("%d", &t);
+    
+    while(t--){
+        min_value = INT_MAX;
+        scanf("%d", &n);
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            if (min_value > a)
+                min_value = a;
+        }
+        
+        printf("%d\n", min_value);
+        
+    }
+    
+	return 0;
+}
+
+
+
