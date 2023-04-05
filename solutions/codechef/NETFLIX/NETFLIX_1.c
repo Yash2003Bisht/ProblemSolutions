c36b99eeede3afc640aa7d60d990ba8332982bb1
yash2003bisht <yash2003bisht@gmail.com>
2023-04-05 19:16:01 +0530

    attempted question NETFLIX

---
+++ solutions/codechef/NETFLIX/NETFLIX_1.c
@@ -0,0 +1,29 @@
+// DATE: 05/04/2023, 07:06:06
+// PROBLEM NAME: Netflix
+// PROBLEM URL: https://www.codechef.com/problems/NETFLIX
+// PROBLEM DIFFICULTY RATTING: 493
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &c, &x);
+        
+        if (a+b >= x || a+c >= x || b+c >= x)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
