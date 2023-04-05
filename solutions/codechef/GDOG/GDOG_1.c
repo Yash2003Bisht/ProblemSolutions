c0223432c4b7939fd47540d8bf96f30a6f065dd6
yash2003bisht <yash2003bisht@gmail.com>
2023-04-05 08:40:21 +0530

    attempted question GDOG

---
+++ solutions/codechef/GDOG/GDOG_1.c
@@ -0,0 +1,30 @@
+// DATE: 05/04/2023, 07:24:53
+// PROBLEM NAME: Greedy puppy
+// PROBLEM URL: https://www.codechef.com/problems/GDOG
+// PROBLEM DIFFICULTY RATTING: 1306
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#define max(a, b)(a>b ? a:b)
+
+int main(void) {
+    int t, n, k, max_coin;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &n, &k);
+        max_coin = 0;
+
+        for (int i=1; i<=k; i++)
+            max_coin = max(max_coin, n%i);
+
+        printf("%d\n", max_coin);
+        
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/GDOG/GDOG_2.c
@@ -0,0 +1,31 @@
+// DATE: 05/04/2023, 07:22:48
+// PROBLEM NAME: Greedy puppy
+// PROBLEM URL: https://www.codechef.com/problems/GDOG
+// PROBLEM DIFFICULTY RATTING: 1306
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, max_coin;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &n, &k);
+        max_coin = 0;
+
+        for (int i=1; i<=k; i++){
+            if (max_coin < n%i)
+                max_coin = n%i;
+        }
+        
+        printf("%d\n", max_coin);
+        
+    }
+
+	return 0;
+}
+
+
