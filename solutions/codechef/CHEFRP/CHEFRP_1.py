3c1385dfdb79cc3617ad0f03263395f6954fad71
yash2003bisht <yash2003bisht@gmail.com>
2023-04-06 09:06:27 +0530

    attempted question CHEFRP

---
+++ solutions/codechef/CHEFRP/CHEFRP_1.py
@@ -0,0 +1,22 @@
+# DATE: 06/04/2023, 09:05:16
+# PROBLEM NAME: Chef and new recipe
+# PROBLEM URL: https://www.codechef.com/problems/CHEFRP
+# PROBLEM DIFFICULTY RATTING: 1310
+# STATUS: accepted
+# TIME: 0.26
+# MEMORY: 25.3M
+
+for _ in range(int(input())):
+    
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+
+    if arr[n-1] == 1:
+        print(-1)
+    elif n == 1:
+        print(2)
+    else:
+        print(sum(arr) - arr[n-1] + 2)
+
+
---
+++ solutions/codechef/CHEFRP/CHEFRP_2.py
@@ -0,0 +1,22 @@
+# DATE: 06/04/2023, 09:04:32
+# PROBLEM NAME: Chef and new recipe
+# PROBLEM URL: https://www.codechef.com/problems/CHEFRP
+# PROBLEM DIFFICULTY RATTING: 1310
+# STATUS: accepted
+# TIME: 0.27
+# MEMORY: 25.3M
+
+for _ in range(int(input())):
+    
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+
+    if arr[n-1] == 1:
+        print(-1)
+    elif n == 1:
+        print(2)
+    else:
+        print(sum(arr[:n-1]) + 2)
+
+
