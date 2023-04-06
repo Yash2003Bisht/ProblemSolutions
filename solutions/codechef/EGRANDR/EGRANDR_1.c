42715ecfbad35c47ce8c34cfd067cd6e69123c2b
yash2003bisht <yash2003bisht@gmail.com>
2023-04-06 15:33:12 +0530

    attempted question EGRANDR

---
+++ solutions/codechef/EGRANDR/EGRANDR_1.c
@@ -0,0 +1,42 @@
+// DATE: 06/04/2023, 03:29:19
+// PROBLEM NAME: Andrash and Stipendium
+// PROBLEM URL: https://www.codechef.com/problems/EGRANDR
+// PROBLEM DIFFICULTY RATTING: 1299
+// STATUS: accepted
+// TIME: 0.24
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(){
+    int t, n, a, sum, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        sum = 0, flag = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            
+            if (flag != -1){
+                if (a == 2)
+                    flag = -1;
+                else if (a == 5)
+                    flag = 1;
+                sum += a;
+            }
+            
+        }
+        
+        if (sum/n >= 4 && flag && flag != -1)
+            printf("Yes\n");
+        else
+            printf("No\n");
+        
+    }
+    
+    return 0;
+}
+
+
---
+++ solutions/codechef/EGRANDR/EGRANDR_2.c
@@ -0,0 +1,46 @@
+// DATE: 06/04/2023, 03:25:46
+// PROBLEM NAME: Andrash and Stipendium
+// PROBLEM URL: https://www.codechef.com/problems/EGRANDR
+// PROBLEM DIFFICULTY RATTING: 1299
+// STATUS: accepted
+// TIME: 0.22
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, flag, sum;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int a[n];
+        flag = 0, sum = 0;
+
+        for (int i=0; i<n; i++)
+            scanf("%d", &a[i]);
+            
+        for (int i=0; i<n; i++){
+
+            if (a[i] == 2){
+                flag = 0;
+                break;
+            } else if (a[i] == 5)
+                flag = 1;
+
+            sum += a[i];
+        }
+        
+        if (sum/n >= 4 && flag)
+            printf("Yes\n");
+        else
+            printf("No\n");
+            
+
+    }
+    
+	return 0;
+}
+
+
+
