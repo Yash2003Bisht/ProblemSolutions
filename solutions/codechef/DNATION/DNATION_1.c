009a7851b4f5ed308f63e23e4860b83fae82d997
yash2003bisht <yash2003bisht@gmail.com>
2023-04-10 21:22:20 +0530

    attempted question DNATION

---
+++ solutions/codechef/DNATION/DNATION_1.c
@@ -0,0 +1,24 @@
+// DATE: 10/04/2023, 09:15:45
+// PROBLEM NAME: Chef and Donation
+// PROBLEM URL: https://www.codechef.com/problems/DNATION
+// PROBLEM DIFFICULTY RATTING: 305
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, y, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        printf("%d\n", y-x);
+    }
+    
+	return 0;
+}
+
+
+
