34240afaad0f46bf4a8927add90b7510498b8fd1
yash2003bisht <yash2003bisht@gmail.com>
2023-04-10 20:34:08 +0530

    attempted question CHFIDEAL

---
+++ solutions/codechef/CHFIDEAL/CHFIDEAL_1.c
@@ -0,0 +1,28 @@
+// DATE: 10/04/2023, 08:02:50
+// PROBLEM NAME: Chef and An Ideal Problem
+// PROBLEM URL: https://www.codechef.com/problems/CHFIDEAL
+// PROBLEM DIFFICULTY RATTING: 1294
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+    int x;
+
+    printf("1\n");
+    fflush(stdout);
+    scanf("%d", &x);
+    
+    if (x == 2)
+        printf("3\n");
+    else
+        printf("2\n");
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CHFIDEAL/CHFIDEAL_2.c
@@ -0,0 +1,26 @@
+// DATE: 10/04/2023, 07:57:40
+// PROBLEM NAME: Chef and An Ideal Problem
+// PROBLEM URL: https://www.codechef.com/problems/CHFIDEAL
+// PROBLEM DIFFICULTY RATTING: 1294
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+     int y;
+
+     printf("1\n");
+     fflush(stdout);
+     scanf("%d", &y);
+
+     if(y==2)
+         printf("3\n");
+     else
+         printf("2\n");
+	return 0;
+}
+
+
