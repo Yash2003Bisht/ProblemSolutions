8641cbe6df6721ccf900a0594b84f6dc85b6602b
yash2003bisht <yash2003bisht@gmail.com>
2023-04-10 21:22:20 +0530

    attempted question INVESTMENT

---
+++ solutions/codechef/INVESTMENT/INVESTMENT_1.c
@@ -0,0 +1,29 @@
+// DATE: 10/04/2023, 09:20:55
+// PROBLEM NAME: Good Investment or Not
+// PROBLEM URL: https://www.codechef.com/problems/INVESTMENT
+// PROBLEM DIFFICULTY RATTING: 357
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (x >= y*2)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
