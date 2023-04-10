d2623ee67d2b0937c1ae32ae02acd77498500f44
yash2003bisht <yash2003bisht@gmail.com>
2023-04-10 21:22:20 +0530

    attempted question FINE

---
+++ solutions/codechef/FINE/FINE_1.c
@@ -0,0 +1,31 @@
+// DATE: 10/04/2023, 09:17:05
+// PROBLEM NAME: Overspeeding Fine
+// PROBLEM URL: https://www.codechef.com/problems/FINE
+// PROBLEM DIFFICULTY RATTING: 335
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        
+        if (x <= 70)
+            printf("0\n");
+        else if (x <= 100)
+            printf("500\n");
+        else 
+            printf("2000\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
