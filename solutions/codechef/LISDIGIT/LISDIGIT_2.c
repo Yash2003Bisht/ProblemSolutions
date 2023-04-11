2ed776bba0f17069d16637619aa620b2a34b3a01
yash2003bisht <yash2003bisht@gmail.com>
2023-04-11 08:45:41 +0530

    attempted question LISDIGIT

---
+++ solutions/codechef/LISDIGIT/LISDIGIT_2.c
@@ -0,0 +1,31 @@
+// DATE: 11/04/2023, 07:40:23
+// PROBLEM NAME: Digit Longest Increasing Subsequences 2
+// PROBLEM URL: https://www.codechef.com/problems/LISDIGIT
+// PROBLEM DIFFICULTY RATTING: 1297
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            printf("%d", a);
+        }
+
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
