b58848893a4a2f7aeb79ecb1dc5b7911bb3b000d
yash2003bisht <yash2003bisht@gmail.com>
2023-04-11 08:45:41 +0530

    attempted question ISPAR

---
+++ solutions/codechef/ISPAR/ISPAR_1.c
@@ -0,0 +1,35 @@
+// DATE: 11/04/2023, 08:43:39
+// PROBLEM NAME: Is this priority or parity!
+// PROBLEM URL: https://www.codechef.com/problems/ISPAR
+// PROBLEM DIFFICULTY RATTING: 1305
+// STATUS: accepted
+// TIME: 0.04
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int n, k;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%lld%lld", &n, &k);
+
+        if (k == 1){
+            if (n%2 == 0)
+                printf("EVEN\n");
+            else
+                printf("ODD\n");
+        } else if (k == 2){
+            printf("ODD\n");
+        } else{
+            printf("EVEN\n");
+        }
+
+    }
+    
+	return 0;
+}
+
+
