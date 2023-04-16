3819c986bf4080e562798c703200a982663a36fd
yash2003bisht <yash2003bisht@gmail.com>
2023-04-16 22:48:50 +0530

    attempted question IPLTRSH

---
+++ solutions/codechef/IPLTRSH/IPLTRSH_1.c
@@ -0,0 +1,26 @@
+// DATE: 16/04/2023, 10:36:56
+// PROBLEM NAME: IPL Ticket Rush
+// PROBLEM URL: https://www.codechef.com/problems/IPLTRSH
+// PROBLEM DIFFICULTY RATTING: 273
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#define max(a, b)(a>b ? a:b)
+
+int main(void) {
+    int t, n, m, ans;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &m);
+        ans = max(0, n-m);
+        printf("%d\n", ans);
+    }
+    
+	return 0;
+}
+
+
+
