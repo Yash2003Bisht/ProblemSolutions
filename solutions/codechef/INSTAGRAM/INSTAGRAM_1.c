dea9cd55a7b3f4657df0673710002f549857d7f2
yash2003bisht <yash2003bisht@gmail.com>
2023-04-16 22:48:50 +0530

    attempted question INSTAGRAM

---
+++ solutions/codechef/INSTAGRAM/INSTAGRAM_1.c
@@ -0,0 +1,29 @@
+// DATE: 16/04/2023, 10:48:15
+// PROBLEM NAME: Instagram
+// PROBLEM URL: https://www.codechef.com/problems/INSTAGRAM
+// PROBLEM DIFFICULTY RATTING: 408
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (x > y*10)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
