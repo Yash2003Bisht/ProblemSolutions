69ef9be11aff84deb02ba1e04a38036d13de98ba
yash2003bisht <yash2003bisht@gmail.com>
2023-04-16 22:48:50 +0530

    attempted question TABLETS

---
+++ solutions/codechef/TABLETS/TABLETS_1.c
@@ -0,0 +1,28 @@
+// DATE: 16/04/2023, 10:39:04
+// PROBLEM NAME: Multivitamin Tablets
+// PROBLEM URL: https://www.codechef.com/problems/TABLETS
+// PROBLEM DIFFICULTY RATTING: 376
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+
+        if (y >= x*3)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
