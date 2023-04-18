daf8e2faba538ed681ed7b902fb96d2df7cfbd81
yash2003bisht <yash2003bisht@gmail.com>
2023-04-18 08:31:45 +0530

    attempted question FUNHAND

---
+++ solutions/codechef/FUNHAND/FUNHAND_1.c
@@ -0,0 +1,32 @@
+// DATE: 18/04/2023, 07:25:40
+// PROBLEM NAME: Funny Hand
+// PROBLEM URL: https://www.codechef.com/problems/FUNHAND
+// PROBLEM DIFFICULTY RATTING: 1306
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, n, a, b, ans;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &n, &a, &b);
+
+        if (abs(a-b) <= 2 && a != b){
+            if (a == 1 || b == 1 || a == n || b == n || abs(a-b) == 2)
+                printf("1\n");
+            else
+                printf("2\n");
+        } else
+            printf("0\n");
+
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/FUNHAND/FUNHAND_4.c
@@ -0,0 +1,32 @@
+// DATE: 18/04/2023, 07:19:56
+// PROBLEM NAME: Funny Hand
+// PROBLEM URL: https://www.codechef.com/problems/FUNHAND
+// PROBLEM DIFFICULTY RATTING: 1306
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, n, a, b, ans;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &n, &a, &b);
+        
+        if (abs(a-b) > 2 || (a-b) == 0)
+            printf("0\n");
+        else{
+            if (a == 1 || b == 1 || a == n || b == n || abs(a-b) == 2)
+                printf("1\n");
+            else
+                printf("2\n");
+        }
+    }
+    
+	return 0;
+}
+
+
