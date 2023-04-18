7f783dd58a2ef4e237c050879860308ee873fdc2
yash2003bisht <yash2003bisht@gmail.com>
2023-04-18 08:31:45 +0530

    attempted question PERMCLEAR

---
+++ solutions/codechef/PERMCLEAR/PERMCLEAR_1.c
@@ -0,0 +1,43 @@
+// DATE: 18/04/2023, 08:28:59
+// PROBLEM NAME: Permutation Clear
+// PROBLEM URL: https://www.codechef.com/problems/PERMCLEAR
+// PROBLEM DIFFICULTY RATTING: 1308
+// STATUS: accepted
+// TIME: 0.04
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k, b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int a[n], counter[n];
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a[i]);
+            counter[i] = 0;
+        }
+        
+        scanf("%d", &k);
+        for (int i=0; i<k; i++){
+            scanf("%d", &b);
+            counter[b-1]++;
+        }
+        
+        for (int i=0; i<n; i++){
+            if (counter[a[i]-1] == 0)
+                printf("%d ", a[i]);
+        }
+        
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/PERMCLEAR/PERMCLEAR_2.py
@@ -0,0 +1,18 @@
+# DATE: 18/04/2023, 08:16:34
+# PROBLEM NAME: Permutation Clear
+# PROBLEM URL: https://www.codechef.com/problems/PERMCLEAR
+# PROBLEM DIFFICULTY RATTING: 1308
+# STATUS: accepted
+# TIME: 0.10
+# MEMORY: 25.6M
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    k = int(input())
+    b = set(map(int, input().split()))
+    
+    print(*[i for i in a if i not in b])
+
+
