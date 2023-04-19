065bb5639a03838d729a35c716cd489fc2cb8185
yash2003bisht <yash2003bisht@gmail.com>
2023-04-19 08:59:04 +0530

    attempted question EXPSTP

---
+++ solutions/codechef/EXPSTP/EXPSTP_2.c
@@ -0,0 +1,33 @@
+// DATE: 19/04/2023, 08:54:10
+// PROBLEM NAME: Expensive Steps 
+// PROBLEM URL: https://www.codechef.com/problems/EXPSTP
+// PROBLEM DIFFICULTY RATTING: 1310
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define min(a, b)(a>b?b:a)
+
+int main(void) {
+    int t, n, x1, y1, x2, y2, inside, outside;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d%d", &n, &x1, &y1, &x2, &y2);
+        inside = 0, outside = 0;
+
+        inside = abs(x1-x2) + abs(y1-y2);
+        outside = min(min(x1, (n-x1)+1), min(y1, (n-y1)+1)) + min(min(x2, (n-x2)+1), min(y2, (n-y2)+1));
+        
+        printf("%d\n", min(inside, outside));
+
+    }
+    
+	return 0;
+}
+
+
+
