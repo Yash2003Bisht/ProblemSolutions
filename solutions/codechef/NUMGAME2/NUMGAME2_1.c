15f49e7e13d64a85a1cfa9ae8905d66e2054b7cb
yash2003bisht <yash2003bisht@gmail.com>
2023-04-24 08:41:23 +0530

    attempted question NUMGAME2

---
+++ solutions/codechef/NUMGAME2/NUMGAME2_1.c
@@ -0,0 +1,30 @@
+// DATE: 24/04/2023, 08:38:57
+// PROBLEM NAME: Number Game Revisited
+// PROBLEM URL: https://www.codechef.com/problems/NUMGAME2
+// PROBLEM DIFFICULTY RATTING: 1316
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    long long int t, n, i;
+    
+    scanf("%lld", &t);
+    
+    while(t--){
+        scanf("%lld", &n);
+        
+        if (n%4 == 1)
+            printf("ALICE\n");
+        else
+            printf("BOB\n");
+
+    }
+    
+	return 0;
+}
+
+
+
