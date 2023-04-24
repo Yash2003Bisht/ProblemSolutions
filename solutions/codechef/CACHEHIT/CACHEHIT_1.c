cd74103e46e1a133a90267708dc6d36f12d89cd7
yash2003bisht <yash2003bisht@gmail.com>
2023-04-24 08:39:54 +0530

    attempted question CACHEHIT

---
+++ solutions/codechef/CACHEHIT/CACHEHIT_1.c
@@ -0,0 +1,36 @@
+// DATE: 24/04/2023, 07:48:21
+// PROBLEM NAME: Cache Hits
+// PROBLEM URL: https://www.codechef.com/problems/CACHEHIT
+// PROBLEM DIFFICULTY RATTING: 1320
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n ,b, m, x, count, block, cache_block;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &n, &b, &m);
+        count = 0, block = 0, cache_block = -1;
+
+        for (int i=0; i<m; i++){
+            scanf("%d", &x);
+            block = x/b;
+            if (block != cache_block){
+                count++;
+                cache_block = block;
+            }
+        }
+        
+        printf("%d\n", count);
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CACHEHIT/CACHEHIT_3.py
@@ -0,0 +1,22 @@
+# DATE: 24/04/2023, 07:44:39
+# PROBLEM NAME: Cache Hits
+# PROBLEM URL: https://www.codechef.com/problems/CACHEHIT
+# PROBLEM DIFFICULTY RATTING: 1320
+# STATUS: accepted
+# TIME: 0.08
+# MEMORY: 10.5M
+
+for _ in range(int(input())):
+    n, b, m = map(int, input().split())
+    x = list(map(int, input().split()))
+    cache_block, count = -1, 0
+
+    for i in x:
+        block = i // b
+        if cache_block != block:
+            count += 1
+            cache_block = block
+
+    print(count)
+
+
