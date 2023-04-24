58db894fd7071386a12eb18d5e924f288e0f3c38
yash2003bisht <yash2003bisht@gmail.com>
2023-04-24 08:39:54 +0530

    attempted question NUMGAME

---
+++ solutions/codechef/NUMGAME/NUMGAME_1.c
@@ -0,0 +1,29 @@
+// DATE: 24/04/2023, 08:37:31
+// PROBLEM NAME: Yet Another Number Game
+// PROBLEM URL: https://www.codechef.com/problems/NUMGAME
+// PROBLEM DIFFICULTY RATTING: -1
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    long long int t, n;
+    scanf("%lld", &t);
+    
+    while(t--){
+        scanf("%lld", &n);
+        
+        if (n%2 == 0)
+            printf("ALICE\n");
+        else
+            printf("BOB\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
