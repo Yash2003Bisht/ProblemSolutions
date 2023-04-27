4157f185d62abb3b334e7a95a73a18eb55bd9f50
yash2003bisht <yash2003bisht@gmail.com>
2023-04-27 08:15:49 +0530

    attempted question PINS - manual commit

---
+++ solutions/codechef/PINS/PINS_1.c
@@ -0,0 +1,30 @@
+// DATE: 27/04/2023, 07:32:06
+// PROBLEM NAME: Strike or Spare
+// PROBLEM URL: https://www.codechef.com/problems/PINS
+// PROBLEM DIFFICULTY RATTING: 1320
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        printf("1 1");
+        
+        n = n/2;
+        for (int i=0; i<n; i++)
+            printf("0");
+        
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
