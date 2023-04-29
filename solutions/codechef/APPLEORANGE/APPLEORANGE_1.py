1b415881c4142f9244815d0c06c6738a7334a613
yash2003bisht <yash2003bisht@gmail.com>
2023-04-29 15:14:00 +0530

    attempted question APPLEORANGE

---
+++ solutions/codechef/APPLEORANGE/APPLEORANGE_1.py
@@ -0,0 +1,15 @@
+# DATE: 29/04/2023, 07:41:48
+# PROBLEM NAME: Apples and oranges
+# PROBLEM URL: https://www.codechef.com/problems/APPLEORANGE
+# PROBLEM DIFFICULTY RATTING: 1040
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.7M
+
+import math
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(math.gcd(a, b))
+
+
