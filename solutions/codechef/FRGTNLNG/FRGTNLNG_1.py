41be0909f9c0defce233939a8d94a248a033fa2b
yash2003bisht <yash2003bisht@gmail.com>
2023-05-01 08:20:10 +0530

    attempted question FRGTNLNG

---
+++ solutions/codechef/FRGTNLNG/FRGTNLNG_1.py
@@ -0,0 +1,29 @@
+# DATE: 01/05/2023, 08:19:21
+# PROBLEM NAME: Forgotten Language
+# PROBLEM URL: https://www.codechef.com/problems/FRGTNLNG
+# PROBLEM DIFFICULTY RATTING: 1329
+# STATUS: accepted
+# TIME: 0.05
+# MEMORY: 9.9M
+
+from functools import reduce
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    n_words = input().split()
+    k_words = []
+
+    for __ in range(k):
+        k_words.append(input().split()[1:])
+    
+    k_words = reduce(lambda a, b: a+b, k_words)
+
+    for word in n_words:
+        if word in k_words:
+            print("YES", end=" ")
+        else:
+            print("NO", end=" ")
+    
+    print()
+
+
---
+++ solutions/codechef/FRGTNLNG/FRGTNLNG_2.py
@@ -0,0 +1,27 @@
+# DATE: 01/05/2023, 08:17:38
+# PROBLEM NAME: Forgotten Language
+# PROBLEM URL: https://www.codechef.com/problems/FRGTNLNG
+# PROBLEM DIFFICULTY RATTING: 1329
+# STATUS: accepted
+# TIME: 0.05
+# MEMORY: 9.7M
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    n_words = input().split()
+    k_words = []
+
+    for __ in range(k):
+        k_words.append(input().split()[1:])
+    
+    k_words = sum(k_words, [])
+
+    for word in n_words:
+        if word in k_words:
+            print("YES", end=" ")
+        else:
+            print("NO", end=" ")
+    
+    print()
+
+
