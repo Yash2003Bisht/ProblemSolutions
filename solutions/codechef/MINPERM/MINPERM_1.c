ff3787e71449c4978251b4928cc8782ac38266e2
yash2003bisht <yash2003bisht@gmail.com>
2023-05-02 08:37:26 +0530

    attempted question MINPERM

---
+++ solutions/codechef/MINPERM/MINPERM_1.c
@@ -0,0 +1,36 @@
+// DATE: 02/05/2023, 07:25:38
+// PROBLEM NAME: Minimum Good Permutation
+// PROBLEM URL: https://www.codechef.com/problems/MINPERM
+// PROBLEM DIFFICULTY RATTING: 1315
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        count = n%2 == 0 ? n+1 : n;
+        for (int i=1; i<count; i++){
+            if (i == count-1 && n%2 != 0)
+                printf("%d %d", n, i-1);
+            else if (i%2 == 0)
+                printf("%d ", i-1);
+            else
+                printf("%d ", i+1);
+        }
+        
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MINPERM/MINPERM_3.c
@@ -0,0 +1,44 @@
+// DATE: 02/05/2023, 07:16:05
+// PROBLEM NAME: Minimum Good Permutation
+// PROBLEM URL: https://www.codechef.com/problems/MINPERM
+// PROBLEM DIFFICULTY RATTING: 1315
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n%2 == 0){
+            for (int i=1; i<=n; i++){
+                if (i%2 == 0)
+                    printf("%d ", i-1);
+                else
+                    printf("%d ", i+1);
+            }
+        } else {
+            for (int i=1; i<n; i++){
+                if (i == n-1)
+                    printf("%d %d", n, i-1);
+                else if (i%2 == 0)
+                    printf("%d ", i-1);
+                else
+                    printf("%d ", i+1);
+            }
+        }
+        
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
