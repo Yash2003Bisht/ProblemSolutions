04ff11e2feeec8e7ff65493efadd04bea1416038
yash2003bisht <yash2003bisht@gmail.com>
2023-05-03 07:59:19 +0530

    attempted question ACBALL

---
+++ solutions/codechef/ACBALL/ACBALL_1.c
@@ -0,0 +1,39 @@
+// DATE: 03/05/2023, 07:08:32
+// PROBLEM NAME: Akhil And Colored Balls
+// PROBLEM URL: https://www.codechef.com/problems/ACBALL
+// PROBLEM DIFFICULTY RATTING: 1329
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, i;
+    char x[100001], y[100001], result[100002];
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%s\n%s", x, y);
+        
+        for (i=0; x[i] != '\0'; i++){
+            if (x[i] == y[i]){
+                if (x[i] == 'W')
+                    result[i] = 'B';
+                else
+                    result[i] = 'W';
+            } else{
+                result[i] = 'B';
+            }
+        }
+        
+        result[i] = '\0';
+        printf("%s\n", result);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/ACBALL/ACBALL_3.c
@@ -0,0 +1,38 @@
+// DATE: 03/05/2023, 07:04:31
+// PROBLEM NAME: Akhil And Colored Balls
+// PROBLEM URL: https://www.codechef.com/problems/ACBALL
+// PROBLEM DIFFICULTY RATTING: 1329
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    char x[100001], y[100000];
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%s\n%s", x, y);
+        
+        for (int i=0; x[i] != '\0'; i++){
+            if (x[i] == y[i]){
+                if (x[i] == 'W')
+                    printf("B");
+                else
+                    printf("W");
+            } else{
+                printf("B");
+            }
+        }
+        
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
