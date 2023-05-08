5f6998fc653da854438e3947a4deca3723f7112e
yash2003bisht <yash2003bisht@gmail.com>
2023-05-08 17:49:13 +0530

    attempted question TOP10

---
+++ solutions/codechef/TOP10/TOP10_1.c
@@ -0,0 +1,29 @@
+// DATE: 08/05/2023, 05:23:17
+// PROBLEM NAME: Masterchef finals
+// PROBLEM URL: https://www.codechef.com/problems/TOP10
+// PROBLEM DIFFICULTY RATTING: 255
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        
+        if (x > 10)
+            printf("NO\n");
+        else
+            printf("YES\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
