2f7e6f0ab54e783326f26b722d707fc47dbba556
yash2003bisht <yash2003bisht@gmail.com>
2023-05-08 17:49:13 +0530

    attempted question CCLEARN

---
+++ solutions/codechef/CCLEARN/CCLEARN_1.c
@@ -0,0 +1,19 @@
+// DATE: 08/05/2023, 05:26:09
+// PROBLEM NAME: CodeChef Learn Problem Solving
+// PROBLEM URL: https://www.codechef.com/problems/CCLEARN
+// PROBLEM DIFFICULTY RATTING: 287
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n*2);
+	return 0;
+}
+
+
+
