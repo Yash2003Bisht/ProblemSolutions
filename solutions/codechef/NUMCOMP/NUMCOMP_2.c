ce49d8858ed0330f9b976fda9f823b952adc44f7
yash2003bisht <yash2003bisht@gmail.com>
2023-05-10 07:55:30 +0530

    attempted question NUMCOMP

---
+++ solutions/codechef/NUMCOMP/NUMCOMP_2.c
@@ -0,0 +1,35 @@
+// DATE: 10/05/2023, 07:51:30
+// PROBLEM NAME: Archi and Comparsion
+// PROBLEM URL: https://www.codechef.com/problems/NUMCOMP
+// PROBLEM DIFFICULTY RATTING: 1320
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t;
+    long long a, b, n;
+
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld%lld", &a, &b, &n);
+        a = n%2 == 0 ? abs(a) : a;
+        b = n%2 == 0 ? abs(b) : b;
+
+        if (a > b)
+            printf("1\n");
+        else if (a < b)
+            printf("2\n");
+        else
+            printf("0\n");
+
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/NUMCOMP/NUMCOMP_3.c
@@ -0,0 +1,43 @@
+// DATE: 10/05/2023, 07:49:46
+// PROBLEM NAME: Archi and Comparsion
+// PROBLEM URL: https://www.codechef.com/problems/NUMCOMP
+// PROBLEM DIFFICULTY RATTING: 1320
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t;
+    long long a, b, n;
+
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld%lld", &a, &b, &n);
+
+        if (n%2 == 0){
+            if (abs(a) > abs(b))
+                printf("1\n");
+            else if (abs(a) < abs(b))
+                printf("2\n");
+            else
+                printf("0\n");
+        } else {
+            if (a > b)
+                printf("1\n");
+            else if (a < b)
+                printf("2\n");
+            else
+                printf("0\n");
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
