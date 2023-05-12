73e49703162220f27387b6cc009c7102ceacd276
yash2003bisht <yash2003bisht@gmail.com>
2023-05-12 13:58:54 +0530

    attempted question XORPAL

---
+++ solutions/codechef/XORPAL/XORPAL_1.c
@@ -0,0 +1,38 @@
+// DATE: 12/05/2023, 08:31:59
+// PROBLEM NAME: Xor Palindrome
+// PROBLEM URL: https://www.codechef.com/problems/XORPAL
+// PROBLEM DIFFICULTY RATTING: 1339
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, O, I;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        scanf("%s", s);
+        I = 0, O = 0;
+
+        for (int i=0; i<n; i++){
+            if (s[i] == '1')
+                I++;
+            else
+                O++;
+        }
+
+        if (I%2 == 0 || O%2 == 0 || I == O)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/XORPAL/XORPAL_3.c
@@ -0,0 +1,38 @@
+// DATE: 12/05/2023, 08:24:03
+// PROBLEM NAME: Xor Palindrome
+// PROBLEM URL: https://www.codechef.com/problems/XORPAL
+// PROBLEM DIFFICULTY RATTING: 1339
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, count_1, count_0;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        scanf("%s", s);
+        count_1 = 0, count_0 = 0;
+
+        for (int i=0; i<n; i++){
+            if (s[i] == '1')
+                count_1++;
+            else
+                count_0++;
+        }
+        
+        if (count_1%2 == 0 || count_0%2 == 0 || count_1 == count_0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
