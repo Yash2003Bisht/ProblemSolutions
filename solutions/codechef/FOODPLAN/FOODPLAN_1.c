990be26ef20a5f5be130979b5572029437fc9af3
yash2003bisht <yash2003bisht@gmail.com>
2023-05-12 18:05:18 +0530

    attempted question FOODPLAN

---
+++ solutions/codechef/FOODPLAN/FOODPLAN_1.c
@@ -0,0 +1,31 @@
+// DATE: 12/05/2023, 06:04:47
+// PROBLEM NAME: Online or Offline
+// PROBLEM URL: https://www.codechef.com/problems/FOODPLAN
+// PROBLEM DIFFICULTY RATTING: 713
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, m;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &m);
+        
+        if (n - n*0.1 < m)
+            printf("ONLINE\n");
+        else if (n - n*0.1 > m)
+            printf("DINING\n");
+        else
+            printf("EITHER\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
