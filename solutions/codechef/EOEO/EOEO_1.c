62f0e8c2dc261ead6f8d03ad41bd9e7dc3d980be
yash2003bisht <yash2003bisht@gmail.com>
2023-05-12 13:58:54 +0530

    attempted question EOEO

---
+++ solutions/codechef/EOEO/EOEO_1.c
@@ -0,0 +1,27 @@
+// DATE: 12/05/2023, 07:47:21
+// PROBLEM NAME: The Tom and Jerry Game!
+// PROBLEM URL: https://www.codechef.com/problems/EOEO
+// PROBLEM DIFFICULTY RATTING: 1337
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long ts;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%lld", &ts);
+        while(ts%2 == 0)
+            ts /= 2;
+        printf("%lld\n", ts/2);
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/EOEO/EOEO_2.c
@@ -0,0 +1,36 @@
+// DATE: 12/05/2023, 07:46:47
+// PROBLEM NAME: The Tom and Jerry Game!
+// PROBLEM URL: https://www.codechef.com/problems/EOEO
+// PROBLEM DIFFICULTY RATTING: 1337
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long ts;
+    
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld", &ts);
+        
+        if (ts <= 2)
+            printf("0\n");
+        else if (ts%2 != 0)
+            printf("%lld\n", ts/2);
+        else{
+            while(ts%2 == 0)
+                ts /= 2;
+            printf("%lld\n", ts/2);
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
