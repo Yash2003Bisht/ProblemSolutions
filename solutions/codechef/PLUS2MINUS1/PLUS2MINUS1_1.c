9a6d0e204d60c32af01ca484b4b6263f968a140e
yash2003bisht <yash2003bisht@gmail.com>
2023-05-13 08:23:23 +0530

    attempted question PLUS2MINUS1

---
+++ solutions/codechef/PLUS2MINUS1/PLUS2MINUS1_1.c
@@ -0,0 +1,28 @@
+// DATE: 13/05/2023, 07:24:28
+// PROBLEM NAME: Plus 2 or Minus 1
+// PROBLEM URL: https://www.codechef.com/problems/PLUS2MINUS1
+// PROBLEM DIFFICULTY RATTING: 1098
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &y);
+        
+        if (y == 0)
+            printf("1\n");
+        else
+            printf("%d\n", y*3);
+    }
+    
+	return 0;
+}
+
+
+
