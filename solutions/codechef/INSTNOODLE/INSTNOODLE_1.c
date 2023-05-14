7c998af2ee1caa83c93d8ee09f407c8b9079a541
yash2003bisht <yash2003bisht@gmail.com>
2023-05-14 21:52:26 +0530

    attempted question INSTNOODLE

---
+++ solutions/codechef/INSTNOODLE/INSTNOODLE_1.c
@@ -0,0 +1,19 @@
+// DATE: 14/05/2023, 09:51:25
+// PROBLEM NAME: Chef and Instant Noodles
+// PROBLEM URL: https://www.codechef.com/problems/INSTNOODLE
+// PROBLEM DIFFICULTY RATTING: 456
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+    scanf("%d%d", &x, &y);
+    printf("%d\n", x*y);
+	return 0;
+}
+
+
+
