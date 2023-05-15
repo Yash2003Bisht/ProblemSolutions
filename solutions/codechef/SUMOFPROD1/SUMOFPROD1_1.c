bc5e087a63ba43051cd7dffdda9929d0988edf27
yash2003bisht <yash2003bisht@gmail.com>
2023-05-15 08:53:01 +0530

    attempted question SUMOFPROD1

---
+++ solutions/codechef/SUMOFPROD1/SUMOFPROD1_1.c
@@ -0,0 +1,39 @@
+// DATE: 15/05/2023, 07:32:30
+// PROBLEM NAME: Sum of Product 1
+// PROBLEM URL: https://www.codechef.com/problems/SUMOFPROD1
+// PROBLEM DIFFICULTY RATTING: 1340
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long int count = 0, ans = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            if (a == 1)
+                count++;
+            else{
+                ans += count*(count+1)/2;
+                count = 0;
+            }
+        }
+        
+        ans += count*(count+1)/2;
+
+        printf("%ld\n", ans);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/SUMOFPROD1/SUMOFPROD1_2.c
@@ -0,0 +1,42 @@
+// DATE: 15/05/2023, 07:31:34
+// PROBLEM NAME: Sum of Product 1
+// PROBLEM URL: https://www.codechef.com/problems/SUMOFPROD1
+// PROBLEM DIFFICULTY RATTING: 1340
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int arr[n];
+        long int count = 0, ans = 0;
+
+        for (int i=0; i<n; i++)
+            scanf("%d", &arr[i]);
+        
+        for (int i=0; i<n; i++){
+            if (arr[i] == 1)
+                count++;
+            else{
+                ans += count*(count+1)/2;
+                count = 0;
+            }
+        }
+        
+        ans += count*(count+1)/2;
+
+        printf("%ld\n", ans);
+        
+    }
+    
+	return 0;
+}
+
+
+
