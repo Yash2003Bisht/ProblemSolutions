2b7d577e8e8f8d0dbe3287732c1a988fb2f7be60
yash2003bisht <yash2003bisht@gmail.com>
2023-05-16 21:36:48 +0530

    attempted question PRACTICEPERF

---
+++ solutions/codechef/PRACTICEPERF/PRACTICEPERF_1.c
@@ -0,0 +1,30 @@
+// DATE: 16/05/2023, 09:31:49
+// PROBLEM NAME: Practice makes us perfect
+// PROBLEM URL: https://www.codechef.com/problems/PRACTICEPERF
+// PROBLEM DIFFICULTY RATTING: 467
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int p1, p2, p3, p4, count = 0;
+    scanf("%d%d%d%d", &p1, &p2, &p3, &p4);
+    
+    if (p1 >= 10)
+        count++;
+    if (p2 >= 10)
+        count++;
+    if (p3 >= 10)
+        count++;
+    if (p4 >= 10)
+        count++;
+        
+    printf("%d\n", count);
+    
+	return 0;
+}
+
+
+
