985678687cb0ff1a73205fdc02d194d5de6eb3e5
yash2003bisht <yash2003bisht@gmail.com>
2023-05-16 21:36:47 +0530

    attempted question COURSEREG

---
+++ solutions/codechef/COURSEREG/COURSEREG_1.c
@@ -0,0 +1,27 @@
+// DATE: 16/05/2023, 09:34:02
+// PROBLEM NAME: Course Registration
+// PROBLEM URL: https://www.codechef.com/problems/COURSEREG
+// PROBLEM DIFFICULTY RATTING: 470
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, m, k;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &m, &k);
+        if (n+k <= m)
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+    
+	return 0;
+}
+
+
+
