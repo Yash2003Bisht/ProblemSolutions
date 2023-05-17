0f7992c23ff80727312bdce76eb71efa08e5e95d
yash2003bisht <yash2003bisht@gmail.com>
2023-05-17 08:58:24 +0530

    attempted question CHCHCL

---
+++ solutions/codechef/CHCHCL/CHCHCL_1.c
@@ -0,0 +1,30 @@
+// DATE: 17/05/2023, 07:49:52
+// PROBLEM NAME: Chef and Chocolate
+// PROBLEM URL: https://www.codechef.com/problems/CHCHCL
+// PROBLEM DIFFICULTY RATTING: 1343
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int n, m;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &m);
+        
+        if (n*m % 2 == 0)
+            printf("Yes\n");
+        else
+            printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CHCHCL/CHCHCL_2.c
@@ -0,0 +1,30 @@
+// DATE: 17/05/2023, 07:18:07
+// PROBLEM NAME: Chef and Chocolate
+// PROBLEM URL: https://www.codechef.com/problems/CHCHCL
+// PROBLEM DIFFICULTY RATTING: 1343
+// STATUS: partially accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int n, m;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &m);
+        
+        if ((n%2 != 0 && m%2 == 0) || (n%2 == 0 && m%2 != 0))
+            printf("Yes\n");
+        else
+            printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
