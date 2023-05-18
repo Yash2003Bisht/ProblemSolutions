0cdbd8027df277a6b71d75fac4d5f4ddbf7913f3
yash2003bisht <yash2003bisht@gmail.com>
2023-05-18 09:03:36 +0530

    attempted question LOSTSEQ

---
+++ solutions/codechef/LOSTSEQ/LOSTSEQ_1.c
@@ -0,0 +1,36 @@
+// DATE: 18/05/2023, 08:02:49
+// PROBLEM NAME: Careless Chef
+// PROBLEM URL: https://www.codechef.com/problems/LOSTSEQ
+// PROBLEM DIFFICULTY RATTING: 1345
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, b;
+    long long int total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        total = 0;
+
+        for (int i=0; i<n*2; i++){
+            scanf("%d", &b);
+            total += b;
+        }
+        
+        if (total%2 == 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
