10c1c3b0ab53786dd37b03936d4d2f72e296f748
yash2003bisht <yash2003bisht@gmail.com>
2023-05-18 09:03:36 +0530

    attempted question APPROX

---
+++ solutions/codechef/APPROX/APPROX_1.c
@@ -0,0 +1,45 @@
+// DATE: 18/05/2023, 09:02:32
+// PROBLEM NAME: Approximately
+// PROBLEM URL: https://www.codechef.com/problems/APPROX
+// PROBLEM DIFFICULTY RATTING: 1310
+// STATUS: accepted
+// TIME: 0.06
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+inline void scanint(long long *x) { 
+	register char c = getchar_unlocked(); 
+	*x = 0; 
+	for(; (c<48)||(c>57);
+	    c = getchar_unlocked()); 
+	for(; (c>47)&&(c<58); c = getchar_unlocked())
+		*x = (int)((((*x)<<1) + ((*x)<<3)) + c - 48); 
+}
+
+int main(void) {
+    int t, k, n, d;
+    scanint(&t);
+    
+    while(t--){
+        scanint(&k);
+        n = 103993, d = 33102;
+        printf("3");
+
+        if (k != 0){
+            printf(".");
+            while(k--){
+                n = (n%d)*10;
+                printf("%d", n/d);
+            }
+        }
+
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/APPROX/APPROX_3.c
@@ -0,0 +1,36 @@
+// DATE: 18/05/2023, 08:54:23
+// PROBLEM NAME: Approximately
+// PROBLEM URL: https://www.codechef.com/problems/APPROX
+// PROBLEM DIFFICULTY RATTING: 1310
+// STATUS: accepted
+// TIME: 0.06
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, k, n, d;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &k);
+        n = 103993, d = 33102;
+        
+        if (k == 0)
+            printf("3\n");
+        else{
+            printf("3.");
+            while(k--){
+                n = (n%d)*10;
+                printf("%d", n/d);
+            }
+            printf("\n");
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
