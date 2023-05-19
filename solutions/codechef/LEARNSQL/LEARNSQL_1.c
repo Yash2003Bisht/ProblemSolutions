93a57b0230db671cdd945b1be7c5305cdd9720d4
yash2003bisht <yash2003bisht@gmail.com>
2023-05-19 22:16:12 +0530

    attempted question LEARNSQL

---
+++ solutions/codechef/LEARNSQL/LEARNSQL_1.c
@@ -0,0 +1,19 @@
+// DATE: 19/05/2023, 10:13:13
+// PROBLEM NAME: Learning SQL
+// PROBLEM URL: https://www.codechef.com/problems/LEARNSQL
+// PROBLEM DIFFICULTY RATTING: 339
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int r, c, e;
+    scanf("%d%d%d", &r, &c, &e);
+    printf("%d\n", (r+e)*c);
+	return 0;
+}
+
+
+
