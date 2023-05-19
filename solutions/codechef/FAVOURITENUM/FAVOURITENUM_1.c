56a196f2a3b0a365e8f37531554d980aa6edb548
yash2003bisht <yash2003bisht@gmail.com>
2023-05-19 22:16:12 +0530

    attempted question FAVOURITENUM

---
+++ solutions/codechef/FAVOURITENUM/FAVOURITENUM_1.c
@@ -0,0 +1,31 @@
+// DATE: 19/05/2023, 10:15:38
+// PROBLEM NAME: Favourite Numbers
+// PROBLEM URL: https://www.codechef.com/problems/FAVOURITENUM
+// PROBLEM DIFFICULTY RATTING: 477
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &a);
+
+        if (a%2 == 0 && a%7 == 0)
+            printf("Alice\n");
+        else if (a%2 != 0 && a%9 == 0)
+            printf("Bob\n");
+        else
+            printf("Charlie\n");
+
+    }
+    
+	return 0;
+}
+
+
+
