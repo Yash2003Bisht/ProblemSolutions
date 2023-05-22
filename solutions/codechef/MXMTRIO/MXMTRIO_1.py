bdd85d60482c345ce7a2e31a70c44343dbbd8e17
yash2003bisht <yash2003bisht@gmail.com>
2023-05-22 08:44:28 +0530

    attempted question MXMTRIO

---
+++ solutions/codechef/MXMTRIO/MXMTRIO_1.py
@@ -0,0 +1,16 @@
+# DATE: 22/05/2023, 08:42:10
+# PROBLEM NAME: Maximum Trio
+# PROBLEM URL: https://www.codechef.com/problems/MXMTRIO
+# PROBLEM DIFFICULTY RATTING: 1349
+# STATUS: accepted
+# TIME: 0.14
+# MEMORY: 41.8M
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+
+    print((arr[n-1] - arr[0]) * arr[n-2])
+
+
