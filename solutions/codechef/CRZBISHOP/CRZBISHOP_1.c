3dbe2913ee0c033b5caaed3a7c8776744aaba2fd
yash2003bisht <yash2003bisht@gmail.com>
2023-05-23 08:18:56 +0530

    attempted question CRZBISHOP

---
+++ solutions/codechef/CRZBISHOP/CRZBISHOP_1.c
@@ -0,0 +1,30 @@
+// DATE: 23/05/2023, 08:17:15
+// PROBLEM NAME: Crazy Bishops on Chessboard
+// PROBLEM URL: https://www.codechef.com/problems/CRZBISHOP
+// PROBLEM DIFFICULTY RATTING: 1351
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, ans, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n <= 2)
+            printf("0\n");
+        else{
+            ans = n*2-(n/2)-3;
+            printf("%d\n", ans);
+        }
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CRZBISHOP/CRZBISHOP_2.c
@@ -0,0 +1,31 @@
+// DATE: 23/05/2023, 08:12:56
+// PROBLEM NAME: Crazy Bishops on Chessboard
+// PROBLEM URL: https://www.codechef.com/problems/CRZBISHOP
+// PROBLEM DIFFICULTY RATTING: 1351
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, ans, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n <= 2)
+            printf("0\n");
+        else{
+            temp = (n-2)/2 + (n-2)%2;
+            ans = n%2 == 0 ? temp*2 + temp : temp*2 + temp - 1;
+            printf("%d\n", ans);
+        }
+    }
+    
+	return 0;
+}
+
+
+
