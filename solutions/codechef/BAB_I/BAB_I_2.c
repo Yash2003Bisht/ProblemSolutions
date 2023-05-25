6d205894d47ded8d4fffc83e4190092b03e37d85
yash2003bisht <yash2003bisht@gmail.com>
2023-05-25 08:15:09 +0530

    attempted question BAB_I

---
+++ solutions/codechef/BAB_I/BAB_I_2.c
@@ -0,0 +1,36 @@
+// DATE: 25/05/2023, 08:10:53
+// PROBLEM NAME: Chef And Babla
+// PROBLEM URL: https://www.codechef.com/problems/BAB_I
+// PROBLEM DIFFICULTY RATTING: 1346
+// STATUS: wrong answer
+// TIME: 0.02
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(void) {
+    int t, n, a, min_value;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        min_value = INT_MAX;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            if (abs(a) < min_value)
+                min_value = a;
+        }
+
+        printf("%d\n", min_value - 1);
+
+    }
+    
+	return 0;
+}
+
+
+
