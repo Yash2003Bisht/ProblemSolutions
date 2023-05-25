31e9ac07396e42363b0b90c3366c5dd0fe1f9a41
yash2003bisht <yash2003bisht@gmail.com>
2023-05-25 08:15:09 +0530

    attempted question MAGDOORS

---
+++ solutions/codechef/MAGDOORS/MAGDOORS_2.c
@@ -0,0 +1,35 @@
+// DATE: 25/05/2023, 07:15:50
+// PROBLEM NAME: Magical Doors
+// PROBLEM URL: https://www.codechef.com/problems/MAGDOORS
+// PROBLEM DIFFICULTY RATTING: 1355
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, ans;
+    scanf("%d", &t);
+    
+    while(t--){
+        char s[100000], crr_match = '0';
+        ans = 0;
+        scanf("%s", &s);
+        
+        for (int i=0; s[i] != '\0'; i++){
+            if (s[i] == crr_match){
+                crr_match = crr_match == '0' ? '1' : '0';
+                ans++;
+            }
+        }
+        
+        printf("%d\n", ans);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MAGDOORS/MAGDOORS_4.c
@@ -0,0 +1,37 @@
+// DATE: 25/05/2023, 07:14:26
+// PROBLEM NAME: Magical Doors
+// PROBLEM URL: https://www.codechef.com/problems/MAGDOORS
+// PROBLEM DIFFICULTY RATTING: 1355
+// STATUS: time limit exceeded
+// TIME: -
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, ans;
+    scanf("%d", &t);
+    
+    while(t--){
+        char s[100000];
+        ans = 0;
+        scanf("%s", &s);
+        
+        for (int i=0; s[i] != '\0'; i++){
+            if (s[i] == '0'){
+                for (int j=i+1; s[j] != '\0'; j++){
+                    s[j] = s[j] == '0' ? '1' : '0';
+                }
+                ans++;
+            }
+        }
+        
+        printf("%d\n", ans);
+        
+    }
+    
+	return 0;
+}
+
+
+
