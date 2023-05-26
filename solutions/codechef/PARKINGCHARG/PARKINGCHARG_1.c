1bacaabc96d7cc2aa310c285b37c7f024c5123bf
yash2003bisht <yash2003bisht@gmail.com>
2023-05-26 17:32:49 +0530

    attempted question PARKINGCHARG

---
+++ solutions/codechef/PARKINGCHARG/PARKINGCHARG_1.c
@@ -0,0 +1,19 @@
+// DATE: 26/05/2023, 08:44:06
+// PROBLEM NAME: Parking Charges
+// PROBLEM URL: https://www.codechef.com/problems/PARKINGCHARG
+// PROBLEM DIFFICULTY RATTING: 416
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int x, h, y;
+    scanf("%d%d%d", &x, &y, &h);
+    printf("%d", (y*(h-1))+x);
+	return 0;
+}
+
+
+
