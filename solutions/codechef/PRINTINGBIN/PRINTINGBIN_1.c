aa300cdf24e5f45346ddb2be6f37f5af86b12511
yash2003bisht <yash2003bisht@gmail.com>
2023-05-29 07:46:02 +0530

    attempted question PRINTINGBIN

---
+++ solutions/codechef/PRINTINGBIN/PRINTINGBIN_1.c
@@ -0,0 +1,37 @@
+// DATE: 29/05/2023, 07:19:55
+// PROBLEM NAME: Printing Binary Array
+// PROBLEM URL: https://www.codechef.com/problems/PRINTINGBIN
+// PROBLEM DIFFICULTY RATTING: 1130
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+
+            if (a == 0)
+                printf("1 ");
+            else
+                printf("0 ");
+
+        }
+
+        printf("\n");
+
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/PRINTINGBIN/PRINTINGBIN_2.c
@@ -0,0 +1,38 @@
+// DATE: 29/05/2023, 07:18:47
+// PROBLEM NAME: Printing Binary Array
+// PROBLEM URL: https://www.codechef.com/problems/PRINTINGBIN
+// PROBLEM DIFFICULTY RATTING: 1130
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        int a[n];
+
+        for (int i=0; i<n; i++)
+            scanf("%d", &a[i]);
+
+        for (int i=0; i<n; i++){
+            if (a[i] == 0)
+                printf("1 ");
+            else
+                printf("0 ");
+        }
+
+        printf("\n");
+
+
+    }
+    
+	return 0;
+}
+
+
+
