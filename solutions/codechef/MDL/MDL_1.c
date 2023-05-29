f13b6975c1190134745e586e4be43a0fae450445
yash2003bisht <yash2003bisht@gmail.com>
2023-05-29 07:46:02 +0530

    attempted question MDL

---
+++ solutions/codechef/MDL/MDL_1.c
@@ -0,0 +1,54 @@
+// DATE: 29/05/2023, 07:42:04
+// PROBLEM NAME: Medel
+// PROBLEM URL: https://www.codechef.com/problems/MDL
+// PROBLEM DIFFICULTY RATTING: 1139
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#include <limits.h>
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, n, minimum, maximum, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d",  &n);
+        int a[n];
+
+        for (int i=0; i<n; i++)
+            scanf("%d", &a[i]);
+        
+        minimum = INT_MAX, maximum = a[0], count = 0;
+
+        for (int i=1; i<n; i++){
+            if (a[i] > maximum){
+                minimum = min(minimum, maximum);
+                maximum = a[i];
+            }
+            else if (minimum > a[i])
+                minimum = a[i];
+        }
+        
+        for (int i=0; i<n; i++){
+            if (minimum == a[i])
+                printf("%d ", minimum);
+            else if (maximum == a[i])
+                printf("%d ", maximum);
+            
+            if (count == 2)
+                break;
+            
+        }
+
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MDL/MDL_3.py
@@ -0,0 +1,21 @@
+# DATE: 29/05/2023, 07:34:29
+# PROBLEM NAME: Medel
+# PROBLEM URL: https://www.codechef.com/problems/MDL
+# PROBLEM DIFFICULTY RATTING: 1139
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    i = 0
+    
+    while len(arr) != 2:
+        sub_arr = arr[i:i+3]
+        sub_arr.sort()
+        arr.remove(sub_arr[1])    
+    
+    print(arr[0], arr[1])
+
+
