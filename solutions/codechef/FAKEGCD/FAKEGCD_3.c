ad0f7846ebfb398c81967d3763c1f780085901a4
yash2003bisht <yash2003bisht@gmail.com>
2023-05-31 08:07:29 +0530

    attempted question FAKEGCD

---
+++ solutions/codechef/FAKEGCD/FAKEGCD_3.c
@@ -0,0 +1,26 @@
+// DATE: 31/05/2023, 07:22:33
+// PROBLEM NAME: Fake GCD
+// PROBLEM URL: https://www.codechef.com/problems/FAKEGCD
+// PROBLEM DIFFICULTY RATTING: 1362
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        for (int i=1; i<=n; i++)
+            printf("%d ", i);
+        printf("\n");
+    }
+
+	return 0;
+}
+
+
+
