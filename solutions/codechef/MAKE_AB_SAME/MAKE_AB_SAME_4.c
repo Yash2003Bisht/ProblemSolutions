6943a31b8d3c296f163229b4acef4c792fce42de
yash2003bisht <yash2003bisht@gmail.com>
2023-05-31 08:07:29 +0530

    attempted question MAKE_AB_SAME

---
+++ solutions/codechef/MAKE_AB_SAME/MAKE_AB_SAME_4.c
@@ -0,0 +1,54 @@
+// DATE: 31/05/2023, 08:02:29
+// PROBLEM NAME: Make A-B Same
+// PROBLEM URL: https://www.codechef.com/problems/MAKE_AB_SAME
+// PROBLEM DIFFICULTY RATTING: 1335
+// STATUS: accepted
+// TIME: 0.06
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, count_0, count_1, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char a[n+1], b[n+1];
+        count_0 = 0, count_1 = 0, flag = 1;
+
+        scanf("\n");
+
+        for (int i=0; i<n*2; i++){
+            if (i < n)
+                scanf("%c ", &a[i]);
+            else
+                scanf("%c ", &b[i-n]);
+        }
+        
+        for (int i=0; i<n; i++){
+            if (a[i] == '1')
+                count_1++;
+        }
+        
+        for (int i=0; i<n; i++){
+            if (a[i] != b[i]){
+                if (b[i] == '0' || count_1 == 0 || i == 0 || i == n-1){
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        
+        if (flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
