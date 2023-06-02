2236a8f38b48e16c8b5ff41c8e18701986466f43
yash2003bisht <yash2003bisht@gmail.com>
2023-06-02 07:38:02 +0530

    attempted question NOPAL

---
+++ solutions/codechef/NOPAL/NOPAL_1.c
@@ -0,0 +1,34 @@
+// DATE: 02/06/2023, 07:24:32
+// PROBLEM NAME: Palindromes Not Allowed
+// PROBLEM URL: https://www.codechef.com/problems/NOPAL
+// PROBLEM DIFFICULTY RATTING: 1360
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char alphabets = 'a';
+
+        while(n--){
+            printf("%c", alphabets++);
+            if (alphabets > 122)
+                alphabets = 'a';
+   
+        }
+
+        printf("\n");
+
+    }
+
+	return 0;
+}
+
+
+
