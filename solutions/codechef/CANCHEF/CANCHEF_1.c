62460f0a79a469038f73bf88084ce0addc3c35bc
yash2003bisht <yash2003bisht@gmail.com>
2023-06-03 09:18:13 +0530

    attempted question CANCHEF

---
+++ solutions/codechef/CANCHEF/CANCHEF_1.c
@@ -0,0 +1,29 @@
+// DATE: 03/06/2023, 09:16:25
+// PROBLEM NAME: Can Chef 
+// PROBLEM URL: https://www.codechef.com/problems/CANCHEF
+// PROBLEM DIFFICULTY RATTING: 471
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (x*15 >= y*2)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CANCHEF/CANCHEF_2.c
@@ -0,0 +1,29 @@
+// DATE: 03/06/2023, 09:14:48
+// PROBLEM NAME: Can Chef 
+// PROBLEM URL: https://www.codechef.com/problems/CANCHEF
+// PROBLEM DIFFICULTY RATTING: 471
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        
+        if (y*2 / 15 >= x)
+            printf("NO\n");
+        else
+            printf("YES\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
