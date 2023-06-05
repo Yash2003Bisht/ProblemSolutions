65309b4949b31acc7cc7247d65ce0050057c4b6a
yash2003bisht <yash2003bisht@gmail.com>
2023-06-05 08:26:04 +0530

    attempted question NOTDIVISIBLE

---
+++ solutions/codechef/NOTDIVISIBLE/NOTDIVISIBLE_1.c
@@ -0,0 +1,33 @@
+// DATE: 05/06/2023, 07:51:01
+// PROBLEM NAME: Not Divisible
+// PROBLEM URL: https://www.codechef.com/problems/NOTDIVISIBLE
+// PROBLEM DIFFICULTY RATTING: 1363
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        for (int i=0; i<n; i++){
+            if (i%2 == 0)
+                printf("1 ");
+            else
+                printf("0 ");
+        }
+
+        printf("\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
