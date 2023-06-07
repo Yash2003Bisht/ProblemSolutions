b71e3602d76adce3f4b11bb48b107385c7dfe4de
yash2003bisht <yash2003bisht@gmail.com>
2023-06-07 08:07:00 +0530

    attempted question WATMELON

---
+++ solutions/codechef/WATMELON/WATMELON_1.c
@@ -0,0 +1,35 @@
+// DATE: 07/06/2023, 07:23:35
+// PROBLEM NAME: Watermelon
+// PROBLEM URL: https://www.codechef.com/problems/WATMELON
+// PROBLEM DIFFICULTY RATTING: 1365
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        total = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            total += a;
+        }
+
+        if (total >= 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/WATMELON/WATMELON_2.c
@@ -0,0 +1,38 @@
+// DATE: 07/06/2023, 07:15:19
+// PROBLEM NAME: Watermelon
+// PROBLEM URL: https://www.codechef.com/problems/WATMELON
+// PROBLEM DIFFICULTY RATTING: 1365
+// STATUS: partially accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        flag = 1;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+
+            if (flag && (a%(i+1) != 0 || a < 0))
+                flag = 0;
+
+        }
+
+        if (flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
