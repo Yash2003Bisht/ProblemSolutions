80b35c4a3128d409a8c9edbcadc3c3d2047ce472
yash2003bisht <yash2003bisht@gmail.com>
2023-06-12 16:45:45 +0530

    attempted question CHEFCHOCO

---
+++ solutions/codechef/CHEFCHOCO/CHEFCHOCO_1.c
@@ -0,0 +1,24 @@
+// DATE: 12/06/2023, 04:41:07
+// PROBLEM NAME: Chef and Chocolates
+// PROBLEM URL: https://www.codechef.com/problems/CHEFCHOCO
+// PROBLEM DIFFICULTY RATTING: 492
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    // ================= TESTING SUBMISSION =================
+    int i,t,c,x,y;
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+	{
+	    scanf("%d %d %d",&c,&x,&y);
+	    printf("%d\n",(c-x)*y);
+	}
+	return 0;
+}
+
+
+
