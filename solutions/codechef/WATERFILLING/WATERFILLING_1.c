b8171c6e695eb1d9e23e9577af9e84b4941841fb
yash2003bisht <yash2003bisht@gmail.com>
2023-06-12 15:27:52 +0530

    attempted question WATERFILLING

---
+++ solutions/codechef/WATERFILLING/WATERFILLING_1.c
@@ -0,0 +1,29 @@
+// DATE: 12/06/2023, 08:42:05
+// PROBLEM NAME: Water Filling
+// PROBLEM URL: https://www.codechef.com/problems/WATERFILLING
+// PROBLEM DIFFICULTY RATTING: 541
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, b1, b2, b3;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &b1, &b2, &b3);
+        
+        if (b1 + b2 + b3 <= 1)
+            printf("Water filling time\n");
+        else
+            printf("Not now\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
