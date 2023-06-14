04cfb596b8edd974cc3f72c53b7e3a180ce9c4bd
yash2003bisht <yash2003bisht@gmail.com>
2023-06-14 07:56:04 +0530

    attempted question LCM_GCD

---
+++ solutions/codechef/LCM_GCD/LCM_GCD_1.c
@@ -0,0 +1,34 @@
+// DATE: 14/06/2023, 07:26:18
+// PROBLEM NAME: Lcm hates Gcd
+// PROBLEM URL: https://www.codechef.com/problems/LCM_GCD
+// PROBLEM DIFFICULTY RATTING: 1369
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+long long int gcd(long long int a, long long int b){
+    if (b == 0)
+        return a;
+    
+    return gcd(b, a%b);
+    
+}
+
+
+int main(void) {
+    int t;
+    long long int a, b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &a, &b);
+        printf("%lld\n", a - gcd(a, b));
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/LCM_GCD/LCM_GCD_3.c
@@ -0,0 +1,39 @@
+// DATE: 14/06/2023, 07:24:07
+// PROBLEM NAME: Lcm hates Gcd
+// PROBLEM URL: https://www.codechef.com/problems/LCM_GCD
+// PROBLEM DIFFICULTY RATTING: 1369
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+long long int gcd(long long int a, long long int b){
+    if (b == 0)
+        return a;
+    
+    return gcd(b, a%b);
+    
+}
+
+long long int lcm(long long int a, long long int b){
+    return a*b/gcd(a, b);
+}
+
+
+int main(void) {
+    int t;
+    long long int a, b, divisor;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &a, &b);
+        divisor = gcd(a ,b);
+        printf("%lld\n", lcm(a, divisor) - gcd(b, divisor));
+    }
+
+	return 0;
+}
+
+
+
