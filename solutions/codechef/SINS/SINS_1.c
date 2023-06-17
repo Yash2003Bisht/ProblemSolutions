9670f108d168e5b9032f22683df7b8d1fef6fda1
yash2003bisht <yash2003bisht@gmail.com>
2023-06-17 08:54:22 +0530

    attempted question SINS

---
+++ solutions/codechef/SINS/SINS_1.c
@@ -0,0 +1,32 @@
+// DATE: 16/06/2023, 07:33:44
+// PROBLEM NAME: The Deadly Sin
+// PROBLEM URL: https://www.codechef.com/problems/SINS
+// PROBLEM DIFFICULTY RATTING: 1372
+// STATUS: accepted
+// TIME: 0.04
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+long long int gcd(long long int a, long long int b){
+    if (b == 0)
+        return a;
+    
+    return gcd(b, a%b);
+    
+}
+
+int main(void) {
+    int t;
+    long long int x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &x, &y);
+        printf("%lld\n", gcd(x,y)*2);
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/SINS/SINS_2.c
@@ -0,0 +1,33 @@
+// DATE: 16/06/2023, 07:32:26
+// PROBLEM NAME: The Deadly Sin
+// PROBLEM URL: https://www.codechef.com/problems/SINS
+// PROBLEM DIFFICULTY RATTING: 1372
+// STATUS: time limit exceeded
+// TIME: -
+// MEMORY: 5.2M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t;
+    long long int x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &x, &y);
+        while (x >= 1 && y >= 1 && x != y){
+            if (x > y)
+                x -= y;
+            else if (x < y)
+                y -= x;
+        }
+
+        printf("%lld\n", x+y);
+    }
+    
+	return 0;
+}
+
+
