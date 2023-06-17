065005f02e738bdbe7cc67635fa5d1e36f5fe66c
yash2003bisht <yash2003bisht@gmail.com>
2023-06-17 08:54:22 +0530

    attempted question CS2023_STK

---
+++ solutions/codechef/CS2023_STK/CS2023_STK_1.c
@@ -0,0 +1,63 @@
+// DATE: 17/06/2023, 08:46:27
+// PROBLEM NAME: CodeChef Streak
+// PROBLEM URL: https://www.codechef.com/problems/CS2023_STK
+// PROBLEM DIFFICULTY RATTING: 1009
+// STATUS: accepted
+// TIME: 0.13
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+
+int main(void) {
+    int t, n, count1, count2, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        long long int a[n], b[n];
+        
+        for (int i=0; i<n*2; i++){
+            if (i < n)
+                scanf("%lld", &a[i]);
+            else
+                scanf("%lld", &b[i-n]);
+        }
+
+        count1 = 0, count2 = 0, temp = 0;
+        for (int i=0; i<n; i++){
+            if (a[i] > 0)
+                temp++;
+            else{
+                count1 = max(count1, temp);
+                temp = 0;
+            }
+        }
+        
+        count1 = max(count1, temp);
+        temp = 0;
+        for (int i=0; i<n; i++){
+            if (b[i] > 0)
+                temp++;
+            else{
+                count2 = max(count2, temp);
+                temp = 0;
+            }
+        }
+        
+        count2 = max(count2, temp);
+
+        if (count1 > count2)
+            printf("Om\n");
+        else if (count1 < count2)
+            printf("Addy\n");
+        else
+            printf("Draw\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
