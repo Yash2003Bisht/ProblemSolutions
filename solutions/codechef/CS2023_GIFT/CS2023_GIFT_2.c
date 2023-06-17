17021c63561233654e8f9017a6c5f7ea6366d1c7
yash2003bisht <yash2003bisht@gmail.com>
2023-06-17 08:54:21 +0530

    attempted question CS2023_GIFT

---
+++ solutions/codechef/CS2023_GIFT/CS2023_GIFT_2.c
@@ -0,0 +1,24 @@
+// DATE: 17/06/2023, 08:48:32
+// PROBLEM NAME: The Gift
+// PROBLEM URL: https://www.codechef.com/problems/CS2023_GIFT
+// PROBLEM DIFFICULTY RATTING: 390
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int x, n, m;
+    scanf("%d%d%d", &x, &n, &m);
+    
+    if (n <= x*m)
+        printf("Yes");
+    else
+        printf("No");
+    
+	return 0;
+}
+
+
+
