e7fc9bde332e227377f877b49cf262cdb5e96530
yash2003bisht <yash2003bisht@gmail.com>
2023-06-20 08:22:18 +0530

    attempted question MEANMEDIAN

---
+++ solutions/codechef/MEANMEDIAN/MEANMEDIAN_1.c
@@ -0,0 +1,26 @@
+// DATE: 19/06/2023, 08:14:24
+// PROBLEM NAME: Mean and Median
+// PROBLEM URL: https://www.codechef.com/problems/MEANMEDIAN
+// PROBLEM DIFFICULTY RATTING: 1371
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, num;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        num = 3*x - 2*y;
+        printf("%d %d %d\n", 3*x - y - num, y, num);
+        
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MEANMEDIAN/MEANMEDIAN_2.c
@@ -0,0 +1,28 @@
+// DATE: 19/06/2023, 08:14:11
+// PROBLEM NAME: Mean and Median
+// PROBLEM URL: https://www.codechef.com/problems/MEANMEDIAN
+// PROBLEM DIFFICULTY RATTING: 1371
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void) {
+    int t, x, y, num;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        num = 3*x - 2*y;
+        printf("%d %d %d\n", 3*x - y - num, y, num);
+        
+    }
+
+	return 0;
+}
+
+
+
