5a70d956e0550bca0276543e2487e4eb2c51bbc9
yash2003bisht <yash2003bisht@gmail.com>
2023-06-20 08:22:18 +0530

    attempted question BIGARRAY

---
+++ solutions/codechef/BIGARRAY/BIGARRAY_1.c
@@ -0,0 +1,31 @@
+// DATE: 20/06/2023, 08:09:44
+// PROBLEM NAME: Obtain the Sum
+// PROBLEM URL: https://www.codechef.com/problems/BIGARRAY
+// PROBLEM DIFFICULTY RATTING: 1368
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long n, s, diff;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &s);
+        diff = (((n+1)*n)/2) - s;
+        
+        if (diff < 1 || diff > n)
+            printf("-1\n");
+        else
+            printf("%lld\n", diff);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/BIGARRAY/BIGARRAY_3.c
@@ -0,0 +1,31 @@
+// DATE: 20/06/2023, 08:04:27
+// PROBLEM NAME: Obtain the Sum
+// PROBLEM URL: https://www.codechef.com/problems/BIGARRAY
+// PROBLEM DIFFICULTY RATTING: 1368
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long n, s, total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &s);
+        total = ((n+1)*n)/2;
+        
+        if (total <= s || total-s > n)
+            printf("-1\n");
+        else
+            printf("%lld\n", total - s);
+        
+    }
+    
+	return 0;
+}
+
+
+
