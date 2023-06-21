aa6064a7b716a1242e8e96fd2e1e3c24f2175b68
yash2003bisht <yash2003bisht@gmail.com>
2023-06-21 09:06:49 +0530

    attempted question MISSSUMS

---
+++ solutions/codechef/MISSSUMS/MISSSUMS_1.c
@@ -0,0 +1,29 @@
+// DATE: 21/06/2023, 07:49:18
+// PROBLEM NAME: Missing Sums
+// PROBLEM URL: https://www.codechef.com/problems/MISSSUMS
+// PROBLEM DIFFICULTY RATTING: 1374
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        for (int i=1; i<n+n; i+=2)
+            printf("%d ", i);
+
+        printf("\n");
+        
+    }
+	
+	return 0;
+}
+
+
+
