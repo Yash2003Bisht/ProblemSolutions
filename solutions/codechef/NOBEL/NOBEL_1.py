90c591ad4044c38301f29a963c7b7d40a1e04386
yash2003bisht <yash2003bisht@gmail.com>
2023-06-21 09:06:49 +0530

    attempted question NOBEL

---
+++ solutions/codechef/NOBEL/NOBEL_1.py
@@ -0,0 +1,18 @@
+# DATE: 21/06/2023, 08:10:40
+# PROBLEM NAME: Nobel Prize
+# PROBLEM URL: https://www.codechef.com/problems/NOBEL
+# PROBLEM DIFFICULTY RATTING: 1372
+# STATUS: accepted
+# TIME: 0.47
+# MEMORY: 27.2M
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    total = len(set(map(int, input().split())))
+
+    if total == m:
+        print("No")
+    else:
+        print("Yes")
+
+
---
+++ solutions/codechef/NOBEL/NOBEL_2.py
@@ -0,0 +1,20 @@
+# DATE: 21/06/2023, 08:09:25
+# PROBLEM NAME: Nobel Prize
+# PROBLEM URL: https://www.codechef.com/problems/NOBEL
+# PROBLEM DIFFICULTY RATTING: 1372
+# STATUS: accepted
+# TIME: 0.72
+# MEMORY: 40.6M
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    counter = Counter(map(int, input().split()))
+
+    if not len(set(range(1, m+1)).difference(counter.keys())):
+        print("No")
+    else:
+        print("Yes")
+
+
