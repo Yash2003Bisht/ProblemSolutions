c0c4372ae15ed5dfeb2440f6fe0b4b0d9957256f
yash2003bisht <yash2003bisht@gmail.com>
2023-06-22 14:01:04 +0530

    attempted question ANUWTA

---
+++ solutions/codechef/ANUWTA/ANUWTA_1.c
@@ -0,0 +1,24 @@
+// DATE: 22/06/2023, 07:59:11
+// PROBLEM NAME: Walk on the Axis
+// PROBLEM URL: https://www.codechef.com/problems/ANUWTA
+// PROBLEM DIFFICULTY RATTING: 1374
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    long long int t, n, total;
+    scanf("%lld", &t);
+    
+    while(t--){
+        scanf("%lld", &n);
+        total = (n*(n-1))/2 + n*2;
+        printf("%lld\n", total);
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/ANUWTA/ANUWTA_2.c
@@ -0,0 +1,24 @@
+// DATE: 22/06/2023, 07:58:55
+// PROBLEM NAME: Walk on the Axis
+// PROBLEM URL: https://www.codechef.com/problems/ANUWTA
+// PROBLEM DIFFICULTY RATTING: 1374
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    long long int t, n;
+    scanf("%lld", &t);
+    
+    while(t--){
+        scanf("%lld", &n);
+        printf("%lld\n", (((n+2)*(n+1))/2)-1);
+    }
+    
+	return 0;
+}
+
+
+
