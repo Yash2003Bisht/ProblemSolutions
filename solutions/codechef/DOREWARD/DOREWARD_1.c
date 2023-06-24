ad52c576d13e077f9979476813aed0a8dc3f5752
yash2003bisht <yash2003bisht@gmail.com>
2023-06-24 08:53:24 +0530

    attempted question DOREWARD

---
+++ solutions/codechef/DOREWARD/DOREWARD_1.c
@@ -0,0 +1,31 @@
+// DATE: 24/06/2023, 08:52:48
+// PROBLEM NAME: Donation Rewards
+// PROBLEM URL: https://www.codechef.com/problems/DOREWARD
+// PROBLEM DIFFICULTY RATTING: 395
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n <= 3)
+            printf("BRONZE\n");
+        else if (n <= 6)
+            printf("SILVER\n");
+        else
+            printf("GOLD\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
