22ef3341395fa13e443ddf352aa821f01e82399a
yash2003bisht <yash2003bisht@gmail.com>
2023-06-27 11:21:02 +0530

    attempted question CHEAPFOOD

---
+++ solutions/codechef/CHEAPFOOD/CHEAPFOOD_2.c
@@ -0,0 +1,26 @@
+// DATE: 27/06/2023, 07:30:40
+// PROBLEM NAME: Best Coupon
+// PROBLEM URL: https://www.codechef.com/problems/CHEAPFOOD
+// PROBLEM DIFFICULTY RATTING: 496
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        int discount = x*0.1;
+        printf("%d\n", max(discount, 100));
+    }
+    
+	return 0;
+}
+
+
+
