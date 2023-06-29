b129cd3dfbb389a7a07eef7768b687bc1ca92d58
yash2003bisht <yash2003bisht@gmail.com>
2023-06-29 21:45:31 +0530

    attempted question JASSIGNMENTS

---
+++ solutions/codechef/JASSIGNMENTS/JASSIGNMENTS_1.c
@@ -0,0 +1,29 @@
+// DATE: 29/06/2023, 04:50:59
+// PROBLEM NAME: Janmansh and Assignments
+// PROBLEM URL: https://www.codechef.com/problems/JASSIGNMENTS
+// PROBLEM DIFFICULTY RATTING: 513
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        
+        if (x+3 <= 10)
+            printf("Yes\n");
+        else
+            printf("No\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
