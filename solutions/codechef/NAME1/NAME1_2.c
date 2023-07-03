f6baea02a89298152a0bc261a9570d2fddc0a4cb
yash2003bisht <yash2003bisht@gmail.com>
2023-07-03 09:52:53 +0530

    attempted question NAME1

---
+++ solutions/codechef/NAME1/NAME1_2.c
@@ -0,0 +1,54 @@
+// DATE: 03/07/2023, 07:38:37
+// PROBLEM NAME: Name Reduction
+// PROBLEM URL: https://www.codechef.com/problems/NAME1
+// PROBLEM DIFFICULTY RATTING: 1404
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, flag;
+    char name[40001];
+    
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf(" %[^\n]s", name);
+        scanf("%d", &n);
+        flag = 1;
+
+        int counter_p[26] = {0}, counter_c[26] = {0};
+        for (int i=0; name[i] != '\0'; i++){
+            if (name[i] != ' ')
+                counter_p[name[i] - 97]++;
+        }
+
+        for (int i=0; i<n; i++){
+            scanf(" %[^\n]s", name);
+            for (int i=0; name[i] != '\0'; i++){
+                if (name[i] != ' ')
+                    counter_c[name[i] - 97]++;
+            }
+        }
+        
+        for (int i=0; i<26; i++){
+            if (counter_c[i] > 0 && counter_c[i] > counter_p[i]){
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
