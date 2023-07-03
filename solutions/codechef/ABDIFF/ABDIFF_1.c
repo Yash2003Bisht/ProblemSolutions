e899889759876d08c0504966e775bcb01f0873ac
yash2003bisht <yash2003bisht@gmail.com>
2023-07-03 09:52:53 +0530

    attempted question ABDIFF

---
+++ solutions/codechef/ABDIFF/ABDIFF_1.c
@@ -0,0 +1,20 @@
+// DATE: 03/07/2023, 08:04:19
+// PROBLEM NAME: AB Difference
+// PROBLEM URL: https://www.codechef.com/problems/ABDIFF
+// PROBLEM DIFFICULTY RATTING: 347
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int ans = abs(a*b - (a+b));
+    printf("%d\n", ans);
+	return 0;
+}
+
+
