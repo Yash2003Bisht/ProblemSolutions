c98ee66cc8153cda75b21be14152f7e370a02c82
yash2003bisht <yash2003bisht@gmail.com>
2023-07-04 13:57:08 +0530

    attempted question HOTEL

---
+++ solutions/codechef/HOTEL/HOTEL_2.c
@@ -0,0 +1,43 @@
+// DATE: 04/07/2023, 07:52:46
+// PROBLEM NAME: Hotel Bytelandia
+// PROBLEM URL: https://www.codechef.com/problems/HOTEL
+// PROBLEM DIFFICULTY RATTING: 1405
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a_d_time, guest;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int counter[1000] = {0};
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a_d_time);
+            counter[a_d_time]++;
+        }
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a_d_time);
+            counter[a_d_time]--;
+        }
+        
+        guest = counter[0];
+        for(int i=2; i<=1000; i++){
+    		counter[i] = counter[i] + counter[i-1];
+    		if(counter[i] > guest) guest = counter[i];
+    	}
+        
+        printf("%d\n", guest);
+        
+    }
+    
+	return 0;
+}
+
+
+
