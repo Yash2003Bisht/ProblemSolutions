0818581f4ea97c2ce688e64f32783aeded1d4314
yash2003bisht <yash2003bisht@gmail.com>
2023-07-10 08:14:03 +0530

    attempted question MAKEPAL2

---
+++ solutions/codechef/MAKEPAL2/MAKEPAL2_2.c
@@ -0,0 +1,44 @@
+// DATE: 10/07/2023, 08:10:27
+// PROBLEM NAME: Make Palindrome 2
+// PROBLEM URL: https://www.codechef.com/problems/MAKEPAL2
+// PROBLEM DIFFICULTY RATTING: 1393
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, count_0, count_1;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char b[n+1];
+        count_0 = 0, count_1 = 0;
+        scanf("%s", b);
+
+        for (int i=0; i<n; i++){
+            if (b[i] == '1')
+                count_1++;
+            else
+                count_0++;
+        }
+        
+        if (count_1 < count_0){
+            for (int i=0; i<count_0; i++)
+                printf("0");
+        } else{
+            for (int i=0; i<count_1; i++)
+                printf("1");
+        }
+
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
