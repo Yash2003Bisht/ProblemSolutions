2d763f0ae4b208b228a694146b1b2113db88639a
yash2003bisht <yash2003bisht@gmail.com>
2023-07-10 08:14:03 +0530

    attempted question RIP2000

---
+++ solutions/codechef/RIP2000/RIP2000_1.c
@@ -0,0 +1,18 @@
+// DATE: 08/07/2023, 08:42:28
+// PROBLEM NAME: 2000
+// PROBLEM URL: https://www.codechef.com/problems/RIP2000
+// PROBLEM DIFFICULTY RATTING: 284
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", (n*2000)/500);
+	return 0;
+}
+
+
---
+++ solutions/codechef/RIP2000/RIP2000_2.c
@@ -0,0 +1,21 @@
+// DATE: 08/07/2023, 08:42:16
+// PROBLEM NAME: 2000
+// PROBLEM URL: https://www.codechef.com/problems/RIP2000
+// PROBLEM DIFFICULTY RATTING: 284
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    
+    printf("%d\n", (n*2000)/500);
+    
+	return 0;
+}
+
+
+
