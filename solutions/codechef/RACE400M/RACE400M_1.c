4b813bf993a599620092c912e232e61872efea70
yash2003bisht <yash2003bisht@gmail.com>
2023-07-11 08:34:46 +0530

    attempted question RACE400M

---
+++ solutions/codechef/RACE400M/RACE400M_1.c
@@ -0,0 +1,29 @@
+// DATE: 11/07/2023, 08:34:06
+// PROBLEM NAME: 400M Race
+// PROBLEM URL: https://www.codechef.com/problems/RACE400M
+// PROBLEM DIFFICULTY RATTING: 548
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        if (x < y && x < z)
+            printf("ALICE\n");
+        else if (y < x && y < z)
+            printf("BOB\n");
+        else
+            printf("CHARLIE\n");
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/RACE400M/RACE400M_2.c
@@ -0,0 +1,29 @@
+// DATE: 11/07/2023, 08:31:43
+// PROBLEM NAME: 400M Race
+// PROBLEM URL: https://www.codechef.com/problems/RACE400M
+// PROBLEM DIFFICULTY RATTING: 548
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        if (400/x > 400/y && 400/x > 400/z)
+            printf("ALICE\n");
+        else if (400/y > 400/x && 400/y > 400/z)
+            printf("BOB\n");
+        else
+            printf("CHARLIE\n");
+    }
+    
+	return 0;
+}
+
+
+
