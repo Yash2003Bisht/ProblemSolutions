7f1dc831cf7ae1e0efff815edb98c0a7250aa0a9
yash2003bisht <yash2003bisht@gmail.com>
2023-07-12 08:01:29 +0530

    attempted question EID2

---
+++ solutions/codechef/EID2/EID2_1.c
@@ -0,0 +1,35 @@
+// DATE: 12/07/2023, 06:57:47
+// PROBLEM NAME: Eidi Gift
+// PROBLEM URL: https://www.codechef.com/problems/EID2
+// PROBLEM DIFFICULTY RATTING: 1378
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+
+int main(void) {
+    int t, a1, a2, a3, c1, c2, c3;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d%d%d", &a1, &a2, &a3, &c1, &c2, &c3);
+        if (
+            ((a1 < a2 && c1 >= c2) || (a1 < a3 && c1 >= c3)) ||
+            ((a2 < a1 && c2 >= c1) || (a2 < a3 && c2 >= c3)) ||
+            ((a3 < a1 && c3 >= c1) || (a3 < a2 && c3 >= c2)) ||
+            ((a1 == a2 && c1 != c2) || (a2 == a3 && c2 != c3) || (a1 == a3 && c1 != c3))
+        )
+            printf("NOT FAIR\n");
+        
+        else
+            printf("FAIR\n");
+
+    }
+    
+	return 0;
+}
+
+
+
