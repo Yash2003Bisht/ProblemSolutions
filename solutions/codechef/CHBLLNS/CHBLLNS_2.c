04626558061f96a9f2863ad30a169767838f1936
yash2003bisht <yash2003bisht@gmail.com>
2023-07-12 08:01:29 +0530

    attempted question CHBLLNS

---
+++ solutions/codechef/CHBLLNS/CHBLLNS_2.c
@@ -0,0 +1,29 @@
+// DATE: 12/07/2023, 07:58:05
+// PROBLEM NAME: Chef and Ballons
+// PROBLEM URL: https://www.codechef.com/problems/CHBLLNS
+// PROBLEM DIFFICULTY RATTING: 1399
+// STATUS: partially accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+#define min(a, b)(a > b ? b : a)
+
+int main(void) {
+    int t, r, g, b, k, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &r, &g, &b);
+        scanf("%d", &k);
+        k--;
+
+        count = min(r, k) + min(g, k) + min(b, k) + 1;
+        printf("%d\n", count);
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CHBLLNS/CHBLLNS_4.c
@@ -0,0 +1,29 @@
+// DATE: 12/07/2023, 07:51:15
+// PROBLEM NAME: Chef and Ballons
+// PROBLEM URL: https://www.codechef.com/problems/CHBLLNS
+// PROBLEM DIFFICULTY RATTING: 1399
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#define min(a, b)(a > b ? b : a)
+
+int main(void) {
+    int t;
+    long long int r, g, b, k, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld%lld", &r, &g, &b);
+        scanf("%lld", &k);
+        k--;
+        count = min(r, k) + min(g, k) + min(b, k) + 1;
+        printf("%lld\n", count);
+    }
+    
+	return 0;
+}
+
+
+
