b36ca060113c9fb0f8a86992ee76f7a7813e648b
yash2003bisht <yash2003bisht@gmail.com>
2023-07-14 08:40:52 +0530

    attempted question T20MCH

---
+++ solutions/codechef/T20MCH/T20MCH_1.c
@@ -0,0 +1,24 @@
+// DATE: 14/07/2023, 07:36:00
+// PROBLEM NAME: Possible Victory
+// PROBLEM URL: https://www.codechef.com/problems/T20MCH
+// PROBLEM DIFFICULTY RATTING: 769
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int r, o, c;
+    scanf("%d%d%d", &r, &o, &c);
+    
+    if (r < c + (20-o)*36)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    
+	return 0;
+}
+
+
+
