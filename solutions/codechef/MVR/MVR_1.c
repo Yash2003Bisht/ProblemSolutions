ac4290dac1615c60f2441143bc2bc2e08947f20b
yash2003bisht <yash2003bisht@gmail.com>
2023-07-14 08:40:52 +0530

    attempted question MVR

---
+++ solutions/codechef/MVR/MVR_1.c
@@ -0,0 +1,26 @@
+// DATE: 14/07/2023, 07:39:22
+// PROBLEM NAME: Messi vs Ronaldo
+// PROBLEM URL: https://www.codechef.com/problems/MVR
+// PROBLEM DIFFICULTY RATTING: 316
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int a, b, x, y, messi_total, ronaldo_total;
+    scanf("%d%d%d%d", &a, &b, &x, &y);
+    messi_total = (a*2)+b, ronaldo_total = (x*2)+y;
+
+    if (messi_total > ronaldo_total)
+        printf("Messi\n");
+    else if (messi_total < ronaldo_total)
+        printf("Ronaldo\n");
+    else
+        printf("Equal\n");
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MVR/MVR_2.c
@@ -0,0 +1,25 @@
+// DATE: 14/07/2023, 07:38:20
+// PROBLEM NAME: Messi vs Ronaldo
+// PROBLEM URL: https://www.codechef.com/problems/MVR
+// PROBLEM DIFFICULTY RATTING: 316
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int a, b, x, y;
+    scanf("%d%d%d%d", &a, &b, &x, &y);
+    
+    if ((a*2)+b > (x*2)+y)
+        printf("Messi\n");
+    else if ((a*2)+b < (x*2)+y)
+        printf("Ronaldo\n");
+    else
+        printf("Equal\n");
+	return 0;
+}
+
+
+
