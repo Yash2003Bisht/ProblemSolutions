79be0adafcdb80d281ba0db0988d8b2b4caa7a6c
yash2003bisht <yash2003bisht@gmail.com>
2023-07-15 08:51:21 +0530

    attempted question QUALIFY

---
+++ solutions/codechef/QUALIFY/QUALIFY_1.c
@@ -0,0 +1,29 @@
+// DATE: 15/07/2023, 08:47:04
+// PROBLEM NAME: Qualify the round
+// PROBLEM URL: https://www.codechef.com/problems/QUALIFY
+// PROBLEM DIFFICULTY RATTING: 594
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, a, b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &a, &b);
+
+        if (x <= a+b*2)
+            printf("Qualify\n");
+        else
+            printf("NotQualify\n");
+
+    }
+    
+	return 0;
+}
+
+
+
