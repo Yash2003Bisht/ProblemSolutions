14bf03828ae1d5b6f7b4c425fa087cd643d6527e
yash2003bisht <yash2003bisht@gmail.com>
2023-07-15 08:51:21 +0530

    attempted question PROBSET

---
+++ solutions/codechef/PROBSET/PROBSET_1.py
@@ -0,0 +1,24 @@
+# DATE: 15/07/2023, 08:39:49
+# PROBLEM NAME: Chef and Problem Setting
+# PROBLEM URL: https://www.codechef.com/problems/PROBSET
+# PROBLEM DIFFICULTY RATTING: 1406
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.6M
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    test_result = 'FINE'
+
+    for __ in range(n):
+        test_type, tests = input().split()
+
+        if test_result != 'INVALID':
+            if test_type == 'wrong' and '0' not in tests and test_result != 'INVALID':
+                test_result = 'WEAK'
+            elif test_type == 'correct' and '0' in tests:
+                test_result = 'INVALID'
+
+    print(test_result)
+
+
---
+++ solutions/codechef/PROBSET/PROBSET_2.py
@@ -0,0 +1,23 @@
+# DATE: 15/07/2023, 08:38:27
+# PROBLEM NAME: Chef and Problem Setting
+# PROBLEM URL: https://www.codechef.com/problems/PROBSET
+# PROBLEM DIFFICULTY RATTING: 1406
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.4M
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    test_result = "FINE"
+
+    for __ in range(n):
+        test_type, tests = input().split()
+
+        if test_type == 'wrong' and '0' not in tests and test_result != 'INVALID':
+            test_result = 'WEAK'
+        elif test_type == 'correct' and '0' in tests:
+            test_result = 'INVALID'
+
+    print(test_result)
+
+
