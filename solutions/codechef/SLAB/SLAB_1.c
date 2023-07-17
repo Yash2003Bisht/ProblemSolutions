cc6e305a88f3d0058ce44a8b14f15719b71117e0
yash2003bisht <yash2003bisht@gmail.com>
2023-07-17 09:08:00 +0530

    attempted question SLAB

---
+++ solutions/codechef/SLAB/SLAB_1.c
@@ -0,0 +1,50 @@
+// DATE: 17/07/2023, 08:03:38
+// PROBLEM NAME: Tax Slabs
+// PROBLEM URL: https://www.codechef.com/problems/SLAB
+// PROBLEM DIFFICULTY RATTING: 1406
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, n, total_tax, temp, slab;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        total_tax = 0, slab = n/250000;
+
+        switch (slab){
+            case 0:
+                total_tax = 0;
+                break;
+            case 1:
+                total_tax = (n - 250000) * 0.05;
+                break;
+            case 2:
+                total_tax = 12500 + (n - 500000) * 0.10;
+                break;
+            case 3:
+                total_tax = 37500 + (n - 750000) * 0.15;
+                break;
+            case 4:
+                total_tax = 75000 + (n - 1000000) * 0.20;
+                break;
+            case 5:
+                total_tax = 125000 + (n - 1250000) * 0.25;
+                break;
+            default:
+                total_tax = 187500 + (n - 1500000) * 0.30;
+        }
+
+        printf("%d\n", n - total_tax);
+
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/SLAB/SLAB_6.c
@@ -0,0 +1,52 @@
+// DATE: 17/07/2023, 07:46:37
+// PROBLEM NAME: Tax Slabs
+// PROBLEM URL: https://www.codechef.com/problems/SLAB
+// PROBLEM DIFFICULTY RATTING: 1406
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    long long int t, n, total_tax, temp;
+    scanf("%lld", &t);
+
+    while(t--){
+        scanf("%lld", &n);
+        total_tax = 0;
+
+        // from Rs. 250,001 to Rs. 500,000, Tax rate 5%
+        temp = min(n, 500000) - 250000;
+        total_tax += temp > 0 ? temp * 0.05 : 0;
+        
+        // from Rs. 500,001 to Rs. 750,000, Tax rate 10%
+        temp = min(n, 750000) - 500000;
+        total_tax += temp > 0 ? temp * 0.10 : 0;
+        
+        // from Rs. 750,001 to Rs. 1,000,000, Tax rate 15%
+        temp = min(n, 1000000) - 750000;
+        total_tax += temp > 0 ? temp * 0.15 : 0;
+
+        // from Rs. 1,000,001 to Rs. 1,250,000, Tax rate 20%
+        temp = min(n, 1250000) - 1000000;
+        total_tax += temp > 0 ? temp * 0.20 : 0;
+
+        // from Rs. 1,250,001 to Rs. 1,500,000, Tax rate 25%
+        temp = min(n, 1500000) - 1250000;
+        total_tax += temp > 0 ? temp * 0.25 : 0;
+
+        // above Rs. 1,500,000, Tax rate 30%
+        temp = n - 1500000;
+        total_tax += temp > 0 ? temp * 0.30 : 0;
+        
+        printf("%lld\n", n - total_tax);
+
+    }
+    
+	return 0;
+}
+
+
+
