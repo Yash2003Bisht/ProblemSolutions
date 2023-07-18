3184317ff1930ca4f01589da53e45f79c5adeef5
yash2003bisht <yash2003bisht@gmail.com>
2023-07-18 08:52:01 +0530

    attempted question Dot_and_Cross

---
+++ solutions/hackerrank/Dot_and_Cross/Dot_and_Cross_1.py
@@ -0,0 +1,16 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/np-dot-and-cross/problem
+# STATUS: Accepted
+
+import numpy as np
+
+a = []
+b = []
+n = int(input())
+
+for i in range(n*2):
+    if i < n:
+        a.append(list(map(int, input().split())))
+    else:
+        b.append(list(map(int, input().split())))
+
+print(np.dot(a, b))
