ac2820a5bb696c1e7a4563d82032cb5e6e909d32
yash2003bisht <yash2003bisht@gmail.com>
2023-07-19 08:54:44 +0530

    attempted question SUMNEQ

---
+++ solutions/codechef/SUMNEQ/SUMNEQ_1.c
@@ -0,0 +1,19 @@
+// DATE: 19/07/2023, 08:53:19
+// PROBLEM NAME: Integers that sum to the same value
+// PROBLEM URL: https://www.codechef.com/problems/SUMNEQ
+// PROBLEM DIFFICULTY RATTING: 592
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n-1);
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/SUMNEQ/SUMNEQ_3.c
@@ -0,0 +1,27 @@
+// DATE: 19/07/2023, 08:47:35
+// PROBLEM NAME: Integers that sum to the same value
+// PROBLEM URL: https://www.codechef.com/problems/SUMNEQ
+// PROBLEM DIFFICULTY RATTING: 592
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.4M
+
+#include <stdio.h>
+
+int main(void) {
+    int n, count;
+    scanf("%d", &n);
+    
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=n; j++)
+            if (i+j == n)
+                count++;
+    }
+    
+    printf("%d\n", count);
+    
+	return 0;
+}
+
+
+
