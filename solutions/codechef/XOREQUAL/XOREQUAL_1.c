19d6242e686aca8443e1a5926889a3f18c9fd5b2
yash2003bisht <yash2003bisht@gmail.com>
2023-07-19 08:54:44 +0530

    attempted question XOREQUAL

---
+++ solutions/codechef/XOREQUAL/XOREQUAL_1.c
@@ -0,0 +1,43 @@
+// DATE: 19/07/2023, 08:42:43
+// PROBLEM NAME: Xor Equality
+// PROBLEM URL: https://www.codechef.com/problems/XOREQUAL
+// PROBLEM DIFFICULTY RATTING: 1405
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 5.2M
+
+#include <stdio.h>
+#define modulo 1000000007
+#define ll long long
+
+ll power_off(ll a, ll b){
+    // base case
+    if (b==0)
+        return 1;
+
+    // recursive call
+    ll temp = power_off(a, b/2);
+    
+    if (b%2 != 0)
+        return (a*temp*temp) % modulo;
+    else
+        return (temp*temp) % modulo;
+
+}
+
+int main(void) {
+    int t, n;
+    ll ans;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        ans = power_off(2, n-1);
+        printf("%llu\n", ans % modulo);
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/XOREQUAL/XOREQUAL_3.c
@@ -0,0 +1,31 @@
+// DATE: 19/07/2023, 07:56:42
+// PROBLEM NAME: Xor Equality
+// PROBLEM URL: https://www.codechef.com/problems/XOREQUAL
+// PROBLEM DIFFICULTY RATTING: 1405
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 5.1M
+
+#include <stdio.h>
+#define modulo 1000000007
+#define max_n 100000
+
+int main(void) {
+    int t, n, counter[max_n];
+    counter[0] = 1;
+
+    for (int i=1; i<max_n; i++)
+        counter[i] = (counter[i-1]*2) % modulo;
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        printf("%d\n", counter[n-1]);
+    }
+
+	return 0;
+}
+
+
+
