a16492db92fcca703d9bf78027c9d811496c678c
yash2003bisht <yash2003bisht@gmail.com>
2023-07-21 10:55:38 +0530

    attempted question ONEFROMK

---
+++ solutions/codechef/ONEFROMK/ONEFROMK_3.c
@@ -0,0 +1,103 @@
+// DATE: 20/07/2023, 08:04:55
+// PROBLEM NAME: Exclusion-Inclusion
+// PROBLEM URL: https://www.codechef.com/problems/ONEFROMK
+// PROBLEM DIFFICULTY RATTING: 1307
+// STATUS: accepted
+// TIME: 0.07
+// MEMORY: 5.3M
+
+#include <stdio.h>
+#define lli long long int
+
+void merge(long long int arr[], int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    long long int L[n1], R[n2];
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0, j = 0, k = l;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+
+void mergeSort(long long int arr[], int l, int r){
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+
+lli sum(lli *arr, int size){
+    lli total = 0;
+    
+    for (int i=0; i<size; i++)
+        total += arr[i];
+
+    return total;
+
+}
+
+int main(void) {
+    int t, n;
+    lli arr_sum;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        lli arr[n];
+
+        for (int i=0; i<n; i++)
+            scanf("%lld", &arr[i]);
+
+        mergeSort(arr, 0, n-1);
+        arr_sum = sum(arr, n);
+
+        printf("%lld ", arr_sum);
+
+        for (int i=0; i<n-1; i++){
+            arr_sum -= arr[i];
+            printf("%lld ", arr_sum);
+        }
+
+        printf("\n");
+
+    }
+
+	return 0;
+}
+
+
