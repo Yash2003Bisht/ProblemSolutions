2ba00f0cb363a0e58556a395837a0a5896aef674
yash2003bisht <yash2003bisht@gmail.com>
2023-07-21 10:55:39 +0530

    attempted question PUZHUNT

---
+++ solutions/codechef/PUZHUNT/PUZHUNT_1.c
@@ -0,0 +1,24 @@
+// DATE: 20/07/2023, 08:22:47
+// PROBLEM NAME: Puzzle Hunt
+// PROBLEM URL: https://www.codechef.com/problems/PUZHUNT
+// PROBLEM DIFFICULTY RATTING: 279
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.2M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    
+    if (n >= 6 && n <= 8)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+	return 0;
+}
+
+
+
