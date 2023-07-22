797d32ee699510ad15a9aa2c7c2dc634cb22c69b
yash2003bisht <yash2003bisht@gmail.com>
2023-07-22 09:03:52 +0530

    attempted question CHEFA

---
+++ solutions/codechef/CHEFA/CHEFA_4.py
@@ -0,0 +1,16 @@
+# DATE: 22/07/2023, 08:55:17
+# PROBLEM NAME: Chef and easy problem
+# PROBLEM URL: https://www.codechef.com/problems/CHEFA
+# PROBLEM DIFFICULTY RATTING: 1408
+# STATUS: accepted
+# TIME: 0.37
+# MEMORY: 24.4M
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    count = sum(arr[::2])
+    print(count)
+
+
---
+++ solutions/codechef/CHEFA/CHEFA_5.py
@@ -0,0 +1,21 @@
+# DATE: 22/07/2023, 08:54:09
+# PROBLEM NAME: Chef and easy problem
+# PROBLEM URL: https://www.codechef.com/problems/CHEFA
+# PROBLEM DIFFICULTY RATTING: 1408
+# STATUS: accepted
+# TIME: 0.47
+# MEMORY: 24.4M
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    count = 0
+
+    for i in range(n):
+        if i%2 == 0:
+            count += arr[i]
+
+    print(count)
+
+
