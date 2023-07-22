7ac3d9cbf23d762aff6809124541ebe8c1447492
yash2003bisht <yash2003bisht@gmail.com>
2023-07-22 09:03:53 +0530

    attempted question ARRHALVES

---
+++ solutions/codechef/ARRHALVES/ARRHALVES_1.c
@@ -0,0 +1,39 @@
+// DATE: 22/07/2023, 08:32:54
+// PROBLEM NAME: Array Halves
+// PROBLEM URL: https://www.codechef.com/problems/ARRHALVES
+// PROBLEM DIFFICULTY RATTING: 1407
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, size;
+    long long int count;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        count = 0, size = n*2;
+        int arr[size];
+
+        for (int i=0; i<size; i++)
+            scanf("%d", &arr[i]);
+        
+        for (int i=size-1; i>=0; i--){
+            if (arr[i] > n){
+                count += size - i - 1;
+                size--;
+            }
+        }
+        
+        printf("%lld\n", count);
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/ARRHALVES/ARRHALVES_3.c
@@ -0,0 +1,47 @@
+// DATE: 22/07/2023, 08:26:31
+// PROBLEM NAME: Array Halves
+// PROBLEM URL: https://www.codechef.com/problems/ARRHALVES
+// PROBLEM DIFFICULTY RATTING: 1407
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 5.4M
+
+#include <stdio.h>
+#define lld long long int
+
+int main(void) {
+    lld t, n, i, count, size;
+    scanf("%lld", &t);
+
+    while(t--){
+        scanf("%lld", &n);
+        count = 0, size = n*2;
+        lld arr[size];
+        
+        for (i=0; i<size; i++)
+            scanf("%lld", &arr[i]);
+        
+        // Count all swaps for second-half elements that are greater than n 
+        for (i=size-1; i>=n; i--){
+            if (arr[i] > n){
+                count += size - i - 1;
+                size--;
+            }
+        }
+        
+        // Count all swaps for first-half elements that are greater than n 
+        for (i=n-1; i>=0; i--){
+            if (arr[i] > n){
+                count += size - i - 1;
+                size--;
+            }
+        }
+        
+        printf("%lld\n", count);
+
+    }
+    
+	return 0;
+}
+
+
