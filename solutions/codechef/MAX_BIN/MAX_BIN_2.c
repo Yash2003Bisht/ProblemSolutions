c87822370719aed55781fcd74bca0e75df27f425
yash2003bisht <yash2003bisht@gmail.com>
2023-07-24 08:44:54 +0530

    attempted question MAX_BIN

---
+++ solutions/codechef/MAX_BIN/MAX_BIN_2.c
@@ -0,0 +1,41 @@
+// DATE: 24/07/2023, 08:42:47
+// PROBLEM NAME: Max Binary
+// PROBLEM URL: https://www.codechef.com/problems/MAX_BIN
+// PROBLEM DIFFICULTY RATTING: 1143
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, k, n, size;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &k);
+        size = n+k+1;
+        char s[size];
+
+        for (int i=0; i<size; i++)
+            s[i] = '\0';
+
+        scanf("%s", s);
+        
+        if (s[0] == '0'){
+            s[0] = '1';
+            k--;
+        }
+        
+        for (int i=0; i<k; i++)
+            s[n+i] = '0';
+        
+        printf("%s\n", s);
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MAX_BIN/MAX_BIN_3.c
@@ -0,0 +1,40 @@
+// DATE: 24/07/2023, 08:42:06
+// PROBLEM NAME: Max Binary
+// PROBLEM URL: https://www.codechef.com/problems/MAX_BIN
+// PROBLEM DIFFICULTY RATTING: 1143
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, k, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &k);
+        char s[n+k+1];
+
+        for (int i=0; i<n+k+1; i++)
+            s[i] = '\0';
+
+        scanf("%s", s);
+        
+        if (s[0] == '0'){
+            s[0] = '1';
+            k--;
+        }
+        
+        for (int i=0; i<k; i++)
+            s[n+i] = '0';
+        
+        printf("%s\n", s);
+        
+    }
+    
+	return 0;
+}
+
+
+
