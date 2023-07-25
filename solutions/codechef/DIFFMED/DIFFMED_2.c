73442a483770bc69693ba9fe52a11cbbc83b01a1
yash2003bisht <yash2003bisht@gmail.com>
2023-07-25 09:02:45 +0530

    attempted question DIFFMED

---
+++ solutions/codechef/DIFFMED/DIFFMED_2.c
@@ -0,0 +1,32 @@
+// DATE: 25/07/2023, 07:59:58
+// PROBLEM NAME: Different Medians
+// PROBLEM URL: https://www.codechef.com/problems/DIFFMED
+// PROBLEM DIFFICULTY RATTING: 1408
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, i;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        for (i=0; i<n/2; i++)
+            printf("%d %d ", n-i, i+1);
+        
+        if (n%2 != 0)
+            printf("%d ", n-i);
+        
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/DIFFMED/DIFFMED_3.c
@@ -0,0 +1,37 @@
+// DATE: 25/07/2023, 07:42:36
+// PROBLEM NAME: Different Medians
+// PROBLEM URL: https://www.codechef.com/problems/DIFFMED
+// PROBLEM DIFFICULTY RATTING: 1408
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 5.3M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, temp1, temp2;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        temp1 = n, temp2 = 1; 
+        
+        for (int i=0; i<n; i++){
+            if (i%2 == 0){
+                printf("%d ", temp1);
+                temp1--;
+            } else {
+                printf("%d ", temp2);
+                temp2++;
+            }
+        }
+        
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
