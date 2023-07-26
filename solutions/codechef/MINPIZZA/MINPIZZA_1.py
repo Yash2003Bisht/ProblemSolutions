f3c77efdc93255f699e9f4564792558bf1222bc9
yash2003bisht <yash2003bisht@gmail.com>
2023-07-26 10:06:35 +0530

    attempted question MINPIZZA

---
+++ solutions/codechef/MINPIZZA/MINPIZZA_1.py
@@ -0,0 +1,14 @@
+# DATE: 26/07/2023, 10:05:07
+# PROBLEM NAME: Minimum Pizzas
+# PROBLEM URL: https://www.codechef.com/problems/MINPIZZA
+# PROBLEM DIFFICULTY RATTING: 546
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 9.6M
+
+import math
+
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    print(math.ceil((x*n)/4))
+
