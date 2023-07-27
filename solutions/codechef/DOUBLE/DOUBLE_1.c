1581d3dbd84b57af5e3cca7ae733664665fa856b
yash2003bisht <yash2003bisht@gmail.com>
2023-07-27 08:45:07 +0530

    attempted question DOUBLE

---
+++ solutions/codechef/DOUBLE/DOUBLE_1.c
@@ -0,0 +1,29 @@
+// DATE: 27/07/2023, 08:38:55
+// PROBLEM NAME: Double Strings
+// PROBLEM URL: https://www.codechef.com/problems/DOUBLE
+// PROBLEM DIFFICULTY RATTING: 1134
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        
+        if (n%2 == 0)
+            printf("%d\n", n);
+        else
+            printf("%d\n", n-1);
+        
+    }
+    
+	return 0;
+}
+
+
+
