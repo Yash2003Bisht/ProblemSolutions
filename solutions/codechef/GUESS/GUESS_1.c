61e9838ad5207afb6ff13f65fb487be0348dcf17
yash2003bisht <yash2003bisht@gmail.com>
2023-07-27 08:45:08 +0530

    attempted question GUESS

---
+++ solutions/codechef/GUESS/GUESS_1.c
@@ -0,0 +1,34 @@
+// DATE: 27/07/2023, 08:30:50
+// PROBLEM NAME: Guessing Game
+// PROBLEM URL: https://www.codechef.com/problems/GUESS
+// PROBLEM DIFFICULTY RATTING: 1410
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int n, m, total_outcomes, favourable_outcomes;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &m);
+
+        total_outcomes = n*m;
+
+        if (total_outcomes%2 != 0){
+            favourable_outcomes = total_outcomes/2;
+            printf("%lld/%lld\n", favourable_outcomes, total_outcomes);
+        }
+        else
+            printf("1/2\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/GUESS/GUESS_3.c
@@ -0,0 +1,33 @@
+// DATE: 27/07/2023, 08:24:28
+// PROBLEM NAME: Guessing Game
+// PROBLEM URL: https://www.codechef.com/problems/GUESS
+// PROBLEM DIFFICULTY RATTING: 1410
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int n, m, total_outcomes, prob;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &n, &m);
+        
+        total_outcomes = n*m;
+        prob = total_outcomes/2;
+
+        if (total_outcomes%2 != 0)
+            printf("%lld/%lld\n", prob, total_outcomes);
+        else
+            printf("1/2\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
