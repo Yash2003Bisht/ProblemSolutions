87cae54da11b8481180a03e525b0eaa40bda00dc
yash2003bisht <yash2003bisht@gmail.com>
2023-07-28 09:20:19 +0530

    attempted question VCOUPLE

---
+++ solutions/codechef/VCOUPLE/VCOUPLE_5.py
@@ -0,0 +1,23 @@
+# DATE: 28/07/2023, 07:33:16
+# PROBLEM NAME: B - Valentine Couples
+# PROBLEM URL: https://www.codechef.com/problems/VCOUPLE
+# PROBLEM DIFFICULTY RATTING: 1412
+# STATUS: accepted
+# TIME: 0.07
+# MEMORY: 11.4M
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    total = 0
+    
+    a.sort()
+    b.sort(reverse=True)
+    
+    for i in range(n):
+        total = max(total, a[i] + b[i])
+    
+    print(total)
+
+
