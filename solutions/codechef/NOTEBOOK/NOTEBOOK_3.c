aa31527ccadfeb8d93de1f7db4e1d59f88b08ab7
yash2003bisht <yash2003bisht@gmail.com>
2023-07-31 08:46:00 +0530

    attempted question NOTEBOOK

---
+++ solutions/codechef/NOTEBOOK/NOTEBOOK_3.c
@@ -0,0 +1,25 @@
+// DATE: 29/07/2023, 09:01:23
+// PROBLEM NAME: Count the Notebooks
+// PROBLEM URL: https://www.codechef.com/problems/NOTEBOOK
+// PROBLEM DIFFICULTY RATTING: 563
+// STATUS: compilation error
+// TIME: -
+// MEMORY: -
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        printf("%d\n", n*10)
+        
+    }
+    
+	return 0;
+}
+
+
+
