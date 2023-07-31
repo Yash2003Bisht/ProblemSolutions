ef5dd093cbd8a85824ed94170f53134938b17db9
yash2003bisht <yash2003bisht@gmail.com>
2023-07-31 08:46:00 +0530

    attempted question BINTREE

---
+++ solutions/codechef/BINTREE/BINTREE_1.c
@@ -0,0 +1,35 @@
+// DATE: 31/07/2023, 08:01:10
+// PROBLEM NAME: Shortest Path in Binary Trees
+// PROBLEM URL: https://www.codechef.com/problems/BINTREE
+// PROBLEM DIFFICULTY RATTING: 1413
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, i, j, depth;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d %d", &i, &j);
+        depth = 0;
+        
+        while(i != j){
+            if (i>j)
+                i /= 2;
+            else
+                j /= 2;
+            depth++;
+        }
+        
+        printf("%d\n", depth);
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/BINTREE/BINTREE_3.c
@@ -0,0 +1,40 @@
+// DATE: 29/07/2023, 08:56:51
+// PROBLEM NAME: Shortest Path in Binary Trees
+// PROBLEM URL: https://www.codechef.com/problems/BINTREE
+// PROBLEM DIFFICULTY RATTING: 1413
+// STATUS: time limit exceeded
+// TIME: -
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, n, count_1, count_0;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        scanf("%s", s);
+        count_1 = 0, count_0 = 0;
+        
+        for (int i=0; i<n; i++){
+            if (s[i] == '1')
+                count_1++;
+            else
+                count_0++;
+        }
+        
+        if (min(count_1, count_0)%2 == 0)
+            printf("Ramos\n");
+        else
+            printf("Zlatan\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
