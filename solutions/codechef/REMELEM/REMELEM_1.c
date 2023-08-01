b4829e3c2662154da4eecc35d6f17ceac9b7d0a3
yash2003bisht <yash2003bisht@gmail.com>
2023-08-01 08:34:00 +0530

    attempted question REMELEM

---
+++ solutions/codechef/REMELEM/REMELEM_1.c
@@ -0,0 +1,46 @@
+// DATE: 01/08/2023, 07:46:12
+// PROBLEM NAME: Remove Element
+// PROBLEM URL: https://www.codechef.com/problems/REMELEM
+// PROBLEM DIFFICULTY RATTING: 1415
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+
+int main(void) {
+    int t, n, k, min, max;
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &n, &k);
+        int a[n];
+
+        for (int i=0; i<n; i++)
+            scanf("%d", &a[i]);
+
+        min = a[0], max = a[0];
+        
+        if (n > 1){
+            for (int i=1; i<n; i++){
+                if (max < a[i])
+                    max = a[i];
+                else if (min > a[i])
+                    min = a[i];
+            }
+        }
+
+        if (min + max <= k || n == 1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/REMELEM/REMELEM_4.py
@@ -0,0 +1,19 @@
+# DATE: 01/08/2023, 07:37:35
+# PROBLEM NAME: Remove Element
+# PROBLEM URL: https://www.codechef.com/problems/REMELEM
+# PROBLEM DIFFICULTY RATTING: 1415
+# STATUS: accepted
+# TIME: 0.06
+# MEMORY: 22.5M
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a.sort()
+
+    if (a[0] + a[n-1]) <= k or n == 1:
+        print("YES")
+    else:
+        print("NO")
+
+
