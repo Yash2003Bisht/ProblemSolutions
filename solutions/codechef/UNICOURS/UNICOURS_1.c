7f930357a6a15fc9a6c555ccbd4f18d3a807cf56
yash2003bisht <yash2003bisht@gmail.com>
2023-08-02 08:13:22 +0530

    attempted question UNICOURS

---
+++ solutions/codechef/UNICOURS/UNICOURS_1.c
@@ -0,0 +1,33 @@
+// DATE: 02/08/2023, 07:40:51
+// PROBLEM NAME: Courses in an university
+// PROBLEM URL: https://www.codechef.com/problems/UNICOURS
+// PROBLEM DIFFICULTY RATTING: 1417
+// STATUS: accepted
+// TIME: 0.13
+// MEMORY: 1.9M
+
+#include <stdio.h>
+#define max(a, b)(a>b?a:b)
+
+int main(void) {
+    int t, n, a, max_value;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        max_value = 0;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            max_value = max(max_value, a);
+        }
+        
+        printf("%d\n", n - max_value);
+        
+    }
+    
+	return 0;
+}
+
+
+
