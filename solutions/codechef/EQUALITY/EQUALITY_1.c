4149147fe1821ea9b3291c3a175f3fa834ca3092
yash2003bisht <yash2003bisht@gmail.com>
2023-08-04 07:47:00 +0530

    attempted question EQUALITY

---
+++ solutions/codechef/EQUALITY/EQUALITY_1.c
@@ -0,0 +1,39 @@
+// DATE: 04/08/2023, 07:12:42
+// PROBLEM NAME: Equality
+// PROBLEM URL: https://www.codechef.com/problems/EQUALITY
+// PROBLEM DIFFICULTY RATTING: 1419
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    long long int total;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        long long int arr[n];
+        total = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%lld", &arr[i]);
+            total += arr[i];
+        }
+
+        total = total/(n-1);
+
+        for (int i=0; i<n; i++)
+            printf("%lld ", total-arr[i]);
+
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
