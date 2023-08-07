abd15220e0190602b8dd019f093352d8017e770c
yash2003bisht <yash2003bisht@gmail.com>
2023-08-07 12:39:36 +0530

    attempted question KCHESS

---
+++ solutions/codechef/KCHESS/KCHESS_2.c
@@ -0,0 +1,46 @@
+// DATE: 07/08/2023, 08:55:00
+// PROBLEM NAME: Knight Chess
+// PROBLEM URL: https://www.codechef.com/problems/KCHESS
+// PROBLEM DIFFICULTY RATTING: 1420
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, b, flag;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        int knight_moves_x[n], knight_moves_y[n];
+        flag = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d%d", &a, &b);
+            knight_moves_x[i] = a;
+            knight_moves_y[i] = b;
+        }
+
+        scanf("%d%d", &a, &b);
+
+        for (int i=0; i<n; i++){
+            if (a == knight_moves_x[i] || b == knight_moves_y[i]){
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/KCHESS/KCHESS_5.py
@@ -0,0 +1,39 @@
+# DATE: 07/08/2023, 08:45:46
+# PROBLEM NAME: Knight Chess
+# PROBLEM URL: https://www.codechef.com/problems/KCHESS
+# PROBLEM DIFFICULTY RATTING: 1420
+# STATUS: accepted
+# TIME: 0.53
+# MEMORY: 78.2M
+
+def get_knight_moves(a, b):
+    moves = []
+
+    moves.append([a-2, b-1])
+    moves.append([a-2, b+1])
+    moves.append([a+2, b-1])
+    moves.append([a+2, b+1])
+    moves.append([a-1, b+2])
+    moves.append([a+1, b+2])
+    moves.append([a-1, b-2])
+    moves.append([a+1, b-2])
+    
+    return moves
+
+
+for _ in range(int(input())):
+    n = int(input())
+    knights_moves = []
+    
+    for __ in range(n):
+        x, y = map(int, input().split())
+        knights_moves.extend(get_knight_moves(x, y))
+        
+    a, b = map(int, input().split())
+
+    if [a, b] in knights_moves:
+        print("YES")
+    else:
+        print("NO")
+
+
