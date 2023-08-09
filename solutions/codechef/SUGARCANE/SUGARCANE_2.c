d67da8e9fd1d7c710a956b69849d97548424eddd
yash2003bisht <yash2003bisht@gmail.com>
2023-08-09 08:13:44 +0530

    attempted question SUGARCANE

---
+++ solutions/codechef/SUGARCANE/SUGARCANE_2.c
@@ -0,0 +1,26 @@
+// DATE: 09/08/2023, 08:11:46
+// PROBLEM NAME: Sugarcane Juice Business
+// PROBLEM URL: https://www.codechef.com/problems/SUGARCANE
+// PROBLEM DIFFICULTY RATTING: 563
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, coins;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        coins = n*50;
+        coins = coins - coins*0.7;
+        printf("%d\n", coins);
+    }
+    
+	return 0;
+}
+
+
+
