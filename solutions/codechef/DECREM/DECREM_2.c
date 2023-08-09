22d3674ec4ec28bea42abc3085bd586261edf23c
yash2003bisht <yash2003bisht@gmail.com>
2023-08-09 08:13:44 +0530

    attempted question DECREM

---
+++ solutions/codechef/DECREM/DECREM_2.c
@@ -0,0 +1,28 @@
+// DATE: 09/08/2023, 07:45:04
+// PROBLEM NAME: Decreasing Srrnmieeda
+// PROBLEM URL: https://www.codechef.com/problems/DECREM
+// PROBLEM DIFFICULTY RATTING: 1423
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, l, r;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &l, &r);
+        
+        if (r < 2*l)
+            printf("%d\n", r);
+        else
+            printf("-1\n");
+        
+    }
+    
+	return 0;
+}
+
+
