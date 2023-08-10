7efbea28e4f4e9e82ef788e93f7f58a646f795ef
yash2003bisht <yash2003bisht@gmail.com>
2023-08-10 08:58:17 +0530

    attempted question AVGARR

---
+++ solutions/codechef/AVGARR/AVGARR_1.c
@@ -0,0 +1,35 @@
+// DATE: 10/08/2023, 08:40:32
+// PROBLEM NAME: Average Array
+// PROBLEM URL: https://www.codechef.com/problems/AVGARR
+// PROBLEM DIFFICULTY RATTING: 1424
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &x);
+
+        for (int i=x-n/2; i<x; i++)
+            printf("%d ", i);
+
+        for (int i=x+1; i<=x+n/2; i++)
+            printf("%d ", i);
+            
+        if (n%2 != 0)
+            printf("%d ", x);
+
+        printf("\n");
+                
+    }
+    
+	return 0;
+}
+
+
+
