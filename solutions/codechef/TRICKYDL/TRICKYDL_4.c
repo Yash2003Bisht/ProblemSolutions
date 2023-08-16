a98f9d3237e72a9b2d77f7d5f45ed8ddb321c7f1
yash2003bisht <yash2003bisht@gmail.com>
2023-08-16 08:02:02 +0530

    attempted question TRICKYDL

---
+++ solutions/codechef/TRICKYDL/TRICKYDL_4.c
@@ -0,0 +1,41 @@
+// DATE: 16/08/2023, 07:57:56
+// PROBLEM NAME: The Tricky Deal
+// PROBLEM URL: https://www.codechef.com/problems/TRICKYDL
+// PROBLEM DIFFICULTY RATTING: 1431
+// STATUS: accepted
+// TIME: 0.08
+// MEMORY: 2.1M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, a, d1, d2;
+    long long int chef, chef_friend, max_diff;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &a);
+        d1 = 1, d2 = 1, chef = a, chef_friend = 1;
+        max_diff = chef - chef_friend;
+        
+        while (chef >= chef_friend){
+            chef += a;
+            chef_friend += pow(2, d1);
+            d1++;
+            
+            if (chef - chef_friend > max_diff){
+                max_diff = chef - chef_friend;
+                d2 = d1;
+            }
+
+        }
+
+        printf("%d %d\n", d1-1, d2);
+        
+    }
+    
+	return 0;
+}
+
+
---
+++ solutions/codechef/TRICKYDL/TRICKYDL_8.c
@@ -0,0 +1,44 @@
+// DATE: 16/08/2023, 07:55:02
+// PROBLEM NAME: The Tricky Deal
+// PROBLEM URL: https://www.codechef.com/problems/TRICKYDL
+// PROBLEM DIFFICULTY RATTING: 1431
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#define lld long long int
+
+int main(void) {
+    int t, a, d1, d2;
+    lld chef, chef_friend, exponent, max_diff, temp;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &a);
+        d1 = 1, d2 = 1, chef = a, chef_friend = 1, exponent = 2;
+        max_diff = chef - chef_friend;
+        
+        while (chef >= chef_friend){
+            chef += a;
+            chef_friend += exponent;
+            exponent *= 2;
+            d1++;
+
+            temp = chef - chef_friend;
+            if (temp > max_diff){
+                max_diff = temp;
+                d2 = d1;
+            }
+
+        }
+
+        printf("%d %d\n", d1-1, d2);
+        
+    }
+    
+	return 0;
+}
+
+
+
