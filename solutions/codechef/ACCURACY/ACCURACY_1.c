c5d16897e29205a1ae516aa770919c513b5b2f8a
yash2003bisht <yash2003bisht@gmail.com>
2023-08-17 09:11:29 +0530

    attempted question ACCURACY

---
+++ solutions/codechef/ACCURACY/ACCURACY_1.c
@@ -0,0 +1,28 @@
+// DATE: 17/08/2023, 09:10:41
+// PROBLEM NAME: High Accuracy
+// PROBLEM URL: https://www.codechef.com/problems/ACCURACY
+// PROBLEM DIFFICULTY RATTING: 580
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, ans;
+    float x;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%f", &x);
+        ans = ceil((float) x/3)*3 - x;
+        printf("%d\n", ans);
+        
+    }
+    
+	return 0;
+}
+
+
+
