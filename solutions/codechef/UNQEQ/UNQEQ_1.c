96ab4ade47598378c69d235b38dfae3f602856e0
yash2003bisht <yash2003bisht@gmail.com>
2023-08-17 09:11:29 +0530

    attempted question UNQEQ

---
+++ solutions/codechef/UNQEQ/UNQEQ_1.c
@@ -0,0 +1,44 @@
+// DATE: 17/08/2023, 07:32:49
+// PROBLEM NAME: Prefix Sums
+// PROBLEM URL: https://www.codechef.com/problems/UNQEQ
+// PROBLEM DIFFICULTY RATTING: 1431
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, range_sum, temp;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        range_sum = (n+1)*n/2;
+
+        if (range_sum%2 == 0){
+            printf("YES\n");
+            temp = n/4;
+
+            for (int i=1; i<=temp; i++)
+                printf("%d ", i);
+
+            for (int i=0; i<temp; i++)
+                printf("%d ", n-i);
+
+            printf("\n");
+            temp++;
+
+            for (int i=temp; i<temp+n/2; i++)
+                printf("%d ", i);
+
+        } else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
