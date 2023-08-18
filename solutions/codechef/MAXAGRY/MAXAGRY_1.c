314e5d3a9b33b9c60a81e717d7a3b0cafac0accf
yash2003bisht <yash2003bisht@gmail.com>
2023-08-18 11:47:04 +0530

    attempted question MAXAGRY

---
+++ solutions/codechef/MAXAGRY/MAXAGRY_1.c
@@ -0,0 +1,35 @@
+// DATE: 18/08/2023, 09:14:22
+// PROBLEM NAME: Maximum Angriness
+// PROBLEM URL: https://www.codechef.com/problems/MAXAGRY
+// PROBLEM DIFFICULTY RATTING: 1432
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#define lld unsigned long long
+
+int main(void) {
+    int t;
+    lld n, k, total_angriness, k_sum;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%lld%lld", &n, &k);
+        
+        if (k >= n/2){
+            total_angriness = (n-1)*n/2;
+            printf("%lld\n", total_angriness);
+        } else{
+            k_sum = (k+1)*k/2;
+            total_angriness = (k*n-k_sum) + ((n-k*2)*k) + (k*k-k_sum);
+            printf("%lld\n", total_angriness);
+        }
+        
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MAXAGRY/MAXAGRY_8.py
@@ -0,0 +1,20 @@
+# DATE: 18/08/2023, 08:14:18
+# PROBLEM NAME: Maximum Angriness
+# PROBLEM URL: https://www.codechef.com/problems/MAXAGRY
+# PROBLEM DIFFICULTY RATTING: 1432
+# STATUS: accepted
+# TIME: 0.34
+# MEMORY: 7.7M
+
+import math
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    
+    if k >= n//2:
+        print(math.comb(n, 2))
+    else:
+        k_sum = (k+1)*k//2
+        print((k*n-k_sum) + ((n-k*2)*k) + (k**2-k_sum))
+
+
