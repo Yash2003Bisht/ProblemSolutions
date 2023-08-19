0f781084ea1b31a24bf992f1c31b6c36711d4555
yash2003bisht <yash2003bisht@gmail.com>
2023-08-19 09:10:42 +0530

    attempted question NEARESTEXIT

---
+++ solutions/codechef/NEARESTEXIT/NEARESTEXIT_1.c
@@ -0,0 +1,29 @@
+// DATE: 19/08/2023, 09:08:10
+// PROBLEM NAME: Nearest Exit
+// PROBLEM URL: https://www.codechef.com/problems/NEARESTEXIT
+// PROBLEM DIFFICULTY RATTING: 585
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+        
+        if (x<=50)
+            printf("LEFT\n");
+        else
+            printf("RIGHT\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
