fe29e6c35737df7e1482b5bf1010ee0c3c28bb1c
yash2003bisht <yash2003bisht@gmail.com>
2023-08-21 08:59:45 +0530

    attempted question MAKEAP

---
+++ solutions/codechef/MAKEAP/MAKEAP_1.c
@@ -0,0 +1,30 @@
+// DATE: 21/08/2023, 08:53:47
+// PROBLEM NAME: Make AP
+// PROBLEM URL: https://www.codechef.com/problems/MAKEAP
+// PROBLEM DIFFICULTY RATTING: 577
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, c, num;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &a, &c);
+        num = a+c;
+        
+        if (num%2 == 0)
+            printf("%d\n", num/2);
+        else
+            printf("-1\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MAKEAP/MAKEAP_2.c
@@ -0,0 +1,38 @@
+// DATE: 21/08/2023, 08:51:22
+// PROBLEM NAME: Make AP
+// PROBLEM URL: https://www.codechef.com/problems/MAKEAP
+// PROBLEM DIFFICULTY RATTING: 577
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, c, flag, num;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &a, &c);
+        flag = 0, num;
+
+        for (int i=a; i<=c; i++){
+            if (i-a == c-i){
+                flag = 1;
+                num = i;
+                break;
+            }
+        }
+        
+        if (flag)
+            printf("%d\n", num);
+        else
+            printf("-1\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
