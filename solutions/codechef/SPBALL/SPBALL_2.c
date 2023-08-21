b88695438eb7d7677c43a98bb8097094dce1a8d8
yash2003bisht <yash2003bisht@gmail.com>
2023-08-21 08:59:45 +0530

    attempted question SPBALL

---
+++ solutions/codechef/SPBALL/SPBALL_2.c
@@ -0,0 +1,47 @@
+// DATE: 21/08/2023, 08:40:12
+// PROBLEM NAME: Spliting Balls
+// PROBLEM URL: https://www.codechef.com/problems/SPBALL
+// PROBLEM DIFFICULTY RATTING: 1435
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 9.2M
+
+#include <stdio.h>
+#define lld long long int
+#define modulo 1000000007
+#define limit 1000001
+
+void precompute_factorial(lld* arr){
+    arr[1] = 1;
+    
+    for (int i=2; i<limit; i++)
+        arr[i] = (arr[i-1] * i) % modulo;
+    
+}
+
+int main(void) {
+    int t, n, a;
+    lld ans;
+    scanf("%d", &t);
+
+    lld precomputed_fact[limit];
+    precompute_factorial(precomputed_fact);
+
+    while(t--){
+        scanf("%d", &n);
+        ans = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            ans = (ans + precomputed_fact[a]) % modulo;
+        }
+
+        printf("%lld\n", ans);
+
+    }
+    
+	return 0;
+}
+
+
+
