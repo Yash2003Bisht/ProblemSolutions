7b0a57b8b2db539a911a5633235dfbf13346dad5
yash2003bisht <yash2003bisht@gmail.com>
2023-08-25 21:33:12 +0530

    attempted question CHEFST

---
+++ solutions/codechef/CHEFST/CHEFST_1.c
@@ -0,0 +1,30 @@
+// DATE: 25/08/2023, 08:34:47
+// PROBLEM NAME: Chef and the stones
+// PROBLEM URL: https://www.codechef.com/problems/CHEFST
+// PROBLEM DIFFICULTY RATTING: 1441
+// STATUS: accepted
+// TIME: 0.05
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#define lld long long int
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t;
+    lld n1, n2, m, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld%lld", &n1, &n2, &m);
+        x = (m+1)*m/2;
+        m = min(x, min(n1, n2));
+        n1 -= m, n2 -= m;
+        printf("%lld\n", n1+n2);
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CHEFST/CHEFST_3.c
@@ -0,0 +1,34 @@
+// DATE: 25/08/2023, 08:15:39
+// PROBLEM NAME: Chef and the stones
+// PROBLEM URL: https://www.codechef.com/problems/CHEFST
+// PROBLEM DIFFICULTY RATTING: 1441
+// STATUS: partially accepted
+// TIME: 2.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#define lld long long int
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t;
+    lld n1, n2, m, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld%lld", &n1, &n2, &m);
+        
+        while(m && n1 && n2){
+            x = min(min(n1, n2), m);
+            n1 -= x, n2 -= x, m--;
+        }
+
+        printf("%lld\n", n1+n2);
+
+    }
+    
+	return 0;
+}
+
+
+
