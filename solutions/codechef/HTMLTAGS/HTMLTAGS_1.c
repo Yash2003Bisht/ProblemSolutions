e0f49478e378c9fba7eefe434bb4bf861b5d5048
yash2003bisht <yash2003bisht@gmail.com>
2023-08-25 21:33:12 +0530

    attempted question HTMLTAGS

---
+++ solutions/codechef/HTMLTAGS/HTMLTAGS_1.c
@@ -0,0 +1,47 @@
+// DATE: 25/08/2023, 07:45:58
+// PROBLEM NAME: HTML Tags
+// PROBLEM URL: https://www.codechef.com/problems/HTMLTAGS
+// PROBLEM DIFFICULTY RATTING: 1441
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, i, flag;
+    char tag[1001];
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%s", tag);
+        flag = 1;
+        
+        if (tag[0] != '<' || tag[1] != '/')
+            flag = 0;
+        else if (tag[2] == '>' || tag[2] == '\0')
+            flag = 0;
+        else{
+            for (i=2; tag[i] != '>' && tag[i] != '\0'; i++){
+                if (tag[i] < 48 || (tag[i] > 57 && tag[i] < 97) || tag[i] > 122){
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+
+        if (flag && tag[i] != '>')
+            flag = 0;
+
+        if (flag)
+            printf("Success\n");
+        else
+            printf("Error\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
