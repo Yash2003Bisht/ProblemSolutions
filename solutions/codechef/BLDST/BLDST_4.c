f32ad0e0e22edaf0c9bdd71e077ce98c9479d30d
yash2003bisht <yash2003bisht@gmail.com>
2023-08-29 09:03:14 +0530

    attempted question BLDST

---
+++ solutions/codechef/BLDST/BLDST_4.c
@@ -0,0 +1,37 @@
+// DATE: 28/08/2023, 08:07:16
+// PROBLEM NAME: Ball Distribution
+// PROBLEM URL: https://www.codechef.com/problems/BLDST
+// PROBLEM DIFFICULTY RATTING: 1442
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, m, a, total;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &n, &m);
+        total = 0;
+
+        for (int i=0; i<m; i++){
+            scanf("%d", &a);
+            total += a;
+        }
+
+        total = n - (n*m - total);
+
+        if (total > 0)
+            printf("%d\n", total);
+        else
+            printf("0\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/BLDST/BLDST_5.c
@@ -0,0 +1,40 @@
+// DATE: 28/08/2023, 08:06:32
+// PROBLEM NAME: Ball Distribution
+// PROBLEM URL: https://www.codechef.com/problems/BLDST
+// PROBLEM DIFFICULTY RATTING: 1442
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 2.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, m, total;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &n, &m);
+        int arr[m];
+        total = 0;
+
+        for (int i=0; i<m; i++){
+            scanf("%d", &arr[i]);
+        }
+   
+        for (int i=0; i<m; i++)
+            total += arr[i];
+
+        total = n - (n*m - total);
+
+        if (total > 0)
+            printf("%d\n", total);
+        else
+            printf("0\n");
+
+    }
+    
+	return 0;
+}
+
+
+
