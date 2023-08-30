35f014243f9bbb9875345ad1e6e37938ef6abe93
yash2003bisht <yash2003bisht@gmail.com>
2023-08-30 14:21:58 +0530

    attempted question TEA

---
+++ solutions/codechef/TEA/TEA_1.c
@@ -0,0 +1,33 @@
+// DATE: 30/08/2023, 01:57:50
+// PROBLEM NAME: Chef Drinks Tea
+// PROBLEM URL: https://www.codechef.com/problems/TEA
+// PROBLEM DIFFICULTY RATTING: 591
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        
+        if (y >= x)
+            printf("%d\n", z);
+        else{
+            if (x%y == 0)
+                printf("%d\n", (x/y)*z);
+            else
+                printf("%d\n", ((x/y)+1)*z);
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/TEA/TEA_3.c
@@ -0,0 +1,29 @@
+// DATE: 30/08/2023, 01:46:30
+// PROBLEM NAME: Chef Drinks Tea
+// PROBLEM URL: https://www.codechef.com/problems/TEA
+// PROBLEM DIFFICULTY RATTING: 591
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        
+        if (y > x)
+            printf("%d\n", z);
+        else
+            printf("%d\n", (x/y + x%y)*z);
+        
+    }
+    
+	return 0;
+}
+
+
+
