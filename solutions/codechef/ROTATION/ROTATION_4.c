2cb3d65b0431c1f43403b5a415dce17cdfc513e1
yash2003bisht <yash2003bisht@gmail.com>
2023-09-02 13:07:39 +0530

    attempted question ROTATION

---
+++ solutions/codechef/ROTATION/ROTATION_4.c
@@ -0,0 +1,56 @@
+// DATE: 01/09/2023, 08:26:23
+// PROBLEM NAME: Fun with Rotation
+// PROBLEM URL: https://www.codechef.com/problems/ROTATION
+// PROBLEM DIFFICULTY RATTING: 1446
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.8M
+
+#include <stdio.h>
+
+int rotate_pos(char query, int dis, int size, int crr_idx){
+    int idx, total;
+
+    if (query == 67)
+        idx = size > dis ? size-dis : size-(dis%size);
+    else
+        idx = size > dis ? dis : dis%size;
+
+    total = crr_idx + idx;
+
+    if (total >= size)
+        return total % size;
+
+    return total;
+    
+}
+
+int get_idx(int position, int dis, int size){
+    return dis < position ? size - position + dis : dis - position;
+}
+
+int main(void) {
+    int n, m, d, idx, pos = 0;
+    char query;
+    scanf("%d %d", &n, &m);
+
+    int a[n];
+
+    for (int i=0; i<n; i++)
+        scanf("%d ", &a[i]);
+
+    for (int i=0; i<m; i++){
+        scanf("%c %d\n", &query, &d);
+
+        if (query == 82){
+            idx = get_idx(pos, d-1, n);
+            printf("%d\n", a[idx]);
+        } else
+            pos = rotate_pos(query, d, n, pos);
+
+    }
+
+    return 0;
+}
+
+
---
+++ solutions/codechef/ROTATION/ROTATION_7.c
@@ -0,0 +1,59 @@
+// DATE: 31/08/2023, 09:17:52
+// PROBLEM NAME: Fun with Rotation
+// PROBLEM URL: https://www.codechef.com/problems/ROTATION
+// PROBLEM DIFFICULTY RATTING: 1446
+// STATUS: time limit exceeded
+// TIME: -
+// MEMORY: 2.1M
+
+#include <stdio.h>
+
+void copy_array(int size, int *arr1, int *arr2){
+    for (int i=0; i<size; i++)
+        arr1[i] = arr2[i];
+}
+
+void rotate(char *query, int dis, int size, int *a, int *b){
+    int idx, i;
+
+    if (query[0] == 67)
+        idx = size > dis ? size-dis : size-(dis%size);
+    else
+        idx = size > dis ? dis : dis%size;
+
+    b[idx] = a[0];
+
+    for (i=idx+1; i<size; i++)
+        b[i] = a[i - idx];
+
+    for (i=0; i<idx; i++)
+        b[i] = a[size - idx + i];
+
+    copy_array(size, a, b);
+    
+}
+
+int main(void) {
+    int n, m, d;
+    char query[2];
+    scanf("%d%d", &n, &m);
+
+    int a[n], b[n];
+
+    for (int i=0; i<n; i++)
+        scanf("%d", &a[i]);
+
+    for (int i=0; i<m; i++){
+        scanf("%s %d", query, &d);
+
+        if (query[0] == 82)
+            printf("%d\n", a[d-1]);
+        else
+            rotate(query, d, n, a, b);
+        
+    }
+
+    return 0;
+}
+
+
