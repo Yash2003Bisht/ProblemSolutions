657aafbf2b1dcc0ce183ace478c1bfd840feda75
yash2003bisht <yash2003bisht@gmail.com>
2023-09-02 13:07:38 +0530

    attempted question ELECTN

---
+++ solutions/codechef/ELECTN/ELECTN_1.c
@@ -0,0 +1,35 @@
+// DATE: 02/09/2023, 12:54:03
+// PROBLEM NAME: Elections in Chefland
+// PROBLEM URL: https://www.codechef.com/problems/ELECTN
+// PROBLEM DIFFICULTY RATTING: 604
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, x, total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &x);
+        total = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+
+            if (a >= x)
+                total++;
+
+        }
+        
+        printf("%d\n", total);
+        
+    }
+    
+	return 0;
+}
+
+
+
