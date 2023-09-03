9798ec7d45e1915873eb75defc1b781741cde580
yash2003bisht <yash2003bisht@gmail.com>
2023-09-03 14:07:01 +0530

    attempted question GSQ05

---
+++ solutions/codechef/GSQ05/GSQ05_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 22/08/2023, 08:07:50
+-- PROBLEM NAME: Insert table 
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ05
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
