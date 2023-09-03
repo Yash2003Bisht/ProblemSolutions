52e91f05b0cab679fdf304088979de466b5a33a9
yash2003bisht <yash2003bisht@gmail.com>
2023-09-03 14:07:01 +0530

    attempted question GSQ06

---
+++ solutions/codechef/GSQ06/GSQ06_1.sql
@@ -0,0 +1,16 @@
+-- DATE: 22/08/2023, 08:10:53
+-- PROBLEM NAME: Debug this query
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ06
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+his query to output the entire table */
+
+INSERT INTO employee (Id,Name,Age,Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway');
+
+SELECT * FROM employee; 
+
