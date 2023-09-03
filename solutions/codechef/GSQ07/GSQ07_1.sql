bf44ceca7ca9858f55a71fef77c98f7f9651567f
yash2003bisht <yash2003bisht@gmail.com>
2023-09-03 14:07:01 +0530

    attempted question GSQ07

---
+++ solutions/codechef/GSQ07/GSQ07_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 22/08/2023, 08:09:20
+-- PROBLEM NAME: How to insert to a table
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ07
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
