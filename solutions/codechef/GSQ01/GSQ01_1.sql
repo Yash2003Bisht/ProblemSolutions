dccfa2189a4c6b3cbe4fc95aa8a334f3447cacb4
yash2003bisht <yash2003bisht@gmail.com>
2023-09-03 14:07:01 +0530

    attempted question GSQ01

---
+++ solutions/codechef/GSQ01/GSQ01_1.sql
@@ -0,0 +1,10 @@
+-- DATE: 22/08/2023, 08:03:27
+-- PROBLEM NAME: Introduction
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ01
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+e;
+
