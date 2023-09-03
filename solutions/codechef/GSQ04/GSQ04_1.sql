633e46b25e04f2744eb733bd18e959ca923e40bb
yash2003bisht <yash2003bisht@gmail.com>
2023-09-03 14:07:01 +0530

    attempted question GSQ04

---
+++ solutions/codechef/GSQ04/GSQ04_1.sql
@@ -0,0 +1,10 @@
+-- DATE: 22/08/2023, 08:05:33
+-- PROBLEM NAME: Create table 
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ04
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+C
+
