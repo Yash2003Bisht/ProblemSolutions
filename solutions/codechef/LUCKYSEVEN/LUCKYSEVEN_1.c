f1bf8e66cb56e9e966852c83fdcd905232d681d6
yash2003bisht <yash2003bisht@gmail.com>
2023-09-08 08:55:02 +0530

    attempted question LUCKYSEVEN

---
+++ solutions/codechef/LUCKYSEVEN/LUCKYSEVEN_1.c
@@ -0,0 +1,19 @@
+// DATE: 08/09/2023, 08:54:22
+// PROBLEM NAME: Lucky Seven
+// PROBLEM URL: https://www.codechef.com/problems/LUCKYSEVEN
+// PROBLEM DIFFICULTY RATTING: 213
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    char s[11];
+    scanf("%s", s);
+    printf("%c", s[6]);
+	return 0;
+}
+
+
+
