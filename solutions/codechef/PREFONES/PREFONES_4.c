7cace69f53ca4f00a795c297dc824ae0862fad15
yash2003bisht <yash2003bisht@gmail.com>
2023-09-08 08:55:02 +0530

    attempted question PREFONES

---
+++ solutions/codechef/PREFONES/PREFONES_4.c
@@ -0,0 +1,40 @@
+// DATE: 08/09/2023, 08:49:01
+// PROBLEM NAME: Prefix Ones
+// PROBLEM URL: https://www.codechef.com/problems/PREFONES
+// PROBLEM DIFFICULTY RATTING: 1455
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.8M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+
+int main(void) {
+    int t, n, i, count, temp, start;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        scanf("%s", s);
+        i = 0, temp = 0, count = 0, start = 0;
+
+        while (s[i] != '0' && i<n)
+            start++, i++;
+
+        for (i=i+1; i<n; i++){
+            if (s[i] == '1')
+                temp++;
+            else
+                count = max(count, temp), temp = 0;
+        }
+
+        count = max(count, temp);
+        printf("%d\n", count + start);
+
+    }
+
+	return 0;
+}
+
+
