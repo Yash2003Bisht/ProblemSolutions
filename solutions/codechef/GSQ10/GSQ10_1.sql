a6edc1e3da7344c0701f04d71be0d4e850a57adc
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question GSQ10

---
+++ solutions/codechef/GSQ10/GSQ10_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:29:39
+-- PROBLEM NAME: Practice Problem - Alter table
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ10
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 4M
+
+
+
+
+
+
+
+
