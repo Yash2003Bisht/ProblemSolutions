c8371ea24a894e6bc41957a56ed4071dc2c5007a
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question JENGA

---
+++ solutions/codechef/JENGA/JENGA_1.c
@@ -0,0 +1,27 @@
+// DATE: 09/09/2023, 10:13:16
+// PROBLEM NAME: Jenga Night
+// PROBLEM URL: https://www.codechef.com/problems/JENGA
+// PROBLEM DIFFICULTY RATTING: 613
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, x;
+    scanf("%d", &t);
+    
+    while (t--){
+        scanf("%d%d", &n, &x);
+        if (x % n == 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
