0fde3e68491aa46c0dd89639d3fa19bb86b34640
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question GSQ11

---
+++ solutions/codechef/GSQ11/GSQ11_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:32:28
+-- PROBLEM NAME: Practice Problem - Update table
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ11
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 4.1M
+
+
+
+
+
+
+
+
---
+++ solutions/codechef/GSQ11/GSQ11_2.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:32:13
+-- PROBLEM NAME: Practice Problem - Update table
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ11
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: wrong answer
+-- TIME: 0.00
+-- MEMORY: 3.8M
+
+
+
+
+
+
+
+
