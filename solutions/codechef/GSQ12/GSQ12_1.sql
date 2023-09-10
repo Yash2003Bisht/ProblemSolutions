ff6b3beaa1e960559712bc231551e096f788160a
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question GSQ12

---
+++ solutions/codechef/GSQ12/GSQ12_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:33:55
+-- PROBLEM NAME: Delete From
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ12
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
