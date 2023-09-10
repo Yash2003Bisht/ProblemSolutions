43349265a012fe8357a3b4e9a9b0baf90d9f031b
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question GSQ14

---
+++ solutions/codechef/GSQ14/GSQ14_3.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:36:41
+-- PROBLEM NAME: Practice - Delete From
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ14
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
