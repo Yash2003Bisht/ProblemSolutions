b149f0836f2fa2b0c49525477e2265d3215829b5
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question GSQ13

---
+++ solutions/codechef/GSQ13/GSQ13_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:35:43
+-- PROBLEM NAME: Constraints
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ13
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+CREATE TABLE employee (
+    employee_id INTEGER PRIMARY KEY,
+    employee_Name TEXT UNIQUE,
+    Department TEXT NOT NULL
+);
+
+
