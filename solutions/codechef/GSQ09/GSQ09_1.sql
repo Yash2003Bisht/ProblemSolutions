37cd24abd3fb4df296fe6886562b4c71ce383dfd
yash2003bisht <yash2003bisht@gmail.com>
2023-09-10 21:38:27 +0530

    attempted question GSQ09

---
+++ solutions/codechef/GSQ09/GSQ09_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 10/09/2023, 09:27:50
+-- PROBLEM NAME: update table
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ09
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
