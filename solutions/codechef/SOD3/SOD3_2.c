5e520db6bbe59f7ea2138c7f0c0f7e9b8ddbc982
yash2003bisht <yash2003bisht@gmail.com>
2023-09-11 09:22:51 +0530

    attempted question SOD3

---
+++ solutions/codechef/SOD3/SOD3_2.c
@@ -0,0 +1,32 @@
+// DATE: 11/09/2023, 09:16:06
+// PROBLEM NAME: Yet another SOD problem
+// PROBLEM URL: https://www.codechef.com/problems/SOD3
+// PROBLEM DIFFICULTY RATTING: 1459
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    long long int l, r, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &l, &r);
+
+        if (l%3 != 0)
+            count = r/3 - l/3;
+        else
+            count = (r/3 - l/3) + 1;
+
+        printf("%lld\n", count);
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/SOD3/SOD3_3.c
@@ -0,0 +1,33 @@
+// DATE: 11/09/2023, 09:15:45
+// PROBLEM NAME: Yet another SOD problem
+// PROBLEM URL: https://www.codechef.com/problems/SOD3
+// PROBLEM DIFFICULTY RATTING: 1459
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t;
+    long long int l, r, total, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%lld%lld", &l, &r);
+
+        if (l%3 != 0)
+            count = r/3 - l/3;
+        else
+            count = (r/3 - l/3) + 1;
+
+        printf("%lld\n", count);
+
+    }
+    
+	return 0;
+}
+
+
+
