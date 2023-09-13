85bf098fc2488054ef4925315e4661085b3b6797
yash2003bisht <yash2003bisht@gmail.com>
2023-09-13 21:21:19 +0530

    attempted question The_World_of_Numbers

---
+++ solutions/hackerrank/The_World_of_Numbers/The_World_of_Numbers_1.sh
@@ -0,0 +1,9 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+// STATUS: Accepted
+
+read num1;
+read num2;
+echo $((num1 + num2));
+echo $((num1 - num2));
+echo $((num1 * num2));
+echo $((num1 / num2));
