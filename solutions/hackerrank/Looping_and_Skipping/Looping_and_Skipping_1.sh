e6164e0f048ecdff3e17dbd0d14fcbe62d41f039
yash2003bisht <yash2003bisht@gmail.com>
2023-09-13 21:21:19 +0530

    attempted question Looping_and_Skipping

---
+++ solutions/hackerrank/Looping_and_Skipping/Looping_and_Skipping_1.sh
@@ -0,0 +1,10 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+// STATUS: Accepted
+
+for i in {1..99}
+do
+    if (( i%2 != 0 ))
+    then
+        echo $i;
+    fi
+done
