e23b3f215668a2076003af195c33eb644e083b0f
yash2003bisht <yash2003bisht@gmail.com>
2023-09-14 09:02:36 +0530

    attempted question Revising_the_Select_Query_I

---
+++ solutions/hackerrank/Revising_the_Select_Query_I/Revising_the_Select_Query_I_1.sql
@@ -0,0 +1,4 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+// STATUS: Accepted
+
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
