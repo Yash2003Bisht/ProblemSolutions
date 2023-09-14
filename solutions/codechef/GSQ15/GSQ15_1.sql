2f710990985892c9deccc09bb1989337e2b5bd4d
yash2003bisht <yash2003bisht@gmail.com>
2023-09-14 09:01:19 +0530

    attempted question GSQ15

---
+++ solutions/codechef/GSQ15/GSQ15_1.sql
@@ -0,0 +1,17 @@
+-- DATE: 14/09/2023, 08:53:46
+-- PROBLEM NAME: Debug this query
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ15
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+es as long as you get the query to run. */
+
+INSERT INTO employee (Id, Name, Age, Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway'),
+        (3, 'Michael Brown', 45,  '124 Main St'),
+        (4, 'Jessica Davis', 28,  '321 Elm St');
+    
+
---
+++ solutions/codechef/GSQ15/GSQ15_2.sql
@@ -0,0 +1,16 @@
+-- DATE: 14/09/2023, 08:53:27
+-- PROBLEM NAME: Debug this query
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ15
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: runtime errorOTHER
+-- TIME: 0.00
+-- MEMORY: 4M
+
+es as long as you get the query to run. */
+
+INSERT INTO employee (Id, Name, Age, Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway'),
+        (3, 'Michael Brown', 45,  '123 Main St'),
+        (4, 'Jessica Davis', 28,  '321 Elm St');
+
