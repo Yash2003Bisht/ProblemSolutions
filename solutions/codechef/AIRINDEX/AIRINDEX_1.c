0e85d7cba8cc5bca12ad04ff14c0d028f79ab1ae
yash2003bisht <yash2003bisht@gmail.com>
2023-09-15 09:30:36 +0530

    attempted question AIRINDEX

---
+++ solutions/codechef/AIRINDEX/AIRINDEX_1.c
@@ -0,0 +1,24 @@
+// DATE: 15/09/2023, 09:18:20
+// PROBLEM NAME: Air Quality Index
+// PROBLEM URL: https://www.codechef.com/problems/AIRINDEX
+// PROBLEM DIFFICULTY RATTING: 347
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int aqi;
+    scanf("%d", &aqi);
+    
+    if (aqi < 100)
+        printf("YES");
+    else
+        printf("NO");
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/AIRINDEX/AIRINDEX_2.c
@@ -0,0 +1,24 @@
+// DATE: 15/09/2023, 09:18:08
+// PROBLEM NAME: Air Quality Index
+// PROBLEM URL: https://www.codechef.com/problems/AIRINDEX
+// PROBLEM DIFFICULTY RATTING: 347
+// STATUS: compilation error
+// TIME: -
+// MEMORY: -
+
+#include <stdio.h>
+
+int main(void) {
+    int aqi;
+    scanf("%d", &aqi);
+    
+    if (aqi < 100)
+        printf("YES");
+    else
+        printf("NO")
+    
+	return 0;
+}
+
+
+
