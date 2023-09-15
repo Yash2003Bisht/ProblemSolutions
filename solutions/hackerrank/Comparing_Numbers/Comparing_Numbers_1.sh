5acb888216d63084938749b436cf4d00d8f48206
yash2003bisht <yash2003bisht@gmail.com>
2023-09-15 09:53:52 +0530

    attempted question Comparing_Numbers

---
+++ solutions/hackerrank/Comparing_Numbers/Comparing_Numbers_1.sh
@@ -0,0 +1,15 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/bash-tutorials---comparing-numbers/problem
+// STATUS: Accepted
+
+read num1;
+read num2;
+
+if (( num1 > num2 ))
+then
+    echo "X is greater than Y";
+elif (( num1 == num2 ))
+then
+    echo "X is equal to Y";
+else
+    echo "X is less than Y";
+fi
