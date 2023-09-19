28029c951ee68618b9011c4af0b94cef6d34b753
yash2003bisht <yash2003bisht@gmail.com>
2023-09-19 09:59:49 +0530

    attempted question Cut__1

---
+++ solutions/hackerrank/Cut__1/Cut__1_2.sh
@@ -0,0 +1,6 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/text-processing-cut-1/problem
+# STATUS: Accepted
+
+#! /bin/bash
+
+cut -c3
---
+++ solutions/hackerrank/Cut__1/Cut__1_3.sh
@@ -0,0 +1,11 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/text-processing-cut-1/problem
+# STATUS: Wrong Answer
+
+#! /bin/bash
+read a;
+read b;
+read c;
+
+echo $a | cut -c3;
+echo $b | cut -c3;
+echo $c | cut -c3;
