fd5944de9330d551c19a521fa503dd0bf0367033
yash2003bisht <yash2003bisht@gmail.com>
2023-09-20 09:08:50 +0530

    attempted question Cut__3

---
+++ solutions/hackerrank/Cut__3/Cut__3_2.sh
@@ -0,0 +1,4 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/text-processing-cut-3/problem
+# STATUS: Accepted
+
+cut -c2-7;
---
+++ solutions/hackerrank/Cut__3/Cut__3_3.sh
@@ -0,0 +1,4 @@
+# QUESTION URL: https://www.hackerrank.com/challenges/text-processing-cut-3/problem
+# STATUS: Accepted
+
+cut -c2,3,4,5,6,7;
