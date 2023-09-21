23d59c04c911038227da90df3bc8148023463849
yash2003bisht <yash2003bisht@gmail.com>
2023-09-21 08:28:26 +0530

    attempted question OFFBY1

---
+++ solutions/codechef/OFFBY1/OFFBY1_1.c
@@ -0,0 +1,19 @@
+// DATE: 21/09/2023, 08:22:46
+// PROBLEM NAME: Off By One
+// PROBLEM URL: https://www.codechef.com/problems/OFFBY1
+// PROBLEM DIFFICULTY RATTING: 271
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d1\n", a+b);
+	return 0;
+}
+
+
+
