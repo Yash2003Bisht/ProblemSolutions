ad1b363d03b56d2702226d79e5eacb72e5fcb833
yash2003bisht <yash2003bisht@gmail.com>
2023-09-22 08:58:34 +0530

    attempted question MOVIE2X

---
+++ solutions/codechef/MOVIE2X/MOVIE2X_1.c
@@ -0,0 +1,19 @@
+// DATE: 22/09/2023, 08:57:59
+// PROBLEM NAME: Watching Movies at 2x
+// PROBLEM URL: https://www.codechef.com/problems/MOVIE2X
+// PROBLEM DIFFICULTY RATTING: 628
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+    scanf("%d%d", &x, &y);
+    printf("%d\n", y/2 + (x-y));
+	return 0;
+}
+
+
+
