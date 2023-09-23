31584e560f7bfacac9ddddb070d4eae4a46598e1
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ18

---
+++ solutions/codechef/GSQ18/GSQ18_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:21:30
+-- PROBLEM NAME: DISTINCT
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ18
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 4M
+
+
+
+
+
+
+
+
