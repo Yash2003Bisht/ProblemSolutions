17a700648d160131d8b0be920f50af6ac8826746
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ16

---
+++ solutions/codechef/GSQ16/GSQ16_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:19:13
+-- PROBLEM NAME: Introduction to Queries
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ16
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 4.1M
+
+
+
+
+
+
+
+
