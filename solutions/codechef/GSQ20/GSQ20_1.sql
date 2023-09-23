cac25e3f76d1a826e61022eafccc082da4bf1515
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ20

---
+++ solutions/codechef/GSQ20/GSQ20_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:25:03
+-- PROBLEM NAME: Practice problem
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ20
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 4M
+
+
+
+
+
+
+
+
