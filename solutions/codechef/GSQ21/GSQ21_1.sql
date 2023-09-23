d973376b539e65e48d32598377777f229bb89fc1
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ21

---
+++ solutions/codechef/GSQ21/GSQ21_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:26:35
+-- PROBLEM NAME: Debug this query
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ21
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
