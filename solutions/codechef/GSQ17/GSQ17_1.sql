29e63d3b233747ebec4f0b4ebc741b99c08721f9
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ17

---
+++ solutions/codechef/GSQ17/GSQ17_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:20:34
+-- PROBLEM NAME: SELECT Query
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ17
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
