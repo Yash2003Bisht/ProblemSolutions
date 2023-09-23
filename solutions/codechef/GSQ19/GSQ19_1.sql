3d324c1b1fddf357ca6235fdac67bc1e22026c13
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ19

---
+++ solutions/codechef/GSQ19/GSQ19_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:22:51
+-- PROBLEM NAME: WHERE
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ19
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
---
+++ solutions/codechef/GSQ19/GSQ19_2.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:22:37
+-- PROBLEM NAME: WHERE
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ19
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.8M
+
+
+
+
+
+
+
+
