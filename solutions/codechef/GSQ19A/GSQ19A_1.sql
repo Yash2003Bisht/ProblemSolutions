08ae1d4768a517a4c5c7d5082f6c25b802ee4a05
yash2003bisht <yash2003bisht@gmail.com>
2023-09-23 08:31:45 +0530

    attempted question GSQ19A

---
+++ solutions/codechef/GSQ19A/GSQ19A_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 23/09/2023, 08:24:03
+-- PROBLEM NAME: BETWEEN
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ19A
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.9M
+
+
+
+
+
+
+
+
