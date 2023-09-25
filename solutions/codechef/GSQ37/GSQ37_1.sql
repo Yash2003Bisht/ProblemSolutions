fac2633726b6448d27676ede16450d557b3152ab
yash2003bisht <yash2003bisht@gmail.com>
2023-09-25 08:43:23 +0530

    attempted question GSQ37

---
+++ solutions/codechef/GSQ37/GSQ37_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 25/09/2023, 07:46:27
+-- PROBLEM NAME: COUNT()
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ37
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 4M
+
+
+
+
+
+
+
+
---
+++ solutions/codechef/GSQ37/GSQ37_3.sql
@@ -0,0 +1,15 @@
+-- DATE: 25/09/2023, 07:45:37
+-- PROBLEM NAME: COUNT()
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ37
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: wrong answer
+-- TIME: 0.00
+-- MEMORY: 4M
+
+
+
+
+
+
+
+
