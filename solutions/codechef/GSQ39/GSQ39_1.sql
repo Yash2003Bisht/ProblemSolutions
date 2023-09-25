5c769c87a1dbae0a1f4e25a64928b326d2b100ba
yash2003bisht <yash2003bisht@gmail.com>
2023-09-25 08:43:23 +0530

    attempted question GSQ39

---
+++ solutions/codechef/GSQ39/GSQ39_1.sql
@@ -0,0 +1,15 @@
+-- DATE: 25/09/2023, 07:49:25
+-- PROBLEM NAME: ROUND() 
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ39
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.8M
+
+
+
+
+
+
+
+
