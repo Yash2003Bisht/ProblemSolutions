e9aa97e4d1ca2de6f33426f5d12db800123b3b5b
yash2003bisht <yash2003bisht@gmail.com>
2023-09-25 08:43:22 +0530

    attempted question GSQ38

---
+++ solutions/codechef/GSQ38/GSQ38_1.sql
@@ -0,0 +1,11 @@
+-- DATE: 25/09/2023, 07:48:19
+-- PROBLEM NAME: MAX() and MIN() 
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ38
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: accepted
+-- TIME: 0.00
+-- MEMORY: 3.8M
+
+mployee;
+
+
---
+++ solutions/codechef/GSQ38/GSQ38_2.sql
@@ -0,0 +1,11 @@
+-- DATE: 25/09/2023, 07:47:50
+-- PROBLEM NAME: MAX() and MIN() 
+-- PROBLEM URL: https://www.codechef.com/problems/GSQ38
+-- PROBLEM DIFFICULTY RATTING: -1
+-- STATUS: runtime errorOTHER
+-- TIME: 0.00
+-- MEMORY: 3.8M
+
+ee;
+
+
