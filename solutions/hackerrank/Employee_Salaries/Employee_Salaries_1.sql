e53279a601af6140e9add57a3ccfa9b611cef94e
yash2003bisht <yash2003bisht@gmail.com>
2023-09-27 08:39:59 +0530

    attempted question Employee_Salaries

---
+++ solutions/hackerrank/Employee_Salaries/Employee_Salaries_1.sql
@@ -0,0 +1,4 @@
+-- QUESTION URL: https://www.hackerrank.com/challenges/salary-of-employees/problem
+-- STATUS: Accepted
+
+SELECT name FROM Employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id;
