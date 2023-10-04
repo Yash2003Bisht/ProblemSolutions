7f4aee099c6874cf69f07e4e429665bea20929ad
yash2003bisht <yash2003bisht@gmail.com>
2023-10-04 08:52:25 +0530

    attempted question WDTBAM

---
+++ solutions/codechef/WDTBAM/WDTBAM_4.c
@@ -0,0 +1,50 @@
+// DATE: 03/10/2023, 09:02:17
+// PROBLEM NAME: Who dares to be a millionaire
+// PROBLEM URL: https://www.codechef.com/problems/WDTBAM
+// PROBLEM DIFFICULTY RATTING: 1497
+// STATUS: accepted
+// TIME: 0.06
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, n, count, ans, i;
+	scanf("%d", &t);
+
+	while(t--) {
+	    scanf("%d", &n);
+
+	    int marks[n];
+	    char correct_ans[n], chef_ans[n];
+
+	    scanf("%s", correct_ans);
+	    scanf("%s", chef_ans);
+	    
+	    for(i=0, count=0; i<n; i++)
+    	    if(correct_ans[i] == chef_ans[i])
+    	        count++;
+
+	    scanf("%d", &ans);
+
+	    for(i=0; i<n; i++)
+	        scanf("%d", &marks[i]);
+
+	    if(count == n)
+    	    printf("%d\n", marks[n-1]);
+	    else {
+	        i=0;
+
+	        while(count--) {
+	            if(ans < marks[i])
+	                ans = marks[i];
+	            i++;
+	        }
+
+	        printf("%d\n", ans);
+	    }
+	}
+	return 0;
+}
+
+
---
+++ solutions/codechef/WDTBAM/WDTBAM_5.c
@@ -0,0 +1,41 @@
+// DATE: 03/10/2023, 08:23:12
+// PROBLEM NAME: Who dares to be a millionaire
+// PROBLEM URL: https://www.codechef.com/problems/WDTBAM
+// PROBLEM DIFFICULTY RATTING: 1497
+// STATUS: wrong answer
+// TIME: 0.07
+// MEMORY: 1.5M
+
+#include <stdio.h>
+#define max(a, b)(a>b?a:b)
+
+int main(void) {
+    int t, n, count;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        char correct_ans[n+1], chef_answer[n+1];
+        int marks[n];
+
+        scanf("%s\n%s", correct_ans, chef_answer);
+
+        for (int i=0; i<n+1; i++)
+            scanf("%d", &marks[i]);
+
+        count = 0;
+
+        for (int i=0; i<n; i++){
+            if (correct_ans[i] == chef_answer[i])
+                count++;
+        }
+
+        printf("%d\n", marks[count]);
+
+    }
+    
+	return 0;
+}
+
+
+
