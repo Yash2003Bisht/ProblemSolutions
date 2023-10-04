7023286f70cdae0dc5e005a773469c1a4f60ed77
yash2003bisht <yash2003bisht@gmail.com>
2023-10-04 08:52:25 +0530

    attempted question N1VALUES

---
+++ solutions/codechef/N1VALUES/N1VALUES_1.c
@@ -0,0 +1,39 @@
+// DATE: 04/10/2023, 07:23:52
+// PROBLEM NAME: Exactly N plus 1 Values
+// PROBLEM URL: https://www.codechef.com/problems/N1VALUES
+// PROBLEM DIFFICULTY RATTING: 1495
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    long long int total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        printf("1 1 ");
+
+        if (n > 1){
+            total = pow(2, n) - 2;
+
+            for (int i=2; i<n; i++){
+                printf("%d ", i);
+                total -= i;
+            }
+
+            printf("%lld", total);
+        }
+
+        printf("\n");
+
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/N1VALUES/N1VALUES_3.c
@@ -0,0 +1,42 @@
+// DATE: 04/10/2023, 07:21:20
+// PROBLEM NAME: Exactly N plus 1 Values
+// PROBLEM URL: https://www.codechef.com/problems/N1VALUES
+// PROBLEM DIFFICULTY RATTING: 1495
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void) {
+    int t, n;
+    long long int total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        printf("1 1 ");
+
+        if (n > 1){
+            total = pow(2, n) - 2;
+
+            for (int i=2; i<n; i++){
+                printf("%d ", i);
+                total -= i;
+            }
+    
+            printf("%lld", total);
+        }
+
+        printf("\n");
+
+    }
+    
+	return 0;
+}
+
+
+
