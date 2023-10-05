bc64b3c9ad2a118c947f21cb622342f8fff04c5f
yash2003bisht <yash2003bisht@gmail.com>
2023-10-05 08:25:16 +0530

    attempted question BATH

---
+++ solutions/codechef/BATH/BATH_1.c
@@ -0,0 +1,24 @@
+// DATE: 05/10/2023, 08:23:28
+// PROBLEM NAME: Bath in Winters
+// PROBLEM URL: https://www.codechef.com/problems/BATH
+// PROBLEM DIFFICULTY RATTING: 643
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        printf("%d\n", x/(y*2));
+    }
+    
+	return 0;
+}
+
+
+
