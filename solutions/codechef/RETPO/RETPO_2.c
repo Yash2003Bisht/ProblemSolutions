c5a98237c4e810087a6d2e30d74fc58746583e88
yash2003bisht <yash2003bisht@gmail.com>
2023-10-06 08:49:30 +0530

    attempted question RETPO

---
+++ solutions/codechef/RETPO/RETPO_2.c
@@ -0,0 +1,27 @@
+// DATE: 06/10/2023, 08:03:30
+// PROBLEM NAME: Reach The Point
+// PROBLEM URL: https://www.codechef.com/problems/RETPO
+// PROBLEM DIFFICULTY RATTING: 1495
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#include <math.h>
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        x = abs(x), y = abs(y);
+        printf("%d\n", 2*min(x, y) + 2*abs(x-y) + ((x-y)%2));
+    }
+
+	return 0;
+}
+
+
+
