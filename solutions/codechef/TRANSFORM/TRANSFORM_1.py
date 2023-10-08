25cbb112acd6c3ae5aac0882c7241c86b542e68f
yash2003bisht <yash2003bisht@gmail.com>
2023-10-08 17:45:12 +0530

    attempted question TRANSFORM

---
+++ solutions/codechef/TRANSFORM/TRANSFORM_1.py
@@ -0,0 +1,15 @@
+# DATE: 07/10/2023, 09:10:29
+# PROBLEM NAME: Mario and Transformation
+# PROBLEM URL: https://www.codechef.com/problems/TRANSFORM
+# PROBLEM DIFFICULTY RATTING: 649
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.5M
+
+for _ in range(int(input())):
+    x = int(input())
+    mario_size = ["NORMAL", "HUGE", "SMALL"]
+    n = len(mario_size)
+    print(mario_size[x%n])
+
+
---
+++ solutions/codechef/TRANSFORM/TRANSFORM_2.py
@@ -0,0 +1,14 @@
+# DATE: 07/10/2023, 09:09:57
+# PROBLEM NAME: Mario and Transformation
+# PROBLEM URL: https://www.codechef.com/problems/TRANSFORM
+# PROBLEM DIFFICULTY RATTING: 649
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 9.5M
+
+for _ in range(int(input())):
+    x = int(input())
+    mario_size = ["NORMAL", "HUGE", "SMALL"]
+    print(mario_size[x%3])
+    
+
