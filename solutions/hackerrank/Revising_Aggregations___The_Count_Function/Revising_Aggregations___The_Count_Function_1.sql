7a58afa7b2856f4779073ffe6aeb5eae1b552d04
yash2003bisht <yash2003bisht@gmail.com>
2023-10-14 10:17:53 +0530

    attempted question Revising_Aggregations___The_Count_Function

---
+++ solutions/hackerrank/Revising_Aggregations___The_Count_Function/Revising_Aggregations___The_Count_Function_1.sql
@@ -0,0 +1,4 @@
+-- QUESTION URL: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+-- STATUS: Accepted
+
+SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000;
---
+++ solutions/hackerrank/Revising_Aggregations___The_Count_Function/Revising_Aggregations___The_Count_Function_2.sql
@@ -0,0 +1,4 @@
+-- QUESTION URL: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+-- STATUS: Accepted
+
+SELECT COUNT(name) FROM CITY WHERE population > 100000;
