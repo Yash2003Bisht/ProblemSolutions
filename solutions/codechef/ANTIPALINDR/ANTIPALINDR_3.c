b33baa02e293714a61a72e1327760ac833a46ac5
yash2003bisht <yash2003bisht@gmail.com>
2023-10-15 20:50:09 +0530

    attempted question ANTIPALINDR

---
+++ solutions/codechef/ANTIPALINDR/ANTIPALINDR_3.c
@@ -0,0 +1,60 @@
+// DATE: 12/10/2023, 08:26:07
+// PROBLEM NAME: Anti Palindrome
+// PROBLEM URL: https://www.codechef.com/problems/ANTIPALINDR
+// PROBLEM DIFFICULTY RATTING: 1488
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int is_palindrome_by_char(char *s, int size){
+    int counter[26] = {0}, odd = 0, even = 0;
+
+    for (int i=0; i<size; i++)
+        counter[s[i] - 97]++;
+
+    for (int i=0; i<26; i++){
+        if (counter[i]){
+            if (counter[i] == size)
+                return size;
+            else if (counter[i]%2 == 0)
+                even++;
+            else
+                odd++;
+        }
+    }
+
+    if ((even && !odd) || (even && odd == 1))
+        return 1;
+
+    return 0;
+
+}
+
+int main(void) {
+    int t, n, count;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        char s[n+1];
+        scanf("%s", s);
+
+        count = is_palindrome_by_char(s, n);
+
+        if (count){
+            if (count != n || count%2 == 0)
+                printf("1\n");
+            else
+                printf("2\n");
+        } else {
+            printf("0\n");
+        }
+
+    }
+
+	return 0;
+}
+
+
