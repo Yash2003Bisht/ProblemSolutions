5adbeabd5aeb189b4a30e42158207ee24a36a355
yash2003bisht <yash2003bisht@gmail.com>
2023-10-15 20:50:09 +0530

    attempted question OCTATHON

---
+++ solutions/codechef/OCTATHON/OCTATHON_1.c
@@ -0,0 +1,26 @@
+// DATE: 12/10/2023, 08:42:07
+// PROBLEM NAME: October Marathon
+// PROBLEM URL: https://www.codechef.com/problems/OCTATHON
+// PROBLEM DIFFICULTY RATTING: 319
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    scanf("%d", &x);
+    
+    if (x < 3)
+        printf("GOLD");
+    else if (x < 6)
+        printf("SILVER");
+    else
+        printf("BRONZE");
+    
+	return 0;
+}
+
+
+
