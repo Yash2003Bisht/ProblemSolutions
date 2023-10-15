1d25ee359de8b9afda4fa57e734bbbef04b2ecbd
yash2003bisht <yash2003bisht@gmail.com>
2023-10-15 20:50:09 +0530

    attempted question BRUNCH

---
+++ solutions/codechef/BRUNCH/BRUNCH_1.c
@@ -0,0 +1,30 @@
+// DATE: 12/10/2023, 08:58:26
+// PROBLEM NAME: Sunday Brunch
+// PROBLEM URL: https://www.codechef.com/problems/BRUNCH
+// PROBLEM DIFFICULTY RATTING: 648
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, x, y, hungry_neighbours;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &x, &y);
+        hungry_neighbours = x/y;
+
+        if (hungry_neighbours < 20)
+            printf("%d\n", hungry_neighbours);
+        else
+            printf("20\n");
+
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/BRUNCH/BRUNCH_2.c
@@ -0,0 +1,32 @@
+// DATE: 12/10/2023, 08:54:04
+// PROBLEM NAME: Sunday Brunch
+// PROBLEM URL: https://www.codechef.com/problems/BRUNCH
+// PROBLEM DIFFICULTY RATTING: 648
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, x, y, plates;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &x, &y);
+        plates = y*20;
+
+        if (x < plates){
+            int ans = 20 - ceil((float) (plates-x)/y);
+            printf("%d\n", ans);
+        } else
+            printf("20\n");
+
+    }
+    
+	return 0;
+}
+
+
+
