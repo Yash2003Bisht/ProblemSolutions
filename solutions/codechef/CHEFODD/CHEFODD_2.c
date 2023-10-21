babf7952938d4eac00f4f5a0cdb96c1496fc23d7
yash2003bisht <yash2003bisht@gmail.com>
2023-10-21 09:12:07 +0530

    attempted question CHEFODD

---
+++ solutions/codechef/CHEFODD/CHEFODD_2.c
@@ -0,0 +1,31 @@
+// DATE: 21/10/2023, 09:10:37
+// PROBLEM NAME: Chef Odd
+// PROBLEM URL: https://www.codechef.com/problems/CHEFODD
+// PROBLEM DIFFICULTY RATTING: 1486
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#define lld long long int
+
+int main(void) {
+    int t;
+    lld n, k, total;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%lld%lld", &n, &k);
+        total = n/2 + n%2;
+
+        if (k*2 > n || (total-k)%2 != 0)
+            printf("NO\n");
+        else
+            printf("YES\n");
+
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/CHEFODD/CHEFODD_3.c
@@ -0,0 +1,31 @@
+// DATE: 21/10/2023, 08:36:56
+// PROBLEM NAME: Chef Odd
+// PROBLEM URL: https://www.codechef.com/problems/CHEFODD
+// PROBLEM DIFFICULTY RATTING: 1486
+// STATUS: wrong answer
+// TIME: 0.01
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#define lld long long int
+
+int main(void) {
+    int t;
+    lld n, k, total;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%lld%lld", &n, &k);
+        total = k*2;
+
+        if (total > n || (n - total)%2 != 0)
+            printf("NO\n");
+        else
+            printf("YES\n");
+
+    }
+
+	return 0;
+}
+
+
