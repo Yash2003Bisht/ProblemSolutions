b34497210a45f7524dbe97bd77569af152d18282
yash2003bisht <yash2003bisht@gmail.com>
2023-10-21 07:56:43 +0530

    attempted question MYSERVE

---
+++ solutions/codechef/MYSERVE/MYSERVE_1.c
@@ -0,0 +1,29 @@
+// DATE: 20/10/2023, 08:52:39
+// PROBLEM NAME: It is My Serve
+// PROBLEM URL: https://www.codechef.com/problems/MYSERVE
+// PROBLEM DIFFICULTY RATTING: 691
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, p, q;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &p, &q);
+
+        if (((p+q)/2)%2 == 0)
+            printf("Alice\n");
+        else
+            printf("Bob\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/MYSERVE/MYSERVE_2.c
@@ -0,0 +1,30 @@
+// DATE: 20/10/2023, 08:52:06
+// PROBLEM NAME: It is My Serve
+// PROBLEM URL: https://www.codechef.com/problems/MYSERVE
+// PROBLEM DIFFICULTY RATTING: 691
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, p, q, total;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &p, &q);
+        total = p+q;
+
+        if ((total/2)%2 == 0)
+            printf("Alice\n");
+        else
+            printf("Bob\n");
+
+    }
+    
+	return 0;
+}
+
+
+
