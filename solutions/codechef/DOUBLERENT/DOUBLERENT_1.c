900c188467e36aa1c65f20cb2b3e0dbc9e4aaba6
yash2003bisht <yash2003bisht@gmail.com>
2023-10-21 07:56:43 +0530

    attempted question DOUBLERENT

---
+++ solutions/codechef/DOUBLERENT/DOUBLERENT_1.c
@@ -0,0 +1,19 @@
+// DATE: 20/10/2023, 08:36:36
+// PROBLEM NAME: Double Rent
+// PROBLEM URL: https://www.codechef.com/problems/DOUBLERENT
+// PROBLEM DIFFICULTY RATTING: 234
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+    printf("%d", a*2);
+	return 0;
+}
+
+
+
