fb97f3c36213e8416192236b0c4325641d414f7e
yash2003bisht <yash2003bisht@gmail.com>
2023-10-24 21:03:48 +0530

    attempted question WTRMIXING

---
+++ solutions/codechef/WTRMIXING/WTRMIXING_2.c
@@ -0,0 +1,33 @@
+// DATE: 24/10/2023, 02:18:06
+// PROBLEM NAME: Water Mixing
+// PROBLEM URL: https://www.codechef.com/problems/WTRMIXING
+// PROBLEM DIFFICULTY RATTING: 694
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) {
+    int t, x, y, a, b, req_temp;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &x, &y);
+        req_temp = a - b;
+
+        if (req_temp == 0 || (req_temp > 0 && req_temp <= y) || (req_temp < 0 && abs(req_temp) <= x))
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
+
---
+++ solutions/codechef/WTRMIXING/WTRMIXING_3.c
@@ -0,0 +1,30 @@
+// DATE: 24/10/2023, 02:07:46
+// PROBLEM NAME: Water Mixing
+// PROBLEM URL: https://www.codechef.com/problems/WTRMIXING
+// PROBLEM DIFFICULTY RATTING: 694
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, a, b, req_temp;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &x, &y);
+        req_temp = b - a;
+
+        if (req_temp == 0 || (req_temp > 0 && req_temp >= y) || (req_temp < 0 && abs(req_temp) >= x))
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+	return 0;
+}
+
+
+
