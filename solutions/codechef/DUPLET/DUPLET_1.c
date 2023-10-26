0f4473d6f2637a9c74527d49c4b55496fe35b644
yash2003bisht <yash2003bisht@gmail.com>
2023-10-26 09:19:22 +0530

    attempted question DUPLET

---
+++ solutions/codechef/DUPLET/DUPLET_1.c
@@ -0,0 +1,24 @@
+// DATE: 26/10/2023, 08:00:42
+// PROBLEM NAME: Dull Operation
+// PROBLEM URL: https://www.codechef.com/problems/DUPLET
+// PROBLEM DIFFICULTY RATTING: 1490
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        printf("%d %d\n", n, n-1);
+    }
+    
+	return 0;
+}
+
+
+
