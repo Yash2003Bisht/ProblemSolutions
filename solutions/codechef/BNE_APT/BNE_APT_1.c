0ea7486fd8eb57282f4345ea33a9f17e92c4031a
yash2003bisht <yash2003bisht@gmail.com>
2023-10-27 12:26:01 +0530

    attempted question BNE_APT

---
+++ solutions/codechef/BNE_APT/BNE_APT_1.c
@@ -0,0 +1,19 @@
+// DATE: 27/10/2023, 09:31:23
+// PROBLEM NAME: Bone Appetit
+// PROBLEM URL: https://www.codechef.com/problems/BNE_APT
+// PROBLEM DIFFICULTY RATTING: 280
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int n, m, x, y;
+    scanf("%d%d\n%d%d", &n, &m, &x, &y);
+    printf("%d\n", n*x + m*y);
+	return 0;
+}
+
+
+
