28d47184525c279da8e9db3fcf1f69e21ff57b1a
yash2003bisht <yash2003bisht@gmail.com>
2023-10-30 11:34:57 +0530

    attempted question LUCKY5

---
+++ solutions/codechef/LUCKY5/LUCKY5_1.c
@@ -0,0 +1,33 @@
+// DATE: 30/10/2023, 08:54:36
+// PROBLEM NAME: Lucky Long
+// PROBLEM URL: https://www.codechef.com/problems/LUCKY5
+// PROBLEM DIFFICULTY RATTING: 1478
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, count;
+    char n[100002];
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%s", n);
+        count = 0;
+
+        for (int i=0; n[i] != '\0'; i++){
+            if (n[i] != '4' && n[i] != '7')
+                count++;
+        }
+
+        printf("%d\n", count);
+
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/LUCKY5/LUCKY5_2.py
@@ -0,0 +1,20 @@
+# DATE: 30/10/2023, 08:54:22
+# PROBLEM NAME: Lucky Long
+# PROBLEM URL: https://www.codechef.com/problems/LUCKY5
+# PROBLEM DIFFICULTY RATTING: 1478
+# STATUS: accepted
+# TIME: 0.11
+# MEMORY: 8.8M
+
+t=int(input())
+i=0
+while i<t:
+    s=input().strip()
+    cnt=0
+    for j in s:
+        if j!="4" and j!="7":
+            cnt+=1
+    print(cnt)
+    i+=1
+
+
