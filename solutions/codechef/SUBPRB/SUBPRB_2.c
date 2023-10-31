e94df677f7ebd5331e5227b5f2b15c67d8bcb368
yash2003bisht <yash2003bisht@gmail.com>
2023-10-31 09:50:12 +0530

    attempted question SUBPRB

---
+++ solutions/codechef/SUBPRB/SUBPRB_2.c
@@ -0,0 +1,31 @@
+// DATE: 31/10/2023, 08:44:55
+// PROBLEM NAME: Yet another subarray problem
+// PROBLEM URL: https://www.codechef.com/problems/SUBPRB
+// PROBLEM DIFFICULTY RATTING: 1478
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, counter;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        counter = 1;
+
+        for (int i=0; i<n; i++){
+            printf("%d ", counter);
+            counter += 2;
+        }
+        printf("\n");
+
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/SUBPRB/SUBPRB_3.c
@@ -0,0 +1,33 @@
+// DATE: 31/10/2023, 08:43:38
+// PROBLEM NAME: Yet another subarray problem
+// PROBLEM URL: https://www.codechef.com/problems/SUBPRB
+// PROBLEM DIFFICULTY RATTING: 1478
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, arr[1000] = {0};
+    arr[0] = 1;
+
+    for (int i=1; i<1000; i++)
+        arr[i] = arr[i-1] + 2;
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+
+        for (int i=0; i<n; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+
+    }
+
+	return 0;
+}
+
+
+
