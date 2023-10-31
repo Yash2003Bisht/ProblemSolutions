f26d9aa3b07221a4d9ce211db4b9fb545462d952
yash2003bisht <yash2003bisht@gmail.com>
2023-10-31 09:50:12 +0530

    attempted question CHEFFED

---
+++ solutions/codechef/CHEFFED/CHEFFED_1.c
@@ -0,0 +1,40 @@
+// DATE: 31/10/2023, 09:46:51
+// PROBLEM NAME: Chef and Numbers
+// PROBLEM URL: https://www.codechef.com/problems/CHEFFED
+// PROBLEM DIFFICULTY RATTING: 1477
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int digitSum(int num){
+    int sum = 0;
+
+    while(num){
+        sum += num%10;
+        num = num/10;
+    }
+    
+    return sum;
+}
+
+int main(void) {
+    int n, count = 0, total, temp;
+    scanf("%d", &n);
+
+    for (int i=1; i<81; i++){
+        total = n - (i + digitSum(i));
+
+        if (digitSum(total) == i)
+            count++;
+
+    }
+
+    printf("%d\n", count);
+
+	return 0;
+}
+
+
+
