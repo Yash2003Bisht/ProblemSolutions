ad4a39882409e03b0a8d1d8b4cc6e1a8e04a68e6
yash2003bisht <yash2003bisht@gmail.com>
2023-11-02 09:14:20 +0530

    attempted question FIZZBUZZ2301

---
+++ solutions/codechef/FIZZBUZZ2301/FIZZBUZZ2301_1.c
@@ -0,0 +1,24 @@
+// DATE: 02/11/2023, 09:11:19
+// PROBLEM NAME: Fan Poll
+// PROBLEM URL: https://www.codechef.com/problems/FIZZBUZZ2301
+// PROBLEM DIFFICULTY RATTING: 273
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (a > b && a > c)
+        printf("Yes");
+    else
+        printf("No");
+
+	return 0;
+}
+
+
+
