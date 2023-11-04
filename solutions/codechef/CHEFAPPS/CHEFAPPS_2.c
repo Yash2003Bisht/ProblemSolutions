3db797e51a5e845e2f1da9ec317245266c4fde0d
yash2003bisht <yash2003bisht@gmail.com>
2023-11-04 10:21:49 +0530

    attempted question CHEFAPPS

---
+++ solutions/codechef/CHEFAPPS/CHEFAPPS_2.c
@@ -0,0 +1,33 @@
+// DATE: 04/11/2023, 10:13:00
+// PROBLEM NAME: Chef and his Apps
+// PROBLEM URL: https://www.codechef.com/problems/CHEFAPPS
+// PROBLEM DIFFICULTY RATTING: 702
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, s, x, y, z, remaining_storage, required_storage;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &s, &x, &y, &z);
+        remaining_storage = s - (x+y);
+
+        if (remaining_storage >= z)
+            printf("0\n");
+        else {
+            if (remaining_storage + x >= z || remaining_storage + y >= z)
+                printf("1\n");
+            else
+                printf("2\n");
+        }
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CHEFAPPS/CHEFAPPS_5.c
@@ -0,0 +1,36 @@
+// DATE: 04/11/2023, 10:05:10
+// PROBLEM NAME: Chef and his Apps
+// PROBLEM URL: https://www.codechef.com/problems/CHEFAPPS
+// PROBLEM DIFFICULTY RATTING: 702
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, s, x, y, z, remaining_storage, required_storage;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &s, &x, &y, &z);
+        remaining_storage = s - (x+y);
+        required_storage = z - remaining_storage;
+
+        if (required_storage >= 0){
+            if (required_storage >= (x+y))
+                printf("2\n");
+            else
+                printf("1\n");
+        } else {
+            printf("0\n");
+        }
+
+
+    }
+    
+	return 0;
+}
+
+
+
