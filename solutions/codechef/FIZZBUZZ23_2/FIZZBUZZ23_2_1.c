0bbc6fc2aee64f7a9c35b5d2cea5a224b76dd91f
yash2003bisht <yash2003bisht@gmail.com>
2023-11-04 10:21:49 +0530

    attempted question FIZZBUZZ23_2

---
+++ solutions/codechef/FIZZBUZZ23_2/FIZZBUZZ23_2_1.c
@@ -0,0 +1,23 @@
+// DATE: 03/11/2023, 09:30:07
+// PROBLEM NAME: Survival Time
+// PROBLEM URL: https://www.codechef.com/problems/FIZZBUZZ23_2
+// PROBLEM DIFFICULTY RATTING: 716
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main() {
+    int t, n, x, d;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &x, &d);
+        printf("%d\n", n/(x*5) + d);
+
+    }
+	return 0;
+}
+
+
