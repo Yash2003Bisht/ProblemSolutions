b3db2079728db3c308787d4cacd473c08fdde8fa
yash2003bisht <yash2003bisht@gmail.com>
2023-11-08 09:10:01 +0530

    attempted question TSORT

---
+++ solutions/codechef/TSORT/TSORT_1.py
@@ -0,0 +1,19 @@
+# DATE: 07/11/2023, 09:07:51
+# PROBLEM NAME: Turbo Sort
+# PROBLEM URL: https://www.codechef.com/problems/TSORT
+# PROBLEM DIFFICULTY RATTING: 667
+# STATUS: accepted
+# TIME: 1.46
+# MEMORY: 49.4M
+
+num = []
+
+for _ in range(int(input())):
+    num.append(int(input()))
+
+num.sort()
+
+for i in num:
+    print(i)
+
+
---
+++ solutions/codechef/TSORT/TSORT_2.cpp
@@ -0,0 +1,31 @@
+// DATE: 07/11/2023, 09:06:35
+// PROBLEM NAME: Turbo Sort
+// PROBLEM URL: https://www.codechef.com/problems/TSORT
+// PROBLEM DIFFICULTY RATTING: 667
+// STATUS: accepted
+// TIME: 1.39
+// MEMORY: 6.8M
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	vector <int> a(t);
+	for(int i = 0; i< t ; i++){
+	    cin>>a[i];
+	}
+	sort(a.begin(),a.end());
+	for(int x : a)
+	    cout<<x<<endl;
+	return 0;
+}
+
+
+
+
+
+
