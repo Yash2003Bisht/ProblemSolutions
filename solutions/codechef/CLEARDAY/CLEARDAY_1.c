2665b23b065c782806e7891390bc2f4bb9a105c9
yash2003bisht <yash2003bisht@gmail.com>
2023-11-09 09:20:12 +0530

    attempted question CLEARDAY

---
+++ solutions/codechef/CLEARDAY/CLEARDAY_1.c
@@ -0,0 +1,19 @@
+// DATE: 09/11/2023, 09:17:05
+// PROBLEM NAME: Clear Day
+// PROBLEM URL: https://www.codechef.com/problems/CLEARDAY
+// PROBLEM DIFFICULTY RATTING: 233
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", 7 - (x+y));
+	return 0;
+}
+
+
+
