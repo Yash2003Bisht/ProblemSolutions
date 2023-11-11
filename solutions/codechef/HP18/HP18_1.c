99694631307c9c548e280ca2e77f8c527088cb08
yash2003bisht <yash2003bisht@gmail.com>
2023-11-11 12:19:49 +0530

    attempted question HP18

---
+++ solutions/codechef/HP18/HP18_1.c
@@ -0,0 +1,45 @@
+// DATE: 11/11/2023, 08:57:11
+// PROBLEM NAME: Lucky Number Game
+// PROBLEM URL: https://www.codechef.com/problems/HP18
+// PROBLEM DIFFICULTY RATTING: 1500
+// STATUS: accepted
+// TIME: 0.14
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, b, A, count1, count2, common;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &a, &b);
+        count1 = 0, count2 = 0, common = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &A);
+
+            if (A%a == 0 && A%b == 0)
+                common++;
+            else if (A%a == 0)
+                count1++;
+            else if (A%b == 0)
+                count2++;
+
+        }
+
+        if (common)
+            count1++;
+
+        if (count1 > count2)
+            printf("BOB\n");
+        else
+            printf("ALICE\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/HP18/HP18_2.c
@@ -0,0 +1,43 @@
+// DATE: 11/11/2023, 08:56:57
+// PROBLEM NAME: Lucky Number Game
+// PROBLEM URL: https://www.codechef.com/problems/HP18
+// PROBLEM DIFFICULTY RATTING: 1500
+// STATUS: partially accepted
+// TIME: 0.15
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, b, A, count1, count2;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &a, &b);
+        count1 = 0, count2 = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &A);
+
+            if (A%a == 0)
+                count1++;
+            if (A%b == 0)
+                count2++;
+
+        }
+
+        if (count1 == count2 || b%a == 0){
+            printf("BOB\n");
+        } else if (count1 > count2){
+            printf("BOB\n");
+        } else {
+            printf("ALICE\n");
+        }
+
+    }
+    
+	return 0;
+}
+
+
+
