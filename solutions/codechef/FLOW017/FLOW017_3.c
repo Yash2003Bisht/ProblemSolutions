e8b322e707a56685bd265035c8367da41077a146
yash2003bisht <yash2003bisht@gmail.com>
2023-11-13 10:38:22 +0530

    attempted question FLOW017

---
+++ solutions/codechef/FLOW017/FLOW017_3.c
@@ -0,0 +1,27 @@
+// DATE: 13/11/2023, 10:12:01
+// PROBLEM NAME: Second Largest
+// PROBLEM URL: https://www.codechef.com/problems/FLOW017
+// PROBLEM DIFFICULTY RATTING: 730
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, a, b, c;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &a, &b, &c);
+        printf("%d\n", max(min(a,b), max(min(b, c), min(a, c))));
+    }
+
+	return 0;
+}
+
+
+
+
---
+++ solutions/codechef/FLOW017/FLOW017_4.c
@@ -0,0 +1,27 @@
+// DATE: 13/11/2023, 10:03:44
+// PROBLEM NAME: Second Largest
+// PROBLEM URL: https://www.codechef.com/problems/FLOW017
+// PROBLEM DIFFICULTY RATTING: 730
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, a, b, c;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &a, &b, &c);
+        printf("%d\n", min(max(a,b), max(b, c)));
+    }
+
+
+	return 0;
+}
+
+
+
