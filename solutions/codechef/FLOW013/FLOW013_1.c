e3e43352c1fa425675323079cf5b32c48b9becb3
yash2003bisht <yash2003bisht@gmail.com>
2023-11-16 08:56:46 +0530

    attempted question FLOW013

---
+++ solutions/codechef/FLOW013/FLOW013_1.c
@@ -0,0 +1,28 @@
+// DATE: 14/11/2023, 08:18:43
+// PROBLEM NAME: Valid Triangles
+// PROBLEM URL: https://www.codechef.com/problems/FLOW013
+// PROBLEM DIFFICULTY RATTING: 750
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &a, &b, &c);
+        if ((a+b+c) == 180)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+	return 0;
+}
+
+
+
