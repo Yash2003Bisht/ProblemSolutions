565d65e4866180219d01ff19a512c0ca5f7e3671
yash2003bisht <yash2003bisht@gmail.com>
2023-11-16 08:56:46 +0530

    attempted question DECINC

---
+++ solutions/codechef/DECINC/DECINC_1.c
@@ -0,0 +1,24 @@
+// DATE: 16/11/2023, 08:41:12
+// PROBLEM NAME: Decrement OR Increment
+// PROBLEM URL: https://www.codechef.com/problems/DECINC
+// PROBLEM DIFFICULTY RATTING: 722
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    
+    if (n%4 == 0)
+        printf("%d", n+1);
+    else
+        printf("%d", n-1);
+    
+	return 0;
+}
+
+
+
