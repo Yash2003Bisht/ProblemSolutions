3864db8c2d7eebb44e0ff2c7834e2e2900c74b33
yash2003bisht <yash2003bisht@gmail.com>
2023-11-16 08:56:46 +0530

    attempted question CPRIVAL

---
+++ solutions/codechef/CPRIVAL/CPRIVAL_1.c
@@ -0,0 +1,24 @@
+// DATE: 16/11/2023, 08:36:00
+// PROBLEM NAME: Rivalry
+// PROBLEM URL: https://www.codechef.com/problems/CPRIVAL
+// PROBLEM DIFFICULTY RATTING: 501
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int r1, r2, r3, r4;
+    scanf("%d%d\n%d%d", &r1, &r2, &r3, &r4);
+
+    if ((r1+r3) > (r2+r4))
+        printf("Dominater");
+    else
+        printf("Everule");
+
+	return 0;
+}
+
+
+
