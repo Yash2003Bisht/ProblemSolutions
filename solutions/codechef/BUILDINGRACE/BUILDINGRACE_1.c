6af7770c4662bfb7e93a948a3b5c91c2b593f620
yash2003bisht <yash2003bisht@gmail.com>
2023-11-18 09:02:50 +0530

    attempted question BUILDINGRACE

---
+++ solutions/codechef/BUILDINGRACE/BUILDINGRACE_1.c
@@ -0,0 +1,32 @@
+// DATE: 18/11/2023, 08:44:09
+// PROBLEM NAME: Building Race
+// PROBLEM URL: https://www.codechef.com/problems/BUILDINGRACE
+// PROBLEM DIFFICULTY RATTING: 739
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.8M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &x, &y);
+        float chef_time = (float) a/x, chefina_time = (float) b/y;
+
+        if (chef_time == chefina_time)
+            printf("Both\n");
+        else if (chef_time > chefina_time)
+            printf("Chefina\n");
+        else
+            printf("Chef\n");
+
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/BUILDINGRACE/BUILDINGRACE_3.c
@@ -0,0 +1,31 @@
+// DATE: 18/11/2023, 08:42:11
+// PROBLEM NAME: Building Race
+// PROBLEM URL: https://www.codechef.com/problems/BUILDINGRACE
+// PROBLEM DIFFICULTY RATTING: 739
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &x, &y);
+
+        if (a/x == b/y)
+            printf("Both\n");
+        else if (a/x > b/y)
+            printf("Chefina\n");
+        else
+            printf("Chef\n");
+
+    }
+    
+	return 0;
+}
+
+
+
