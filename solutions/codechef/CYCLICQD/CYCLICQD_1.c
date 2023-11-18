9f15cbf4eb030ddc12018d8adb5487fd53697803
yash2003bisht <yash2003bisht@gmail.com>
2023-11-18 09:02:50 +0530

    attempted question CYCLICQD

---
+++ solutions/codechef/CYCLICQD/CYCLICQD_1.c
@@ -0,0 +1,27 @@
+// DATE: 17/11/2023, 08:57:49
+// PROBLEM NAME: Cyclic Quadrilateral
+// PROBLEM URL: https://www.codechef.com/problems/CYCLICQD
+// PROBLEM DIFFICULTY RATTING: 735
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c, d;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        if (a+c == 180 && b+d == 180)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    
+	return 0;
+}
+
+
+
