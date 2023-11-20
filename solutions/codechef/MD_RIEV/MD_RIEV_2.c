269c0101da752f087d8493fdb21d6000ddd85245
yash2003bisht <yash2003bisht@gmail.com>
2023-11-20 09:10:20 +0530

    attempted question MD_RIEV

---
+++ solutions/codechef/MD_RIEV/MD_RIEV_2.c
@@ -0,0 +1,30 @@
+// DATE: 20/11/2023, 08:35:13
+// PROBLEM NAME: Palindromic Prime Numbers
+// PROBLEM URL: https://www.codechef.com/problems/MD_RIEV
+// PROBLEM DIFFICULTY RATTING: 1253
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+
+        if (n > 4){
+            printf("1 %d\n", n-1);
+        } else {
+            printf("0 %d\n", n);
+        }
+
+    }
+
+	return 0;
+}
+
+
+
