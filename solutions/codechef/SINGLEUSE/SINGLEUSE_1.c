aa5ac9b2cbd1b34577e3aafc50e73619146e4987
yash2003bisht <yash2003bisht@gmail.com>
2023-11-22 09:07:26 +0530

    attempted question SINGLEUSE

---
+++ solutions/codechef/SINGLEUSE/SINGLEUSE_1.c
@@ -0,0 +1,32 @@
+// DATE: 22/11/2023, 09:06:43
+// PROBLEM NAME: Single-use Attack
+// PROBLEM URL: https://www.codechef.com/problems/SINGLEUSE
+// PROBLEM DIFFICULTY RATTING: 777
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, h, x, y, attacks;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &h, &x, &y);
+        h -= y;
+
+        if (h > 0){
+            attacks = ceil((float) h/x) + 1;
+            printf("%d\n", attacks);
+        } else
+            printf("1\n");
+
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/SINGLEUSE/SINGLEUSE_2.c
@@ -0,0 +1,31 @@
+// DATE: 22/11/2023, 09:06:32
+// PROBLEM NAME: Single-use Attack
+// PROBLEM URL: https://www.codechef.com/problems/SINGLEUSE
+// PROBLEM DIFFICULTY RATTING: 777
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, h, x, y, attacks;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &h, &x, &y);
+        h -= y;
+
+        if (h > 0){
+            attacks = ceil((float) h/x) + 1;
+            printf("%d\n", attacks);
+        } else
+            printf("1\n");
+
+    }
+
+	return 0;
+}
+
+
+
