c9de5c4028e40782a7d6aa4c0118a51762aa6540
yash2003bisht <yash2003bisht@gmail.com>
2023-11-24 08:11:49 +0530

    attempted question DISTINCTCOL

---
+++ solutions/codechef/DISTINCTCOL/DISTINCTCOL_1.c
@@ -0,0 +1,35 @@
+// DATE: 23/11/2023, 09:32:04
+// PROBLEM NAME: Distinct Colors
+// PROBLEM URL: https://www.codechef.com/problems/DISTINCTCOL
+// PROBLEM DIFFICULTY RATTING: 760
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, max_boxes;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        max_boxes = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            
+            if (a > max_boxes)
+                max_boxes = a;
+
+        }
+
+        printf("%d\n", max_boxes);
+        
+    }
+    
+	return 0;
+}
+
+
+
