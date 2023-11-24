440c786259133fd249fdf4dfe85f20689b92d76b
yash2003bisht <yash2003bisht@gmail.com>
2023-11-24 08:11:49 +0530

    attempted question CWC23QUALIF

---
+++ solutions/codechef/CWC23QUALIF/CWC23QUALIF_1.c
@@ -0,0 +1,24 @@
+// DATE: 24/11/2023, 07:44:23
+// PROBLEM NAME: Cricket World Cup Qualifier
+// PROBLEM URL: https://www.codechef.com/problems/CWC23QUALIF
+// PROBLEM DIFFICULTY RATTING: 203
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int score;
+    scanf("%d", &score);
+
+    if (score >= 12)
+        printf("Yes");
+    else
+        printf("No");
+
+	return 0;
+}
+
+
+
