11ce02ffa88c000178324bf137d19767be677224
yash2003bisht <yash2003bisht@gmail.com>
2023-11-25 19:25:12 +0530

    attempted question FLOW018

---
+++ solutions/codechef/FLOW018/FLOW018_1.py
@@ -0,0 +1,14 @@
+# DATE: 25/11/2023, 09:54:57
+# PROBLEM NAME: Small Factorial
+# PROBLEM URL: https://www.codechef.com/problems/FLOW018
+# PROBLEM DIFFICULTY RATTING: 760
+# STATUS: accepted
+# TIME: 0.01
+# MEMORY: 8.3M
+
+import math
+
+for _ in range(int(input())):
+    print(math.factorial(int(input())))
+
+
