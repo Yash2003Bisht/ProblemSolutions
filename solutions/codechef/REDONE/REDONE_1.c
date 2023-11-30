80fa09bfe0f1efe0af349a29032134511568278d
yash2003bisht <yash2003bisht@gmail.com>
2023-11-30 09:14:34 +0530

    attempted question REDONE

---
+++ solutions/codechef/REDONE/REDONE_1.c
@@ -0,0 +1,34 @@
+// DATE: 30/11/2023, 08:46:40
+// PROBLEM NAME: Reduce to One
+// PROBLEM URL: https://www.codechef.com/problems/REDONE
+// PROBLEM DIFFICULTY RATTING: 1507
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 9.2M
+
+#include <stdio.h>
+#define lld long long int
+#define mod 1000000007
+#define size 1000000
+
+int main(void) {
+    int t, n;
+    lld ans, pre_compute[size];
+    pre_compute[0] = 1;
+
+    for (int i=2; i<=size; i++)
+        pre_compute[i-1] = (pre_compute[i-2]+i+pre_compute[i-2]*i) % mod;
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        ans = pre_compute[n-1];
+        printf("%lld\n", ans);
+    }
+
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/REDONE/REDONE_3.c
@@ -0,0 +1,31 @@
+// DATE: 30/11/2023, 08:43:39
+// PROBLEM NAME: Reduce to One
+// PROBLEM URL: https://www.codechef.com/problems/REDONE
+// PROBLEM DIFFICULTY RATTING: 1507
+// STATUS: partially accepted
+// TIME: 1.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#define mod 1000000007
+
+int main(void) {
+    int t, n;
+    long long int ans;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        ans = 1;
+
+        for (int i=2; i<=n; i++)
+            ans = (ans+i+ans*i) % mod;
+
+        printf("%lld\n", ans);
+
+    }
+
+	return 0;
+}
+
+
