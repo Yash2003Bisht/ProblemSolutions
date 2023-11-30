a2d8c41f7416a9dd592e5f9163a3d4279c310df6
yash2003bisht <yash2003bisht@gmail.com>
2023-11-30 09:14:34 +0530

    attempted question INDIVISIBLE

---
+++ solutions/codechef/INDIVISIBLE/INDIVISIBLE_1.c
@@ -0,0 +1,31 @@
+// DATE: 29/11/2023, 09:44:04
+// PROBLEM NAME: Indivisible
+// PROBLEM URL: https://www.codechef.com/problems/INDIVISIBLE
+// PROBLEM DIFFICULTY RATTING: 787
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &a, &b, &c);
+        
+        for (int i=2; i<100; i++){
+            if (a%i != 0 && b%i != 0 && c%i != 0){
+                printf("%d\n", i);
+                break;
+            }
+        }
+        
+    }
+    
+	return 0;
+}
+
+
+
