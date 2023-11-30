ad6e94b7d1d34f2c80c89930b7c6b728cb89836a
yash2003bisht <yash2003bisht@gmail.com>
2023-11-30 09:14:34 +0530

    attempted question SPCP1

---
+++ solutions/codechef/SPCP1/SPCP1_1.c
@@ -0,0 +1,24 @@
+// DATE: 30/11/2023, 09:13:49
+// PROBLEM NAME: Water Park
+// PROBLEM URL: https://www.codechef.com/problems/SPCP1
+// PROBLEM DIFFICULTY RATTING: 485
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+    int w, h;
+    scanf("%d%d", &w, &h);
+    
+    if (60 <= w && 130 >= h)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    
+	return 0;
+}
+
+
+
