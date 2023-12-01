8b9af4c67fefe108530634f66f0d68b415bdf7f7
yash2003bisht <yash2003bisht@gmail.com>
2023-12-01 12:03:32 +0530

    attempted question SPCP2

---
+++ solutions/codechef/SPCP2/SPCP2_1.c
@@ -0,0 +1,32 @@
+// DATE: 01/12/2023, 09:26:56
+// PROBLEM NAME: Airlines
+// PROBLEM URL: https://www.codechef.com/problems/SPCP2
+// PROBLEM DIFFICULTY RATTING: 712
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t, x, n;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &x, &n);
+        int total = x*100;
+
+        if (total >= n)
+            printf("0\n");
+        else {
+            int ans = ceil((float) (n-total)/100);
+            printf("%d\n", ans);
+        }
+
+    }
+
+	return 0;
+}
+
+
---
+++ solutions/codechef/SPCP2/SPCP2_2.c
@@ -0,0 +1,31 @@
+// DATE: 01/12/2023, 09:24:38
+// PROBLEM NAME: Airlines
+// PROBLEM URL: https://www.codechef.com/problems/SPCP2
+// PROBLEM DIFFICULTY RATTING: 712
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, n;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d", &x, &n);
+        int total = x*100;
+
+        if (total >= n)
+            printf("0\n");
+        else {
+            int ans = ceil((float) (n-total)/100);
+            printf("%d\n", ans);
+        }
+
+    }
+
+	return 0;
+}
+
+
