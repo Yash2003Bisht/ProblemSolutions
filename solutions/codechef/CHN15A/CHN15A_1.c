03a2a5cbcc59dc94aa43122e36866aa7ca32b1a5
yash2003bisht <yash2003bisht@gmail.com>
2023-12-01 12:03:32 +0530

    attempted question CHN15A

---
+++ solutions/codechef/CHN15A/CHN15A_1.c
@@ -0,0 +1,34 @@
+// DATE: 01/12/2023, 09:17:41
+// PROBLEM NAME: Mutated Minions
+// PROBLEM URL: https://www.codechef.com/problems/CHN15A
+// PROBLEM DIFFICULTY RATTING: 777
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, k;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &n, &k);
+        int a, total = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+
+            if ((a+k)%7 == 0)
+                total++;
+
+        }
+
+        printf("%d\n", total);
+
+    }
+    
+	return 0;
+}
+
+
