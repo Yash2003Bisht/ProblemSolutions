a6bb89a3cc62c56e6ee825a40d4b542c83762915
yash2003bisht <yash2003bisht@gmail.com>
2023-12-02 09:41:51 +0530

    attempted question JGAMES

---
+++ solutions/codechef/JGAMES/JGAMES_1.c
@@ -0,0 +1,27 @@
+// DATE: 02/12/2023, 09:40:28
+// PROBLEM NAME: Janmansh and Games
+// PROBLEM URL: https://www.codechef.com/problems/JGAMES
+// PROBLEM DIFFICULTY RATTING: 772
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &x, &y);
+        if ((y%2 == 0 && x%2 == 0) || (y%2 != 0 && x%2 != 0))
+            printf("Janmansh\n");
+        else
+            printf("Jay\n");
+    }
+    
+	return 0;
+}
+
+
+
