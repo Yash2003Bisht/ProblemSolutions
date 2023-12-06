26126d53b3638f7a60f051e8c72b5bc20d139aab
yash2003bisht <yash2003bisht@gmail.com>
2023-12-06 09:19:40 +0530

    attempted question PRDRG

---
+++ solutions/codechef/PRDRG/PRDRG_2.c
@@ -0,0 +1,39 @@
+// DATE: 05/12/2023, 09:00:02
+// PROBLEM NAME: Chef and Ridges
+// PROBLEM URL: https://www.codechef.com/problems/PRDRG
+// PROBLEM DIFFICULTY RATTING: 1505
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#define max(a,b)(a>b?a:b)
+
+int main(void) {
+    int t, n, num, denum;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d", &n);
+        num = 0, denum = 1;
+
+        for (int i=1; i<=n; i++){
+            num *= 2;
+            denum *= 2;
+
+            if (i%2 == 1)
+                num++;
+            else
+                num--;
+
+        }
+
+        printf("%d %d ", num, denum);
+
+    }
+    
+	return 0;
+}
+
+
+
