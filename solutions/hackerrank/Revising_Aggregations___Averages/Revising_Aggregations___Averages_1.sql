f17099d68d668d351806bab3d94eec292688ac2b
yash2003bisht <yash2003bisht@gmail.com>
2023-12-09 09:11:01 +0530

    attempted question Revising_Aggregations___Averages

---
+++ solutions/hackerrank/Revising_Aggregations___Averages/Revising_Aggregations___Averages_1.sql
@@ -0,0 +1,4 @@
+-- QUESTION URL: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- STATUS: Accepted
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = "California";
