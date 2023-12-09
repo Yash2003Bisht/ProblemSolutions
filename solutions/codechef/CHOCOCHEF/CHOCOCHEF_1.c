df479d9f55ba759a0f50dffa874170ebd5195f4b
yash2003bisht <yash2003bisht@gmail.com>
2023-12-09 09:10:59 +0530

    attempted question CHOCOCHEF

---
+++ solutions/codechef/CHOCOCHEF/CHOCOCHEF_1.c
@@ -0,0 +1,24 @@
+// DATE: 09/12/2023, 08:49:05
+// PROBLEM NAME: Chocolate Distribution
+// PROBLEM URL: https://www.codechef.com/problems/CHOCOCHEF
+// PROBLEM DIFFICULTY RATTING: 1104
+// STATUS: accepted
+// TIME: 0.01
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        printf("%d 1 1\n", n-2);
+    }
+    
+	return 0;
+}
+
+
+
---
+++ solutions/codechef/CHOCOCHEF/CHOCOCHEF_2.c
@@ -0,0 +1,24 @@
+// DATE: 09/12/2023, 08:48:51
+// PROBLEM NAME: Chocolate Distribution
+// PROBLEM URL: https://www.codechef.com/problems/CHOCOCHEF
+// PROBLEM DIFFICULTY RATTING: 1104
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        printf("%d 1 1\n", n-2);
+    }
+    
+	return 0;
+}
+
+
+
