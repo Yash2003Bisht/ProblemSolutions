95e1bbe8ca2b158ca528cf8f48868674035a80c4
yash2003bisht <yash2003bisht@gmail.com>
2023-12-27 12:53:54 +0530

    attempted question FOODCOST

---
+++ solutions/codechef/FOODCOST/FOODCOST_1.c
@@ -0,0 +1,18 @@
+// DATE: 16/12/2023, 08:45:11
+// PROBLEM NAME: Food Costs
+// PROBLEM URL: https://www.codechef.com/problems/FOODCOST
+// PROBLEM DIFFICULTY RATTING: 183
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+    scanf("%d%d", &x, &y);
+    printf("%d\n", x*6 + y);
+	return 0;
+}
+
+
