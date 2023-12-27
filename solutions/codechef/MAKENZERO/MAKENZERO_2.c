4209ac52bf9d145a5e91ad735b74db999e81dabf
yash2003bisht <yash2003bisht@gmail.com>
2023-12-27 12:53:54 +0530

    attempted question MAKENZERO

---
+++ solutions/codechef/MAKENZERO/MAKENZERO_2.c
@@ -0,0 +1,35 @@
+// DATE: 27/12/2023, 08:59:10
+// PROBLEM NAME: Make N Zero
+// PROBLEM URL: https://www.codechef.com/problems/MAKENZERO
+// PROBLEM DIFFICULTY RATTING: 846
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+
+        while(n >= 3){
+            if (n%4 == 0){
+                n = 0;
+                break;
+            }
+            n -= 3;
+        }
+
+        if (n == 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+
+}
+
+
