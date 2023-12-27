5f150e05885f7db3132a190a80c841c19d1ed04b
yash2003bisht <yash2003bisht@gmail.com>
2023-12-27 13:03:02 +0530

    attempted question Weather_Observation_Station_14

---
+++ solutions/hackerrank/Weather_Observation_Station_14/Weather_Observation_Station_14_2.sql
@@ -0,0 +1,4 @@
+-- QUESTION URL: https://www.hackerrank.com/challenges/weather-observation-station-14/problem
+-- STATUS: Accepted
+
+SELECT ROUND(MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
