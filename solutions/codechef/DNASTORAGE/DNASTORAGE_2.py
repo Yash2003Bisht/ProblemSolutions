db3023502ce5b656bff2ffdd44725135f39d6d46
yash2003bisht <yash2003bisht@gmail.com>
2023-12-27 12:53:54 +0530

    attempted question DNASTORAGE

---
+++ solutions/codechef/DNASTORAGE/DNASTORAGE_2.py
@@ -0,0 +1,29 @@
+# DATE: 26/12/2023, 08:59:07
+# PROBLEM NAME: DNA Storage
+# PROBLEM URL: https://www.codechef.com/problems/DNASTORAGE
+# PROBLEM DIFFICULTY RATTING: 801
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 8.5M
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    encoded = []
+    i = 0
+
+    while i < n-1:
+        if s[i] == '0' and s[i+1] == '0':
+            encoded.append('A')
+        elif s[i] == '0' and s[i+1] == '1':
+            encoded.append('T')
+        elif s[i] == '1' and s[i+1] == '0':
+            encoded.append('C')
+        else:
+            encoded.append('G')
+
+        i += 2
+
+    print("".join(encoded))
+
+
