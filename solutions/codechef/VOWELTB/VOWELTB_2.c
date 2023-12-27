df7d190d2787cd71ff49426ec1deb377c65056f9
yash2003bisht <yash2003bisht@gmail.com>
2023-12-27 12:53:54 +0530

    attempted question VOWELTB

---
+++ solutions/codechef/VOWELTB/VOWELTB_2.c
@@ -0,0 +1,22 @@
+// DATE: 26/12/2023, 09:07:44
+// PROBLEM NAME: Is it a VOWEL or CONSONANT
+// PROBLEM URL: https://www.codechef.com/problems/VOWELTB
+// PROBLEM DIFFICULTY RATTING: 840
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    char c;
+    scanf("%c", &c);
+
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+        printf("Vowel");
+    else
+        printf("Consonant");
+
+}
+
+
