d26b0848a3f5e3711002ae4987d810e7ed176528
yash2003bisht <yash2003bisht@gmail.com>
2023-12-27 12:53:54 +0530

    attempted question FINDK3

---
+++ solutions/codechef/FINDK3/FINDK3_1.c
@@ -0,0 +1,31 @@
+// DATE: 26/12/2023, 08:48:17
+// PROBLEM NAME: FIND A and B
+// PROBLEM URL: https://www.codechef.com/problems/FINDK3
+// PROBLEM DIFFICULTY RATTING: 802
+// STATUS: accepted
+// TIME: 0.02
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+
+        if ((x*y)%z == 0)
+            printf("%d %d\n", (x*y), z);
+        else if ((x*z)%y == 0)
+            printf("%d %d\n", (x*z), y);
+        else if ((y*z)%x == 0)
+            printf("%d %d\n", (y*z), x);
+        else
+            printf("-1\n");
+
+    }
+
+}
+
+
