0217043e22594c251632efb77c9c1b4521de6730
yash2003bisht <yash2003bisht@gmail.com>
2023-12-28 09:27:34 +0530

    attempted question PAINTCHRIS

---
+++ solutions/codechef/PAINTCHRIS/PAINTCHRIS_1.c
@@ -0,0 +1,25 @@
+// DATE: 28/12/2023, 09:26:19
+// PROBLEM NAME: Painting Walls
+// PROBLEM URL: https://www.codechef.com/problems/PAINTCHRIS
+// PROBLEM DIFFICULTY RATTING: 567
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z, area;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        area = x*y;
+        z /= 2;
+        printf("%d\n", z/area);
+    }
+
+}
+
+
+
---
+++ solutions/codechef/PAINTCHRIS/PAINTCHRIS_2.c
@@ -0,0 +1,25 @@
+// DATE: 28/12/2023, 09:26:07
+// PROBLEM NAME: Painting Walls
+// PROBLEM URL: https://www.codechef.com/problems/PAINTCHRIS
+// PROBLEM DIFFICULTY RATTING: 567
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x, y, z, area;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &y, &z);
+        area = x*y;
+        z /= 2;
+        printf("%d\n", z/area);
+    }
+
+}
+
+
+
