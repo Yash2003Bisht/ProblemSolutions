b7a9d46f7e80d4deb9ff57b518f97ebe3317a858
yash2003bisht <yash2003bisht@gmail.com>
2023-12-28 09:27:34 +0530

    attempted question CHRISTGREET

---
+++ solutions/codechef/CHRISTGREET/CHRISTGREET_1.c
@@ -0,0 +1,23 @@
+// DATE: 28/12/2023, 09:21:54
+// PROBLEM NAME: Christmas Greetings
+// PROBLEM URL: https://www.codechef.com/problems/CHRISTGREET
+// PROBLEM DIFFICULTY RATTING: 205
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    scanf("%d", &x);
+
+    if (x == 25)
+        printf("CHRISTMAS");
+    else
+        printf("ORDINARY");
+
+}
+
+
+
---
+++ solutions/codechef/CHRISTGREET/CHRISTGREET_2.c
@@ -0,0 +1,23 @@
+// DATE: 28/12/2023, 09:11:32
+// PROBLEM NAME: Christmas Greetings
+// PROBLEM URL: https://www.codechef.com/problems/CHRISTGREET
+// PROBLEM DIFFICULTY RATTING: 205
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    scanf("%d", &x);
+
+    if (x == 25)
+        printf("CHRISTMAS");
+    else
+        printf("ORDINARY");
+
+}
+
+
+
