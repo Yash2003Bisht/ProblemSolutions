cbccca9ceb1e0f3832d630f290a09cc1b206c1a0
yash2003bisht <yash2003bisht@gmail.com>
2024-01-04 09:07:02 +0530

    attempted question Day_6__Bitwise_Operators

---
+++ solutions/hackerrank/Day_6__Bitwise_Operators/Day_6__Bitwise_Operators_5.js
@@ -0,0 +1,20 @@
+// QUESTION URL: https://www.hackerrank.com/challenges/js10-bitwise/problem
+// STATUS: Wrong Answer
+
+
+
+function getMaxLessThanK(n, k){
+    let maxNum = 0;
+    for (let i=1; i<n; i++){
+        for (let j=i+1; j<n; j++){
+            const num = i & j;
+            if (num < k){
+                maxNum = maxNum > num ? maxNum : num;
+            }
+        }
+    }
+
+    return maxNum;
+
+}
+
