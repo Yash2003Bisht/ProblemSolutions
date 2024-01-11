f5b3829f969317594845fe191d8f09f3eee92edc
yash2003bisht <yash2003bisht@gmail.com>
2024-01-11 09:00:35 +0530

    attempted question CHRISCANDY

---
+++ solutions/codechef/CHRISCANDY/CHRISCANDY_1.c
@@ -0,0 +1,37 @@
+// DATE: 30/12/2023, 08:39:19
+// PROBLEM NAME: Christmas Candy
+// PROBLEM URL: https://www.codechef.com/problems/CHRISCANDY
+// PROBLEM DIFFICULTY RATTING: 1214
+// STATUS: accepted
+// TIME: 0.07
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, count, a, b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        count = 0;
+
+        scanf("%d", &a);
+        
+        for (int i=1; i<n; i++){
+            scanf("%d", &b);
+
+            if (b < a)
+                count++;
+            else
+                a = b;
+
+        }
+
+        printf("%d\n", count);
+
+    }
+
+}
+
+
