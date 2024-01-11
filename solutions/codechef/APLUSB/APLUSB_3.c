c55682eb917abc0faf6f7435c824baf72fb9e476
yash2003bisht <yash2003bisht@gmail.com>
2024-01-11 09:00:35 +0530

    attempted question APLUSB

---
+++ solutions/codechef/APLUSB/APLUSB_3.c
@@ -0,0 +1,68 @@
+// DATE: 11/01/2024, 08:41:11
+// PROBLEM NAME: A PLUS B Remastered
+// PROBLEM URL: https://www.codechef.com/problems/APLUSB
+// PROBLEM DIFFICULTY RATTING: 1284
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpfunc(const void * a, const void * b){
+   return ( *(int*)a - *(int*)b );
+}
+
+int cmpreversefunc(const void * a, const void * b){
+   return ( *(int*)b - *(int*)a );
+}
+
+int main(void) {
+    int t, n, num, flag;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        int a[n], b[n];
+        num = n*2, flag = 1;
+
+        for (int i=0; i<num; i++){
+            if (i < n)
+                scanf("%d", &a[i]);
+            else
+                scanf("%d", &b[i-n]);
+        }
+
+        qsort(a, n, sizeof(int), cmpfunc);
+        qsort(b, n, sizeof(int), cmpreversefunc);
+
+        num = a[0] + b[0];
+
+        for (int i=1; i<n; i++){
+            if (a[i] + b[i] != num){
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag){
+            for (int i=0; i<n; i++)
+                printf("%d ", a[i]);
+
+            printf("\n");
+
+            for (int i=0; i<n; i++)
+                printf("%d ", b[i]);
+
+            printf("\n");
+
+        } else {
+            printf("-1\n");
+        }
+
+    }
+    
+}
+
+
+
---
+++ solutions/codechef/APLUSB/APLUSB_4.py
@@ -0,0 +1,30 @@
+# DATE: 11/01/2024, 08:28:47
+# PROBLEM NAME: A PLUS B Remastered
+# PROBLEM URL: https://www.codechef.com/problems/APLUSB
+# PROBLEM DIFFICULTY RATTING: 1284
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 8.6M
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    flag = True
+
+    a.sort()
+    b.sort(reverse=True)
+    num = a[0] + b[0]
+
+    for ai, bi, in zip(a, b):
+        if ai + bi != num:
+            flag = False
+            break
+    
+    if flag:
+        print(" ".join(map(str, a)))
+        print(" ".join(map(str, b)))
+    else:
+        print(-1)
+    
+
