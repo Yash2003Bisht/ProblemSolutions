a49b133846cc6c966afe13642abbdfbb106bdd66
yash2003bisht <yash2003bisht@gmail.com>
2024-01-17 09:19:03 +0530

    attempted question MINWAGE

---
+++ solutions/codechef/MINWAGE/MINWAGE_1.c
@@ -0,0 +1,23 @@
+// DATE: 17/01/2024, 09:18:14
+// PROBLEM NAME: Minimum Wage
+// PROBLEM URL: https://www.codechef.com/problems/MINWAGE
+// PROBLEM DIFFICULTY RATTING: 247
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int x;
+	scanf("%d", &x);
+	
+	if (x > 11)
+	    printf("YES");
+	else
+	    printf("NO");
+	
+}
+
+
+
