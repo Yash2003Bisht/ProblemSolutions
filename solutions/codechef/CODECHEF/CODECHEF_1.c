a49eeb79449e998ba44e46da4aa2b4868c3ae376
yash2003bisht <yash2003bisht@gmail.com>
2024-01-17 09:19:03 +0530

    attempted question CODECHEF

---
+++ solutions/codechef/CODECHEF/CODECHEF_1.c
@@ -0,0 +1,22 @@
+// DATE: 17/01/2024, 09:16:58
+// PROBLEM NAME: Codechef Round
+// PROBLEM URL: https://www.codechef.com/problems/CODECHEF
+// PROBLEM DIFFICULTY RATTING: 232
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    if (n == 4)
+        printf("YES");
+    else
+        printf("NO");
+}
+
+
+
