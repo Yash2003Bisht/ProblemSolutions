84032194c57935bb4038bab7a958173766e24a0e
yash2003bisht <yash2003bisht@gmail.com>
2024-01-20 10:31:56 +0530

    attempted question PHONEYR

---
+++ solutions/codechef/PHONEYR/PHONEYR_1.c
@@ -0,0 +1,18 @@
+// DATE: 18/01/2024, 02:28:32
+// PROBLEM NAME: Yearly Phone
+// PROBLEM URL: https://www.codechef.com/problems/PHONEYR
+// PROBLEM DIFFICULTY RATTING: 541
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+	char x[4];
+	scanf("%s", x);
+    printf("K%c%c", x[2], x[3]);
+}
+
+
+
