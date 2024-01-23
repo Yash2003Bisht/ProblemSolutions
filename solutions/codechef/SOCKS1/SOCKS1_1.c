36bcc4ac679eab01f12129c28db248da41859860
yash2003bisht <yash2003bisht@gmail.com>
2024-01-23 15:07:08 +0530

    attempted question SOCKS1

---
+++ solutions/codechef/SOCKS1/SOCKS1_1.c
@@ -0,0 +1,23 @@
+// DATE: 23/01/2024, 02:58:25
+// PROBLEM NAME: Valid Pair
+// PROBLEM URL: https://www.codechef.com/problems/SOCKS1
+// PROBLEM DIFFICULTY RATTING: 851
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	
+	if (a == b || b == a || a == c)
+	    printf("YES");
+	else
+	    printf("NO");
+	
+}
+
+
+
