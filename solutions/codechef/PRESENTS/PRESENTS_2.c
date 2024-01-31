2e879d8fdbc140d4a6d7fd2a2abac5969652a6d6
yash2003bisht <yash2003bisht@gmail.com>
2024-01-31 22:17:02 +0530

    attempted question PRESENTS

---
+++ solutions/codechef/PRESENTS/PRESENTS_2.c
@@ -0,0 +1,23 @@
+// DATE: 31/01/2024, 10:16:05
+// PROBLEM NAME: Presents for Cheffina
+// PROBLEM URL: https://www.codechef.com/problems/PRESENTS
+// PROBLEM DIFFICULTY RATTING: 757
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, n;
+	scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d", &n);
+	    printf("%d\n", n-(n/5));
+	}
+	
+}
+
+
+
