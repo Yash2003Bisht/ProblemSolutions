8cbcc3b08a7ed786b3f96c541218cab2ba90ba9a
yash2003bisht <yash2003bisht@gmail.com>
2024-02-01 18:14:01 +0530

    attempted question ADVITIYA1

---
+++ solutions/codechef/ADVITIYA1/ADVITIYA1_1.c
@@ -0,0 +1,23 @@
+// DATE: 01/02/2024, 05:45:30
+// PROBLEM NAME: Advitiya
+// PROBLEM URL: https://www.codechef.com/problems/ADVITIYA1
+// PROBLEM DIFFICULTY RATTING: 243
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	
+	if (n >= 16 && n <=18)
+	    printf("ADVITIYA");
+	else
+	    printf("WAITING FOR ADVITIYA");
+	
+}
+
+
+
