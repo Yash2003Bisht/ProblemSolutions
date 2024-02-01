ead1c6153997fa153c63f6cf93a9d95743d9ec9c
yash2003bisht <yash2003bisht@gmail.com>
2024-02-01 18:14:01 +0530

    attempted question ADVITIYA2

---
+++ solutions/codechef/ADVITIYA2/ADVITIYA2_1.c
@@ -0,0 +1,28 @@
+// DATE: 01/02/2024, 06:13:26
+// PROBLEM NAME: Judged
+// PROBLEM URL: https://www.codechef.com/problems/ADVITIYA2
+// PROBLEM DIFFICULTY RATTING: 453
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, r1, r2, r3, r4, r5;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d%d", &r1, &r2, &r3, &r4, &r5);
+        
+        if ((r1+r2+r3+r4+r5) >= 4)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+}
+
+
+
