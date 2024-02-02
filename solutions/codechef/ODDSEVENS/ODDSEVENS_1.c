4669a7b4b5309c3f5656e1e155f381c97b86917d
yash2003bisht <yash2003bisht@gmail.com>
2024-02-02 14:19:43 +0530

    attempted question ODDSEVENS

---
+++ solutions/codechef/ODDSEVENS/ODDSEVENS_1.c
@@ -0,0 +1,28 @@
+// DATE: 02/02/2024, 02:19:12
+// PROBLEM NAME: Odds and Evens
+// PROBLEM URL: https://www.codechef.com/problems/ODDSEVENS
+// PROBLEM DIFFICULTY RATTING: 872
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, a, b;
+	scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d%d", &a, &b);
+	    
+	    if ((a+b)%2 == 0)
+	        printf("Bob\n");
+	    else
+	        printf("Alice\n");
+	    
+	}
+	
+}
+
+
+
