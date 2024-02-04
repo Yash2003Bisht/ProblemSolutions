4fd94c0943551bff2fccb6534d1611c7c8a7c9a0
yash2003bisht <yash2003bisht@gmail.com>
2024-02-04 16:07:54 +0530

    attempted question LAZYCHF

---
+++ solutions/codechef/LAZYCHF/LAZYCHF_2.c
@@ -0,0 +1,24 @@
+// DATE: 04/02/2024, 04:07:20
+// PROBLEM NAME: Lazy Chef
+// PROBLEM URL: https://www.codechef.com/problems/LAZYCHF
+// PROBLEM DIFFICULTY RATTING: 801
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+#define min(a,b)(a>b?b:a)
+
+int main(void) {
+    int t, x, m, d;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &x, &m, &d);
+        printf("%d\n", min(x*m, x+d));
+    }
+    
+}
+
+
+
