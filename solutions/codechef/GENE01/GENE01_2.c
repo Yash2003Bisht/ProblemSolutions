0437288e036272f47c1bc29ba01e42a4ce267405
yash2003bisht <yash2003bisht@gmail.com>
2024-02-04 16:07:54 +0530

    attempted question GENE01

---
+++ solutions/codechef/GENE01/GENE01_2.c
@@ -0,0 +1,26 @@
+// DATE: 04/02/2024, 03:55:37
+// PROBLEM NAME: Genes
+// PROBLEM URL: https://www.codechef.com/problems/GENE01
+// PROBLEM DIFFICULTY RATTING: 826
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    char c1, c2;
+    scanf("%c %c", &c1, &c2);
+
+    if (c1 == c2)
+        printf("%c", c1);
+    else{
+        if (c1 == 'R' || c2 == 'R')
+            printf("R");
+        else
+            printf("B");
+    }
+
+}
+
+
---
+++ solutions/codechef/GENE01/GENE01_4.c
@@ -0,0 +1,23 @@
+// DATE: 04/02/2024, 03:55:22
+// PROBLEM NAME: Genes
+// PROBLEM URL: https://www.codechef.com/problems/GENE01
+// PROBLEM DIFFICULTY RATTING: 826
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    char c1, c2;
+    scanf("%c %c", &c1, &c2);
+
+    if (c1 == c2)
+        printf("%c", c1);
+    else
+        printf("R");
+
+}
+
+
+
