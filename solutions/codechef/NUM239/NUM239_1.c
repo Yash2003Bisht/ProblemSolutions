1a49e142e416b6b73059075bf964e57d3a2cff86
yash2003bisht <yash2003bisht@gmail.com>
2024-02-05 19:06:03 +0530

    attempted question NUM239

---
+++ solutions/codechef/NUM239/NUM239_1.c
@@ -0,0 +1,34 @@
+// DATE: 05/02/2024, 06:33:51
+// PROBLEM NAME: Counting Pretty Numbers
+// PROBLEM URL: https://www.codechef.com/problems/NUM239
+// PROBLEM DIFFICULTY RATTING: 873
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, l, r, n, count;
+	scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d%d", &l, &r);
+	    count = 0;
+
+	    for (int i=l; i<=r; i++){
+	        n = i%10;
+
+            if (n == 2 || n == 3 || n == 9)
+                count++;
+
+	    }
+
+        printf("%d\n", count);
+
+	}
+	
+}
+
+
+
