94722f9932945f86fd3812314be70720c7cd9438
yash2003bisht <yash2003bisht@gmail.com>
2024-02-06 18:15:55 +0530

    attempted question FLOW010

---
+++ solutions/codechef/FLOW010/FLOW010_2.c
@@ -0,0 +1,36 @@
+// DATE: 06/02/2024, 06:15:21
+// PROBLEM NAME: Id and Ship
+// PROBLEM URL: https://www.codechef.com/problems/FLOW010
+// PROBLEM DIFFICULTY RATTING: 847
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    char id;
+
+    scanf("%d\n", &t);
+
+    while(t--){
+        scanf("%c\n", &id);
+
+        if (id == 'B' || id == 'b')
+            printf("BattleShip");
+        else if (id == 'C' || id == 'c')
+            printf("Cruiser");
+        else if (id == 'D' || id == 'd')
+            printf("Destroyer");
+        else
+            printf("Frigate");
+
+        printf("\n");
+        
+    }
+
+}
+
+
+
---
+++ solutions/codechef/FLOW010/FLOW010_3.c
@@ -0,0 +1,36 @@
+// DATE: 06/02/2024, 06:14:45
+// PROBLEM NAME: Id and Ship
+// PROBLEM URL: https://www.codechef.com/problems/FLOW010
+// PROBLEM DIFFICULTY RATTING: 847
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    char id;
+
+    scanf("%d\n", &t);
+
+    while(t--){
+        scanf("%c\n", &id);
+
+        if (id == 'B' || id == 'b')
+            printf("BattleShip");
+        else if (id == 'C' || id == 'c')
+            printf("Cruise");
+        else if (id == 'D' || id == 'd')
+            printf("Destroyer");
+        else
+            printf("Frigate");
+
+        printf("\n");
+        
+    }
+
+}
+
+
+
