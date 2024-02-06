6622f6db58a491553a165bd2cd76033d9d54344d
yash2003bisht <yash2003bisht@gmail.com>
2024-02-06 18:15:55 +0530

    attempted question FLOW014

---
+++ solutions/codechef/FLOW014/FLOW014_1.c
@@ -0,0 +1,41 @@
+// DATE: 06/02/2024, 04:03:19
+// PROBLEM NAME: Grade The Steel
+// PROBLEM URL: https://www.codechef.com/problems/FLOW014
+// PROBLEM DIFFICULTY RATTING: 838
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.5M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, c, condition1, condition2, condition3;
+    float b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%f%d", &a, &b, &c);
+
+        condition1 = a > 50;
+        condition2 = b < 0.7;
+        condition3 = c > 5600;
+
+        if (condition1 && condition2 && condition3)
+            printf("10\n");
+        else if (condition1 && condition2)
+            printf("9\n");
+        else if (condition2 && condition3)
+            printf("8\n");
+        else if (condition1 && condition3)
+            printf("7\n");
+        else if (condition1 || condition2 || condition3)
+            printf("6\n");
+        else
+            printf("5\n");
+
+    }
+    
+}
+
+
+
---
+++ solutions/codechef/FLOW014/FLOW014_4.c
@@ -0,0 +1,41 @@
+// DATE: 06/02/2024, 04:02:11
+// PROBLEM NAME: Grade The Steel
+// PROBLEM URL: https://www.codechef.com/problems/FLOW014
+// PROBLEM DIFFICULTY RATTING: 838
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.9M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, a, c, condition1, condition2, condition3;
+    float b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%f%d", &a, &b, &c);
+
+        condition1 = a > 50 ? 1 : 0;
+        condition2 = b < 0.7 ? 1 : 0;
+        condition3 = c > 5600 ? 1 : 0;
+        
+        if (condition1 && condition2 && condition3)
+            printf("10\n");
+        else if (condition1 && condition2)
+            printf("9\n");
+        else if (condition2 && condition3)
+            printf("8\n");
+        else if (condition1 && condition3)
+            printf("7\n");
+        else if (condition1 || condition2 || condition3)
+            printf("6\n");
+        else
+            printf("5\n");
+
+    }
+    
+}
+
+
+
