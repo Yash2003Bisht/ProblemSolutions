08565e6c8d59cea6f8122f2ca086a7954396015d
yash2003bisht <yash2003bisht@gmail.com>
2024-02-07 17:59:46 +0530

    attempted question DEVSPORTS

---
+++ solutions/codechef/DEVSPORTS/DEVSPORTS_1.c
@@ -0,0 +1,29 @@
+// DATE: 07/02/2024, 05:45:48
+// PROBLEM NAME: Devendra and Water Sports
+// PROBLEM URL: https://www.codechef.com/problems/DEVSPORTS
+// PROBLEM DIFFICULTY RATTING: 859
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, z, y, a, b, c, rem;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d%d", &z, &y, &a, &b, &c);
+        rem = z - y;
+
+        if (rem >= a+b+c)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        
+    }
+    
+}
+
+
+
