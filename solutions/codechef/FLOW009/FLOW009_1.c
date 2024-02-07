711702d8da29e2f52904c7dfcc04c60b754ceced
yash2003bisht <yash2003bisht@gmail.com>
2024-02-07 17:59:46 +0530

    attempted question FLOW009

---
+++ solutions/codechef/FLOW009/FLOW009_1.c
@@ -0,0 +1,29 @@
+// DATE: 07/02/2024, 05:58:03
+// PROBLEM NAME: Total Expenses
+// PROBLEM URL: https://www.codechef.com/problems/FLOW009
+// PROBLEM DIFFICULTY RATTING: 861
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.8M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, q, p;
+    float ans;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &q, &p);
+
+        if (q >= 1000)
+            printf("%f\n", (float) (q*p)-(q*p*0.1));
+        else
+            printf("%f\n", (float) q*p);
+
+    }
+
+}
+
+
+
---
+++ solutions/codechef/FLOW009/FLOW009_3.c
@@ -0,0 +1,32 @@
+// DATE: 07/02/2024, 05:49:06
+// PROBLEM NAME: Total Expenses
+// PROBLEM URL: https://www.codechef.com/problems/FLOW009
+// PROBLEM DIFFICULTY RATTING: 861
+// STATUS: wrong answer
+// TIME: 0.00
+// MEMORY: 2M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, q, p;
+    float ans;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d", &q, &p);
+        ans = (q*p);
+        
+        if (q > 1000){
+            float off = (float) ans * 0.1;
+            ans -= off;
+        }
+
+        printf("%f\n", ans);
+
+    }
+
+}
+
+
+
