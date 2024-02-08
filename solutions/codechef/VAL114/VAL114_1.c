fd60dbe8ad8a819a2adfba9f08302935d2f99438
yash2003bisht <yash2003bisht@gmail.com>
2024-02-08 21:51:41 +0530

    attempted question VAL114

---
+++ solutions/codechef/VAL114/VAL114_1.c
@@ -0,0 +1,23 @@
+// DATE: 08/02/2024, 08:49:53
+// PROBLEM NAME: Valentines Contest
+// PROBLEM URL: https://www.codechef.com/problems/VAL114
+// PROBLEM DIFFICULTY RATTING: 318
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    
+    if (n == 121)
+        printf("Likely");
+    else
+        printf("Unlikely");
+    
+}
+
+
+
