17c13718dcc8847e192d6b107f4b2d60d23ba32f
yash2003bisht <yash2003bisht@gmail.com>
2024-02-08 21:51:41 +0530

    attempted question DRUNKALK

---
+++ solutions/codechef/DRUNKALK/DRUNKALK_2.c
@@ -0,0 +1,25 @@
+// DATE: 08/02/2024, 09:48:42
+// PROBLEM NAME: Drunk Alcoholic
+// PROBLEM URL: https://www.codechef.com/problems/DRUNKALK
+// PROBLEM DIFFICULTY RATTING: 874
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, k, mid;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &k);
+        mid = k/2;
+        printf("%d\n", (k - mid)*3 - mid);
+
+    }
+    
+}
+
+
+
---
+++ solutions/codechef/DRUNKALK/DRUNKALK_3.c
@@ -0,0 +1,25 @@
+// DATE: 08/02/2024, 09:48:08
+// PROBLEM NAME: Drunk Alcoholic
+// PROBLEM URL: https://www.codechef.com/problems/DRUNKALK
+// PROBLEM DIFFICULTY RATTING: 874
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, k;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &k);
+
+        printf("%d\n", (k - k/2)*3 - k/2);
+
+    }
+    
+}
+
+
+
