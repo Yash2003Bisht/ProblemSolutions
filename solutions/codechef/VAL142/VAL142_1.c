311f0d804b883d2381d96945dcbc1c22576852de
yash2003bisht <yash2003bisht@gmail.com>
2024-02-08 21:51:41 +0530

    attempted question VAL142

---
+++ solutions/codechef/VAL142/VAL142_1.c
@@ -0,0 +1,28 @@
+// DATE: 08/02/2024, 09:38:33
+// PROBLEM NAME: Valentine Gifts
+// PROBLEM URL: https://www.codechef.com/problems/VAL142
+// PROBLEM DIFFICULTY RATTING: 729
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+
+        if (x >= 127)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+}
+
+
+
---
+++ solutions/codechef/VAL142/VAL142_2.c
@@ -0,0 +1,28 @@
+// DATE: 08/02/2024, 09:38:26
+// PROBLEM NAME: Valentine Gifts
+// PROBLEM URL: https://www.codechef.com/problems/VAL142
+// PROBLEM DIFFICULTY RATTING: 729
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, x;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &x);
+
+        if (x >= 127)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+}
+
+
+
