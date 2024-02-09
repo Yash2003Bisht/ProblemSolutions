74505982403a5d5ba658a961b003854b2b0240fe
yash2003bisht <yash2003bisht@gmail.com>
2024-02-09 20:17:40 +0530

    attempted question FACEDIR

---
+++ solutions/codechef/FACEDIR/FACEDIR_1.py
@@ -0,0 +1,16 @@
+# DATE: 09/02/2024, 07:05:11
+# PROBLEM NAME: Find the Direction
+# PROBLEM URL: https://www.codechef.com/problems/FACEDIR
+# PROBLEM DIFFICULTY RATTING: 880
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 8.5M
+
+directions = ["North", "East", "South", "West"]
+
+for _ in range(int(input())):
+    
+    x = int(input())
+    print(directions[x%4])
+
+
