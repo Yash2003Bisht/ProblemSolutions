5e3b527b94de432639dfd55d4d59815cf59b87e1
yash2003bisht <yash2003bisht@gmail.com>
2024-02-11 14:35:35 +0530

    attempted question UTKPLC

---
+++ solutions/codechef/UTKPLC/UTKPLC_2.c
@@ -0,0 +1,29 @@
+// DATE: 11/02/2024, 02:26:34
+// PROBLEM NAME: Utkarsh and Placement tests
+// PROBLEM URL: https://www.codechef.com/problems/UTKPLC
+// PROBLEM DIFFICULTY RATTING: 886
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	char a, b, c, x, y;
+	scanf("%d\n", &t);
+	
+	while(t--){
+	    scanf(" %c %c %c\n %c %c", &a, &b, &c, &x, &y);
+
+        if (x == a || y == a)
+            printf("%c\n", a);
+        else
+            printf("%c\n", b);
+
+	}
+	
+}
+
+
+
---
+++ solutions/codechef/UTKPLC/UTKPLC_3.c
@@ -0,0 +1,31 @@
+// DATE: 11/02/2024, 02:26:00
+// PROBLEM NAME: Utkarsh and Placement tests
+// PROBLEM URL: https://www.codechef.com/problems/UTKPLC
+// PROBLEM DIFFICULTY RATTING: 886
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	char a, b, c, x, y;
+	scanf("%d\n", &t);
+	
+	while(t--){
+	    scanf(" %c %c %c\n %c %c", &a, &b, &c, &x, &y);
+
+        if (x == a || y == a)
+            printf("%c\n", a);
+        else if (x == b || y == b)
+            printf("%c\n", b);
+        else
+            printf("%c\n", c);
+
+	}
+	
+}
+
+
+
