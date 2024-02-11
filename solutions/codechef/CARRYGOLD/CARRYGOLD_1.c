9d98654eeb1098dbc2c9c737a3e22eb857762ca2
yash2003bisht <yash2003bisht@gmail.com>
2024-02-11 14:35:35 +0530

    attempted question CARRYGOLD

---
+++ solutions/codechef/CARRYGOLD/CARRYGOLD_1.c
@@ -0,0 +1,29 @@
+// DATE: 11/02/2024, 02:06:17
+// PROBLEM NAME: Gold Mining
+// PROBLEM URL: https://www.codechef.com/problems/CARRYGOLD
+// PROBLEM DIFFICULTY RATTING: 880
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, n, x, y, total;
+	scanf("%d", &t);
+
+	while(t--){
+	    scanf("%d%d%d", &n, &x, &y);
+
+	    total = (n+1)*y;
+
+	    if (total >= x)
+	        printf("YES\n");
+	    else
+	        printf("NO\n");
+
+	}
+
+}
+
+
