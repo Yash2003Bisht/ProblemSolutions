b0e41cb897a283f0dde55bddd00fe73b0674c212
yash2003bisht <yash2003bisht@gmail.com>
2024-02-14 15:31:21 +0530

    attempted question SUNDAY

---
+++ solutions/codechef/SUNDAY/SUNDAY_1.c
@@ -0,0 +1,42 @@
+// DATE: 14/02/2024, 03:29:47
+// PROBLEM NAME: Count the Holidays
+// PROBLEM URL: https://www.codechef.com/problems/SUNDAY
+// PROBLEM DIFFICULTY RATTING: 907
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, a, count, flag, holiday_days[] = {6, 7, 13, 14, 20, 21, 27, 28};
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        count = 8;
+        
+        for (int i=0; i<n; i++){
+            scanf("%d", &a);
+            flag = 1;
+
+            for (int j=0; j<8; j++){
+                if (a == holiday_days[j]){
+                    flag = 0;
+                    break;
+                }
+            }
+
+            if (flag)
+                count++;
+
+        }
+
+        printf("%d\n", count);
+
+    }
+    
+}
+
+
+
