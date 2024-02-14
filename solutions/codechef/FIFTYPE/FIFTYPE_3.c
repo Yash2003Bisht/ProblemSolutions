b2049aebe318aa3a897137550c2517a369461aec
yash2003bisht <yash2003bisht@gmail.com>
2024-02-14 15:31:21 +0530

    attempted question FIFTYPE

---
+++ solutions/codechef/FIFTYPE/FIFTYPE_3.c
@@ -0,0 +1,34 @@
+// DATE: 14/02/2024, 09:46:52
+// PROBLEM NAME: Chef and Battery 
+// PROBLEM URL: https://www.codechef.com/problems/FIFTYPE
+// PROBLEM DIFFICULTY RATTING: 901
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, n, count;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        count = 0;
+        
+        while (n != 50){
+            if (n > 50)
+                n -= 3;
+            else
+                n += 2;
+            count++;
+        }
+
+        printf("%d\n", count);
+
+    }
+    
+}
+
+
+
