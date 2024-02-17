b61b8948c9ce49f9dab7f2d36d9d3c5a4abf8d5a
yash2003bisht <yash2003bisht@gmail.com>
2024-02-17 19:05:14 +0530

    attempted question NAICHEF

---
+++ solutions/codechef/NAICHEF/NAICHEF_1.c
@@ -0,0 +1,36 @@
+// DATE: 17/02/2024, 06:58:03
+// PROBLEM NAME: Naive Chef
+// PROBLEM URL: https://www.codechef.com/problems/NAICHEF
+// PROBLEM DIFFICULTY RATTING: 990
+// STATUS: accepted
+// TIME: 0.03
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(){
+    int t, n, a, b, x, occurrence_a, occurrence_b;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d", &n, &a, &b);
+        occurrence_a = 0, occurrence_b = 0;
+
+        for (int i=0; i<n; i++){
+            scanf("%d", &x);
+
+            if (x == a)
+                occurrence_a++;
+            if (x == b)
+                occurrence_b++;
+
+        }
+
+        float ans = (float) occurrence_a/n * (float) occurrence_b/n;
+        printf("%.10f\n", ans);
+
+    }
+    
+    return 0;
+}
+
