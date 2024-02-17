75612154a1298025490caa951bf03fa3d25f4fe2
yash2003bisht <yash2003bisht@gmail.com>
2024-02-17 19:05:14 +0530

    attempted question EXISTENCE

---
+++ solutions/codechef/EXISTENCE/EXISTENCE_1.c
@@ -0,0 +1,30 @@
+// DATE: 16/02/2024, 09:30:57
+// PROBLEM NAME: Existence
+// PROBLEM URL: https://www.codechef.com/problems/EXISTENCE
+// PROBLEM DIFFICULTY RATTING: 928
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.7M
+
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    unsigned long long x, y, ans1, ans2;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%llu%llu", &x, &y);
+        ans1 = (x*x*x*x) + (4 * (y*y));
+        ans2 = 4 * (x*x) * y;
+        if (ans1 == ans2)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+    }
+    
+}
+
+
+
---
+++ solutions/codechef/EXISTENCE/EXISTENCE_3.py
@@ -0,0 +1,17 @@
+# DATE: 16/02/2024, 09:20:56
+# PROBLEM NAME: Existence
+# PROBLEM URL: https://www.codechef.com/problems/EXISTENCE
+# PROBLEM DIFFICULTY RATTING: 928
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 8.4M
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    
+    if x**4 + 4 * y**2 == 4 * x**2 * y:
+        print("YES")
+    else:
+        print("NO")
+    
+
