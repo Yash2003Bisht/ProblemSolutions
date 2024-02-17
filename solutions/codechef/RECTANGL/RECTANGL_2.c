0ee29e2157fb58e53134723501b07c37debd2a51
yash2003bisht <yash2003bisht@gmail.com>
2024-02-17 19:05:14 +0530

    attempted question RECTANGL

---
+++ solutions/codechef/RECTANGL/RECTANGL_2.c
@@ -0,0 +1,28 @@
+// DATE: 17/02/2024, 07:03:47
+// PROBLEM NAME: Rectangle
+// PROBLEM URL: https://www.codechef.com/problems/RECTANGL
+// PROBLEM DIFFICULTY RATTING: 1146
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+	int t, a, b, c, d;
+	scanf("%d", &t);
+	
+	while(t--){
+	    scanf("%d%d%d%d", &a, &b, &c, &d);
+
+        if ((a == b && c == d) || (a == c && b == d) || (a == d && b == c))
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+	}
+	
+}
+
+
+
