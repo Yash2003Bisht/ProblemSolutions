dabfeee698b9033c5ecd1a1662b3808e4dca175c
yash2003bisht <yash2003bisht@gmail.com>
2024-02-19 21:13:23 +0530

    attempted question OLYRANK

---
+++ solutions/codechef/OLYRANK/OLYRANK_2.c
@@ -0,0 +1,28 @@
+// DATE: 19/02/2024, 08:48:38
+// PROBLEM NAME: Olympics Ranking
+// PROBLEM URL: https://www.codechef.com/problems/OLYRANK
+// PROBLEM DIFFICULTY RATTING: 893
+// STATUS: accepted
+// TIME: 0.00
+// MEMORY: 1.6M
+
+#include <stdio.h>
+
+int main(void) {
+    int t, g1, g2, s1, s2, b1, b2;
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d%d%d%d%d%d", &g1, &s1, &b1, &g2, &s2, &b2);
+
+        if (g1+s1+b1 > g2+s2+b2)
+            printf("1\n");
+        else
+            printf("2\n");
+
+    }
+    
+}
+
+
+
