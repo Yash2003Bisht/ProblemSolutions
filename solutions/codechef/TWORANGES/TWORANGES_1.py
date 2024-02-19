aa7914eb6728632c56468cc1e0ec98421ce96128
yash2003bisht <yash2003bisht@gmail.com>
2024-02-19 21:13:55 +0530

    attempted question TWORANGES

---
+++ solutions/codechef/TWORANGES/TWORANGES_1.py
@@ -0,0 +1,21 @@
+# DATE: 19/02/2024, 09:08:49
+# PROBLEM NAME: Two Ranges
+# PROBLEM URL: https://www.codechef.com/problems/TWORANGES
+# PROBLEM DIFFICULTY RATTING: 918
+# STATUS: accepted
+# TIME: 0.02
+# MEMORY: 8.6M
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    r1 = range(a, b+1)
+    r2 = range(c, d+1)
+    count = len(r1)
+
+    for i in r2:
+        if i not in r1:
+            count += 1
+
+    print(count)
+
+
---
+++ solutions/codechef/TWORANGES/TWORANGES_4.py
@@ -0,0 +1,22 @@
+# DATE: 19/02/2024, 09:07:24
+# PROBLEM NAME: Two Ranges
+# PROBLEM URL: https://www.codechef.com/problems/TWORANGES
+# PROBLEM DIFFICULTY RATTING: 918
+# STATUS: accepted
+# TIME: 0.03
+# MEMORY: 8.4M
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    r1 = range(a, b+1)
+    r2 = range(c, d+1)
+    common = list(r1)
+    count = 0
+
+    for i in r2:
+        if i not in common:
+            count += 1
+
+    print(len(common) + count)
+
+
